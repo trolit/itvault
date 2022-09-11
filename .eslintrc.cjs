10d6ba2da1f1a7dddeb4258de12aeb0bef1d9509
pawel idzikowski <iddziku@gmail.com>
2022-09-11 15:01:27 +0200

    Recreate app template with ts support
    cypress dir changes
    
    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,36 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution');
+require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
-    root: true,
-    extends: [
-        'plugin:vue/vue3-essential',
-        'eslint:recommended',
-        '@vue/eslint-config-prettier'
+  root: true,
+
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-typescript",
+    "@vue/eslint-config-prettier",
+  ],
+
+  overrides: [
+    {
+      files: [
+        "**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}",
+        "cypress/e2e/**.{cy,spec}.{js,ts,jsx,tsx}",
+      ],
+      extends: ["plugin:cypress/recommended"],
+    },
+  ],
+
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
     ],
-    parserOptions: {
-        ecmaVersion: 'latest'
-    }
+  },
+
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
 };
