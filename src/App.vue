fb00e6e0185b7b28f5d4d33ced5dae318fbb02bb
pawel idzikowski <iddziku@gmail.com>
2022-09-12 16:51:09 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,91 +1,34 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
-
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
+  <n-config-provider inline-theme-disabled :theme="theme">
+    <router-view />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+    <n-button type="primary" @click="switchTheme"> Primary </n-button>
+  </n-config-provider>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<script setup lang="ts">
+// ***********************************************************************
+// *
+// * Itvault
+// * 09.2022
+// * https://github.com/trolit/itvault/
+// *
+// ***********************************************************************
+
+import { computed } from "vue";
+import { RouterView } from "vue-router";
+import { darkTheme, NButton, NConfigProvider } from "naive-ui";
+
+import { DARK_THEME } from "@/assets/constants/themes";
+import { usePreferencesStore } from "@/stores/preferences";
+
+const preferencesStore = usePreferencesStore();
+
+function switchTheme() {
+  preferencesStore.toggleTheme();
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+const theme = computed(() => {
+  return preferencesStore.theme === DARK_THEME ? darkTheme : null;
+});
+</script>
