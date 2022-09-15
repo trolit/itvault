f9603c539b15ad8f1899682c527f85797b898895
pawel idzikowski <iddziku@gmail.com>
2022-09-15 18:08:06 +0200

    Update Home.vue

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import CustomHeader from "@/components/Header.vue";
+import NavigationBar from "@/components/navigation-bar/Index.vue";
 </script>
 
 <template>
   <main>
     <custom-header />
+
+    <navigation-bar />
   </main>
 </template>
