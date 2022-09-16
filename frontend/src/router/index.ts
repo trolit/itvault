4fea68c1f510b13f3d48160f27ec726d97e377a6
pawel idzikowski <iddziku@gmail.com>
2022-09-16 22:33:04 +0200

    Update router

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -9,15 +9,15 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: Home,
+      children: [
+        {
+          path: "",
+          components: {
+            workspace: () => import("@/components/Welcome.vue"),
+          },
+        },
+      ],
     },
-    // {
-    //   path: "/about",
-    //   name: "about",
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import("../views/AboutView.vue"),
-    // },
   ],
 });
 
