cb491c5afe955fc910b9a019fffef5bb60cb4d3c
pawel idzikowski <iddziku@gmail.com>
2022-09-16 23:21:21 +0200

    Create Brand.vue

---
+++ frontend/src/components/common/Brand.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <n-icon>
+      <brand-icon />
+    </n-icon>
+
+    <span>itvault</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { NIcon } from "naive-ui";
+import { Development as BrandIcon } from "@vicons/carbon";
+</script>
