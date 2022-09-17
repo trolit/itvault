9d18c7841ce589e0c829b5b6601a9015c0bfdbfd
pawel idzikowski <iddziku@gmail.com>
2022-09-18 00:39:05 +0200

    Update DarkDimmed.ts

--- frontend/src/components/custom-themes/DarkDimmed.ts
+++ frontend/src/components/custom-themes/DarkDimmed.ts
@@ -2,14 +2,22 @@ import type { GlobalThemeOverrides } from "naive-ui";
 
 export const darkDimmedTheme: GlobalThemeOverrides = {
   common: {
+    baseColor: "#ADBAC7",
     bodyColor: "#303437",
     textColor1: "#9da9b6",
     textColor2: "#9da9b6",
-    primaryColor: "#242125",
+    primaryColor: "#62666A",
+    hoverColor: "#565D63",
+    popoverColor: "#3C4145",
+    borderColor: "#3C4145",
   },
   Layout: {
     color: "#303437",
-    siderBorderColor: "#ADBAC7",
+    siderBorderColor: "#3C4145",
+    siderToggleBarColor: "#454A4F",
+    siderToggleButtonColor: "#454A4F",
+    siderToggleButtonBorder: "#454A4F",
+    siderToggleButtonIconColor: "#B4B6B8",
   },
   PageHeader: {
     backColor: "red",
@@ -17,8 +25,5 @@ export const darkDimmedTheme: GlobalThemeOverrides = {
   Menu: {
     color: "#3C4145",
     dividerColor: "#ADBAC7",
-    itemColorHover: "#565D63",
-    itemIconColorHover: "#D0D1D2",
-    itemTextColorHover: "#D0D1D2",
   },
 };
