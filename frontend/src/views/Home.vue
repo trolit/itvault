e5736b76ab9edd01f6a2669220953478820f51e1
pawel idzikowski <iddziku@gmail.com>
2022-09-25 00:37:45 +0200

    Wrap Home content with scrollbar

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,5 +1,11 @@
 <template>
   <main>
-    <router-view name="content" />
+    <n-scrollbar>
+      <router-view name="content" />
+    </n-scrollbar>
   </main>
 </template>
+
+<script setup lang="ts">
+import { NScrollbar } from "naive-ui";
+</script>
