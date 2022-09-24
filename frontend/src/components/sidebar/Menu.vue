8f879869ba11433208a1695ed424dfdebc30a477
pawel idzikowski <iddziku@gmail.com>
2022-09-24 14:00:13 +0200

    Update Menu.vue

--- frontend/src/components/sidebar/Menu.vue
+++ frontend/src/components/sidebar/Menu.vue
@@ -1,31 +1,27 @@
 <template>
   <n-menu
-    :collapsed="isCollapsed"
     v-model:value="activeKey"
+    :collapsed="isCollapsed"
     :collapsed-width="64"
     :options="menuOptions"
     :collapsed-icon-size="22"
-    @expand="toggleCollapse"
-    @collapse="toggleCollapse"
   />
 </template>
 
 <script setup lang="ts">
 import { NIcon, NMenu } from "naive-ui";
 import type { MenuOption } from "naive-ui";
-import { h, ref, type Component, watch } from "vue";
 import { Catalog as WorkspaceIcon } from "@vicons/carbon";
 import { useRoute, type RouteRecordName } from "vue-router";
+import { h, ref, type Component, watch, computed } from "vue";
 
 import { usePreferencesStore } from "@/stores/preferences";
 
 const preferencesStore = usePreferencesStore();
 
-const isCollapsed = ref<boolean>(preferencesStore.isSidebarCollapsed);
-
-function toggleCollapse(): void {
-  preferencesStore.toggleSidebar();
-}
+const isCollapsed = computed(() => {
+  return preferencesStore.isSidebarCollapsed;
+});
 
 function renderIcon(icon: Component) {
   return () => h(NIcon, null, { default: () => h(icon) });
