366cf240e0097506ef1e8c0c1360afdbb6e1cb18
ⓘ p4w31 !d21k0w5k1 <iddziku@gmail.com>
2022-09-25 15:26:50 +0200

    Welcome page -> sketch content (#13)

---
+++ frontend/src/components/pages/welcome/Index.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="welcome-page page">
+    <n-grid
+      x-gap="16"
+      y-gap="16"
+      class="grid"
+      item-responsive
+      responsive="screen"
+      cols="s:1 m:3 l:3 xl:3 2xl:3"
+    >
+      <n-grid-item class="information">
+        <ref-card :icon="FaceActivatedIcon" title="Welcome,">
+          <template #description> elo </template>
+        </ref-card>
+      </n-grid-item>
+
+      <n-grid-item class="workspaces">
+        <ref-card
+          :icon="WorkspacesIcon"
+          title="Workspaces"
+          description="Elo4"
+        />
+      </n-grid-item>
+
+      <n-grid-item class="others">
+        <n-grid y-gap="20" cols="1">
+          <n-grid-item
+            v-for="({ title, to, icon, description }, index) of otherCards"
+            :key="index"
+          >
+            <ref-card
+              :to="to"
+              :icon="icon"
+              :title="title"
+              :description="description"
+            />
+          </n-grid-item>
+        </n-grid>
+      </n-grid-item>
+    </n-grid>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {
+  Help as HelpIcon,
+  UpdateNow as UpdatesIcon,
+  DataCenter as WorkspacesIcon,
+  InformationSquare as AboutIcon,
+} from "@vicons/carbon";
+import { ref, shallowRef, type Component } from "vue";
+import { NGrid, NGridItem } from "naive-ui";
+import { FaceActivated as FaceActivatedIcon } from "@vicons/carbon";
+
+import {
+  ROUTE_ABOUT,
+  ROUTE_GUIDE,
+  ROUTE_UPDATES,
+} from "@/assets/constants/routes";
+import RefCard from "./RefCard.vue";
+
+interface OtherCard {
+  title: string;
+  to: string;
+  icon: Component;
+  description: string;
+}
+
+const otherCards = ref<Array<OtherCard>>([
+  {
+    title: "About",
+    to: ROUTE_ABOUT,
+    icon: shallowRef(AboutIcon),
+    description: "Learn more about project, it's features and integrations.",
+  },
+
+  {
+    title: "Guide",
+    to: ROUTE_GUIDE,
+    icon: shallowRef(HelpIcon),
+    description: "This section describes how to use itvault.",
+  },
+
+  {
+    title: "Updates",
+    to: ROUTE_UPDATES,
+    icon: shallowRef(UpdatesIcon),
+    description: "Complete log of changes made to the project.",
+  },
+]);
+</script>
