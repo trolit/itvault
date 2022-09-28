d500f71af8cf2218549fea0d35cc2a6f573d2528
ⓘ p4w31 !d21k0w5k1 <iddziku@gmail.com>
2022-09-28 19:49:11 +0200

    Sketch up profile card (#18)

---
+++ frontend/src/components/dashboard/Profile.vue
@@ -0,0 +1,94 @@
+<template>
+  <ref-card :icon="UserProfileIcon" title="account.email@company.com">
+    <template #content>
+      <div class="statistics">
+        <n-statistic label="Workspaces"> 10 </n-statistic>
+
+        <n-statistic label="Account type">
+          <!-- @TODO Itvault admin, Company admin, Company user-->
+          <n-tag type="info"> Company user </n-tag>
+        </n-statistic>
+      </div>
+
+      <n-divider />
+
+      <div class="scrollable-data">
+        <!-- @TODO hide for account type that has R/W access to all workspaces -->
+        <div class="access">
+          <div class="title">Privileges</div>
+
+          <n-scrollbar trigger="none">
+            <div class="levels">
+              <n-tag type="success"> vue (2) </n-tag>
+
+              <n-tag type="success"> .NET (0) </n-tag>
+            </div>
+          </n-scrollbar>
+        </div>
+
+        <div class="timeline">
+          <div class="title">Latest sessions</div>
+
+          <n-scrollbar trigger="none">
+            <n-timeline>
+              <n-timeline-item
+                title="Success"
+                content="success content"
+                time="2018-04-03 20:46"
+              />
+
+              <n-timeline-item
+                type="warning"
+                title="Warning"
+                content="warning content"
+                time="2018-04-03 20:46"
+              />
+
+              <n-timeline-item
+                title="Info"
+                content="info content"
+                time="2018-04-03 20:46"
+                line-type="dashed"
+              />
+
+              <n-timeline-item
+                title="Info"
+                content="info content"
+                time="2018-04-03 20:46"
+                line-type="dashed"
+              />
+
+              <n-timeline-item
+                title="Info"
+                content="info content"
+                time="2018-04-03 20:46"
+                line-type="dashed"
+              />
+
+              <n-timeline-item
+                title="Info"
+                content="info content"
+                time="2018-04-03 20:46"
+                line-type="dashed"
+              />
+            </n-timeline>
+          </n-scrollbar>
+        </div>
+      </div>
+    </template>
+  </ref-card>
+</template>
+
+<script setup lang="ts">
+import {
+  NTag,
+  NDivider,
+  NTimeline,
+  NScrollbar,
+  NStatistic,
+  NTimelineItem,
+} from "naive-ui";
+import { UserProfile as UserProfileIcon } from "@vicons/carbon";
+
+import RefCard from "./RefCard.vue";
+</script>
--- frontend/src/components/dashboard/Workspaces.vue
+++ frontend/src/components/dashboard/Workspaces.vue
@@ -23,17 +23,15 @@
         :pagination="pagination"
       >
         <template #empty>
-          <n-empty description="No workspaces defined">
-            <template #extra>
-              <n-button type="warning" ghost> Create first workspace </n-button>
-            </template>
-          </n-empty>
+          <n-empty description="No workspaces found." />
         </template>
       </n-data-table>
 
       <div class="actions">
+        <n-button ghost type="success" size="small"> New workspace </n-button>
+
         <router-link to="/">
-          <n-button type="info" dashed> Open workspace </n-button>
+          <n-button dashed type="info" size="small"> Open workspace </n-button>
         </router-link>
       </div>
     </template>
