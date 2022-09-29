4495edb99dbe81dcd8d0a663eb49a33a7964b514
pawel idzikowski <iddziku@gmail.com>
2022-09-29 19:25:26 +0200

    Add workspace route name

--- frontend/src/assets/constants/routes.ts
+++ frontend/src/assets/constants/routes.ts
@@ -2,3 +2,4 @@ export const ROUTE_ABOUT_NAME = "about";
 export const ROUTE_GUIDE_NAME = "guide";
 export const ROUTE_UPDATES_NAME = "updates";
 export const ROUTE_DASHBOARD_NAME = "dashboard";
+export const ROUTE_WORKSPACE_NAME = "workspace";
