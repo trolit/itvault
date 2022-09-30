000bb011548a3aa72774750a32fb5a1677b505c5
pawel idzikowski <iddziku@gmail.com>
2022-09-30 16:35:09 +0200

    Create Form.vue

---
+++ frontend/src/components/login/Form.vue
@@ -0,0 +1,25 @@
+<template>
+  <n-form :show-label="false">
+    <n-form-item path="inputValue">
+      <n-input type="text" size="large" placeholder="Login" />
+    </n-form-item>
+
+    <n-form-item path="inputValue">
+      <n-input
+        size="large"
+        :maxlength="8"
+        type="password"
+        placeholder="Password"
+        show-password-on="mousedown"
+      />
+    </n-form-item>
+
+    <div class="actions">
+      <n-button text :bordered="false" type="tertiary"> Sign in </n-button>
+    </div>
+  </n-form>
+</template>
+
+<script setup lang="ts">
+import { NInput, NForm, NFormItem, NButton } from "naive-ui";
+</script>
