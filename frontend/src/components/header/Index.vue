c4925d62f39d3067cc7ec863798ae3bdc6a60986
pawel idzikowski <iddziku@gmail.com>
2022-10-01 21:24:43 +0200

    Apply `ThemeSelector` to `Header`

--- frontend/src/components/header/Index.vue
+++ frontend/src/components/header/Index.vue
@@ -14,59 +14,23 @@
     <custom-menu />
 
     <div class="actions">
-      <n-popselect
-        :value="theme"
-        :options="options"
-        label-field="value"
-        @update:value="setTheme"
-      >
-        <n-button
-          :bordered="false"
-          size="small"
-          type="tertiary"
-          :focusable="false"
-        >
-          Theme ({{ theme }})
-        </n-button>
-      </n-popselect>
+      <theme-selector />
     </div>
   </n-page-header>
 </template>
 
 <script setup lang="ts">
 import { ref, computed } from "vue";
-import { NPopselect, NPageHeader, NButton, useThemeVars } from "naive-ui";
+import { NPageHeader, useThemeVars } from "naive-ui";
 
-import {
-  THEME_DARK,
-  THEME_LIGHT,
-  THEME_DARK_DIMMED,
-} from "@/assets/constants/themes";
 import CustomMenu from "./Menu.vue";
 import Brand from "@/components/common/Brand.vue";
-import { usePreferencesStore } from "@/stores/preferences";
 import { ROUTE_DASHBOARD_NAME } from "@/assets/constants/routes";
+import ThemeSelector from "@/components/common/ThemeSelector.vue";
 
 const themeVars = useThemeVars();
-const preferencesStore = usePreferencesStore();
-
-const options = ref([
-  {
-    value: THEME_DARK,
-  },
-  {
-    value: THEME_LIGHT,
-  },
-  {
-    value: THEME_DARK_DIMMED,
-  },
-]);
 
 let isBrandHovered = ref<boolean>(false);
-
-const theme = computed<string>((): string => {
-  return preferencesStore.theme;
-});
 
 const textColor = computed<string>((): string => {
   return themeVars.value.primaryColor;
@@ -75,8 +39,4 @@ const textColor = computed<string>((): string => {
 const borderBottom = computed<string>((): string => {
   return `1px solid ${themeVars.value.borderColor}`;
 });
-
-function setTheme(theme: string): void {
-  preferencesStore.setTheme(theme);
-}
 </script>
