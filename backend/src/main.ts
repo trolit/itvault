d5beff4040196870aa144b4bdc54a9bc64c0273c
pawel idzikowski <iddziku@gmail.com>
2022-10-06 20:25:54 +0200

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,16 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 
+import { constants } from "./config/index";
+
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
-app.listen(port, () => {
+app.listen(constants.PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
 });
