e0a0ff151c42a2284713ab25b9f8cd1b0ebddbeb
pawel idzikowski <iddziku@gmail.com>
2022-10-15 23:09:49 +0200

    Create IBaseRepository.ts

---
+++ backend/src/interfaces/IBaseRepository.ts
@@ -0,0 +1,11 @@
+export interface IBaseRepository<T> {
+  getAll(): Promise<T[]>;
+
+  getById(id: number): Promise<T>;
+
+  create(item: T): Promise<T>;
+
+  update(item: T): Promise<T>;
+
+  delete(id: number): Promise<T>;
+}
