94806d8faad8bc5bf63ecd4e02bd5c4fd2188e83
pawel idzikowski <iddziku@gmail.com>
2022-10-16 22:51:25 +0200

    Create app.ts

---
+++ backend/src/app.ts
@@ -0,0 +1,14 @@
+import { server } from "server";
+import { APP_PORT } from "./config";
+
+const startServer = async () => {
+  const app = await server();
+
+  app.listen(APP_PORT, () => {
+    console.log(
+      `⚡️[server]: Server is running at https://localhost:${APP_PORT}`
+    );
+  });
+};
+
+startServer();
