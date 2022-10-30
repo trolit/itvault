a5fdba346452e0a780d17ef89fe6428381dca2d5
pawel idzikowski <iddziku@gmail.com>
2022-10-30 20:21:16 +0100

    Limit `email` from Login to 254 characters (RFC max)

--- backend/src/schemas/login.ts
+++ backend/src/schemas/login.ts
@@ -2,11 +2,13 @@ import { z } from "zod";
 import { LoginDto } from "dtos/Login";
 import { schemaForType } from "@helpers/schemaForType";
 
-// @INFO https://github.com/colinhacks/zod/issues/372#issuecomment-826380330
-
 export const loginSchema = schemaForType<LoginDto>()(
   z.object({
-    email: z.string(),
-    password: z.string(),
+    email: z
+      .string()
+      .email()
+      .max(254) // @INFO https://www.rfc-editor.org/rfc/rfc5321#section-4.5.3
+      .transform(value => value.toLowerCase()),
+    password: z.string().max(100),
   })
 );
