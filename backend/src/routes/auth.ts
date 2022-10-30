dad23bc538a119d82f4a0566ef2f2002f9d2318e
pawel idzikowski <iddziku@gmail.com>
2022-10-30 18:05:34 +0100

    Update auth.ts

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
 
+import { loginSchema } from "@schemas/login";
 import { LoginController } from "@controllers/Auth/Login";
 import { processRequestWith } from "./processRequestWith";
+import { validateRequestWith } from "@middlewares/validateRequestWith";
 
 const authRoutes = Router();
 
-authRoutes.post("/v1/login", processRequestWith(LoginController));
+authRoutes.post(
+  "/v1/login",
+  validateRequestWith(loginSchema),
+  processRequestWith(LoginController)
+);
 
 export = authRoutes;
