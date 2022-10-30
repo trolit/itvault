ad793132fbd7778933c4bea2544bc59d74711e4b
pawel idzikowski <iddziku@gmail.com>
2022-10-30 20:16:46 +0100

    Add comment to `schemaForType` helper

--- backend/src/helpers/schemaForType.ts
+++ backend/src/helpers/schemaForType.ts
@@ -1,5 +1,6 @@
 import { ZodType } from "zod";
 
+// @INFO https://github.com/colinhacks/zod/issues/372#issuecomment-826380330
 export const schemaForType =
   <T>() =>
   <S extends ZodType<T>>(arg: S) => {
