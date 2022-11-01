b86dbd1c80985dbf972b0654f82be4f7934028ec
pawel idzikowski <iddziku@gmail.com>
2022-11-01 20:27:10 +0100

    Update setupExpress.ts

--- backend/src/utilities/setupExpress.ts
+++ backend/src/utilities/setupExpress.ts
@@ -3,11 +3,16 @@ import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import express, { Application } from "express";
 
-import routes from "@routes/index";
-import { ROUTES_PREFIX } from "@config/index";
+import routes from "@routes";
+import { ROUTES_PREFIX, APP_URL } from "@config";
 
 export const setupExpress = (app: Application) => {
-  app.use(cors());
+  app.use(
+    cors({
+      credentials: true,
+      origin: APP_URL,
+    })
+  );
 
   app.use(express.json());
 
