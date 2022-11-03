508af21a47c8513cdfe3cc84c6e0ee106033780a
pawel idzikowski <iddziku@gmail.com>
2022-11-03 22:10:29 +0100

    Update index.ts

--- frontend/src/config/index.ts
+++ frontend/src/config/index.ts
@@ -1,1 +1,5 @@
 export const API_URL: string = import.meta.env.VITE_API_URL;
+
+export const APP_URL: string = import.meta.env.VITE_APP_URL;
+
+export const APP_PORT: number = parseInt(import.meta.env.VITE_APP_PORT);
