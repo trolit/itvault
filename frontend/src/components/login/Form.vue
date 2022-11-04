0064ae9e4b2c841e543a1ff749b49f8f31c747b6
pawel idzikowski <iddziku@gmail.com>
2022-11-04 22:18:35 +0100

    Update Form.vue

--- frontend/src/components/login/Form.vue
+++ frontend/src/components/login/Form.vue
@@ -1,11 +1,17 @@
 <template>
   <n-form :show-label="false">
     <n-form-item path="inputValue">
-      <n-input type="text" size="large" placeholder="Login" />
+      <n-input
+        v-model:value="formData.email"
+        type="text"
+        size="large"
+        placeholder="Login"
+      />
     </n-form-item>
 
     <n-form-item path="inputValue">
       <n-input
+        v-model:value="formData.password"
         size="large"
         :maxlength="8"
         type="password"
@@ -15,11 +21,29 @@
     </n-form-item>
 
     <div class="actions">
-      <n-button text :bordered="false" type="tertiary"> Sign in </n-button>
+      <n-button text :bordered="false" type="tertiary" @click="login">
+        Sign in
+      </n-button>
     </div>
   </n-form>
 </template>
 
 <script setup lang="ts">
+import { ref, type Ref } from "vue";
 import { NInput, NForm, NFormItem, NButton } from "naive-ui";
+
+import { useAuthStore } from "@/stores/auth";
+import type { ILoginForm } from "@/interfaces/ILoginForm";
+
+const authStore = useAuthStore();
+
+const formData: Ref<ILoginForm> = ref({ email: "", password: "" });
+
+async function login() {
+  try {
+    await authStore.login(formData.value);
+  } catch (error) {
+    console.error(error);
+  }
+}
 </script>
