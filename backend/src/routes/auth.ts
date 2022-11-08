bcd9421643b2a6336c1d13f9d38f985548483c3d
pawel idzikowski <iddziku@gmail.com>
2022-11-08 18:07:38 +0100

    Update auth.ts

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -5,6 +5,7 @@ import { loginSchema } from "@schemas/login";
 import { LoginController } from "@controllers/Auth/Login";
 import { processRequestWith } from "./processRequestWith";
 import { safeParseRequest } from "@middlewares/safeParseRequest";
+import { TokenVerificationController } from "@controllers/Auth/TokenVerification";
 
 const authRoutes = Router();
 
@@ -12,6 +13,11 @@ authRoutes.post(
   "/v1/login",
   safeParseRequest<LoginDto>(loginSchema),
   processRequestWith(LoginController)
+);
+
+authRoutes.get(
+  "/v1/is-authenticated",
+  processRequestWith(TokenVerificationController)
 );
 
 export = authRoutes;
