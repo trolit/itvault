a24ddaf3ad272e4c971606a13e67ab5c0fdffc75
pawel idzikowski <iddziku@gmail.com>
2022-11-08 18:10:10 +0100

    Remove unnecessary decorator from AuthService

--- backend/src/services/Auth.ts
+++ backend/src/services/Auth.ts
@@ -1,9 +1,7 @@
 import jwt from "jsonwebtoken";
-import { injectable } from "tsyringe";
 
 import { JWT_SECRET_KEY } from "@config";
 
-@injectable()
 export class AuthService {
   isTokenValid(token: string) {
     const isValid = jwt.verify(token, JWT_SECRET_KEY);
