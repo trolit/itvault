ed89852adeaf198519556d86ab7a6ea4a4580aca
ⓘ p4w31 !d21k0w541 <iddziku@gmail.com>
2023-03-07 21:28:12 +0100

    Frontend - guest page minor changes (#60)

--- frontend/src/views/Guest.vue
+++ frontend/src/views/Guest.vue
@@ -1,93 +1,90 @@
 <template>
   <div class="guest-page page">
-    <n-card>
-      <theme-selector>
-        <template #default>
-          <n-button text :focusable="false" class="theme-selector">
-            <icon :value="RainDropIcon" :size="35" />
-          </n-button>
-        </template>
-      </theme-selector>
+    <theme-selector>
+      <template #default>
+        <n-button text :focusable="false" class="theme-selector">
+          <icon :value="RainDropIcon" :size="35" />
+        </n-button>
+      </template>
+    </theme-selector>
 
-      <n-grid
-        x-gap="20"
-        y-gap="20"
-        class="grid"
-        responsive="screen"
-        cols="1 s:1 m:2 l:2 xl:2 2xl:2"
-      >
-        <n-grid-item :span="2">
-          <brand>
-            <template #extra-text> smart source code storage </template>
-          </brand>
-        </n-grid-item>
+    <n-grid
+      x-gap="20"
+      y-gap="20"
+      class="grid"
+      responsive="screen"
+      cols="1 s:1 m:2 l:2 xl:2 2xl:2"
+    >
+      <n-grid-item :span="2">
+        <brand>
+          <template #extra-text> smart source code storage </template>
+        </brand>
+      </n-grid-item>
 
-        <n-grid-item class="top-buttons" :span="2">
-          <router-link :to="ROUTE_LOGIN_NAME">
-            <n-button ghost type="tertiary" :focusable="false">
-              <div class="primary-icon">
-                <icon :value="SignInIcon" />
-              </div>
-
-              <div class="text">open app</div>
-            </n-button>
-          </router-link>
-        </n-grid-item>
-
-        <n-grid-item class="label" :span="2">
-          About
-          <n-divider />
-        </n-grid-item>
-
-        <n-grid-item :span="2"> T.B.D. </n-grid-item>
-
-        <n-grid-item class="label" :span="2">
-          Features
-          <n-divider />
-        </n-grid-item>
-
-        <n-grid-item
-          v-for="({ icon, header, text }, index) of features"
-          :key="index"
-          :span="2"
-          class="feature"
-        >
-          <n-descriptions label-placement="top" :column="4">
-            <n-descriptions-item>
-              <div class="icon-wrapper">
-                <icon :value="icon" />
-              </div>
-            </n-descriptions-item>
-
-            <n-descriptions-item>
-              <div class="text-wrapper">
-                <div class="header">{{ header }}</div>
-
-                <div class="text">
-                  {{ text }}
-                </div>
-              </div>
-            </n-descriptions-item>
-          </n-descriptions>
-        </n-grid-item>
-
-        <n-grid-item class="bottom-buttons" :span="2">
+      <n-grid-item class="top-buttons" :span="2">
+        <router-link :to="ROUTE_LOGIN_NAME">
           <n-button ghost type="tertiary" :focusable="false">
             <div class="primary-icon">
-              <icon :value="EmailIcon" />
+              <icon :value="SignInIcon" />
             </div>
 
-            <div class="text">Request demo</div>
+            <div class="text">open app</div>
           </n-button>
-        </n-grid-item>
-      </n-grid>
-    </n-card>
+        </router-link>
+      </n-grid-item>
+
+      <n-grid-item class="label" :span="2">
+        About
+        <n-divider />
+      </n-grid-item>
+
+      <n-grid-item :span="2"> T.B.D. </n-grid-item>
+
+      <n-grid-item class="label" :span="2">
+        Features
+        <n-divider />
+      </n-grid-item>
+
+      <n-grid-item
+        v-for="({ icon, header, text }, index) of features"
+        :key="index"
+        :span="2"
+        class="feature"
+      >
+        <n-descriptions label-placement="top" :column="4">
+          <n-descriptions-item>
+            <div class="icon-wrapper">
+              <icon :value="icon" />
+            </div>
+          </n-descriptions-item>
+
+          <n-descriptions-item>
+            <div class="text-wrapper">
+              <div class="header">{{ header }}</div>
+
+              <div class="text">
+                {{ text }}
+              </div>
+            </div>
+          </n-descriptions-item>
+        </n-descriptions>
+      </n-grid-item>
+
+      <n-grid-item class="bottom-buttons" :span="2">
+        <n-button ghost type="tertiary" :focusable="false">
+          <div class="primary-icon">
+            <icon :value="EmailIcon" />
+          </div>
+
+          <div class="text">Request demo</div>
+        </n-button>
+      </n-grid-item>
+    </n-grid>
   </div>
 </template>
 
 <script setup lang="ts">
 import {
-  NCard,
   NGrid,
   NButton,
   NDivider,
