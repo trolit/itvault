5a713f98f362561e78b0fa0c750e03ad9ae4c56d
pawel idzikowski <iddziku@gmail.com>
2023-03-10 23:45:24 +0100

    Update entities.ts

--- backend/src/config/data-source/entities.ts
+++ backend/src/config/data-source/entities.ts
@@ -1,3 +1,5 @@
 import { User } from "@entities/User";
+import { Workflow } from "@entities/Workflow";
+import { UserToWorkflow } from "@entities/UserToWorkflow";
 
-export const entities = [User];
+export const entities = [User, Workflow, UserToWorkflow];
