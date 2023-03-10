d5c73b263aacb6ca5a04378cae8f82a1bac872eb
pawel idzikowski <iddziku@gmail.com>
2023-03-10 22:56:31 +0100

    Create UserToWorkflow.ts

---
+++ backend/src/entities/UserToWorkflow.ts
@@ -0,0 +1,23 @@
+import { Entity, Column, ManyToOne } from "typeorm";
+import { Base } from "./Base";
+import { User } from "./User";
+import { Workflow } from "./Workflow";
+import { WorkflowAccess } from "@enums/WorkflowAccess";
+
+@Entity("users_workflows")
+export class UserToWorkflow extends Base {
+  @Column()
+  public userId: number;
+
+  @Column()
+  public workflowId: number;
+
+  @Column({ type: "enum", enum: WorkflowAccess, default: WorkflowAccess.read })
+  public access: number;
+
+  @ManyToOne(() => User, user => user.workflows)
+  public user: User;
+
+  @ManyToOne(() => Workflow, workflow => workflow.users)
+  public workflow: Workflow;
+}
