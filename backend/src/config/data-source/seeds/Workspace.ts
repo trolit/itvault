63e82e5ba1b2496bb47bdf1d7a6477d465d77fef
pawel idzikowski <iddziku@gmail.com>
2023-03-11 20:59:31 +0100

    rename `Workflow` seeder

--- backend/src/config/data-source/seeds/Workspace.ts
+++ backend/src/config/data-source/seeds/Workspace.ts
@@ -3,24 +3,24 @@ import { DataSource } from "typeorm";
 import { Seeder } from "typeorm-extension";
 
 import { BCRYPT_SALT_ROUNDS } from "@config";
-import { Workflow } from "@entities/Workflow";
-import { TEST_LOCKED_WORKFLOW, TEST_UNLOCKED_WORKFLOW } from "./common";
+import { Workspace } from "@entities/Workspace";
+import { TEST_LOCKED_WORKSPACE, TEST_UNLOCKED_WORKSPACE } from "./common";
 
-export class WorkflowSeeder implements Seeder {
+export class WorkspaceSeeder implements Seeder {
   public async run(dataSource: DataSource): Promise<void> {
-    const repository = dataSource.getRepository(Workflow);
+    const repository = dataSource.getRepository(Workspace);
 
     const password = await bcrypt.hash(
-      TEST_LOCKED_WORKFLOW.password,
+      TEST_LOCKED_WORKSPACE.password,
       BCRYPT_SALT_ROUNDS
     );
 
     await repository.save([
       {
-        name: TEST_UNLOCKED_WORKFLOW.name,
+        name: TEST_UNLOCKED_WORKSPACE.name,
       },
       {
-        name: TEST_LOCKED_WORKFLOW.name,
+        name: TEST_LOCKED_WORKSPACE.name,
         password,
       },
     ]);
