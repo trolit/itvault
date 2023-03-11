a0721c8dded7f7b7135ad1706f26f3fae1c6173c
pawel idzikowski <iddziku@gmail.com>
2023-03-11 17:27:29 +0100

    Use `save` in Workflow seeder

--- backend/src/config/data-source/seeds/Workflow.ts
+++ backend/src/config/data-source/seeds/Workflow.ts
@@ -10,39 +10,19 @@ export class WorkflowSeeder implements Seeder {
   public async run(dataSource: DataSource): Promise<void> {
     const repository = dataSource.getRepository(Workflow);
 
-    const { name: TEST_UNLOCKED_WORKFLOW_NAME } = TEST_UNLOCKED_WORKFLOW;
+    const password = await bcrypt.hash(
+      TEST_LOCKED_WORKFLOW.password,
+      BCRYPT_SALT_ROUNDS
+    );
 
-    const unlockedWorkflow = await repository.findOneBy({
-      name: TEST_UNLOCKED_WORKFLOW_NAME,
-    });
-
-    if (!unlockedWorkflow) {
-      await repository.insert([
-        {
-          name: TEST_UNLOCKED_WORKFLOW_NAME,
-        },
-      ]);
-    }
-
-    const { name: TEST_LOCKED_WORKFLOW_NAME } = TEST_LOCKED_WORKFLOW;
-
-    const lockedWorkflow = await repository.findOneBy({
-      name: TEST_LOCKED_WORKFLOW_NAME,
-    });
-
-    if (!lockedWorkflow) {
-      const password = await bcrypt.hash(
-        TEST_LOCKED_WORKFLOW.password,
-        BCRYPT_SALT_ROUNDS
-      );
-
-      await repository.insert([
-        {
-          name: TEST_LOCKED_WORKFLOW_NAME,
-
-          password,
-        },
-      ]);
-    }
+    await repository.save([
+      {
+        name: TEST_UNLOCKED_WORKFLOW.name,
+      },
+      {
+        name: TEST_LOCKED_WORKFLOW.name,
+        password,
+      },
+    ]);
   }
 }
