690c8e145e9a9edbcce1dbba3648edc0b6cc39b7
pawel idzikowski <iddziku@gmail.com>
2023-03-11 13:59:08 +0100

    Update User.ts

--- backend/src/config/data-source/seeds/User.ts
+++ backend/src/config/data-source/seeds/User.ts
@@ -1,9 +1,10 @@
 import bcrypt from "bcrypt";
 import { DataSource } from "typeorm";
-import { BCRYPT_SALT_ROUNDS } from "@config";
 import { Seeder, SeederFactoryManager } from "typeorm-extension";
 
 import { User } from "@entities/User";
+import { BCRYPT_SALT_ROUNDS } from "@config";
+import { TEST_ACCOUNT_EMAIL, TEST_ACCOUNT_PASSWORD } from "./common";
 
 export class UserSeeder implements Seeder {
   public async run(
@@ -12,11 +13,14 @@ export class UserSeeder implements Seeder {
   ): Promise<void> {
     const repository = dataSource.getRepository(User);
 
-    const password = await bcrypt.hash("1234", BCRYPT_SALT_ROUNDS);
+    const password = await bcrypt.hash(
+      TEST_ACCOUNT_PASSWORD,
+      BCRYPT_SALT_ROUNDS
+    );
 
     await repository.insert([
       {
-        email: "admin@itvault.dev",
+        email: TEST_ACCOUNT_EMAIL,
         password,
       },
     ]);
