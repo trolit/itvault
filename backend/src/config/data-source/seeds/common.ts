edfe9ce7953b4be34e86daed06b648a06844c6c2
pawel idzikowski <iddziku@gmail.com>
2023-03-11 14:37:41 +0100

    Update common.ts

--- backend/src/config/data-source/seeds/common.ts
+++ backend/src/config/data-source/seeds/common.ts
@@ -1,3 +1,12 @@
 export const TEST_ACCOUNT_EMAIL = "admin@itvault.dev";
 
 export const TEST_ACCOUNT_PASSWORD = "1234";
+
+export const TEST_UNLOCKED_WORKFLOW = {
+  name: "unlocked test 0001",
+};
+
+export const TEST_LOCKED_WORKFLOW = {
+  name: "locked test 0010",
+  password: TEST_ACCOUNT_PASSWORD,
+};
