a8d7c03fee2c9aab33113fa4d1d5444429b968fd
pawel idzikowski <iddziku@gmail.com>
2023-03-11 21:03:04 +0100

    rename `Workflow` repository

--- backend/src/repositories/WorkflowRepository.ts
+++
@@ -1,7 +0,0 @@
-import { Repository } from "typeorm";
-import { injectable } from "tsyringe";
-
-import { Workflow } from "@entities/Workflow";
-
-@injectable()
-export class WorkflowRepository extends Repository<Workflow> {}
