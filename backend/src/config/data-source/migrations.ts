3269650571e079ed6e66078aa02c6c90b68426c5
pawel idzikowski <iddziku@gmail.com>
2023-03-12 00:52:45 +0100

    Update migrations.ts

--- backend/src/config/data-source/migrations.ts
+++ backend/src/config/data-source/migrations.ts
@@ -1,7 +1,11 @@
 import { InitUsersTable1665695149133 } from "@migrations/1665695149133-InitUsersTable";
 import { InitWorkspacesTable1678565252340 } from "@migrations/1678565252340-InitWorkspacesTable";
+import { InitBlueprintsTable1678578515028 } from "@migrations/1678578515028-InitBlueprintsTable";
+import { MarkWorkspacesTablePasswordColumnAsNullable1678578647291 } from "@migrations/1678578647291-MarkWorkspacesTablePasswordColumnAsNullable";
 
 export const migrations = [
   InitUsersTable1665695149133,
   InitWorkspacesTable1678565252340,
+  InitBlueprintsTable1678578515028,
+  MarkWorkspacesTablePasswordColumnAsNullable1678578647291,
 ];
