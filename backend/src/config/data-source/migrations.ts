6770f9cb40d41c3f128cc25e958a79d6bca43c2c
pawel idzikowski <iddziku@gmail.com>
2023-03-11 21:08:01 +0100

    Update migrations.ts

--- backend/src/config/data-source/migrations.ts
+++ backend/src/config/data-source/migrations.ts
@@ -1,3 +1,7 @@
 import { InitUsersTable1665695149133 } from "@migrations/1665695149133-InitUsersTable";
+import { InitWorkspacesTable1678565252340 } from "@migrations/1678565252340-InitWorkspacesTable";
 
-export const migrations = [InitUsersTable1665695149133];
+export const migrations = [
+  InitUsersTable1665695149133,
+  InitWorkspacesTable1678565252340,
+];
