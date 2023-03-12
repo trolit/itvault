ada3c3bc5a030cea1066d0959aa612ca71c9814f
pawel idzikowski <iddziku@gmail.com>
2023-03-12 12:49:17 +0100

    extract blueprint name and color to common

--- backend/src/config/data-source/seeds/Blueprint.ts
+++ backend/src/config/data-source/seeds/Blueprint.ts
@@ -2,15 +2,18 @@ import { DataSource } from "typeorm";
 import { Seeder } from "typeorm-extension";
 
 import { Blueprint } from "@entities/Blueprint";
+import { TEST_COMMON_BLUEPRINT } from "./common";
 
 export class BlueprintSeeder implements Seeder {
   public async run(dataSource: DataSource) {
     const repository = dataSource.getRepository(Blueprint);
 
+    const { name, color } = TEST_COMMON_BLUEPRINT;
+
     await repository.save({
-      name: "Common",
+      name,
+      color,
       description: "Common blueprint. This blueprint cannot be removed.",
-      color: "#C6CED1",
     });
   }
 }
