5d71747e9c8bd573a612b8688f50a84ebbcd83fc
pawel idzikowski <iddziku@gmail.com>
2023-03-12 13:06:40 +0100

    Update index.ts

--- backend/src/config/data-source/seeds/index.ts
+++ backend/src/config/data-source/seeds/index.ts
@@ -1,5 +1,13 @@
 import { UserSeeder } from "./User";
+import { BlueprintSeeder } from "./Blueprint";
 import { WorkspaceSeeder } from "./Workspace";
 import { UserToWorkspaceSeeder } from "./UserToWorkspace";
+import { BlueprintToWorkspaceSeeder } from "./BlueprintToWorkspace";
 
-export const seeds = [UserSeeder, WorkspaceSeeder, UserToWorkspaceSeeder];
+export const seeds = [
+  UserSeeder,
+  WorkspaceSeeder,
+  UserToWorkspaceSeeder,
+  BlueprintSeeder,
+  BlueprintToWorkspaceSeeder,
+];
