4d488ad3fa71bd334bb586c6770d27b03aec88e6
pawel idzikowski <iddziku@gmail.com>
2023-03-12 01:47:12 +0100

    Update entities.ts

--- backend/src/config/data-source/entities.ts
+++ backend/src/config/data-source/entities.ts
@@ -2,5 +2,12 @@ import { User } from "@entities/User";
 import { Blueprint } from "@entities/Blueprint";
 import { Workspace } from "@entities/Workspace";
 import { UserToWorkspace } from "@entities/UserToWorkspace";
+import { BlueprintToWorkspace } from "@entities/BlueprintToWorkspace";
 
-export const entities = [User, Workspace, UserToWorkspace, Blueprint];
+export const entities = [
+  User,
+  Workspace,
+  UserToWorkspace,
+  Blueprint,
+  BlueprintToWorkspace,
+];
