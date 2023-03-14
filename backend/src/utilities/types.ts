a4f5751bf4d0f9c1baec34388bc8afe0a03f7017
pawel idzikowski <iddziku@gmail.com>
2023-03-14 21:32:44 +0100

    Update types.ts

--- backend/src/utilities/types.ts
+++ backend/src/utilities/types.ts
@@ -2,4 +2,6 @@ import { Request, Response } from "express";
 
 export type RequestOfType<T> = Request<unknown, unknown, T>;
 
+export type RequestWithQuery<T> = Request<unknown, unknown, unknown, T>;
+
 export type ResponseOfType<T> = Response<T, Record<string, T>>;
