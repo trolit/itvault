652bf21da5f5c97afb8806d49b2d474fd861ac33
pawel idzikowski <iddziku@gmail.com>
2023-03-16 20:54:48 +0100

    Create EntityMapper.ts

---
+++ backend/src/services/EntityMapper.ts
@@ -0,0 +1,47 @@
+import { Base } from "@entities/Base";
+import { Type } from "@utilities/types";
+
+export class EntityMapperService {
+  mapOneToDto<T extends Base, Y extends Partial<T>>(
+    entity: T,
+    target: Type<Y>,
+    customProps?: (from: T) => Partial<Y>
+  ): Y {
+    return resolveMap(entity, target, customProps);
+  }
+
+  mapToDto<T extends Base, Y extends Partial<T>>(
+    entities: T[],
+    target: Type<Y>,
+    customProps?: (from: T) => Partial<Y>
+  ): Y[] {
+    return entities.map(entity => resolveMap(entity, target, customProps));
+  }
+}
+
+function resolveMap<T extends Base, Y extends Partial<T>>(
+  entity: T,
+  target: Type<Y>,
+  customProps?: (from: T) => Partial<Y>
+) {
+  const targetInstance = new target();
+
+  const targetKeys = Object.keys(targetInstance);
+
+  for (const [key, value] of Object.entries(entity)) {
+    if (targetKeys.includes(key)) {
+      targetInstance[key as keyof Y] = value;
+    }
+  }
+
+  if (customProps) {
+    const customPropsResult = customProps(entity);
+
+    return {
+      ...targetInstance,
+      ...customPropsResult,
+    };
+  }
+
+  return targetInstance;
+}
