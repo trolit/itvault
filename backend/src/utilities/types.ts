a5d4aa4cd9bd449040b88e00f83dac4d8223d392
pawel idzikowski <iddziku@gmail.com>
2023-03-16 20:52:03 +0100

    Update types.ts

--- backend/src/utilities/types.ts
+++ backend/src/utilities/types.ts
@@ -3,3 +3,6 @@ import { Request, Response } from "express";
 export type RequestOfType<T> = Request<unknown, unknown, T>;
 
 export type ResponseOfType<T> = Response<T, Record<string, T>>;
+
+// @NOTE https://stackoverflow.com/questions/39622778/what-is-new-in-typescript
+export type Type<T> = new (...args: unknown[]) => T;
