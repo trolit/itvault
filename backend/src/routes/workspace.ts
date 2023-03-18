1ba8c4a6633f4c0529e895e62f7e4f8d908e7661
pawel idzikowski <iddziku@gmail.com>
2023-03-18 21:29:16 +0100

    Update workspace.ts

--- backend/src/routes/workspace.ts
+++ backend/src/routes/workspace.ts
@@ -1,16 +1,10 @@
 import { Router } from "express";
 
 import { processRequestWith } from "./processRequestWith";
-import { StatusController } from "@controllers/Auth/Status";
-import { safeParseRequest } from "@middlewares/safeParseRequest";
 import { GetAllController } from "@controllers/Workspace/GetAll";
 
 const workspaceRoutes = Router();
 
-workspaceRoutes.get(
-  "/v1",
-  //   safeParseRequest<LoginDto>(loginSchema),
-  processRequestWith(GetAllController)
-);
+workspaceRoutes.get("/v1", processRequestWith(GetAllController));
 
 export = workspaceRoutes;
