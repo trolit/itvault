9c46e0d1712f85b1579f31552229641a2ef446ee
ⓘ p4w31 !d21k0w541 <iddziku@gmail.com>
2023-03-19 22:23:23 +0100

    Backend - minor update to `Permission` entity (#69)

--- backend/src/config/data-source/migrations.ts
+++ backend/src/config/data-source/migrations.ts
@@ -5,6 +5,8 @@ import { InitBlueprintsTable1678582183597 } from "@migrations/1678582183597-Init
 import { InitRoleAndPermissionStructure1679236763287 } from "@migrations/1679236763287-InitRoleAndPermissionStructure";
 import { AddRoleReferenceToUser1679251676652 } from "@migrations/1679251676652-AddRoleReferenceToUser";
 import { ExpandPermissionToRoleWithEnabled1679252477776 } from "@migrations/1679252477776-ExpandPermissionToRoleWithEnabled";
+import { MarkPermissionNamePropertyAsUnique1679260617325 } from "@migrations/1679260617325-MarkPermissionNamePropertyAsUnique";
+import { SetDefaultOfEnabledPropertyInPermissionToRole1679260845721 } from "@migrations/1679260845721-SetDefaultOfEnabledPropertyInPermissionToRole";
 
 export const migrations = [
   InitUsersTable1665695149133,
@@ -14,4 +16,6 @@ export const migrations = [
   InitRoleAndPermissionStructure1679236763287,
   AddRoleReferenceToUser1679251676652,
   ExpandPermissionToRoleWithEnabled1679252477776,
+  MarkPermissionNamePropertyAsUnique1679260617325,
+  SetDefaultOfEnabledPropertyInPermissionToRole1679260845721,
 ];
---
+++ backend/src/migrations/1679260617325-MarkPermissionNamePropertyAsUnique.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class MarkPermissionNamePropertyAsUnique1679260617325 implements MigrationInterface {
+    name = 'MarkPermissionNamePropertyAsUnique1679260617325'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`permissions\` ADD UNIQUE INDEX \`IDX_48ce552495d14eae9b187bb671\` (\`name\`)`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`permissions\` DROP INDEX \`IDX_48ce552495d14eae9b187bb671\``);
+    }
+
+}
