f4a6b0da38387fa87e6eb9ce16700eaba08a577c
pawel idzikowski <iddziku@gmail.com>
2023-03-19 22:05:15 +0100

    Update index.ts

--- backend/src/config/data-source/seeds/index.ts
+++ backend/src/config/data-source/seeds/index.ts
@@ -4,11 +4,13 @@ import { BlueprintSeeder } from "./Blueprint";
 import { WorkspaceSeeder } from "./Workspace";
 import { PermissionSeeder } from "./Permission";
 import { UserToWorkspaceSeeder } from "./UserToWorkspace";
+import { PermissionToRoleSeeder } from "./PermissionToRole";
 import { BlueprintToWorkspaceSeeder } from "./BlueprintToWorkspace";
 
 export const seeds = [
   PermissionSeeder,
   RoleSeeder,
+  PermissionToRoleSeeder,
   UserSeeder,
   WorkspaceSeeder,
   UserToWorkspaceSeeder,
