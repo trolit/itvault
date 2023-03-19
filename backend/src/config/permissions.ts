099ab25335900305d3e4033519e3c9c065125d7e
pawel idzikowski <iddziku@gmail.com>
2023-03-19 21:44:26 +0100

    Update permissions.ts

--- backend/src/config/permissions.ts
+++ backend/src/config/permissions.ts
@@ -1,8 +1,20 @@
-export const CREATE_ROLE_PERMISSION = "Create role";
+import { IPermission } from "@interfaces/IPermission";
+import { Permission } from "@enums/Permission";
 
-export const UPDATE_ROLE_PERMISSION = "Update role";
+export const CREATE_ROLE_PERMISSION: IPermission = {
+  id: Permission.CreateRole,
+  name: "Create role",
+};
 
-export const VIEW_ALL_WORKFLOWS_PERMISSION = "View all workflows";
+export const UPDATE_ROLE_PERMISSION: IPermission = {
+  id: Permission.UpdateRole,
+  name: "Update role",
+};
+
+export const VIEW_ALL_WORKFLOWS_PERMISSION: IPermission = {
+  id: Permission.ViewAllWorkflows,
+  name: "View all workflows",
+};
 
 export const ALL_PERMISSIONS = [
   CREATE_ROLE_PERMISSION,
