e5e2ded3354c90695bc0ad3717e0eb353581c37b
pawel idzikowski <iddziku@gmail.com>
2023-03-19 21:00:08 +0100

    update config files (permissions / roles)

--- backend/src/config/permissions.ts
+++ backend/src/config/permissions.ts
@@ -3,3 +3,9 @@ export const CREATE_ROLE_PERMISSION = "Create role";
 export const UPDATE_ROLE_PERMISSION = "Update role";
 
 export const VIEW_ALL_WORKFLOWS_PERMISSION = "View all workflows";
+
+export const ALL_PERMISSIONS = [
+  CREATE_ROLE_PERMISSION,
+  UPDATE_ROLE_PERMISSION,
+  VIEW_ALL_WORKFLOWS_PERMISSION,
+];
--- backend/src/config/roles.ts
+++ backend/src/config/roles.ts
@@ -3,3 +3,5 @@ export const HEAD_ADMIN_ROLE = "Head Admin";
 export const ADMIN_ROLE = "Admin";
 
 export const MEMBER_ROLE = "Member";
+
+export const ALL_ROLES = [HEAD_ADMIN_ROLE, ADMIN_ROLE, MEMBER_ROLE];
