bb6e574577bae36d04772494107fb558f1c7d11d
pawel idzikowski <iddziku@gmail.com>
2023-03-19 19:53:49 +0100

    Generate migration which connects Role & User

---
+++ backend/src/migrations/1679251676652-AddRoleReferenceToUser.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddRoleReferenceToUser1679251676652 implements MigrationInterface {
+    name = 'AddRoleReferenceToUser1679251676652'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`users\` ADD \`roleId\` int NOT NULL`);
+        await queryRunner.query(`ALTER TABLE \`users\` ADD CONSTRAINT \`FK_368e146b785b574f42ae9e53d5e\` FOREIGN KEY (\`roleId\`) REFERENCES \`roles\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`users\` DROP FOREIGN KEY \`FK_368e146b785b574f42ae9e53d5e\``);
+        await queryRunner.query(`ALTER TABLE \`users\` DROP COLUMN \`roleId\``);
+    }
+
+}
