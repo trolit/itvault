3d021bdd8eaf7b7bf2cc78ed61904cbdeb14c739
pawel idzikowski <iddziku@gmail.com>
2023-03-21 17:30:08 +0100

    Create requirePermission.ts

---
+++ backend/src/middlewares/requirePermission.ts
@@ -0,0 +1,22 @@
+import { StatusCodes as HTTP } from "http-status-codes";
+import { Request, NextFunction, Response } from "express";
+
+import { Permission } from "@enums/Permission";
+import { PermissionService } from "@services/PermissionService";
+
+export const requirePermission = async (permission: Permission) => {
+  return async (request: Request, response: Response, next: NextFunction) => {
+    const permissionService = new PermissionService();
+
+    const hasRequiredPermission = await permissionService.hasPermission(
+      1, // @TBD
+      permission
+    );
+
+    if (!hasRequiredPermission) {
+      return response.status(HTTP.FORBIDDEN).send();
+    }
+
+    next();
+  };
+};
