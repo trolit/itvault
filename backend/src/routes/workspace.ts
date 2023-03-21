ea3d13ad93b4fbe30b62f406a9663a105a14c627
pawel idzikowski <iddziku@gmail.com>
2023-03-21 20:19:21 +0100

    Update workspace.ts

--- backend/src/routes/workspace.ts
+++ backend/src/routes/workspace.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 
 import { processRequestWith } from "./processRequestWith";
+import { validateToken } from "@middlewares/validateToken";
 import { GetAllController } from "@controllers/Workspace/GetAll";
 
 const workspaceRoutes = Router();
 
-workspaceRoutes.get("/v1", processRequestWith(GetAllController));
+workspaceRoutes.get("/v1", validateToken, processRequestWith(GetAllController));
 
 export = workspaceRoutes;
