47cb5188e15225e95f82134fd04c7585cb8cc2dd
pawel idzikowski <iddziku@gmail.com>
2023-03-22 20:15:06 +0100

    Update validateToken.ts

--- backend/src/middlewares/validateToken.ts
+++ backend/src/middlewares/validateToken.ts
@@ -3,10 +3,11 @@ import { StatusCodes as HTTP } from "http-status-codes";
 import { Request, NextFunction, Response } from "express";
 
 import { AuthService } from "@services/Auth";
+import { JWT_TOKEN_COOKIE_KEY } from "@config/index";
 
 export const validateToken = (() => {
   return async (request: Request, response: Response, next: NextFunction) => {
-    const token = request.cookies["token"];
+    const token = request.cookies[JWT_TOKEN_COOKIE_KEY];
 
     if (!token) {
       return response.status(HTTP.FORBIDDEN).send();
