fbb4440b096cf48edf7f6087916b4c4d62050661
pawel idzikowski <iddziku@gmail.com>
2023-03-22 22:04:10 +0100

    Update Status.ts

--- backend/src/controllers/Auth/Status.ts
+++ backend/src/controllers/Auth/Status.ts
@@ -1,37 +1,29 @@
-import { IsNull } from "typeorm";
 import type { Request } from "express";
-import { autoInjectable } from "tsyringe";
+import { inject, injectable } from "tsyringe";
 import { StatusCodes as HTTP } from "http-status-codes";
 
 import { UserDto } from "@dtos/User";
-import { User } from "@entities/User";
-import { AuthService } from "@services/Auth";
-import { dataSource } from "@config/data-source";
 import { ResponseOfType } from "@utilities/types";
 import { IController } from "@interfaces/IController";
-import { UserRepository } from "@repositories/UserRepository";
 import { JWT_TOKEN_COOKIE_KEY } from "@config/index";
+import { IAuthService } from "@interfaces/IAuthService";
+import { IUserRepository } from "@interfaces/IUserRepository";
 
-@autoInjectable()
+@injectable()
 export class StatusController implements IController {
-  private userRepository: UserRepository;
-
-  constructor(private _authService?: AuthService) {
-    this.userRepository = dataSource.getRepository(User);
-  }
+  constructor(
+    @inject("IUserRepository") private userRepository: IUserRepository,
+    @inject("IAuthService") private authService: IAuthService
+  ) {}
 
   async invoke(request: Request, response: ResponseOfType<UserDto>) {
-    if (!this._authService) {
-      return response.status(HTTP.INTERNAL_SERVER_ERROR).send();
-    }
-
     const { token } = request.cookies;
 
     if (!token) {
       return response.status(HTTP.FORBIDDEN).send();
     }
 
-    const result = this._authService.verifyToken(token);
+    const result = this.authService.verifyToken(token);
 
     if (result.error) {
       response.clearCookie(JWT_TOKEN_COOKIE_KEY);
@@ -41,10 +33,7 @@ export class StatusController implements IController {
 
     const { email } = result.payload;
 
-    const user = await this.userRepository.findOneBy({
-      email,
-      deletedAt: IsNull(),
-    });
+    const user = await this.userRepository.findByEmail(email);
 
     if (!user) {
       return response.status(HTTP.FORBIDDEN).send();
