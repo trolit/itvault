7240ff0bd71d66aca6fd97da36a635a8b1291bc7
pawel idzikowski <iddziku@gmail.com>
2023-03-23 20:31:22 +0100

    Create setupSyringe.ts

---
+++ backend/src/utilities/setupSyringe.ts
@@ -0,0 +1,35 @@
+import fs from "fs";
+import path from "path";
+import { container } from "tsyringe";
+
+export const setupSyringe = () => {
+  registerDependenciesFromRequestedLocation("repositories");
+
+  registerDependenciesFromRequestedLocation("services");
+};
+
+function registerDependenciesFromRequestedLocation(requestedLocation: string) {
+  const dependencyInterfacePath = path.join("src", "interfaces");
+
+  fs.readdir(`src/${requestedLocation}`, (error, files) => {
+    files.forEach(async file => {
+      const [dependencyFilename] = file.split(".");
+
+      const interfaceName = `I${dependencyFilename}`;
+
+      console.log(path.join(dependencyInterfacePath, `${interfaceName}.ts`));
+
+      if (
+        fs.existsSync(path.join(dependencyInterfacePath, `${interfaceName}.ts`))
+      ) {
+        const dependency = await import(
+          `@${requestedLocation}/${dependencyFilename}.js`
+        );
+
+        container.register(interfaceName, dependency);
+      } else {
+        console.log(`⚠️⚠️⚠️  Failed to register --> ${dependencyFilename} <--`);
+      }
+    });
+  });
+}
