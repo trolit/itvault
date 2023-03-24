0b6dcc8470300c76b9fbc260a73d7dc8f3708958
pawel idzikowski <iddziku@gmail.com>
2023-03-24 23:18:24 +0100

    Update UserRepository.ts

--- backend/src/repositories/UserRepository.ts
+++ backend/src/repositories/UserRepository.ts
@@ -2,16 +2,19 @@ import { injectable } from "tsyringe";
 import { IsNull, Repository } from "typeorm";
 
 import { User } from "@entities/User";
-import { dataSource } from "@config/data-source";
+import { BaseRepository } from "./BaseRepository";
 import { IUserRepository } from "@interfaces/IUserRepository";
 
 @injectable()
-export class UserRepository implements IUserRepository {
-  private database: Repository<User>;
-
+export class UserRepository
+  extends BaseRepository<User>
+  implements IUserRepository
+{
   constructor() {
-    this.database = dataSource.getRepository(User);
+    super(User);
   }
+
+  protected database: Repository<User>;
 
   findByEmail(email: string): Promise<User | null> {
     return this.database.findOneBy({ email, deletedAt: IsNull() });
