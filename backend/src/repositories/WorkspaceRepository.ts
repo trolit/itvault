bca8f53cde5f34b961fef066eec5f9a8098689be
pawel idzikowski <iddziku@gmail.com>
2023-03-24 19:35:32 +0100

    Update WorkspaceRepository.ts

--- backend/src/repositories/WorkspaceRepository.ts
+++ backend/src/repositories/WorkspaceRepository.ts
@@ -1,7 +1,44 @@
-import { Repository } from "typeorm";
+import { IsNull, Repository } from "typeorm";
 import { injectable } from "tsyringe";
 
 import { Workspace } from "@entities/Workspace";
+import { dataSource } from "@config/data-source";
+import { IWorkspaceRepository } from "@interfaces/IWorkspaceRepository";
 
 @injectable()
-export class WorkspaceRepository extends Repository<Workspace> {}
+export class WorkspaceRepository implements IWorkspaceRepository {
+  private database: Repository<Workspace>;
+
+  constructor() {
+    this.database = dataSource.getRepository(Workspace);
+  }
+
+  getAll(
+    take: number,
+    skip: number,
+    userId?: number
+  ): Promise<[result: Workspace[], total: number]> {
+    const userIdQuery = userId
+      ? {
+          where: {
+            deletedAt: IsNull(),
+            userToWorkspace: {
+              userId,
+            },
+            relations: {
+              userToWorkspace: true,
+            },
+          },
+        }
+      : {};
+
+    return this.database.findAndCount({
+      take,
+      skip,
+      order: {
+        name: "asc",
+      },
+      ...userIdQuery,
+    });
+  }
+}
