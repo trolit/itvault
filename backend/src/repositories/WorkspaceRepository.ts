eab4240c529a2431743b88ba43cff4679bc39c45
pawel idzikowski <iddziku@gmail.com>
2023-03-24 23:20:03 +0100

    Update WorkspaceRepository.ts

--- backend/src/repositories/WorkspaceRepository.ts
+++ backend/src/repositories/WorkspaceRepository.ts
@@ -2,15 +2,18 @@ import { injectable } from "tsyringe";
 import { IsNull, Repository } from "typeorm";
 
 import { Workspace } from "@entities/Workspace";
-import { dataSource } from "@config/data-source";
+import { BaseRepository } from "./BaseRepository";
 import { IWorkspaceRepository } from "@interfaces/IWorkspaceRepository";
 
 @injectable()
-export class WorkspaceRepository implements IWorkspaceRepository {
-  private database: Repository<Workspace>;
+export class WorkspaceRepository
+  extends BaseRepository<Workspace>
+  implements IWorkspaceRepository
+{
+  protected database: Repository<Workspace>;
 
   constructor() {
-    this.database = dataSource.getRepository(Workspace);
+    super(Workspace);
   }
 
   getAll(
