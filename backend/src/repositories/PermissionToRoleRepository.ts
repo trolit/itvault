9a9f13493be2a3f38116433961a36ff26642070b
pawel idzikowski <iddziku@gmail.com>
2023-03-25 13:33:15 +0100

    Update PermissionToRoleRepository.ts

--- backend/src/repositories/PermissionToRoleRepository.ts
+++ backend/src/repositories/PermissionToRoleRepository.ts
@@ -1,7 +1,25 @@
 import { Repository } from "typeorm";
 import { injectable } from "tsyringe";
 
+import { BaseRepository } from "./BaseRepository";
 import { PermissionToRole } from "@entities/PermissionToRole";
+import { IPermissionToRoleRepository } from "@interfaces/IPermissionToRoleRepository";
 
 @injectable()
-export class PermissionToRoleRepository extends Repository<PermissionToRole> {}
+export class PermissionToRoleRepository
+  extends BaseRepository<PermissionToRole>
+  implements IPermissionToRoleRepository
+{
+  protected database: Repository<PermissionToRole>;
+
+  constructor() {
+    super(PermissionToRole);
+  }
+
+  getRolePermission(
+    roleId: number,
+    permissionId: number
+  ): Promise<PermissionToRole | null> {
+    return this.database.findOneBy({ roleId, permissionId });
+  }
+}
