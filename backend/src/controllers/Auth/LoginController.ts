c8659729ba348263af09302c0c045b8f3ee540e2
pawel idzikowski <iddziku@gmail.com>
2023-03-26 00:33:55 +0100

    Update LoginController.ts

--- backend/src/controllers/Auth/LoginController.ts
+++ backend/src/controllers/Auth/LoginController.ts
@@ -11,12 +11,15 @@ import { IAuthService } from "@interfaces/IAuthService";
 import { NODE_ENV, JWT_TOKEN_COOKIE_KEY } from "@config";
 import { IUserRepository } from "@interfaces/IUserRepository";
 import { RequestOfType, ResponseOfType } from "@utilities/types";
+import { IEntityMapperService } from "@interfaces/IEntityMapperService";
 
 @injectable()
 export class LoginController implements IController {
   constructor(
     @inject(Di.UserRepository) private userRepository: IUserRepository,
-    @inject(Di.AuthService) private authService: IAuthService
+    @inject(Di.AuthService) private authService: IAuthService,
+    @inject(Di.EntityMapperService)
+    private entityMapperService: IEntityMapperService
   ) {}
 
   async invoke(
@@ -39,12 +42,25 @@ export class LoginController implements IController {
 
     const token = this.authService.signToken({ email, id: user.id });
 
+    const mappedUser = this.entityMapperService.mapOneToDto(
+      user,
+      UserDto,
+      ({ role: { id, name, permissionToRole } }) => ({
+        roleId: id,
+        roleName: name,
+        permissions: permissionToRole.map(({ enabled, permission }) => ({
+          ...permission,
+          enabled,
+        })),
+      })
+    );
+
     return response
       .cookie(JWT_TOKEN_COOKIE_KEY, token, {
         httpOnly: true,
         secure: NODE_ENV === Environment.Production,
       })
       .status(HTTP.OK)
-      .send({ email });
+      .send(mappedUser);
   }
 }
