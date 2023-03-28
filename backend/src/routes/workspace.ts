52f8a6e2ed14cac7223cd9a7d32aa0e9437adbdc
pawel idzikowski <iddziku@gmail.com>
2023-03-28 20:45:21 +0200

    include paginationSchema in `GetAllController`

--- backend/src/routes/workspace.ts
+++ backend/src/routes/workspace.ts
@@ -1,11 +1,18 @@
 import { Router } from "express";
 
+import { paginationSchema } from "@schemas/pagination";
 import { processRequestWith } from "./processRequestWith";
 import { validateToken } from "@middlewares/validateToken";
+import { safeParseRequest } from "@middlewares/safeParseRequest";
 import { GetAllController } from "@controllers/Workspace/GetAllController";
 
 const workspaceRoutes = Router();
 
-workspaceRoutes.get("/v1", validateToken, processRequestWith(GetAllController));
+workspaceRoutes.get(
+  "/v1",
+  validateToken,
+  safeParseRequest({ query: { withSchema: paginationSchema } }),
+  processRequestWith(GetAllController)
+);
 
 export = workspaceRoutes;
