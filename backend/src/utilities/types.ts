ebfc671b3124c434e695959479ffcd5a6099ee8d
pawel idzikowski <iddziku@gmail.com>
2023-03-28 19:08:50 +0200

    Update types.ts

--- backend/src/utilities/types.ts
+++ backend/src/utilities/types.ts
@@ -4,7 +4,9 @@ export type RequestOfType<T> = Request<unknown, unknown, T>;
 
 export type RequestWithQuery<T> = Request<unknown, unknown, unknown, T>;
 
-export type ResponseOfType<T> = Response<T, Record<string, T>>;
-
 // @NOTE https://stackoverflow.com/questions/39622778/what-is-new-in-typescript
 export type Type<T> = new (...args: unknown[]) => T;
+
+export type CustomRequest<B = void, Q = void> = Request<unknown, unknown, B, Q>;
+
+export type CustomResponse<T> = Response<T, Record<string, T>>;
