f0e55e45e6fb2b7582b813fc05d06d463f6e675d
pawel idzikowski <iddziku@gmail.com>
2023-03-28 17:55:31 +0200

    Update IController.ts

--- backend/src/interfaces/IController.ts
+++ backend/src/interfaces/IController.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from "express";
 
-export interface IController {
-  invoke(request: Request, response: Response): Promise<Response>;
+export interface IController<Q = void, B = void, R = void> {
+  invoke(
+    request: Request<unknown, unknown, B, Q>,
+    response: Response<R, Record<string, R>>
+  ): Promise<Response>;
 }
