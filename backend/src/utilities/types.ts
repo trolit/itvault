f7d7a8464f5a54c8b5e462b3d7fb6c886acd0124
pawel idzikowski <iddziku@gmail.com>
2023-03-28 20:42:43 +0200

    Update types.ts

--- backend/src/utilities/types.ts
+++ backend/src/utilities/types.ts
@@ -1,3 +1,4 @@
+import { ZodSchema } from "zod";
 import { Request, Response } from "express";
 
 export type RequestOfType<T> = Request<unknown, unknown, T>;
@@ -10,3 +11,8 @@ export type Type<T> = new (...args: unknown[]) => T;
 export type CustomRequest<B = void, Q = void> = Request<unknown, unknown, B, Q>;
 
 export type CustomResponse<T> = Response<T, Record<string, T>>;
+
+export type ParseableRequestContent = Partial<{
+  body: { withSchema: ZodSchema };
+  query: { withSchema: ZodSchema };
+}>;
