b1462dc8a7107c09177533adea1e9635c4a017c6
ⓘ p4w31 !d21k0w541 <iddziku@gmail.com>
2023-03-29 20:54:33 +0200

    Backend - store account activity in redis +check it in `reqAuthentication` (#82)

--- backend/src/middlewares/requireAuthentication.ts
+++ backend/src/middlewares/requireAuthentication.ts
@@ -3,10 +3,14 @@ import { StatusCodes as HTTP } from "http-status-codes";
 import { Request, NextFunction, Response } from "express";
 
 import { Di } from "@enums/Di";
+import { UserDto } from "@dtos/UserDto";
 import { JWT_TOKEN_COOKIE_KEY } from "@config/index";
 import { IAuthService } from "@interfaces/IAuthService";
+import { IRedisService } from "@interfaces/IRedisService";
 
-export const requireToken = (() => {
+export const requireAuthentication = ((
+  options = { withActiveAccount: true }
+) => {
   return async (request: Request, response: Response, next: NextFunction) => {
     const token = request.cookies[JWT_TOKEN_COOKIE_KEY];
 
@@ -24,6 +28,30 @@ export const requireToken = (() => {
 
     request.userId = result.payload.id;
 
+    if (!options.withActiveAccount) {
+      next();
+    }
+
+    const isAccountActive = await verifyAccountStatus(request.userId);
+
+    if (!isAccountActive) {
+      return response.status(HTTP.FORBIDDEN).send();
+    }
+
     next();
   };
 })();
+
+async function verifyAccountStatus(userId: number) {
+  const redisService = container.resolve<IRedisService>(Di.RedisService);
+
+  const userDetails = await redisService.getKey(userId.toString());
+
+  if (!userDetails) {
+    return false;
+  }
+
+  const parsedUserDetails = userDetails.asParsed<UserDto>();
+
+  return parsedUserDetails.isActive;
+}
--- backend/src/routes/workspace.ts
+++ backend/src/routes/workspace.ts
@@ -1,16 +1,16 @@
 import { Router } from "express";
 
 import { paginationSchema } from "@schemas/pagination";
-import { requireToken } from "@middlewares/requireToken";
 import { processRequestWith } from "./processRequestWith";
 import { safeParseRequest } from "@middlewares/safeParseRequest";
 import { GetAllController } from "@controllers/Workspace/GetAllController";
+import { requireAuthentication } from "@middlewares/requireAuthentication";
 
 const workspaceRoutes = Router();
 
 workspaceRoutes.get(
   "/v1",
-  requireToken,
+  requireAuthentication,
   safeParseRequest({ query: { withSchema: paginationSchema } }),
   processRequestWith(GetAllController)
 );
