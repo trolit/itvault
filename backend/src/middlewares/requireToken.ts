0e1a7caf36e5f2e65b36a50674300395e8f9c2f9
pawel idzikowski <iddziku@gmail.com>
2023-03-29 20:40:34 +0200

    Update requireToken.ts

--- backend/src/middlewares/requireToken.ts
+++ backend/src/middlewares/requireToken.ts
@@ -3,10 +3,12 @@ import { StatusCodes as HTTP } from "http-status-codes";
 import { Request, NextFunction, Response } from "express";
 
 import { Di } from "@enums/Di";
+import { UserDto } from "@dtos/UserDto";
 import { JWT_TOKEN_COOKIE_KEY } from "@config/index";
 import { IAuthService } from "@interfaces/IAuthService";
+import { IRedisService } from "@interfaces/IRedisService";
 
-export const requireToken = (() => {
+export const requireToken = ((options = { isActive: true }) => {
   return async (request: Request, response: Response, next: NextFunction) => {
     const token = request.cookies[JWT_TOKEN_COOKIE_KEY];
 
@@ -24,6 +26,30 @@ export const requireToken = (() => {
 
     request.userId = result.payload.id;
 
+    if (!options.isActive) {
+      next();
+    }
+
+    const res = await requireActiveAccount(request.userId);
+
+    if (!res) {
+      return response.status(HTTP.FORBIDDEN).send();
+    }
+
     next();
   };
 })();
+
+async function requireActiveAccount(userId: number) {
+  const redisService = container.resolve<IRedisService>(Di.RedisService);
+
+  const userDetails = await redisService.getKey(userId.toString());
+
+  if (!userDetails) {
+    return false;
+  }
+
+  const parsedUserDetails = userDetails.asParsed<UserDto>();
+
+  return parsedUserDetails.isActive;
+}
