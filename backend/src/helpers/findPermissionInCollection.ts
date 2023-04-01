1750ef2d9cbce26a3ca02d97dd11821fa57e6263
pawel idzikowski <iddziku@gmail.com>
2023-04-01 20:38:34 +0200

    create `isPermissionEnabled` helper

--- backend/src/helpers/findPermissionInCollection.ts
+++
@@ -1,7 +0,0 @@
-import { PermissionDto } from "@dtos/PermissionDto";
-import { Permission } from "@enums/Permission";
-
-export const findPermissionInCollection = (
-  permission: Permission,
-  collection: PermissionDto[]
-) => collection.find(({ id }) => id === permission);
