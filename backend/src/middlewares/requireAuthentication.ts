09022cd0da2c6d6fdbd418f5c4376f6ac65f6829
pawel idzikowski <iddziku@gmail.com>
2023-04-01 20:18:52 +0200

    expand `options`

--- backend/src/middlewares/requireAuthentication.ts
+++ backend/src/middlewares/requireAuthentication.ts
@@ -4,12 +4,21 @@ import { Request, NextFunction, Response } from "express";
 
 import { Di } from "@enums/Di";
 import { UserDto } from "@dtos/UserDto";
+import { Permission } from "@enums/Permission";
 import { JWT_TOKEN_COOKIE_KEY } from "@config/index";
 import { IAuthService } from "@interfaces/IAuthService";
 import { IDataStoreService } from "@interfaces/IDataStoreService";
 
+interface IOptions {
+  withActiveAccount: boolean;
+  withPermission?: Permission;
+}
+
 export const requireAuthentication = ((
   options = { withActiveAccount: true }
+  options: IOptions = {
+    withActiveAccount: true,
+  }
 ) => {
   return async (request: Request, response: Response, next: NextFunction) => {
     const token = request.cookies[JWT_TOKEN_COOKIE_KEY];
