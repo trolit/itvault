70e3444e14738f1595d2bd8de759f4582ae520e7
pawel idzikowski <iddziku@gmail.com>
2023-04-02 01:18:32 +0200

    add `softDeleteById` to `BaseRepository`

--- backend/src/interfaces/IBaseRepository.ts
+++ backend/src/interfaces/IBaseRepository.ts
@@ -1,3 +1,7 @@
+import { UpdateResult } from "typeorm";
+
 export interface IBaseRepository<T> {
   findById(id: number): Promise<T | null>;
+
+  softDeleteById(id: number): Promise<UpdateResult>;
 }
--- backend/src/repositories/BaseRepository.ts
+++ backend/src/repositories/BaseRepository.ts
@@ -1,4 +1,4 @@
-import { FindOptionsWhere, Repository } from "typeorm";
+import { FindOptionsWhere, Repository, UpdateResult } from "typeorm";
 
 import { Type } from "@utilities/types";
 import { dataSource } from "@config/data-source";
@@ -15,5 +15,9 @@ export class BaseRepository<T extends { id: number }>
 
   findById(id: number): Promise<T | null> {
     return this.database.findOneBy({ id } as FindOptionsWhere<T>);
+  }
+
+  softDeleteById(id: number): Promise<UpdateResult> {
+    return this.database.softDelete({ id } as FindOptionsWhere<T>);
   }
 }
