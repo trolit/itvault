8b7f4920c12029cd0f4d6a6d583a05d1167dcb62
ⓘ p4w31 !d21k0w541 <iddziku@gmail.com>
2023-04-02 16:23:34 +0200

    Backend - logout controller (#87)

---
+++ backend/src/controllers/Auth/LogoutController.ts
@@ -0,0 +1,22 @@
+import { Response } from "express";
+import { injectable } from "tsyringe";
+import { StatusCodes as HTTP } from "http-status-codes";
+
+import { JWT_TOKEN_COOKIE_KEY } from "@config";
+import { CustomRequest } from "@utilities/types";
+import { IController } from "@interfaces/IController";
+
+@injectable()
+export class LogoutController implements IController {
+  async invoke(request: CustomRequest, response: Response) {
+    const token = request.cookies[JWT_TOKEN_COOKIE_KEY];
+
+    if (!token) {
+      return response.status(HTTP.BAD_REQUEST).send();
+    }
+
+    response.clearCookie(JWT_TOKEN_COOKIE_KEY);
+
+    return response.status(HTTP.OK).send();
+  }
+}
