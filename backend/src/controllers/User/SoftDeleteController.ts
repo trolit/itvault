9132b07d8dddb37466b70e1e8d690d763eb1d8f9
pawel idzikowski <iddziku@gmail.com>
2023-04-02 02:03:17 +0200

    Update SoftDeleteController.ts

--- backend/src/controllers/User/SoftDeleteController.ts
+++ backend/src/controllers/User/SoftDeleteController.ts
@@ -3,9 +3,11 @@ import { inject, injectable } from "tsyringe";
 import { StatusCodes as HTTP } from "http-status-codes";
 
 import { Di } from "@enums/Di";
+import { UserDto } from "@dtos/UserDto";
 import { CustomRequest } from "@utilities/types";
 import { IController } from "@interfaces/IController";
 import { IUserRepository } from "@interfaces/IUserRepository";
+import { IDataStoreService } from "@interfaces/IDataStoreService";
 
 interface IParams {
   id: number;
@@ -15,7 +17,9 @@ interface IParams {
 export class SoftDeleteController implements IController<IParams> {
   constructor(
     @inject(Di.UserRepository)
-    private _userRepository: IUserRepository
+    private _userRepository: IUserRepository,
+    @inject(Di.DataStoreService)
+    private _dataStoreService: IDataStoreService
   ) {}
 
   async invoke(request: CustomRequest<IParams>, response: Response) {
@@ -27,7 +31,9 @@ export class SoftDeleteController implements IController<IParams> {
       return response.status(HTTP.INTERNAL_SERVER_ERROR).send();
     }
 
-    // @TODO REDIS!
+    await this._dataStoreService.updateKey<UserDto>(id, state => {
+      state.isActive = false;
+    });
 
     return response.status(HTTP.NO_CONTENT).send();
   }
