80be2be8ddba5c318abbf7dc35380cf96e1e0847
pawel idzikowski <iddziku@gmail.com>
2023-04-07 00:08:46 +0200

    Create update.ts

---
+++ backend/src/schemas/user/update.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+import { UpdateUserDto } from "@dtos/UpdateUserDto";
+import { schemaForType } from "@helpers/schemaForType";
+
+const updateUserDtoSchema = schemaForType<UpdateUserDto>()(
+  z.object({
+    id: z.number().positive(),
+    data: z.object({
+      roleId: z.number().positive(),
+      deletedAt: z.optional(z.null()),
+    }),
+  })
+);
+
+export const updateUsersSchema = () => {
+  return schemaForType<{ value: UpdateUserDto[] }>()(
+    z.object({
+      value: z.array(updateUserDtoSchema),
+    })
+  );
+};
