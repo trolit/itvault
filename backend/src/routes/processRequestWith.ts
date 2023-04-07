ab22f11fbd109dd1ec5a7809c1b38219d779c1c7
pawel idzikowski <iddziku@gmail.com>
2023-04-07 16:05:00 +0200

    Update processRequestWith.ts

--- backend/src/routes/processRequestWith.ts
+++ backend/src/routes/processRequestWith.ts
@@ -1,10 +1,11 @@
 import { Response } from "express";
 import { InjectionToken } from "tsyringe";
 
+import { CustomRequest } from "@utils/types";
 import { instanceOf } from "@helpers/instanceOf";
 import { IController } from "@interfaces/IController";
-import { CustomRequest } from "@utils/types";
 
+// T-dependency, P-params, B-body, Q-query
 export const processRequestWith =
   <T, P, B, Q>(Controller: InjectionToken<T>) =>
   (...args: [CustomRequest<P, B, Q>, Response]) => {
