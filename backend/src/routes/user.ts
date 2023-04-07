841f5263c7be4cd40e0217526be4e422bf261725
pawel idzikowski <iddziku@gmail.com>
2023-04-07 15:55:40 +0200

    rename middlewares dir to middleware

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -5,14 +5,14 @@ import { User } from "@entities/User";
 import { deleteSchema } from "@schemas/delete";
 import { Permission } from "@enums/Permission";
 import { paginationSchema } from "@schemas/pagination";
+import { updateUsersSchema } from "@schemas/user/update";
 import { processRequestWith } from "./processRequestWith";
-import { safeParseRequest } from "@middlewares/safeParseRequest";
+import { UPDATE_USER_PERMISSIONS } from "@config/permissions";
+import { safeParseRequest } from "middleware/safeParseRequest";
 import { GetAllController } from "@controllers/User/GetAllController";
-import { requireAuthentication } from "@middlewares/requireAuthentication";
+import { requireAuthentication } from "middleware/requireAuthentication";
 import { SoftDeleteController } from "@controllers/User/SoftDeleteController";
 import { UpdateManyController } from "@controllers/User/UpdateManyController";
-import { UPDATE_USER_PERMISSIONS } from "@config/permissions";
-import { updateUsersSchema } from "@schemas/user/update";
 
 const userRoutes = Router();
 
