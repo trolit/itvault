66b119be531c26f8f875baa4f39e2b629b2cf824
pawel idzikowski <iddziku@gmail.com>
2023-04-07 15:33:16 +0200

    remove unused repository (PermissionToRole)

--- backend/src/interfaces/repository/IPermissionToRoleRepository.ts
+++
@@ -1,8 +0,0 @@
-import { PermissionToRole } from "@entities/PermissionToRole";
-
-export interface IPermissionToRoleRepository {
-  getRolePermission(
-    roleId: number,
-    permissionId: number
-  ): Promise<PermissionToRole | null>;
-}
