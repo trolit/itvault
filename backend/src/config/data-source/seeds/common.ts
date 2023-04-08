f25d42532735efc10a5cd481b98638be1154a4b5
pawel idzikowski <iddziku@gmail.com>
2023-04-08 21:08:13 +0200

    update `common` data for `seeders`

--- backend/src/config/data-source/seeds/common.ts
+++ backend/src/config/data-source/seeds/common.ts
@@ -1,17 +1,40 @@
-export const TEST_ACCOUNT_EMAIL = "admin@itvault.dev";
+import { ALL_EDITABLE_ROLES, HEAD_ADMIN_ROLE } from "@config/default-roles";
 
-export const TEST_ACCOUNT_PASSWORD = "1234";
+export const DOMAIN = "itvault.dev";
+export const PASSWORD = "1234";
+
+const { name, permissions } = HEAD_ADMIN_ROLE;
+
+const HEAD_ADMIN_ROLE_TEST_ACCOUNT = {
+  email: generateEmailByRoleName(name),
+  password: PASSWORD,
+  permissions,
+  roleName: name,
+};
+
+export const TEST_ACCOUNTS = [HEAD_ADMIN_ROLE_TEST_ACCOUNT].concat(
+  ALL_EDITABLE_ROLES.map(({ name, permissions }) => ({
+    email: generateEmailByRoleName(name),
+    password: PASSWORD,
+    permissions,
+    roleName: name,
+  }))
+);
 
 export const TEST_UNLOCKED_WORKSPACE = {
-  name: "unlocked test 0001",
+  name: "test 0001",
 };
 
 export const TEST_LOCKED_WORKSPACE = {
-  name: "locked test 0010",
-  password: TEST_ACCOUNT_PASSWORD,
+  name: "test 0010",
+  password: PASSWORD,
 };
 
 export const TEST_COMMON_BLUEPRINT = {
   name: "Common",
   color: "#C6CED1",
 };
+
+function generateEmailByRoleName(name: string) {
+  return `${name.toLowerCase().replace(/ /g, ".")}@${DOMAIN}`;
+}
