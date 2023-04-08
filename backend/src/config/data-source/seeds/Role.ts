7adc39f10cf68a92d3302941a712e21219d42679
pawel idzikowski <iddziku@gmail.com>
2023-04-08 21:20:35 +0200

    Update Role.ts

--- backend/src/config/data-source/seeds/Role.ts
+++ backend/src/config/data-source/seeds/Role.ts
@@ -1,20 +1,17 @@
 import { DataSource } from "typeorm";
 import { Seeder } from "typeorm-extension";
 
-import {
-  ALL_EDITABLE_ROLES,
-  HEAD_ADMIN_ROLE_NAME,
-} from "@config/default-roles";
 import { Role } from "@entities/Role";
+import { TEST_ACCOUNTS } from "./common";
 
 export class RoleSeeder implements Seeder {
   public async run(dataSource: DataSource) {
     const repository = dataSource.getRepository(Role);
 
-    await repository.save({
-      name: HEAD_ADMIN_ROLE_NAME,
-    });
-
-    await repository.save(ALL_EDITABLE_ROLES.map(({ name }) => ({ name })));
+    for (const { roleName } of TEST_ACCOUNTS) {
+      await repository.save({
+        name: roleName,
+      });
+    }
   }
 }
