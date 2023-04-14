a18b23e9cf7f9fb6a4d4552495366c7da7736220
pawel idzikowski <iddziku@gmail.com>
2023-04-15 00:13:14 +0200

    Update delete.ts

--- backend/src/schemas/delete.ts
+++ backend/src/schemas/delete.ts
@@ -1,10 +1,26 @@
 import { z } from "zod";
 
 import { Di } from "@enums/Di";
+import { User } from "@entities/User";
+import { SuperSchemaRunner } from "@utils/types";
 import { schemaForType } from "@helpers/schemaForType";
-import { existsSuperRefine } from "./super-refines/exists";
+import { existsSuperRefine } from "./common/existsSuperRefine";
 
-export const deleteSchema = <T>(repository: Di) => {
+const deleteSchemaRunner: SuperSchemaRunner = () => {
+  const isRecordAvailable = existsSuperRefine<User>(Di.UserRepository);
+
+  return schemaForType<{ id: number }>()(
+    z.object({
+      id: z.coerce.number().superRefine(isRecordAvailable),
+    })
+  );
+};
+
+export const deleteSchema = (() => {
+  return deleteSchemaRunner;
+})();
+
+export const deleteSchema2 = <T>(repository: Di) => {
   const isRecordAvailable = existsSuperRefine<T>(repository);
 
   return schemaForType<{ id: number }>()(
