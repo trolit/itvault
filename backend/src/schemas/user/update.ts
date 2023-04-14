e8b1153e3d60ef9291a34c9cc390757bdc54720c
pawel idzikowski <iddziku@gmail.com>
2023-04-14 22:27:22 +0200

    Update update.ts

--- backend/src/schemas/user/update.ts
+++ backend/src/schemas/user/update.ts
@@ -1,20 +1,69 @@
-import { z } from "zod";
+import Zod, { RefinementCtx, z, ZodIssueCode } from "zod";
 
+import { Permission } from "@enums/Permission";
+import { SuperSchemaRunner } from "@utils/types";
 import { UpdateUserDto } from "@dtos/UpdateUserDto";
 import { schemaForType } from "@helpers/schemaForType";
+import { ISuperSchemaCommonParams } from "@interfaces/ISuperSchemaParams";
 
-const updateUserDtoSchema = schemaForType<UpdateUserDto>()(
-  z.object({
-    id: z.number().positive(),
-    data: z.object({
-      roleId: z.optional(z.number().positive()),
-      isActive: z.optional(z.boolean()),
-    }),
-  })
-);
+const updateUsersSchemaRunner: SuperSchemaRunner = (
+  commonParams: ISuperSchemaCommonParams
+) => {
+  const {
+    request: { permissions },
+  } = commonParams;
 
-export const updateUsersSchema = schemaForType<{ value: UpdateUserDto[] }>()(
-  z.object({
-    value: z.array(updateUserDtoSchema).nonempty(),
-  })
-);
+  const updateUserDtoSchema = schemaForType<UpdateUserDto>()(
+    z.object({
+      id: z.number().positive(),
+      data: z.object({
+        roleId: z.optional(
+          z
+            .number()
+            .positive()
+            .superRefine((value: number, context: RefinementCtx) => {
+              if (!permissions[Permission.ChangeUserRole]) {
+                context.addIssue({
+                  code: ZodIssueCode.custom,
+                  message: "Missing permission to change account's role.",
+                });
+
+                return Zod.NEVER;
+              }
+            })
+        ),
+        isActive: z.optional(
+          z.boolean().superRefine((value: boolean, context: RefinementCtx) => {
+            if (value && !permissions[Permission.RestoreUserAccount]) {
+              context.addIssue({
+                code: ZodIssueCode.custom,
+                message: "Missing permission to restore account.",
+              });
+
+              return Zod.NEVER;
+            }
+
+            if (!value && !permissions[Permission.DeactivateUserAccount]) {
+              context.addIssue({
+                code: ZodIssueCode.custom,
+                message: "Missing permission to deactivate account.",
+              });
+
+              return Zod.NEVER;
+            }
+          })
+        ),
+      }),
+    })
+  );
+
+  return schemaForType<{ value: UpdateUserDto[] }>()(
+    z.object({
+      value: z.array(updateUserDtoSchema).nonempty(),
+    })
+  );
+};
+
+export const updateUsersSchema = (() => {
+  return updateUsersSchemaRunner;
+})();
