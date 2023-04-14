adcede3930f0b7e7d6e7118ca1cda8586f76a65d
pawel idzikowski <iddziku@gmail.com>
2023-04-14 21:26:45 +0200

    Update safeParseRequest.ts

--- backend/src/middleware/safeParseRequest.ts
+++ backend/src/middleware/safeParseRequest.ts
@@ -1,38 +1,41 @@
-import { ZodSchema } from "zod";
 import { StatusCodes as HTTP } from "http-status-codes";
 import { NextFunction, Request, Response } from "express";
 
-import { ParseableRequestContent } from "@utils/types";
+import { RequestParseContext } from "@utils/types";
 
-export const safeParseRequest = (toParse: ParseableRequestContent) => {
+export const safeParseRequest = <T>(context: RequestParseContext<T>) => {
   return async (request: Request, response: Response, next: NextFunction) => {
-    for (const key in toParse) {
-      const castedKey = <keyof ParseableRequestContent>key;
+    for (const key in context) {
+      type ParseableProperties = Omit<RequestParseContext<T>, "data">;
 
-      const schema = toParse[castedKey]?.withSchema;
+      const propertyName = <keyof ParseableProperties>key;
+
+      const parseableProperties: ParseableProperties = context;
+
+      const superSchemaRunner = parseableProperties[propertyName]?.withSchema;
+
+      if (!superSchemaRunner) {
+        return response.status(HTTP.INTERNAL_SERVER_ERROR).send();
+      }
+
+      const schema = superSchemaRunner({ request }, context.data);
 
       if (!schema) {
         return response.status(HTTP.INTERNAL_SERVER_ERROR).send();
       }
 
-      const result = await parseElement(request, castedKey, schema);
+      const result = await schema.safeParseAsync(
+        parseableProperties[propertyName]
+      );
 
       if (!result.success) {
         return response.status(HTTP.BAD_REQUEST).send(result.error.format());
       }
 
       // overwrite body with sanitized result
-      request[castedKey] = result.data;
+      request[propertyName] = result.data;
     }
 
     next();
   };
 };
-
-function parseElement(
-  request: Request,
-  key: keyof ParseableRequestContent,
-  schema: ZodSchema
-) {
-  return schema.safeParseAsync(request[key]);
-}
