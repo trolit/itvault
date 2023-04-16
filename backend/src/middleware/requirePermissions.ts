a7b4d3483cb9c20a253d0beb6b04dfc7d633a3e6
pawel idzikowski <iddziku@gmail.com>
2023-04-16 13:17:18 +0200

    Update requirePermissions.ts

--- backend/src/middleware/requirePermissions.ts
+++ backend/src/middleware/requirePermissions.ts
@@ -10,6 +10,10 @@ export const requirePermissions = (
   return async (request: Request, response: Response, next: NextFunction) => {
     const { permissions } = request;
 
+    if (!permissions) {
+      return response.status(HTTP.FORBIDDEN).send();
+    }
+
     if (Array.isArray(context)) {
       for (const permission of context) {
         if (!isPermissionEnabled(permission, permissions)) {
