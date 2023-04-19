f8996ee9d596ebc4473a640a5e4e663a1e1ae6f2
pawel idzikowski <iddziku@gmail.com>
2023-04-19 19:46:20 +0200

    Update composeDataStoreKey.ts

--- backend/src/helpers/composeDataStoreKey.ts
+++ backend/src/helpers/composeDataStoreKey.ts
@@ -1,5 +1,12 @@
 import { DataStoreKeyType } from "@enums/DataStoreKeyType";
 
-export const composeDataStoreKey = (key: string, keyType: DataStoreKeyType) => {
+export const composeDataStoreKey = (
+  key: string | number,
+  keyType: DataStoreKeyType
+) => {
+  if (typeof key !== "string") {
+    key = key.toString();
+  }
+
   return `data-store-${keyType}-${key}`;
 };
