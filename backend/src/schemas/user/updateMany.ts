3f56b511e9c9623bdb840839fa4cc6659e65d534
pawel idzikowski <iddziku@gmail.com>
2023-04-19 22:11:29 +0200

    prevent changing own account data through `updateMany`

--- backend/src/schemas/user/updateMany.ts
+++ backend/src/schemas/user/updateMany.ts
@@ -4,11 +4,30 @@ import { SuperSchemaRunner } from "@utils/types";
 import { UpdateUserDto } from "@dtos/UpdateUserDto";
 import { schemaForType } from "@helpers/schemaForType";
 import { HEAD_ADMIN_ROLE_ID } from "@config/default-roles";
+import { ISuperSchemaParams } from "@interfaces/ISuperSchemaParams";
 
-const updateManyUsersSchemaRunner: SuperSchemaRunner = () => {
+const updateManyUsersSchemaRunner: SuperSchemaRunner = (
+  commonParams: ISuperSchemaParams
+) => {
+  const {
+    request: { userId },
+  } = commonParams;
+
   const updateUserDtoSchema = schemaForType<UpdateUserDto>()(
     z.object({
-      id: z.number().positive(),
+      id: z
+        .number()
+        .positive()
+        .superRefine((id: number, context: RefinementCtx) => {
+          if (id === userId) {
+            context.addIssue({
+              code: ZodIssueCode.custom,
+              message: "Can't change own data.",
+            });
+
+            return Zod.NEVER;
+          }
+        }),
       data: z.object({
         roleId: z.optional(
           z
