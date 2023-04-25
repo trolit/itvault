e97d4d716b0ad84c30b198d21d8ebb9310a58334
pawel idzikowski <iddziku@gmail.com>
2023-04-25 17:03:17 +0200

    add `UserService` to `Di` enum

--- backend/src/enums/Di.ts
+++ backend/src/enums/Di.ts
@@ -1,6 +1,7 @@
 export enum Di {
   Redis = "Redis",
   AuthService = "IAuthService",
+  UserService = "IUserService",
   RoleRepository = "IRoleRepository",
   UserRepository = "IUserRepository",
   DataStoreService = "IDataStoreService",
