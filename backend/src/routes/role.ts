36b56dba27af8af02d50aa7ae9362142a48820e3
pawel idzikowski <iddziku@gmail.com>
2023-04-28 00:31:36 +0200

    Update role.ts

--- backend/src/routes/role.ts
+++ backend/src/routes/role.ts
@@ -6,6 +6,7 @@ import { safeParseRequest } from "@middleware/safeParseRequest";
 import { processRequestWith } from "@helpers/processRequestWith";
 import { requirePermissions } from "@middleware/requirePermissions";
 import { GetAllController } from "@controllers/Role/GetAllController";
+import { UpdateController } from "@controllers/Role/UpdateController";
 import { requireAuthentication } from "@middleware/requireAuthentication";
 
 const roleRoutes = Router();
@@ -16,6 +17,13 @@ roleRoutes.get(
   requirePermissions([Permission.ViewAllRoles]),
   safeParseRequest({ query: { withSchema: paginationSchema } }),
   processRequestWith(GetAllController)
+);
+
+roleRoutes.put(
+  "/v1/:id",
+  requireAuthentication,
+  requirePermissions([Permission.ViewAllRoles, Permission.UpdateRole]),
+  processRequestWith(UpdateController)
 );
 
 export = roleRoutes;
