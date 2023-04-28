62ab1789e68a8f2ae31af333e92ac05e595904ca
pawel idzikowski <iddziku@gmail.com>
2023-04-28 19:54:35 +0200

    expand `updateRoleBody` schema

--- backend/src/schemas/role/updateRoleBody.ts
+++ backend/src/schemas/role/updateRoleBody.ts
@@ -3,6 +3,7 @@ import Zod, { RefinementCtx, z, ZodIssueCode } from "zod";
 import { SuperSchemaRunner } from "@utils/types";
 import { schemaForType } from "@helpers/schemaForType";
 import { UpdatePermissionDto, UpdateRoleDto } from "@dtos/UpdateRoleDto";
+import { ALL_PERMISSIONS } from "@config/permissions";
 
 const updateRoleBodySchemaRunner: SuperSchemaRunner = () => {
   const permissionSchema = schemaForType<UpdatePermissionDto>()(
@@ -14,8 +15,28 @@ const updateRoleBodySchemaRunner: SuperSchemaRunner = () => {
 
   return schemaForType<UpdateRoleDto>()(
     z.object({
-      name: z.string(),
-      permissions: z.array(permissionSchema).nonempty(),
+      name: z.string().max(15),
+      permissions: z
+        .array(permissionSchema)
+        .nonempty()
+        .length(ALL_PERMISSIONS.length)
+        .superRefine((permissions, context: RefinementCtx) => {
+          const missingPermissions = ALL_PERMISSIONS.filter(
+            ({ id }) =>
+              permissions.findIndex(permission => permission.id === id) === -1
+          );
+
+          if (missingPermissions.length) {
+            for (const { id, name } of missingPermissions) {
+              context.addIssue({
+                code: ZodIssueCode.custom,
+                message: `Missing permission ${name} (${id}).`,
+              });
+            }
+
+            return Zod.NEVER;
+          }
+        }),
     })
   );
 };
