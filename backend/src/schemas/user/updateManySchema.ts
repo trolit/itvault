4d0e37aa566467c6a3123181099d44a54ebc9b9f
pawel idzikowski <iddziku@gmail.com>
2023-04-30 18:19:33 +0200

    Update updateManySchema.ts

--- backend/src/schemas/user/updateManySchema.ts
+++ backend/src/schemas/user/updateManySchema.ts
@@ -1,16 +1,76 @@
 import uniq from "lodash/uniq";
 import isArray from "lodash/isArray";
-import Zod, { RefinementCtx, z, ZodIssueCode } from "zod";
+import Zod, { RefinementCtx, z, ZodIssueCode, ZodSchema } from "zod";
 
 import { Di } from "@enums/Di";
 import { Role } from "@entities/Role";
 import { instanceOf } from "@helpers/instanceOf";
-import { SuperSchemaRunner } from "@utils/types";
 import { UpdateUserDto } from "@dtos/UpdateUserDto";
 import { schemaForType } from "@helpers/schemaForType";
 import { HEAD_ADMIN_ROLE_ID } from "@config/default-roles";
+import { SuperSchemaRunner } from "@utils/types";
 import { ISuperSchemaParams } from "@interfaces/ISuperSchemaParams";
 import { IRoleRepository } from "@interfaces/repository/IRoleRepository";
+
+async function getBodySchema(
+  userId: number,
+  roles: Role[]
+): Promise<ZodSchema> {
+  const updateUserDtoSchema = schemaForType<UpdateUserDto>()(
+    z.object({
+      id: z
+        .number()
+        .positive()
+        .superRefine((id: number, context: RefinementCtx) => {
+          if (id === userId) {
+            context.addIssue({
+              code: ZodIssueCode.custom,
+              message: "Can't change personal account.",
+            });
+
+            return Zod.NEVER;
+          }
+        }),
+      data: z.object({
+        roleId: z.optional(
+          z
+            .number()
+            .positive()
+            .superRefine((roleId: number, context: RefinementCtx) => {
+              if (roleId === HEAD_ADMIN_ROLE_ID) {
+                context.addIssue({
+                  code: ZodIssueCode.custom,
+                  message: "This role is not assignable.",
+                });
+
+                return Zod.NEVER;
+              }
+
+              const role = roles.find(role => role.id === roleId);
+
+              if (!role) {
+                context.addIssue({
+                  code: ZodIssueCode.custom,
+                  message: "Role is not available.",
+                });
+
+                return Zod.NEVER;
+              }
+            })
+        ),
+        isActive: z.optional(z.boolean()),
+      }),
+    })
+  );
+
+  return new Promise(() =>
+    schemaForType<{ value: UpdateUserDto[] }>()(
+      z.object({
+        value: z.array(updateUserDtoSchema).nonempty(),
+      })
+    )
+  );
+}
 
 const updateManyUsersSuperSchemaRunner: SuperSchemaRunner = async (
   commonParams: ISuperSchemaParams
@@ -31,58 +91,7 @@ const updateManyUsersSuperSchemaRunner: SuperSchemaRunner = async (
         requestedRoles = await fetchRequestedRoles(value);
       }
 
-      const updateUserDtoSchema = schemaForType<UpdateUserDto>()(
-        z.object({
-          id: z
-            .number()
-            .positive()
-            .superRefine((id: number, context: RefinementCtx) => {
-              if (id === userId) {
-                context.addIssue({
-                  code: ZodIssueCode.custom,
-                  message: "Can't change personal account.",
-                });
-
-                return Zod.NEVER;
-              }
-            }),
-          data: z.object({
-            roleId: z.optional(
-              z
-                .number()
-                .positive()
-                .superRefine((roleId: number, context: RefinementCtx) => {
-                  if (roleId === HEAD_ADMIN_ROLE_ID) {
-                    context.addIssue({
-                      code: ZodIssueCode.custom,
-                      message: "This role is not assignable.",
-                    });
-
-                    return Zod.NEVER;
-                  }
-
-                  const role = requestedRoles.find(role => role.id === roleId);
-
-                  if (!role) {
-                    context.addIssue({
-                      code: ZodIssueCode.custom,
-                      message: "Role is not available.",
-                    });
-
-                    return Zod.NEVER;
-                  }
-                })
-            ),
-            isActive: z.optional(z.boolean()),
-          }),
-        })
-      );
-
-      return schemaForType<{ value: UpdateUserDto[] }>()(
-        z.object({
-          value: z.array(updateUserDtoSchema).nonempty(),
-        })
-      );
+      return getBodySchema(userId, requestedRoles);
     },
   };
 };
