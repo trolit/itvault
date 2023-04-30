2a957f4ca393c23fc301d4748083914b50384113
pawel idzikowski <iddziku@gmail.com>
2023-04-30 17:17:38 +0200

    Update updateSchema.ts

--- backend/src/schemas/role/updateSchema.ts
+++ backend/src/schemas/role/updateSchema.ts
@@ -1,68 +1,70 @@
 import Zod, { RefinementCtx, z, ZodIssueCode } from "zod";
 
-import { SuperSchemaRunner } from "@utils/types";
 import { ALL_PERMISSIONS } from "@config/permissions";
 import { schemaForType } from "@helpers/schemaForType";
 import { HEAD_ADMIN_ROLE_ID } from "@config/default-roles";
+import { SchemaProvider, SuperSchemaRunner } from "@utils/types";
 import { UpdatePermissionDto, UpdateRoleDto } from "@dtos/UpdateRoleDto";
+
+const paramsSchemaProvider: SchemaProvider = () =>
+  schemaForType<{ id: number }>()(
+    z.object({
+      id: z.coerce
+        .number()
+        .gte(0)
+        .superRefine((id, context: RefinementCtx) => {
+          if (id === HEAD_ADMIN_ROLE_ID) {
+            context.addIssue({
+              code: ZodIssueCode.custom,
+              message: "This role is not editable.",
+            });
+
+            return Zod.NEVER;
+          }
+        }),
+    })
+  );
+
+const bodySchemaProvider: SchemaProvider = () => {
+  const permissionSchema = schemaForType<UpdatePermissionDto>()(
+    z.object({
+      id: z.number().positive(),
+      enabled: z.boolean(),
+    })
+  );
+
+  return schemaForType<UpdateRoleDto>()(
+    z.object({
+      name: z.string().max(15),
+      permissions: z
+        .array(permissionSchema)
+        .nonempty()
+        .length(ALL_PERMISSIONS.length)
+        .superRefine((permissions, context: RefinementCtx) => {
+          const missingPermissions = ALL_PERMISSIONS.filter(
+            ({ id }) =>
+              permissions.findIndex(permission => permission.id === id) === -1
+          );
+
+          if (missingPermissions.length) {
+            for (const { id, name } of missingPermissions) {
+              context.addIssue({
+                code: ZodIssueCode.custom,
+                message: `Permission '${name}' (${id}) must be provided in request.`,
+              });
+            }
+
+            return Zod.NEVER;
+          }
+        }),
+    })
+  );
+};
 
 const updateRoleSuperSchemaRunner: SuperSchemaRunner = async () => {
   return {
-    params: () =>
-      schemaForType<{ id: number }>()(
-        z.object({
-          id: z.coerce
-            .number()
-            .gte(0)
-            .superRefine((id, context: RefinementCtx) => {
-              if (id === HEAD_ADMIN_ROLE_ID) {
-                context.addIssue({
-                  code: ZodIssueCode.custom,
-                  message: "This role is not editable.",
-                });
-
-                return Zod.NEVER;
-              }
-            }),
-        })
-      ),
-
-    body: () => {
-      const permissionSchema = schemaForType<UpdatePermissionDto>()(
-        z.object({
-          id: z.number().positive(),
-          enabled: z.boolean(),
-        })
-      );
-
-      return schemaForType<UpdateRoleDto>()(
-        z.object({
-          name: z.string().max(15),
-          permissions: z
-            .array(permissionSchema)
-            .nonempty()
-            .length(ALL_PERMISSIONS.length)
-            .superRefine((permissions, context: RefinementCtx) => {
-              const missingPermissions = ALL_PERMISSIONS.filter(
-                ({ id }) =>
-                  permissions.findIndex(permission => permission.id === id) ===
-                  -1
-              );
-
-              if (missingPermissions.length) {
-                for (const { id, name } of missingPermissions) {
-                  context.addIssue({
-                    code: ZodIssueCode.custom,
-                    message: `Permission '${name}' (${id}) must be provided in request.`,
-                  });
-                }
-
-                return Zod.NEVER;
-              }
-            }),
-        })
-      );
-    },
+    params: paramsSchemaProvider,
+    body: bodySchemaProvider,
   };
 };
 
