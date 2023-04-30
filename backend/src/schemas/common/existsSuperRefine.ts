7a07fc7700b74a055de1d8be455a0406435ab480
pawel idzikowski <iddziku@gmail.com>
2023-04-30 17:09:05 +0200

    Update existsSuperRefine.ts
    Update existsSuperRefine.ts

--- backend/src/schemas/common/existsSuperRefine.ts
+++ backend/src/schemas/common/existsSuperRefine.ts
@@ -1,4 +1,4 @@
-import Zod, { RefinementCtx, ZodIssueCode } from "zod";
+import { RefinementCtx, ZodIssueCode } from "zod";
 
 import { Di } from "@enums/Di";
 import { instanceOf } from "@helpers/instanceOf";
@@ -6,15 +6,6 @@ import { IBaseRepository } from "@interfaces/repository/IBaseRepository";
 
 export const existsSuperRefine = <T>(repository: Di) => {
   return async (id: number, context: RefinementCtx) => {
-    if (id <= 0) {
-      context.addIssue({
-        code: ZodIssueCode.custom,
-        message: "Must be greater than 0.",
-      });
-
-      return Zod.NEVER;
-    }
-
     const repositoryInstance = instanceOf<IBaseRepository<T>>(repository);
 
     const result = await repositoryInstance.findById(id);
