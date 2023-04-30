704c838931296969a56388c6146c424833f572ed
pawel idzikowski <iddziku@gmail.com>
2023-04-30 16:33:10 +0200

    fix role update schema

--- backend/src/routes/role.ts
+++ backend/src/routes/role.ts
@@ -1,14 +1,12 @@
 import { Router } from "express";
 
 import { Permission } from "@enums/Permission";
-import { paginationSchema } from "@schemas/pagination";
+import { updateSchema } from "@schemas/role/update";
 import { safeParseRequest } from "@middleware/safeParseRequest";
 import { processRequestWith } from "@helpers/processRequestWith";
 import { requirePermissions } from "@middleware/requirePermissions";
-import { updateRoleBodySchema } from "@schemas/role/updateRoleBody";
 import { GetAllController } from "@controllers/Role/GetAllController";
 import { UpdateController } from "@controllers/Role/UpdateController";
-import { updateRoleParamsSchema } from "@schemas/role/updateRoleParams";
 import { requireAuthentication } from "@middleware/requireAuthentication";
 
 const roleRoutes = Router();
@@ -25,10 +23,7 @@ roleRoutes.put(
   "/v1/:id",
   requireAuthentication,
   requirePermissions([Permission.ViewAllRoles, Permission.UpdateRole]),
-  safeParseRequest({
-    params: { withSchema: updateRoleParamsSchema },
-    body: { withSchema: updateRoleBodySchema },
-  }),
+  safeParseRequest(updateSchema),
   processRequestWith(UpdateController)
 );
 
