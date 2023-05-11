e20b4ac5111819c63eaac6f71589a2e439ef2798
pawel idzikowski <iddziku@gmail.com>
2023-05-11 21:54:49 +0200

    Update getAllSchema.ts

--- backend/src/schemas/workspace/getAllSchema.ts
+++ backend/src/schemas/workspace/getAllSchema.ts
@@ -1,12 +1,12 @@
 import { SuperSchemaRunner } from "@utils/types";
 import { paginationSchemaProvider } from "@schemas/common/paginationSchemaProvider";
 
-const getAllUWorkspacesSuperSchemaRunner: SuperSchemaRunner = async () => {
+const getAllWorkspacesSuperSchemaRunner: SuperSchemaRunner = async () => {
   return {
     query: paginationSchemaProvider,
   };
 };
 
 export const getAllSchema = (() => {
-  return getAllUWorkspacesSuperSchemaRunner;
+  return getAllWorkspacesSuperSchemaRunner;
 })();
