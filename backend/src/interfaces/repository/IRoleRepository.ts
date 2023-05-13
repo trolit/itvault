542cc17b50ec24335377d7e89df547ba7c8f6afd
pawel idzikowski <iddziku@gmail.com>
2023-05-13 16:09:23 +0200

    use `IPaginationOptions` in `RoleRepository`

--- backend/src/interfaces/repository/IRoleRepository.ts
+++ backend/src/interfaces/repository/IRoleRepository.ts
@@ -1,6 +1,7 @@
 import { Role } from "@entities/Role";
 import { Result } from "@utils/Result";
 import { UpdateRoleDto } from "@dtos/UpdateRoleDto";
+import { IPaginationOptions } from "@interfaces/IPaginationOptions";
 
 export interface IRoleRepository {
   getAll(options?: {
@@ -8,10 +9,7 @@ export interface IRoleRepository {
     filters?: {
       ids?: number[] | { excluding: number[] };
     };
-    pagination?: {
-      take: number;
-      skip: number;
-    };
+    pagination?: IPaginationOptions;
   }): Promise<[Role[], number]>;
 
   findByName(name: string): Promise<Role | null>;
