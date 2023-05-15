083efb4279bda72b9922e5455cfd7d1aa952b287
pawel idzikowski <iddziku@gmail.com>
2023-05-15 19:25:16 +0200

    Update loginSchema.ts

--- backend/src/schemas/auth/loginSchema.ts
+++ backend/src/schemas/auth/loginSchema.ts
@@ -1,25 +1,23 @@
 import { z } from "zod";
 
 import { LoginDto } from "@dtos/LoginDto";
-import { SuperSchemaRunner } from "@utils/types";
 import { schemaForType } from "@helpers/schemaForType";
+import { SchemaProvider, SuperSchemaRunner } from "@utils/types";
 
-const loginSuperSchemaRunner: SuperSchemaRunner = async () => {
+const bodySchemaProvider: SchemaProvider = () =>
+  schemaForType<LoginDto>()(
+    z.object({
+      email: z
+        .string()
+        .email()
+        .max(254) // @INFO https://www.rfc-editor.org/rfc/rfc5321#section-4.5.3
+        .transform(value => value.toLowerCase()),
+      password: z.string().max(100),
+    })
+  );
+
+export const loginSchema: SuperSchemaRunner = () => {
   return {
-    body: () =>
-      schemaForType<LoginDto>()(
-        z.object({
-          email: z
-            .string()
-            .email()
-            .max(254) // @INFO https://www.rfc-editor.org/rfc/rfc5321#section-4.5.3
-            .transform(value => value.toLowerCase()),
-          password: z.string().max(100),
-        })
-      ),
+    body: bodySchemaProvider,
   };
 };
-
-export const loginSchema = (() => {
-  return loginSuperSchemaRunner;
-})();
