0f8f32413ea2ed4b801fd570ef3bab0b007f163b
pawel idzikowski <iddziku@gmail.com>
2023-05-15 19:03:45 +0200

    Update paginationSchemaProvider.ts

--- backend/src/schemas/common/paginationSchemaProvider.ts
+++ backend/src/schemas/common/paginationSchemaProvider.ts
@@ -2,12 +2,10 @@ import { z } from "zod";
 
 import { SchemaProvider } from "@utils/types";
 import { schemaForType } from "@helpers/schemaForType";
+import { IPaginationOptions } from "@interfaces/IPaginationOptions";
 
 export const paginationSchemaProvider: SchemaProvider = () =>
-  schemaForType<{
-    take: number;
-    skip: number;
-  }>()(
+  schemaForType<IPaginationOptions>()(
     z.object({
       take: z.coerce.number().gte(0).max(9999),
       skip: z.coerce.number().gte(0).max(9999),
