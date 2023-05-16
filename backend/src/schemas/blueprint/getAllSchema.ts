796bf78d46fa139c1e3999d77a13ee8ad134198f
pawel idzikowski <iddziku@gmail.com>
2023-05-16 16:22:25 +0200

    Update getAllSchema.ts

--- backend/src/schemas/blueprint/getAllSchema.ts
+++ backend/src/schemas/blueprint/getAllSchema.ts
@@ -7,39 +7,40 @@ import { SchemaProvider, SuperSchemaRunner } from "@utils/types";
 import { paginationSchema } from "@schemas/common/paginationSchema";
 import { IWorkspaceRepository } from "@interfaces/repository/IWorkspaceRepository";
 
-const paramsSchemaProvider: SchemaProvider = () => {
-  return schemaForType<{ id: number }>()(
-    z.object({
-      id: z.coerce
-        .number()
-        .gte(0)
-        .superRefine(async (id, context: RefinementCtx) => {
-          const workspaceRepository = getInstanceOf<IWorkspaceRepository>(
-            Di.WorkspaceRepository
-          );
-
-          const workspace = await workspaceRepository.findById(id);
-
-          if (!workspace) {
-            context.addIssue({
-              code: ZodIssueCode.custom,
-              message: "Workspace is not available.",
-            });
-
-            return Zod.NEVER;
-          }
-        }),
-    })
-  );
-};
-
-const getAllBlueprintsSuperSchemaRunner: SuperSchemaRunner = async () => {
+export const getAllSchema: SuperSchemaRunner = () => {
   return {
-    params: paramsSchemaProvider,
-    query: () => paginationSchema,
+    params: useParamsSchema(),
+    query: useQuerySchema(),
   };
 };
 
-export const getAllSchema = (() => {
-  return getAllBlueprintsSuperSchemaRunner;
-})();
+function useParamsSchema(): SchemaProvider {
+  return () =>
+    schemaForType<{ id: number }>()(
+      z.object({
+        id: z.coerce
+          .number()
+          .gte(0)
+          .superRefine(async (id, context: RefinementCtx) => {
+            const workspaceRepository = getInstanceOf<IWorkspaceRepository>(
+              Di.WorkspaceRepository
+            );
+
+            const workspace = await workspaceRepository.findById(id);
+
+            if (!workspace) {
+              context.addIssue({
+                code: ZodIssueCode.custom,
+                message: "Workspace is not available.",
+              });
+
+              return Zod.NEVER;
+            }
+          }),
+      })
+    );
+}
+
+function useQuerySchema(): SchemaProvider {
+  return () => paginationSchema;
+}
