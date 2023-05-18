a91f10a70bcbcf2fb9dc6b255361c77476a966d7
pawel idzikowski <iddziku@gmail.com>
2023-05-18 16:28:34 +0200

    Update data-store.ts

--- backend/src/types/data-store.ts
+++ backend/src/types/data-store.ts
@@ -1,18 +1,18 @@
 /* eslint-disable @typescript-eslint/consistent-type-definitions */
 
 declare module "@custom-types/data-store" {
-  export type DataStoreRole = {
-    id: number;
-
-    permissions: DataStorePermission[];
-  };
-
   export type DataStorePermission = {
     id: number;
 
     name: string;
 
     enabled: boolean;
+  };
+
+  export type DataStoreRole = {
+    id: number;
+
+    permissions: DataStorePermission[];
   };
 
   export type DataStoreUser = {
