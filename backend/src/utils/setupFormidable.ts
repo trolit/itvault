5223d6b692767f3c1487623a8f7372b2ff50105c
pawel idzikowski <iddziku@gmail.com>
2023-05-20 22:27:14 +0200

    Create setupFormidable.ts

---
+++ backend/src/utils/setupFormidable.ts
@@ -0,0 +1,12 @@
+import formidable from "formidable";
+
+import { FILES_LOCAL_STORAGE_PATH } from "@config";
+
+export const setupFormidable = () => {
+  const form = formidable({
+    multiples: true,
+    uploadDir: FILES_LOCAL_STORAGE_PATH,
+  });
+
+  return form;
+};
