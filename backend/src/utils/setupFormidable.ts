770c854aab9d36271b7856ab80176a7ca3790a20
pawel idzikowski <iddziku@gmail.com>
2023-05-21 16:53:20 +0200

    Update setupFormidable.ts

--- backend/src/utils/setupFormidable.ts
+++ backend/src/utils/setupFormidable.ts
@@ -1,10 +1,10 @@
 import formidable from "formidable";
-
 import { FILES_LOCAL_STORAGE_PATH } from "@config";
 
 export const setupFormidable = () => {
   const form = formidable({
     multiples: true,
+    keepExtensions: true,
     uploadDir: FILES_LOCAL_STORAGE_PATH,
   });
 
