7056bb1c93cb05cc777750d7b96d6475cc186f76
pawel idzikowski <iddziku@gmail.com>
2023-05-23 19:40:36 +0200

    Update getAllSchema.ts

--- backend/src/schemas/blueprint/getAllSchema.ts
+++ backend/src/schemas/blueprint/getAllSchema.ts
@@ -1,46 +1,12 @@
-import Zod, { RefinementCtx, z, ZodIssueCode } from "zod";
-
-import { Di } from "@enums/Di";
-import { getInstanceOf } from "@helpers/getInstanceOf";
-import { schemaForType } from "@schemas/common/schemaForType";
 import { paginationSchema } from "@schemas/common/paginationSchema";
 import { SuperSchemaRunner, SchemaProvider } from "@custom-types/super-schema";
 import { defineSuperSchemaRunner } from "@schemas/common/defineSuperSchemaRunner";
-import { IWorkspaceRepository } from "@interfaces/repository/IWorkspaceRepository";
 
 export const getAllSchema: SuperSchemaRunner = defineSuperSchemaRunner(() => {
   return {
-    params: useParamsSchema(),
     query: useQuerySchema(),
   };
 });
-
-function useParamsSchema(): SchemaProvider {
-  return () =>
-    schemaForType<{ id: number }>()(
-      z.object({
-        id: z.coerce
-          .number()
-          .gte(0)
-          .superRefine(async (id, context: RefinementCtx) => {
-            const workspaceRepository = getInstanceOf<IWorkspaceRepository>(
-              Di.WorkspaceRepository
-            );
-
-            const workspace = await workspaceRepository.findById(id);
-
-            if (!workspace) {
-              context.addIssue({
-                code: ZodIssueCode.custom,
-                message: "Workspace is not available.",
-              });
-
-              return Zod.NEVER;
-            }
-          }),
-      })
-    );
-}
 
 function useQuerySchema(): SchemaProvider {
   return () => paginationSchema;
