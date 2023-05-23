73c586177a839b8a7f80e161538612507098589f
pawel idzikowski <iddziku@gmail.com>
2023-05-23 19:27:18 +0200

    Update file.ts

--- backend/src/routes/workspace/file.ts
+++ backend/src/routes/workspace/file.ts
@@ -1,14 +1,17 @@
 import { Router } from "express";
 
+import { storeSchema } from "@schemas/File/storeSchema";
 import { processRequestWith } from "@helpers/processRequestWith";
 import { StoreController } from "@controllers/File/StoreController";
+import { validateRequestWith } from "@middleware/validateRequestWith";
 import { requireAuthentication } from "@middleware/requireAuthentication";
 
 const fileRoutes = Router({ mergeParams: true });
 
 fileRoutes.post(
   "/v1",
-  // requireAuthentication,
+  requireAuthentication,
+  validateRequestWith(storeSchema),
   processRequestWith(StoreController)
 );
 
