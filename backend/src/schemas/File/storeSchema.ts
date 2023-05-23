549b0f534b95f399468234293baa5205aa3aa820
pawel idzikowski <iddziku@gmail.com>
2023-05-23 19:27:23 +0200

    Create storeSchema.ts

---
+++ backend/src/schemas/File/storeSchema.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+import { schemaForType } from "@schemas/common/schemaForType";
+import { SuperSchemaRunner, SchemaProvider } from "@custom-types/super-schema";
+import { defineSuperSchemaRunner } from "@schemas/common/defineSuperSchemaRunner";
+
+export const storeSchema: SuperSchemaRunner = defineSuperSchemaRunner(() => {
+  return {
+    params: useParamsSchema(),
+  };
+});
+
+function useParamsSchema(): SchemaProvider {
+  return () =>
+    schemaForType<{ id: number }>()(
+      z.object({
+        id: z.coerce.number().gte(0),
+      })
+    );
+}
