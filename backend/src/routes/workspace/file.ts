4e60e2cf28b3f297c1ea91e485af72c93538cf25
pawel idzikowski <iddziku@gmail.com>
2023-05-30 21:57:20 +0200

    assign `PatchRelativePathController` to route

--- backend/src/routes/workspace/file.ts
+++ backend/src/routes/workspace/file.ts
@@ -5,6 +5,7 @@ import { processRequestWith } from "@helpers/processRequestWith";
 import { StoreController } from "@controllers/File/StoreController";
 import { validateRequestWith } from "@middleware/validateRequestWith";
 import { requireAuthentication } from "@middleware/requireAuthentication";
+import { PatchRelativePathController } from "@controllers/File/PatchRelativePathController";
 
 const fileRoutes = Router({ mergeParams: true });
 
@@ -12,6 +13,11 @@ fileRoutes.post(
   "/v1",
   validateRequestWith(storeSchema),
   processRequestWith(StoreController)
+);
+
+fileRoutes.patch(
+  ":fileId/v1/relative-path",
+  processRequestWith(PatchRelativePathController)
 );
 
 export = fileRoutes;
