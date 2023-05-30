e96afeece4d3a2124b0f4efdb5472a8d9ee11502
pawel idzikowski <iddziku@gmail.com>
2023-05-30 21:49:44 +0200

    add `updateRelativePath` to `FileRepository`

--- backend/src/interfaces/repository/IFileRepository.ts
+++ backend/src/interfaces/repository/IFileRepository.ts
@@ -1,8 +1,15 @@
 import formidable from "formidable";
+import { UpdateResult } from "typeorm";
 
 import { File } from "@entities/File";
 import { IBaseRepository } from "./IBaseRepository";
 
 export interface IFileRepository extends IBaseRepository<File> {
   store(workspaceId: number, files: formidable.Files): Promise<File[]>;
+
+  updateRelativePath(
+    workspaceId: number,
+    fileId: number,
+    relativePath: string
+  ): Promise<UpdateResult>;
 }
