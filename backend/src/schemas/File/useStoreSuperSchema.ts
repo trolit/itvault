327cdd1910bf5bfdd8c99193c584830367bc38b6
pawel idzikowski <iddziku@gmail.com>
2023-05-31 21:24:00 +0200

    create `File/StoreController` related schema

---
+++ backend/src/schemas/File/useStoreSuperSchema.ts
@@ -0,0 +1,15 @@
+import { baseSchema } from "@schemas/Workspace/baseSchema";
+import { SuperSchemaRunner, SchemaProvider } from "@custom-types/super-schema";
+import { defineSuperSchemaRunner } from "@schemas/common/defineSuperSchemaRunner";
+
+export const useStoreSuperSchema: SuperSchemaRunner = defineSuperSchemaRunner(
+  () => {
+    return {
+      params: useParamsSchema(),
+    };
+  }
+);
+
+function useParamsSchema(): SchemaProvider {
+  return () => baseSchema.params;
+}
