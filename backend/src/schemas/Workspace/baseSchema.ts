ebc8e2b5d055370776b3c740201611f00639c6d9
pawel idzikowski <iddziku@gmail.com>
2023-05-31 15:29:02 +0200

    create `Workspace/baseSchema`

---
+++ backend/src/schemas/Workspace/baseSchema.ts
@@ -0,0 +1,40 @@
+import { Di } from "@enums/Di";
+import Zod, { RefinementCtx, z, ZodIssueCode } from "zod";
+
+import { getInstanceOf } from "@helpers/getInstanceOf";
+import { schemaForType } from "@schemas/common/schemaForType";
+import { IWorkspaceRepository } from "@interfaces/repository/IWorkspaceRepository";
+
+const paramsSchema = schemaForType<{
+  workspaceId: number;
+}>()(
+  z.object({
+    workspaceId: z.coerce
+      .number()
+      .gt(0)
+      .superRefine(async (id, context: RefinementCtx) => {
+        if (id <= 0) {
+          return Zod.NEVER;
+        }
+
+        const workspaceRepository = getInstanceOf<IWorkspaceRepository>(
+          Di.WorkspaceRepository
+        );
+
+        const workspace = await workspaceRepository.findById(id);
+
+        if (!workspace) {
+          context.addIssue({
+            code: ZodIssueCode.custom,
+            message: "Workspace is not available.",
+          });
+
+          return Zod.NEVER;
+        }
+      }),
+  })
+);
+
+export const baseSchema = {
+  params: paramsSchema,
+};
