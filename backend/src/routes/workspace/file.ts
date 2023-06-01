93eec8933ef9c31ebe4af7f5806bc30724ee3fb8
pawel idzikowski <iddziku@gmail.com>
2023-06-01 18:25:28 +0200

    add `get` request to `fileRoutes`

--- backend/src/routes/workspace/file.ts
+++ backend/src/routes/workspace/file.ts
@@ -5,6 +5,7 @@ import { StoreController } from "@controllers/File/StoreController";
 import { validateRequestWith } from "@middleware/validateRequestWith";
 import { useStoreSuperSchema } from "@schemas/File/useStoreSuperSchema";
 import { requireAuthentication } from "@middleware/requireAuthentication";
+import { GetAllRootController } from "@controllers/File/GetAllRootController";
 import { PatchRelativePathController } from "@controllers/File/PatchRelativePathController";
 import { usePatchRelativePathSuperSchema } from "@schemas/File/usePatchRelativePathSuperSchema";
 
@@ -21,5 +22,7 @@ fileRoutes.patch(
   validateRequestWith(usePatchRelativePathSuperSchema),
   processRequestWith(PatchRelativePathController)
 );
+
+fileRoutes.get("/v1/root", processRequestWith(GetAllRootController));
 
 export = fileRoutes;
