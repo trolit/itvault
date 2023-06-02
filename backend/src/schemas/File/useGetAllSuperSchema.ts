36f8288cee3728510d323d464eb9ca4907de8768
pawel idzikowski <iddziku@gmail.com>
2023-06-02 17:45:33 +0200

    Create useGetAllSuperSchema.ts

---
+++ backend/src/schemas/File/useGetAllSuperSchema.ts
@@ -0,0 +1,28 @@
+import { z } from "zod";
+
+import { baseSchemas } from "@schemas/Workspace/baseSchemas";
+import { schemaForType } from "@schemas/common/schemaForType";
+import { SuperSchemaRunner, SchemaProvider } from "@custom-types/super-schema";
+import { defineSuperSchemaRunner } from "@schemas/common/defineSuperSchemaRunner";
+
+export const useGetAllSuperSchema: SuperSchemaRunner = defineSuperSchemaRunner(
+  () => {
+    return {
+      params: useParamsSchema(),
+      query: useQuerySchema(),
+    };
+  }
+);
+
+function useParamsSchema(): SchemaProvider {
+  return () => baseSchemas.params;
+}
+
+function useQuerySchema(): SchemaProvider {
+  return () =>
+    schemaForType<{ relativePath?: string }>()(
+      z.object({
+        relativePath: z.string().default("."),
+      })
+    );
+}
