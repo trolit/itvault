77cc1b6b16338c29a6ffc3ebf15da518240b7651
pawel idzikowski <iddziku@gmail.com>
2023-06-03 21:15:27 +0200

    Update blueprint.ts

--- backend/src/routes/workspace/blueprint.ts
+++ backend/src/routes/workspace/blueprint.ts
@@ -4,11 +4,13 @@ import { processRequestWith } from "@helpers/processRequestWith";
 import { validateRequestWith } from "@middleware/validateRequestWith";
 import { requireAuthentication } from "@middleware/requireAuthentication";
 import { GetAllController } from "@controllers/Blueprint/GetAllController";
+import { requireWorkspaceAccess } from "@middleware/requireWorkspaceAccess";
 import { useGetAllSuperSchema } from "@schemas/Blueprint/useGetAllSuperSchema";
 
 const blueprintRoutes = Router({ mergeParams: true });
 
 blueprintRoutes.use(requireAuthentication);
+blueprintRoutes.use(requireWorkspaceAccess);
 
 blueprintRoutes.get(
   "/v1",
