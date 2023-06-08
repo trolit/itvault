ba88b8515754e5cc6070542bd9af9a42cd97e45f
pawel idzikowski <iddziku@gmail.com>
2023-06-08 16:38:24 +0200

    generate migration that creates `palettes` table

---
+++ backend/src/migrations/1686234793370-CreatePalettesTable.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreatePalettesTable1686234793370 implements MigrationInterface {
+    name = 'CreatePalettesTable1686234793370'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE \`palettes\` (\`id\` int NOT NULL AUTO_INCREMENT, \`value\` json NOT NULL, \`variantId\` varchar(36) NULL, \`blueprintToWorkspaceId\` int NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+        await queryRunner.query(`ALTER TABLE \`palettes\` ADD CONSTRAINT \`FK_f11cdfeb697bee5e0b8e7e7c043\` FOREIGN KEY (\`variantId\`) REFERENCES \`variants\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE \`palettes\` ADD CONSTRAINT \`FK_211e1689e015a0463e635268425\` FOREIGN KEY (\`blueprintToWorkspaceId\`) REFERENCES \`blueprints_workspaces\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`palettes\` DROP FOREIGN KEY \`FK_211e1689e015a0463e635268425\``);
+        await queryRunner.query(`ALTER TABLE \`palettes\` DROP FOREIGN KEY \`FK_f11cdfeb697bee5e0b8e7e7c043\``);
+        await queryRunner.query(`DROP TABLE \`palettes\``);
+    }
+
+}
