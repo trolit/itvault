41a099bf9a15f8a0ca05cbe2ed098d0a19f88e66
pawel idzikowski <iddziku@gmail.com>
2023-06-08 14:05:09 +0200

    sketch `Palette` entity

---
+++ backend/src/entities/Palette.ts
@@ -0,0 +1,23 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+import { Variant } from "./Variant";
+import { BlueprintToWorkspace } from "./BlueprintToWorkspace";
+
+// @NOTE holds (workspace's blueprint) color structure in particular file (variant)
+@Entity("palettes")
+export class Palette {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: "json" })
+  value: Record<number, string[]>;
+
+  @ManyToOne(() => Variant, variant => variant.palettes)
+  variant: Variant;
+
+  @ManyToOne(
+    () => BlueprintToWorkspace,
+    blueprintToWorkspace => blueprintToWorkspace.palettes
+  )
+  blueprintToWorkspace: BlueprintToWorkspace;
+}
