f5e547ae8cf10ec1d5e68a333d83e3a5da37c06f
pawel idzikowski <iddziku@gmail.com>
2023-06-10 22:40:37 +0200

    decide to go on with `/workspaces/:id/variants`

--- backend/src/routes/workspace/variant.ts
+++ backend/src/routes/workspace/variant.ts
@@ -1,15 +1,12 @@
-// @TMP @TMP @TMP (will be handled in next 'patch' PR)
-// @TODO - this should go under workspaces/:w-id/files/:f-id/variants/:v-id
-
 import { Router } from "express";
 
 import { processRequestWith } from "@helpers/processRequestWith";
 import { GetByIdController } from "@controllers/Variant/GetByIdController";
+import { requireWorkspaceAccess } from "@middleware/requireWorkspaceAccess";
 
 const variantRoutes = Router({ mergeParams: true });
 
-// variantRoutes.use(requireAuthentication);
-// variantRoutes.use(requireWorkspaceAccess);
+variantRoutes.use(requireWorkspaceAccess);
 
 variantRoutes.get("/:variantId/v1", processRequestWith(GetByIdController));
 
