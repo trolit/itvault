40234282e58f92514f73450cb289b758f5c4f9d2
pawel idzikowski <iddziku@gmail.com>
2023-06-11 19:54:59 +0200

    Update variants.ts

--- backend/src/routes/workspaces/variants.ts
+++ backend/src/routes/workspaces/variants.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 
 import { processRequestWith } from "@helpers/processRequestWith";
-import { requireWorkspaceAccess } from "@middleware/requireWorkspaceAccess";
+import { GetAllController } from "@controllers/Variant/GetAllController";
 import { GetByIdController } from "@controllers/Variant/GetByIdController";
+import { requireWorkspaceAccess } from "@middleware/requireWorkspaceAccess";
 
 const variantsRouter = Router({ mergeParams: true });
 
 variantsRouter.use(requireWorkspaceAccess);
+
+variantsRouter.get("/v1", processRequestWith(GetAllController));
 
 variantsRouter.get("/:variantId/v1", processRequestWith(GetByIdController));
 
