f8c563523fbef4916771d538357253a92f53aac2
pawel idzikowski <iddziku@gmail.com>
2023-06-11 21:07:14 +0200

    add `getAllSuperSchema`

--- backend/src/routes/workspaces/variants.ts
+++ backend/src/routes/workspaces/variants.ts
@@ -1,15 +1,21 @@
 import { Router } from "express";
 
 import { processRequestWith } from "@helpers/processRequestWith";
+import { validateRequestWith } from "@middleware/validateRequestWith";
 import { GetAllController } from "@controllers/Variant/GetAllController";
 import { GetByIdController } from "@controllers/Variant/GetByIdController";
 import { requireWorkspaceAccess } from "@middleware/requireWorkspaceAccess";
+import { useGetAllSuperSchema } from "@schemas/Variant/useGetAllSuperSchema";
 
 const variantsRouter = Router({ mergeParams: true });
 
 variantsRouter.use(requireWorkspaceAccess);
 
-variantsRouter.get("/v1", processRequestWith(GetAllController));
+variantsRouter.get(
+  "/v1",
+  validateRequestWith(useGetAllSuperSchema),
+  processRequestWith(GetAllController)
+);
 
 variantsRouter.get("/:variantId/v1", processRequestWith(GetByIdController));
 
---
+++ backend/src/schemas/Variant/useGetAllSuperSchema.ts
@@ -0,0 +1,47 @@
+import Zod, { RefinementCtx, z, ZodIssueCode } from "zod";
+
+import { Di } from "@enums/Di";
+import { getInstanceOf } from "@helpers/getInstanceOf";
+import { schemaForType } from "@schemas/common/schemaForType";
+import { IFileRepository } from "@interfaces/repository/IFileRepository";
+import { SuperSchemaRunner, SchemaProvider } from "@custom-types/super-schema";
+import { defineSuperSchemaRunner } from "@schemas/common/defineSuperSchemaRunner";
+
+export const useGetAllSuperSchema: SuperSchemaRunner = defineSuperSchemaRunner(
+  () => {
+    return {
+      query: useQuerySchema(),
+    };
+  }
+);
+
+function useQuerySchema(): SchemaProvider {
+  return () =>
+    schemaForType<{ fileId: number }>()(
+      z.object({
+        fileId: z
+          .number()
+          .gt(0)
+          .superRefine(async (value: number, context: RefinementCtx) => {
+            if (value <= 0) {
+              return Zod.NEVER;
+            }
+
+            const fileRepository = getInstanceOf<IFileRepository>(
+              Di.FileRepository
+            );
+
+            const file = await fileRepository.findById(value);
+
+            if (!file) {
+              context.addIssue({
+                code: ZodIssueCode.custom,
+                message: "File is not available.",
+              });
+
+              return Zod.NEVER;
+            }
+          }),
+      })
+    );
+}
