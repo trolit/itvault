3efe8918c201fc6f88af3202f6f044bb260ab627
pawel idzikowski <iddziku@gmail.com>
2023-06-13 19:12:16 +0200

    Create GetAllController.ts

---
+++ backend/src/controllers/Palette/GetAllController.ts
@@ -0,0 +1,45 @@
+import { inject, injectable } from "tsyringe";
+import { StatusCodes as HTTP } from "http-status-codes";
+
+import { Di } from "@enums/Di";
+import { Palette } from "@entities/Palette";
+import { IController } from "@interfaces/IController";
+import { CustomRequest, CustomResponse } from "@custom-types/express";
+import { IPaletteRepository } from "@interfaces/repository/IPaletteRepository";
+
+interface IParams {
+  workspaceId: number;
+}
+
+interface IQuery {
+  variantId: number;
+}
+
+@injectable()
+export class GetAllController
+  implements IController<IParams, undefined, IQuery, Palette[]>
+{
+  constructor(
+    @inject(Di.PaletteRepository)
+    private _paletteRepository: IPaletteRepository
+  ) {}
+
+  async invoke(
+    request: CustomRequest<IParams, undefined, IQuery>,
+    response: CustomResponse<Palette[]>
+  ) {
+    const {
+      query: { variantId },
+    } = request;
+
+    const [result] = await this._paletteRepository.getAll({
+      where: {
+        variant: {
+          id: variantId,
+        },
+      },
+    });
+
+    return response.status(HTTP.OK).send(result);
+  }
+}
