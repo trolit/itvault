01e560895449da03e2c6914bfa4c25dd3c4d5627
pawel idzikowski <iddziku@gmail.com>
2023-06-14 21:16:50 +0200

    Update useGetAllSuperSchema.ts

--- backend/src/schemas/Palette/useGetAllSuperSchema.ts
+++ backend/src/schemas/Palette/useGetAllSuperSchema.ts
@@ -1,9 +1,4 @@
-import Zod, { RefinementCtx, z, ZodIssueCode } from "zod";
-
-import { Di } from "@enums/Di";
-import { getInstanceOf } from "@helpers/getInstanceOf";
-import { schemaForType } from "@schemas/common/schemaForType";
-import { IVariantRepository } from "@interfaces/repository/IVariantRepository";
+import { baseSchemas } from "@schemas/Variant/baseSchemas";
 import { SuperSchemaRunner, SchemaProvider } from "@custom-types/super-schema";
 import { defineSuperSchemaRunner } from "@schemas/common/defineSuperSchemaRunner";
 
@@ -16,27 +11,5 @@ export const useGetAllSuperSchema: SuperSchemaRunner = defineSuperSchemaRunner(
 );
 
 function useParamsSchema(): SchemaProvider {
-  return () =>
-    schemaForType<{ variantId: string }>()(
-      z.object({
-        variantId: z
-          .string()
-          .superRefine(async (value: string, context: RefinementCtx) => {
-            const variantRepository = getInstanceOf<IVariantRepository>(
-              Di.VariantRepository
-            );
-
-            const variant = await variantRepository.getById(value);
-
-            if (!variant) {
-              context.addIssue({
-                code: ZodIssueCode.custom,
-                message: "Variant is not available.",
-              });
-
-              return Zod.NEVER;
-            }
-          }),
-      })
-    );
+  return () => baseSchemas.params;
 }
