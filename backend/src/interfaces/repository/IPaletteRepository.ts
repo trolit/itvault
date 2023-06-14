00c192fcb18ce1e112098525e444f1fcaa959b53
pawel idzikowski <iddziku@gmail.com>
2023-06-14 19:59:00 +0200

    add `save` operation to `PaletteRepository`

--- backend/src/interfaces/repository/IPaletteRepository.ts
+++ backend/src/interfaces/repository/IPaletteRepository.ts
@@ -1,4 +1,10 @@
+import { StorePaletteDto } from "@dtos/StorePaletteDto";
 import { Palette } from "@entities/Palette";
 import { IBaseRepository } from "./IBaseRepository";
 
-export interface IPaletteRepository extends IBaseRepository<Palette> {}
+export interface IPaletteRepository extends IBaseRepository<Palette> {
+  save(
+    variantId: string,
+    palettes: StorePaletteDto[]
+  ): Promise<Palette[] | null>;
+}
