fbd9345b8cba4c5b61a2500a03cf5f41a65ed773
pawel idzikowski <iddziku@gmail.com>
2023-06-17 18:08:19 +0200

    remove unused import

--- backend/src/routes/workspaces/index.ts
+++ backend/src/routes/workspaces/index.ts
@@ -1,7 +1,6 @@
 import { Router } from "express";
 
 import filesRouter from "./files";
-import palettesRouter from "./palettes";
 import variantsRouter from "./variants";
 import blueprintsRouter from "./blueprints";
 import { processRequestWith } from "@helpers/processRequestWith";
@@ -25,7 +24,5 @@ workspacesRouter.use("/:workspaceId/files", filesRouter);
 workspacesRouter.use("/:workspaceId/variants", variantsRouter);
 
 workspacesRouter.use("/:workspaceId/blueprints", blueprintsRouter);
-
-workspacesRouter.use("/:workspaceId/palettes", palettesRouter);
 
 export = workspacesRouter;
