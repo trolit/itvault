6e4669b8f654a3b8c91800740f2100826b417977
pawel idzikowski <iddziku@gmail.com>
2023-06-18 00:49:35 +0200

    init `upload` action in `VariantService`

--- backend/src/interfaces/service/IVariantService.ts
+++ backend/src/interfaces/service/IVariantService.ts
@@ -1,5 +1,12 @@
 import { Variant } from "@entities/Variant";
+import { CustomRequest } from "@custom-types/express";
+import { IBody } from "@controllers/Variant/StoreController";
 
 export interface IVariantService {
   getContent(variant: Variant, directory: string): Promise<string>;
+
+  upload<P, Q>(
+    request: CustomRequest<P, IBody, Q>,
+    options: { destination?: string }
+  ): Promise<Variant | null>;
 }
--- backend/src/services/VariantService.ts
+++ backend/src/services/VariantService.ts
@@ -1,18 +1,90 @@
 import path from "path";
 import fs from "fs-extra";
-import { injectable } from "tsyringe";
+import { inject, injectable } from "tsyringe";
 
+import { Di } from "@enums/Di";
 import { FILES } from "@config";
 import { Variant } from "@entities/Variant";
+import { CustomRequest } from "@custom-types/express";
+import { IBody } from "@controllers/Variant/StoreController";
 import { IVariantService } from "@interfaces/service/IVariantService";
+import { IVariantRepository } from "@interfaces/repository/IVariantRepository";
+import { IFormidableFormFactory } from "@interfaces/factory/IFormidableFormFactory";
 
 @injectable()
 export class VariantService implements IVariantService {
+  constructor(
+    @inject(Di.VariantRepository)
+    private _variantRepository: IVariantRepository,
+    @inject(Di.FormidableFormFactory)
+    private _formidableFormFactory: IFormidableFormFactory
+  ) {}
+
   async getContent(variant: Variant, directory: string): Promise<string> {
     const file = await fs.readFile(
       path.join(FILES.STORAGE.BASE_UPLOADS_PATH, directory, variant.filename)
     );
 
     return file.toString();
+  }
+
+  async upload<P, Q>(
+    request: CustomRequest<P, IBody, Q>,
+    options: { destination?: string | undefined }
+  ): Promise<Variant | null> {
+    const {
+      body: { fileId, variantId, name },
+    } = request;
+
+    if (variantId) {
+      const variant = await this._variantRepository.getById(variantId);
+
+      return variant
+        ? this._variantRepository.primitiveSave({
+            name,
+            size: variant.size,
+            filename: variant.filename,
+            file: {
+              id: fileId,
+            },
+          })
+        : null;
+    }
+
+    const form = await this._formidableFormFactory.create({
+      destination: options.destination,
+      multiples: false,
+    });
+
+    return new Promise((resolve, reject) => {
+      form.parse(request, async (error, fields, files) => {
+        if (error) {
+          reject(error);
+
+          return;
+        }
+
+        if (!Object.keys(files).length) {
+          return resolve(null);
+        }
+
+        const [, file] = Object.entries(files)[0];
+
+        if (Array.isArray(file)) {
+          return resolve(null);
+        }
+
+        const variant = await this._variantRepository.primitiveSave({
+          name,
+          size: file.size,
+          filename: file.newFilename,
+          file: {
+            id: fileId,
+          },
+        });
+
+        return resolve(variant);
+      });
+    });
   }
 }
