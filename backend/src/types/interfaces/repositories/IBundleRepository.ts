0bef2170718a907dfe03b8dd19588884e75567c6
pawel idzikowski <iddziku@gmail.com>
2023-06-25 16:11:13 +0200

    remove `setExpiresAt` - we will handle it with service

--- backend/src/types/interfaces/repositories/IBundleRepository.ts
+++ backend/src/types/interfaces/repositories/IBundleRepository.ts
@@ -7,6 +7,4 @@ import { IBaseRepository } from "./IBaseRepository";
 
 export interface IBundleRepository extends IBaseRepository<Bundle> {
   setStatus(id: number, status: BundleStatus): Promise<UpdateResult>;
-
-  setExpiresAt(id: number, expiresAt: BundleExpire): Promise<UpdateResult>;
 }
