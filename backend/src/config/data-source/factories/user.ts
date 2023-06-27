a8461c8669fc5ed74124a60398c18b0b457d18e8
pawel idzikowski <iddziku@gmail.com>
2023-06-27 17:52:12 +0200

    update `user` seeder

--- backend/src/config/data-source/factories/user.ts
+++ backend/src/config/data-source/factories/user.ts
@@ -6,6 +6,10 @@ import { User } from "@entities/User";
 export const user = setSeederFactory(User, () => {
   const user = new User();
 
+  user.firstName = faker.name.firstName();
+
+  user.lastName = faker.name.lastName();
+
   user.email = faker.internet.email(
     faker.name.firstName().toLowerCase(),
     faker.name.lastName().toLowerCase(),
