4a62ca15226c8370052243e31012bf84c6af905e
pawel idzikowski <iddziku@gmail.com>
2023-07-01 15:46:35 +0200

    Update publisher.ts

--- backend/src/scripts/queues/publisher.ts
+++ backend/src/scripts/queues/publisher.ts
@@ -6,7 +6,7 @@ import { Di } from "@enums/Di";
 export const publisher = async (connection: Connection) => {
   const value = await connection.createChannel();
 
-  container.register(Di.MQPublisher, { useValue: value });
+  container.register(Di.Publisher, { useValue: value });
 
   return value;
 };
