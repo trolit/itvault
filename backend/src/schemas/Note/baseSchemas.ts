6dbc83ab16ba3c08c0694d615e52571299b38c4d
pawel idzikowski <iddziku@gmail.com>
2023-07-04 17:47:26 +0200

    Update baseSchemas.ts

--- backend/src/schemas/Note/baseSchemas.ts
+++ backend/src/schemas/Note/baseSchemas.ts
@@ -1,44 +1,18 @@
-import Zod, { RefinementCtx, z, ZodIssueCode } from "zod";
+import { z } from "zod";
 
 import { NoteDto } from "@dtos/NoteDto";
 import { Resource } from "@enums/Resource";
-import { IBaseRepository } from "@interfaces/repositories/IBaseRepository";
-
-import { getInstanceOf } from "@helpers/getInstanceOf";
 
 import { schemaForType } from "@schemas/common/schemaForType";
 
 type ResourceRequest = Pick<NoteDto, "id" | "resource">;
 
-const resourceSchema = schemaForType<ResourceRequest>()(
-  z
-    .object({
-      id: z.coerce.number().gt(0),
+export const resourceSchema = schemaForType<ResourceRequest>()(
+  z.object({
+    id: z.coerce.number().gt(0),
 
-      resource: z.nativeEnum(Resource),
-    })
-    .superRefine(async (value: ResourceRequest, context: RefinementCtx) => {
-      const { id, resource: resourceName } = value;
-
-      if (!Resource[resourceName]) {
-        return Zod.NEVER;
-      }
-
-      const repository = getInstanceOf<IBaseRepository<unknown>>(
-        `I${resourceName}Repository`
-      );
-
-      const entity = await repository.getById(id);
-
-      if (!entity) {
-        context.addIssue({
-          code: ZodIssueCode.custom,
-          message: `${resourceName} is not available.`,
-        });
-
-        return Zod.NEVER;
-      }
-    })
+    resource: z.nativeEnum(Resource),
+  })
 );
 
 export const baseSchemas = {
