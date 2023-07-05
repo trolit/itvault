ff67a0a5e5e04c4d21a5c4a43797edbe042e3591
pawel idzikowski <iddziku@gmail.com>
2023-07-05 19:25:55 +0200

    Create getRepositoryByOriginalUrl.ts

---
+++ backend/src/helpers/getRepositoryByOriginalUrl.ts
@@ -0,0 +1,21 @@
+import capitalize from "lodash/capitalize";
+
+import { getInstanceOf } from "./getInstanceOf";
+
+import { IBaseRepository } from "@interfaces/repositories/IBaseRepository";
+
+// @TODO adjust after API versioning refactor!
+export const getRepositoryByOriginalUrl = (originalUrl: string) => {
+  // @NOTE e.g. /api/notes/v1/1
+  const resourceInPlural = originalUrl.split("/").at(-2);
+
+  if (!resourceInPlural) {
+    return null;
+  }
+
+  const resource = resourceInPlural.slice(0, -1);
+
+  const dependency = `I${capitalize(resource)}Repository`;
+
+  return getInstanceOf<IBaseRepository<unknown>>(dependency);
+};
