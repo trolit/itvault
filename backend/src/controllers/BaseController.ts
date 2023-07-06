3ddfe8f99072078d40ce74df497e211dade0a4b2
pawel idzikowski <iddziku@gmail.com>
2023-07-06 17:46:32 +0200

    Create BaseController.ts

---
+++ backend/src/controllers/BaseController.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+import { StatusCodes as HTTP } from "http-status-codes";
+
+import { IBaseController } from "@interfaces/IBaseController";
+import { ControllerImplementation } from "miscellaneous-types";
+
+export abstract class BaseController implements IBaseController {
+  abstract implementations: ControllerImplementation[];
+
+  async invoke(request: Request, response: Response) {
+    const {
+      query: { version },
+    } = request;
+
+    if (!version || typeof version !== "string") {
+      return response.status(HTTP.INTERNAL_SERVER_ERROR).send();
+    }
+
+    const implementation = this.implementations.find(
+      implementation => implementation.version === parseFloat(version)
+    );
+
+    if (!implementation) {
+      return response.status(HTTP.INTERNAL_SERVER_ERROR).send();
+    }
+
+    return implementation.handler(request, response);
+  }
+}
