16cbdeb1506f3113c8acd724c96860c5c9520603
pawel idzikowski <iddziku@gmail.com>
2023-07-08 18:43:59 +0200

    Update variants.ts

--- backend/src/routes/v1/workspaces/variants.ts
+++ backend/src/routes/v1/workspaces/variants.ts
@@ -20,15 +20,15 @@ const variantsRouter = Router({ mergeParams: true });
 variantsRouter.use(requireWorkspaceAccess);
 
 variantsRouter.get(
-  "/v1",
+  "",
   validateRequestWith(useGetAllSuperSchema),
   processRequestWith(GetAllController)
 );
 
-variantsRouter.get("/:variantId/v1", processRequestWith(GetByIdController));
+variantsRouter.get("/:variantId", processRequestWith(GetByIdController));
 
 variantsRouter.post(
-  "/v1",
+  "",
   IsWorkspaceAvailable,
   parseUploadFormData(
     {
