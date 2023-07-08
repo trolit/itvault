b47e88667756da861c6d12feb96d573440cb1c77
pawel idzikowski <iddziku@gmail.com>
2023-07-08 23:04:03 +0200

    Update getVersionSchema.ts

--- backend/src/schemas/common/getVersionSchema.ts
+++ backend/src/schemas/common/getVersionSchema.ts
@@ -3,17 +3,21 @@ import Zod, { RefinementCtx, z, ZodIssueCode } from "zod";
 import { schemaForType } from "@schemas/common/schemaForType";
 
 export const getVersionSchema = (versions: number[]) =>
-  schemaForType<number>()(
-    z.coerce.number().superRefine((value: number, context: RefinementCtx) => {
-      if (!versions.includes(value)) {
-        context.addIssue({
-          code: ZodIssueCode.custom,
-          message: `Wrong resource version. Requested: ${value}, available: [${versions.join(
-            ","
-          )}]`,
-        });
+  schemaForType<{ version: number }>()(
+    z.object({
+      version: z.coerce
+        .number()
+        .superRefine((value: number, context: RefinementCtx) => {
+          if (!versions.includes(value)) {
+            context.addIssue({
+              code: ZodIssueCode.custom,
+              message: `Wrong resource version. Requested: ${value}, available: [${versions.join(
+                ","
+              )}]`,
+            });
 
-        return Zod.NEVER;
-      }
+            return Zod.NEVER;
+          }
+        }),
     })
   );
