0eb39a1d2f958d25d0c4d5f57a674aea00c4bc99
pawel idzikowski <iddziku@gmail.com>
2023-07-09 22:11:35 +0200

    rename base schemas

--- backend/src/schemas/Note/baseSchemas.ts
+++ backend/src/schemas/Note/baseSchemas.ts
@@ -13,6 +13,6 @@ export const resourceSchema = schemaForType<Pick<NoteDto, "id" | "resource">>()(
   })
 );
 
-export const baseSchemas = {
+export const baseNoteSchemas = {
   resourceSchema,
 };
--- backend/src/schemas/Note/useGetAllSuperSchema.ts
+++ backend/src/schemas/Note/useGetAllSuperSchema.ts
@@ -1,12 +1,12 @@
 import { SuperSchemaRunner, SchemaProvider } from "super-schema-types";
 
-import { baseSchemas } from "./baseSchemas";
+import { baseNoteSchemas } from "./baseSchemas";
 import { resourceSuperRefine } from "./resourceSuperRefine";
 
 import { paginationSchema } from "@schemas/common/paginationSchema";
 import { defineSuperSchemaRunner } from "@schemas/common/defineSuperSchemaRunner";
 
-const { resourceSchema } = baseSchemas;
+const { resourceSchema } = baseNoteSchemas;
 
 export const useGetAllSuperSchema: SuperSchemaRunner = defineSuperSchemaRunner(
   () => {
