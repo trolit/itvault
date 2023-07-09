827b3c87c60eff36a1d0736fe05a7fc4a958ff47
pawel idzikowski <iddziku@gmail.com>
2023-07-09 17:25:16 +0200

    Update validateRequestWith.ts

--- backend/src/middleware/validateRequestWith.ts
+++ backend/src/middleware/validateRequestWith.ts
@@ -1,7 +1,7 @@
-import { z, ZodType } from "zod";
+import { z, ZodSchema, ZodType } from "zod";
 import { StatusCodes as HTTP } from "http-status-codes";
 import type { NextFunction, Request, Response } from "express";
-import { SuperKeys, SuperSchemaRunner } from "super-schema-types";
+import { SuperKeys, SuperSchema, SuperSchemaRunner } from "super-schema-types";
 
 import { getVersionSchema } from "@schemas/common/getVersionSchema";
 
@@ -15,6 +15,22 @@ export const validateRequestWith = (
     const superSchema = await useSuperSchemaRunner({
       request,
     });
+
+    const versionSchema = getVersionSchema(data.versions);
+
+    const commonSchemasResults = await runCommonSchemasOnMissingKeys(
+      superSchema,
+      request,
+      {
+        versionSchema,
+      }
+    );
+
+    if (commonSchemasResults && !commonSchemasResults.success) {
+      return response
+        .status(HTTP.BAD_REQUEST)
+        .send(commonSchemasResults.error.format());
+    }
 
     for (const key in superSchema) {
       const propertyName = <keyof SuperKeys>key;
@@ -31,7 +47,9 @@ export const validateRequestWith = (
         return response.status(HTTP.INTERNAL_SERVER_ERROR).send();
       }
 
-      const extendedSchema = extendSchema(propertyName, schema, data);
+      const extendedSchema = extendSchema(propertyName, schema, {
+        versionSchema,
+      });
 
       const result = await extendedSchema.safeParseAsync(request[propertyName]);
 
@@ -47,15 +65,31 @@ export const validateRequestWith = (
   };
 };
 
+async function runCommonSchemasOnMissingKeys(
+  superSchema: SuperSchema,
+  request: Request,
+  schemas: {
+    versionSchema: ZodSchema;
+  }
+) {
+  const keys = Object.keys(superSchema);
+
+  if (!keys.includes("query")) {
+    return schemas.versionSchema.safeParseAsync({
+      version: request.query.version,
+    });
+  }
+}
+
 function extendSchema(
   key: keyof SuperKeys,
   schema: ZodType,
-  data: {
-    versions: number[];
+  schemas: {
+    versionSchema: ZodSchema;
   }
 ) {
   if (key === "query") {
-    return z.intersection(schema, getVersionSchema(data.versions));
+    return z.intersection(schema, schemas.versionSchema);
   }
 
   return schema;
