c7fe55392517ebe073ebbe9b479132418bf46e1e
pawel idzikowski <iddziku@gmail.com>
2023-07-10 16:14:33 +0200

    Update notes.ts

--- backend/src/routes/v1/notes.ts
+++ backend/src/routes/v1/notes.ts
@@ -2,7 +2,6 @@ import { Router } from "express";
 
 import { processRequestWith } from "@helpers/processRequestWith";
 import { validateRequestWith } from "@middleware/validateRequestWith";
-import { requireAuthentication } from "@middleware/requireAuthentication";
 import { requireEndpointVersion } from "@middleware/requireEndpointVersion";
 
 import { useStoreSuperSchema } from "@schemas/Note/useStoreSuperSchema";
@@ -13,8 +12,6 @@ import { GetAllController } from "@controllers/Note/GetAllController";
 import { SoftDeleteController } from "@controllers/SoftDeleteController";
 
 const notesRouter = Router();
-
-notesRouter.use(requireAuthentication);
 
 notesRouter.get(
   "",
