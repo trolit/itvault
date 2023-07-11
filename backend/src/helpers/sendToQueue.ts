d556530facb06a4db8885133bb7b56f52302f3e7
pawel idzikowski <iddziku@gmail.com>
2023-07-11 19:08:25 +0200

    Update sendToQueue.ts

--- backend/src/helpers/sendToQueue.ts
+++ backend/src/helpers/sendToQueue.ts
@@ -5,6 +5,7 @@ import { getInstanceOf } from "./getInstanceOf";
 import { Di } from "@enums/Di";
 import { Queue } from "@enums/Queue";
 
+// @NOTE consider extracting to QueueService
 export const sendToQueue = <T>(queue: Queue, data: T) => {
   const publisher = getInstanceOf<Channel>(Di.Publisher);
 
