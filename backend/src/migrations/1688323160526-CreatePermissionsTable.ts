ee3b36ace39c1a94bd537fa51ba5b809c1e37888
pawel idzikowski <iddziku@gmail.com>
2023-07-12 21:32:11 +0200

    modify `CreatePermissionsTable` migration

--- backend/src/migrations/1688323160526-CreatePermissionsTable.ts
+++ backend/src/migrations/1688323160526-CreatePermissionsTable.ts
@@ -4,7 +4,7 @@ export class CreatePermissionsTable1688323160526 implements MigrationInterface {
     name = 'CreatePermissionsTable1688323160526'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE \`permissions\` (\`id\` int NOT NULL AUTO_INCREMENT, \`name\` varchar(255) NOT NULL, UNIQUE INDEX \`IDX_48ce552495d14eae9b187bb671\` (\`name\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+        await queryRunner.query(`CREATE TABLE \`permissions\` (\`signature\` varchar(255) NOT NULL, \`name\` varchar(255) NOT NULL, UNIQUE INDEX \`IDX_48ce552495d14eae9b187bb671\` (\`name\`), PRIMARY KEY (\`signature\`)) ENGINE=InnoDB`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
