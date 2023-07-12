e80259bffb0856addab7a52f47b773391cf5835f
pawel idzikowski <iddziku@gmail.com>
2023-07-12 20:59:10 +0200

    fix wrong schema import

--- backend/src/routes/v1/tags.ts
+++ backend/src/routes/v1/tags.ts
@@ -3,7 +3,7 @@ import { Router } from "express";
 import { processRequestWith } from "@helpers/processRequestWith";
 import { validateRequestWith } from "@middleware/validateRequestWith";
 
-import { useGetAllSuperSchema } from "@schemas/Note/useGetAllSuperSchema";
+import { useGetAllSuperSchema } from "@schemas/Tag/useGetAllSuperSchema";
 
 import { GetAllController } from "@controllers/Tag/GetAllController";
 
