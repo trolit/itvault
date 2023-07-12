3816ec8c9f757cf66d9b04a12c21158f910803fc
pawel idzikowski <iddziku@gmail.com>
2023-07-12 18:59:42 +0200

    Update Workspace.ts

--- backend/src/config/data-source/seeds/Workspace.ts
+++ backend/src/config/data-source/seeds/Workspace.ts
@@ -1,8 +1,10 @@
 import { DataSource } from "typeorm";
+import sampleSize from "lodash/sampleSize";
 import { Seeder, SeederFactoryManager } from "typeorm-extension";
 
 import { TEST_WORKSPACE_2, TEST_WORKSPACE_1 } from "./common";
 
+import { Tag } from "@entities/Tag";
 import { Workspace } from "@entities/Workspace";
 
 export class WorkspaceSeeder implements Seeder {
@@ -12,12 +14,18 @@ export class WorkspaceSeeder implements Seeder {
   ) {
     const workspaceFactory = factoryManager.get(Workspace);
 
+    const tagRepository = dataSource.getRepository(Tag);
+
+    const tags = await tagRepository.find();
+
     await workspaceFactory.save({
       name: TEST_WORKSPACE_1.name,
+      tags: sampleSize(tags, 3),
     });
 
     await workspaceFactory.save({
       name: TEST_WORKSPACE_2.name,
+      tags: sampleSize(tags, 3),
     });
   }
 }
