9d8036c6af0f28d60716630c27329ffaa0f378ef
pawel idzikowski <iddziku@gmail.com>
2023-07-12 18:39:04 +0200

    add `Tag` to `data-source` config

--- backend/src/config/data-source/entities.ts
+++ backend/src/config/data-source/entities.ts
@@ -1,12 +1,13 @@
+import { Tag } from "@entities/Tag";
+import { File } from "@entities/File";
 import { Note } from "@entities/Note";
 import { Role } from "@entities/Role";
-import { File } from "@entities/File";
 import { User } from "@entities/User";
 import { Bundle } from "@entities/Bundle";
 import { Bucket } from "@entities/Bucket";
 import { Variant } from "@entities/Variant";
-import { Workspace } from "@entities/Workspace";
 import { Blueprint } from "@entities/Blueprint";
+import { Workspace } from "@entities/Workspace";
 import { Permission } from "@entities/Permission";
 import { UserToWorkspace } from "@entities/UserToWorkspace";
 import { PermissionToRole } from "@entities/PermissionToRole";
@@ -24,4 +25,5 @@ export const entities = [
   Bundle,
   Bucket,
   Note,
+  Tag,
 ];
