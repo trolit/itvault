0547a3cce339a9a585fb95daed761e08806f9a9c
pawel idzikowski <iddziku@gmail.com>
2023-07-12 21:55:34 +0200

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,6 +7,7 @@ import { setupRedis } from "@utils/setupRedis";
 import { setupRabbit } from "@utils/setupRabbit";
 import { setupExpress } from "@utils/setupExpress";
 
+// @TODO util that detects if more permissions were added - if yes - updates DB
 export const server = async () => {
   const app = express();
 
