6ff24fc61a2a79203e2151710d291e16170e77dd
pawel idzikowski <iddziku@gmail.com>
2023-07-13 19:54:16 +0200

    update mapper service

--- backend/src/types/interfaces/services/IEntityMapperService.ts
+++ backend/src/types/interfaces/services/IEntityMapperService.ts
@@ -1,16 +1,7 @@
-import { Type } from "miscellaneous-types";
 import { Base } from "@entities/Base";
 
 export interface IEntityMapperService {
-  mapOneToDto<T extends Base, Y extends Partial<T>>(
-    entity: T,
-    target: Type<Y>,
-    customProps?: (from: T) => Partial<Y>
-  ): Y;
+  mapOneToDto<T extends Base, Y>(entity: T, target: new (data: T) => Y): Y;
 
-  mapToDto<T extends Base, Y extends Partial<T>>(
-    entities: T[],
-    target: Type<Y>,
-    customProps?: (from: T) => Partial<Y>
-  ): Y[];
+  mapToDto<T extends Base, Y>(entities: T[], target: new (data: T) => Y): Y[];
 }
