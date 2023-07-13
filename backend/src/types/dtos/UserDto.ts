ef4de79f70edeedfcbed7b3de708156613fed52d
pawel idzikowski <iddziku@gmail.com>
2023-07-13 19:47:10 +0200

    Update UserDto.ts

--- backend/src/types/dtos/UserDto.ts
+++ backend/src/types/dtos/UserDto.ts
@@ -1,15 +1,26 @@
+import { BaseDto } from "./BaseDto";
+import { User } from "@entities/User";
 import { PermissionDto } from "./PermissionDto";
 
-export class UserDto {
-  id = -1;
-
-  email = "";
-
-  fullName = "";
-
+export class UserDto extends BaseDto<User> {
   roleId: number;
-
   roleName: string;
-
   permissions: PermissionDto[];
+
+  constructor(data: User, keys: (keyof User)[] = ["id", "email", "fullName"]) {
+    super(data, keys);
+
+    const {
+      role: { id, name, permissionToRole },
+    } = data;
+
+    this.roleId = id;
+    this.roleName = name;
+    this.permissions = permissionToRole.map(({ enabled, permission }) => ({
+      ...permission,
+      enabled,
+    }));
+
+    return this;
+  }
 }
