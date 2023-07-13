d808961b7be65e1083f98582ab08d80145f3850f
pawel idzikowski <iddziku@gmail.com>
2023-07-13 15:30:52 +0200

    include tags in `WorkspaceDto`

--- backend/src/types/dtos/WorkspaceDto.ts
+++ backend/src/types/dtos/WorkspaceDto.ts
@@ -1,6 +1,10 @@
+import { Tag } from "@entities/Tag";
+
 // @NOTE properties took from entity must be initialized for EntityMapperService
 export class WorkspaceDto {
   id = 0;
 
   name = "";
+
+  tags: Tag[];
 }
