158fab800b45dd150321535e8b12062e1b7ada8b
pawel idzikowski <iddziku@gmail.com>
2023-07-13 20:15:33 +0200

    specify `Y` generic type

--- backend/src/services/EntityMapperService.ts
+++ backend/src/services/EntityMapperService.ts
@@ -1,12 +1,19 @@
 import { Base } from "@entities/Base";
+import { BaseMapDto } from "@dtos/BaseMapDto";
 import { IEntityMapperService } from "@interfaces/services/IEntityMapperService";
 
 export class EntityMapperService implements IEntityMapperService {
-  mapOneToDto<T extends Base, Y>(entity: T, target: new (data: T) => Y): Y {
+  mapOneToDto<T extends Base, Y extends BaseMapDto<T>>(
+    entity: T,
+    target: new (data: T) => Y
+  ): Y {
     return new target(entity);
   }
 
-  mapToDto<T extends Base, Y>(entities: T[], target: new (data: T) => Y): Y[] {
+  mapToDto<T extends Base, Y extends BaseMapDto<T>>(
+    entities: T[],
+    target: new (data: T) => Y
+  ): Y[] {
     return entities.map(entity => new target(entity));
   }
 }
--- backend/src/types/interfaces/services/IEntityMapperService.ts
+++ backend/src/types/interfaces/services/IEntityMapperService.ts
@@ -1,7 +1,14 @@
 import { Base } from "@entities/Base";
+import { BaseMapDto } from "@dtos/BaseMapDto";
 
 export interface IEntityMapperService {
-  mapOneToDto<T extends Base, Y>(entity: T, target: new (data: T) => Y): Y;
+  mapOneToDto<T extends Base, Y extends BaseMapDto<T>>(
+    entity: T,
+    target: new (data: T) => Y
+  ): Y;
 
-  mapToDto<T extends Base, Y>(entities: T[], target: new (data: T) => Y): Y[];
+  mapToDto<T extends Base, Y extends BaseMapDto<T>>(
+    entities: T[],
+    target: new (data: T) => Y
+  ): Y[];
 }
