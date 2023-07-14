1dfdcaa9181516c8a850465d1733a08c440d95f0
pawel idzikowski <iddziku@gmail.com>
2023-07-14 18:14:55 +0200

    Update WorkspaceMapDto.ts

--- backend/src/types/dtos/WorkspaceMapDto.ts
+++ backend/src/types/dtos/WorkspaceMapDto.ts
@@ -2,11 +2,12 @@ import { BaseMapDto } from "./BaseMapDto";
 import { Workspace } from "@entities/Workspace";
 
 export class WorkspaceMapDto extends BaseMapDto<Workspace> {
-  constructor(
-    data: Workspace,
-    keys: (keyof Workspace)[] = ["id", "name", "tags"]
-  ) {
+  tags: string[];
+
+  constructor(data: Workspace, keys: (keyof Workspace)[] = ["id", "name"]) {
     super(data, keys);
+
+    this.tags = data.tagToWorkspace.map(({ tag: { value } }) => value);
 
     return this;
   }
