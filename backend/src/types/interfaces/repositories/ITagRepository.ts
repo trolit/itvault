e4dbc2842bf80e5eb20cf30f43b7fc5e824a0b84
pawel idzikowski <iddziku@gmail.com>
2023-07-15 15:39:32 +0200

    add `transactionSaveMany` to `TagRepository`

--- backend/src/types/interfaces/repositories/ITagRepository.ts
+++ backend/src/types/interfaces/repositories/ITagRepository.ts
@@ -1,4 +1,7 @@
 import { Tag } from "@entities/Tag";
+import { QueryRunner, SaveOptions } from "typeorm";
 import { IBaseRepository } from "./IBaseRepository";
 
-export interface ITagRepository extends IBaseRepository<Tag> {}
+export interface ITagRepository extends IBaseRepository<Tag> {
+  transactionSaveMany(transaction: QueryRunner, tags: string[]): Promise<Tag[]>;
+}
