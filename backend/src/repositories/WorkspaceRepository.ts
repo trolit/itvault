0ec03640198a1591d9a4a7e44fd51b4aa540ed5e
pawel idzikowski <iddziku@gmail.com>
2023-07-15 15:59:46 +0200

    remove unused actions from `WorkspaceRepository`

--- backend/src/repositories/WorkspaceRepository.ts
+++ backend/src/repositories/WorkspaceRepository.ts
@@ -1,11 +1,9 @@
+import { Repository } from "typeorm";
 import { injectable } from "tsyringe";
-import { Like, QueryRunner, Repository, UpdateResult } from "typeorm";
 
 import { BaseRepository } from "./BaseRepository";
 
-import { Tag } from "@entities/Tag";
 import { Workspace } from "@entities/Workspace";
-import { TagToWorkspace } from "@entities/TagToWorkspace";
 import { IWorkspaceRepository } from "@interfaces/repositories/IWorkspaceRepository";
 
 @injectable()
@@ -17,85 +15,5 @@ export class WorkspaceRepository
 
   constructor() {
     super(Workspace);
-  }
-
-  async save(name: string, tags: string[]): Promise<Workspace | null> {
-    const transaction = await this.useTransaction();
-
-    try {
-      const tagEntities = await this._adjustTags(transaction, tags);
-
-      const workspace = await transaction.manager.save(
-        Workspace,
-        transaction.manager.create(Workspace, {
-          name,
-          tagToWorkspace: tagEntities.map(tagEntity => ({ tag: tagEntity })),
-        })
-      );
-
-      await transaction.commitTransaction();
-
-      return workspace;
-    } catch (error) {
-      console.log(error);
-
-      await transaction.rollbackTransaction();
-
-      return null;
-    } finally {
-      await transaction.release();
-    }
-  }
-
-  async update(
-    id: number,
-    name: string,
-    tags: string[]
-  ): Promise<UpdateResult | null> {
-    const transaction = await this.useTransaction();
-
-    try {
-      const workspace = await transaction.manager.findOneByOrFail(Workspace, {
-        id,
-      });
-
-      const tagEntities = await this._adjustTags(transaction, tags);
-
-      const tagsToWorkspace = tagEntities.map(eo =>
-        transaction.manager.create(TagToWorkspace, { tag: eo })
-      );
-
-      await transaction.manager.save(Workspace, {
-        ...workspace,
-        name,
-        tagToWorkspace: tagsToWorkspace,
-      });
-
-      await transaction.commitTransaction();
-
-      return null;
-    } catch (error) {
-      console.log(error);
-
-      await transaction.rollbackTransaction();
-
-      return null;
-    } finally {
-      await transaction.release();
-    }
-  }
-
-  private async _adjustTags(transaction: QueryRunner, tags: string[]) {
-    const tagEntities = await Promise.all(
-      tags.map(async value => {
-        const element = await transaction.manager.findOneBy(Tag, {
-          value: Like(value),
-        });
-
-        return element ? element : transaction.manager.save(Tag, { value });
-      })
-    );
-
-    return tagEntities;
   }
 }
--- backend/src/types/interfaces/repositories/IWorkspaceRepository.ts
+++ backend/src/types/interfaces/repositories/IWorkspaceRepository.ts
@@ -1,13 +1,4 @@
-import { UpdateResult } from "typeorm";
 import { Workspace } from "@entities/Workspace";
 import { IBaseRepository } from "./IBaseRepository";
 
-export interface IWorkspaceRepository extends IBaseRepository<Workspace> {
-  save(name: string, tags: string[]): Promise<Workspace | null>;
-
-  update(
-    id: number,
-    name: string,
-    tags: string[]
-  ): Promise<UpdateResult | null>;
-}
+export interface IWorkspaceRepository extends IBaseRepository<Workspace> {}
