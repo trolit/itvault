3c136101848c73e801f7e795c9d004805d165e96
pawel idzikowski <iddziku@gmail.com>
2023-07-16 10:04:10 +0200

    remove create/update from repository

--- backend/src/repositories/RoleRepository.ts
+++ backend/src/repositories/RoleRepository.ts
@@ -4,9 +4,6 @@ import { injectable } from "tsyringe";
 import { BaseRepository } from "./BaseRepository";
 
 import { Role } from "@entities/Role";
-import { Permission } from "@entities/Permission";
-import { AddEditRoleDto } from "@dtos/AddEditRoleDto";
-import { PermissionToRole } from "@entities/PermissionToRole";
 import { IRoleRepository } from "@interfaces/repositories/IRoleRepository";
 
 @injectable()
@@ -18,107 +15,5 @@ export class RoleRepository
 
   constructor() {
     super(Role);
-  }
-
-  async create(data: AddEditRoleDto): Promise<Role | null> {
-    const transaction = await this.useTransaction();
-
-    const { manager } = transaction;
-
-    const { name, permissions } = data;
-
-    try {
-      const permissionEntities = await manager.find(Permission);
-
-      const role = await manager.save(Role, {
-        name,
-        permissionToRole: permissionEntities.map(entity => {
-          const permission = permissions.find(
-            element => element.signature === entity.signature
-          );
-
-          if (!permission) {
-            throw new Error(
-              `Permission with signature ${entity.signature} not included in request!`
-            );
-          }
-
-          return {
-            enabled: permission.enabled,
-            permission: entity,
-          };
-        }),
-      });
-
-      await transaction.commitTransaction();
-
-      return role;
-    } catch (error) {
-      console.log(error);
-
-      await transaction.rollbackTransaction();
-
-      return null;
-    } finally {
-      await transaction.release();
-    }
-  }
-
-  async update(id: number, data: AddEditRoleDto): Promise<Role | null> {
-    const transaction = await this.useTransaction();
-
-    const { manager } = transaction;
-
-    const { name, permissions } = data;
-
-    const updatePermissionToRole = (permissionToRole: PermissionToRole) => {
-      const { permission } = permissionToRole;
-
-      const updatedPermission = permissions.find(
-        ({ signature }) => signature === permission.signature
-      );
-
-      if (!updatedPermission) {
-        throw new Error(
-          `Permission with signature ${permission.signature} not included in request!`
-        );
-      }
-
-      const { enabled } = updatedPermission;
-
-      return {
-        ...permissionToRole,
-        enabled,
-      };
-    };
-
-    try {
-      const currentRole = await manager.findOneOrFail(Role, {
-        where: { id },
-        relations: { permissionToRole: { permission: true } },
-      });
-
-      const { permissionToRole } = currentRole;
-
-      const updatedRole = await manager.save(Role, {
-        ...currentRole,
-        name,
-        permissionToRole: permissionToRole.map(value =>
-          updatePermissionToRole(value)
-        ),
-      });
-
-      await transaction.commitTransaction();
-
-      return updatedRole;
-    } catch (error) {
-      console.log(error);
-
-      await transaction.rollbackTransaction();
-
-      return null;
-    } finally {
-      await transaction.release();
-    }
   }
 }
--- backend/src/types/interfaces/repositories/IRoleRepository.ts
+++ backend/src/types/interfaces/repositories/IRoleRepository.ts
@@ -1,9 +1,4 @@
 import { Role } from "@entities/Role";
-import { AddEditRoleDto } from "@dtos/AddEditRoleDto";
 import { IBaseRepository } from "./IBaseRepository";
 
-export interface IRoleRepository extends IBaseRepository<Role> {
-  create(data: AddEditRoleDto): Promise<Role | null>;
-
-  update(id: number, data: AddEditRoleDto): Promise<Role | null>;
-}
+export interface IRoleRepository extends IBaseRepository<Role> {}
