7ca0bdd1d956806dc392af330e133452ff40df10
pawel idzikowski <iddziku@gmail.com>
2023-07-17 19:12:07 +0200

    Update IMailService.ts

--- backend/src/types/interfaces/services/IMailService.ts
+++ backend/src/types/interfaces/services/IMailService.ts
@@ -1,5 +1,7 @@
 import { Options } from "nodemailer/lib/mailer";
 
 export interface IMailService<T> {
+  buildHtml(rendererName: string, data: T): string;
+
   sendMail(mailOptions: Options): Promise<T>;
 }
