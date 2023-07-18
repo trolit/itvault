ccb241aaa1d0d9d515add1046b8339dea7c5d195
pawel idzikowski <iddziku@gmail.com>
2023-07-18 16:11:19 +0200

    Update RegistrationMailViewBuilder.ts

--- backend/src/services/MailService/view-builders/RegistrationMailViewBuilder.ts
+++ backend/src/services/MailService/view-builders/RegistrationMailViewBuilder.ts
@@ -1,9 +1,31 @@
 import { IMailViewBuilder } from "@interfaces/IMailViewBuilder";
 
+export interface IData {
+  email: string;
+
+  code: string;
+}
+
+export interface IResult {
+  email: string;
+
+  code: string;
+
+  href: string;
+}
+
 export class RegistrationMailViewBuilder
-  implements IMailViewBuilder<{ email: string }>
+  implements IMailViewBuilder<IData, IResult>
 {
-  build(data: { email: string }) {
-    return data;
+  build(data: IData) {
+    const { email, code } = data;
+
+    // @TODO generate code
+
+    return {
+      email,
+      code,
+      href: "",
+    };
   }
 }
