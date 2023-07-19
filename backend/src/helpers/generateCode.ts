ae33895dbb0d6baa35307f2863261bd7504ac462
pawel idzikowski <iddziku@gmail.com>
2023-07-19 19:05:17 +0200

    Update generateCode.ts

--- backend/src/helpers/generateCode.ts
+++ backend/src/helpers/generateCode.ts
@@ -2,8 +2,8 @@ import dayjs from "dayjs";
 import random from "lodash/random";
 
 export const generateCode = (length = 30) => {
-  const randomAddNumber = random(55, 99999999);
-  const randomSubtractNumber = random(15555, 600000);
+  const randomAddNumber = random(50000, 9999999999);
+  const randomSubtractNumber = random(500, 30000);
 
   const now = dayjs();
 
