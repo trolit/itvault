11f020003433f611b72c9dd49b0206e46a94ae42
pawel idzikowski <iddziku@gmail.com>
2023-07-19 18:20:13 +0200

    add transporter verification

--- backend/src/utils/setupMailTransporter.ts
+++ backend/src/utils/setupMailTransporter.ts
@@ -5,13 +5,23 @@ import { EMAIL } from "@config";
 const { HOST, PORT, USER, PASSWORD } = EMAIL;
 
 export const setupMailTransporter = () => {
-  return nodemailer.createTransport({
+  const transporter = nodemailer.createTransport({
     host: HOST,
     port: PORT,
+    secure: false,
     auth: {
       type: "login",
       user: USER,
       pass: PASSWORD,
     },
   });
+
+  transporter.verify(error => {
+    if (error) {
+      console.log("NODEMAILER: invalid config!!");
+      console.log(error);
+    }
+  });
+
+  return transporter;
 };
