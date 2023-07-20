96b00010f2f584346d60faf89e6bcc34e50b80cd
pawel idzikowski <iddziku@gmail.com>
2023-07-20 18:06:52 +0200

    Update LoginController.ts

--- backend/src/types/controllers/v1/Auth/LoginController.ts
+++ backend/src/types/controllers/v1/Auth/LoginController.ts
@@ -4,9 +4,6 @@ import { LoggedUserMapDto } from "@dtos/LoggedUserMapDto";
 export type Body = Pick<User, "email" | "password">;
 
 // @TODO consider if we apply that idea to other controllers when working on unit tests :thinking:
-export type LoginDto = CustomRequest<
-  undefined,
-  { email: string; password: string }
->;
+export type LoginDto = CustomRequest<undefined, Body>;
 
 export type LoggedUserDto = CustomResponse<LoggedUserMapDto>;
