8c61b38d89992fe2e2b8fa0edaf80561c3df618e
pawel idzikowski <iddziku@gmail.com>
2023-07-21 21:33:15 +0200

    Update blueprints.ts

--- backend/src/routes/v1/blueprints.ts
+++ backend/src/routes/v1/blueprints.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 
 import { processRequestWith } from "@helpers/processRequestWith";
 import { validateRequestWith } from "@middleware/validateRequestWith";
+import { requireEndpointVersion } from "@middleware/requireEndpointVersion";
 import { requireWorkspaceAccess } from "@middleware/requireWorkspaceAccess";
 
 import { useStoreSuperSchema } from "@schemas/Blueprint/useStoreSuperSchema";
 import { useGetAllSuperSchema } from "@schemas/Blueprint/useGetAllSuperSchema";
 
+import { SoftDeleteController } from "@controllers/SoftDeleteController";
 import { StoreController } from "@controllers/Blueprint/StoreController";
 import { GetAllController } from "@controllers/Blueprint/GetAllController";
 
@@ -28,6 +30,12 @@ blueprintsRouter.post(
     versions: StoreController.ALL_VERSIONS,
   }),
   processRequestWith(StoreController)
+);
+
+blueprintsRouter.delete(
+  "/:id",
+  requireEndpointVersion(SoftDeleteController.ALL_VERSIONS),
+  processRequestWith(SoftDeleteController)
 );
 
 export = blueprintsRouter;
