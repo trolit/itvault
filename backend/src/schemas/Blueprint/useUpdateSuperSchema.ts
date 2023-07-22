4b1875b52366f198b9ee4715d3079b9f2d8332cb
pawel idzikowski <iddziku@gmail.com>
2023-07-22 12:10:29 +0200

    Create useUpdateSuperSchema.ts

---
+++ backend/src/schemas/Blueprint/useUpdateSuperSchema.ts
@@ -0,0 +1,56 @@
+import {
+  SchemaProvider,
+  SuperCommonParam,
+  SuperSchemaRunner,
+} from "super-schema-types";
+
+import { baseBlueprintSchemas } from "./baseSchemas";
+
+import { Di } from "@enums/Di";
+import { IBlueprintRepository } from "@interfaces/repositories/IBlueprintRepository";
+
+import { getInstanceOf } from "@helpers/getInstanceOf";
+
+import { baseWorkspaceSchemas } from "@schemas/Workspace/baseSchemas";
+import { defineSuperSchemaRunner } from "@schemas/common/defineSuperSchemaRunner";
+import { getIsEntityAvailableSchema } from "@schemas/common/getIsEntityAvailableSchema";
+
+const { getIsWorkspaceAvailableSchema } = baseWorkspaceSchemas;
+const { getAddEditBodySchema } = baseBlueprintSchemas;
+
+export const useStoreSuperSchema: SuperSchemaRunner = defineSuperSchemaRunner(
+  ({ request }: SuperCommonParam) => {
+    const {
+      query: { workspaceId },
+    } = request;
+
+    return {
+      query: useQuerySchema(),
+      params: useParamsSchema(),
+      body: useBodySchema(<string>workspaceId),
+    };
+  }
+);
+
+function useQuerySchema(): SchemaProvider {
+  return () => getIsWorkspaceAvailableSchema("workspaceId");
+}
+
+function useParamsSchema(): SchemaProvider {
+  return () =>
+    getIsEntityAvailableSchema("id", Di.BlueprintRepository, "Blueprint");
+}
+
+function useBodySchema(workspaceId?: string): SchemaProvider {
+  const blueprintRepository = getInstanceOf<IBlueprintRepository>(
+    Di.BlueprintRepository
+  );
+
+  const parsedWorkspaceId = workspaceId ? parseInt(workspaceId) : null;
+
+  if (!parsedWorkspaceId) {
+    return () => null;
+  }
+
+  return () => getAddEditBodySchema(blueprintRepository, parsedWorkspaceId);
+}
