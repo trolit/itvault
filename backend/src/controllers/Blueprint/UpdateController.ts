437d9b1d7768322ede5ebb584130fdf9868d6855
pawel idzikowski <iddziku@gmail.com>
2023-07-22 11:22:59 +0200

    Create UpdateController.ts

---
+++ backend/src/controllers/Blueprint/UpdateController.ts
@@ -0,0 +1,64 @@
+import { Response } from "express";
+import { inject, injectable } from "tsyringe";
+import { StatusCodes as HTTP } from "http-status-codes";
+
+import { Di } from "@enums/Di";
+import { ControllerImplementation } from "miscellaneous-types";
+import { AddEditBlueprintDto } from "@dtos/AddEditBlueprintDto";
+import { IBlueprintRepository } from "@interfaces/repositories/IBlueprintRepository";
+
+import { BaseController } from "@controllers/BaseController";
+
+interface IQuery {
+  workspaceId: number;
+}
+
+interface IParams {
+  id: number;
+}
+
+const { v1_0 } = BaseController.ALL_VERSION_DEFINITIONS;
+
+@injectable()
+export class UpdateController extends BaseController {
+  constructor(
+    @inject(Di.BlueprintRepository)
+    private _blueprintRepository: IBlueprintRepository
+  ) {
+    super();
+  }
+
+  implementations: ControllerImplementation[] = [
+    {
+      version: v1_0,
+      handle: this.v1.bind(this),
+    },
+  ];
+
+  static ALL_VERSIONS = [v1_0];
+
+  async v1(
+    request: CustomRequest<IParams, AddEditBlueprintDto, IQuery>,
+    response: Response
+  ) {
+    const {
+      body,
+      params: { id },
+      query: { workspaceId },
+    } = request;
+
+    const isUpdated = await this._blueprintRepository.primitiveUpdate(
+      {
+        id,
+        workspace: { id: workspaceId },
+      },
+      body
+    );
+
+    if (!isUpdated?.affected) {
+      return response.status(HTTP.UNPROCESSABLE_ENTITY).send();
+    }
+
+    return this.finalizeRequest(response, HTTP.NO_CONTENT);
+  }
+}
