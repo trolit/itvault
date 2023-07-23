9a88c37929ad021aec4e0ee1a1383bbb25a83148
pawel idzikowski <iddziku@gmail.com>
2023-07-23 18:41:24 +0200

    Update paginationSchema.ts

--- backend/src/schemas/common/paginationSchema.ts
+++ backend/src/schemas/common/paginationSchema.ts
@@ -6,7 +6,7 @@ import { schemaForType } from "@schemas/common/schemaForType";
 
 export const paginationSchema = schemaForType<IPaginationOptions>()(
   z.object({
-    take: z.coerce.number().int().gt(0).max(9999),
-    skip: z.coerce.number().int().gte(0).max(9999),
+    take: z.coerce.number().int().gt(0),
+    skip: z.coerce.number().int().gte(0),
   })
 );
