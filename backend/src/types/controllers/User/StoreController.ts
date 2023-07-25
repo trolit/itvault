051bb8d24d8b790bf152995f71367b6e9d9b36c5
pawel idzikowski <iddziku@gmail.com>
2023-07-25 18:29:56 +0200

    update User/StoreController response

--- backend/src/types/controllers/User/StoreController.ts
+++ backend/src/types/controllers/User/StoreController.ts
@@ -1,7 +1,10 @@
+import { UserMapDto } from "@dtos/UserMapDto";
 import { AddEditUserDto } from "@dtos/AddEditUserDto";
 
 export namespace StoreControllerTypes {
   export namespace v1 {
     export type Request = CustomRequest<undefined, AddEditUserDto>;
+
+    export type Response = CustomResponse<UserMapDto>;
   }
 }
