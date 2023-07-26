68819b4694da69549f9ae6d8a0a6e3d8fadb6a34
pawel idzikowski <iddziku@gmail.com>
2023-07-26 22:08:57 +0200

    Update index.d.ts

--- backend/src/types/dependencies/yup/index.d.ts
+++ backend/src/types/dependencies/yup/index.d.ts
@@ -1,10 +1,13 @@
 import yup from "yup";
 import { FindManyOptions, FindOptionsWhere } from "typeorm";
 
+import { Di } from "@enums/Di";
+
 declare module "yup" {
   interface NumberSchema {
     isAvailable(options: {
       message?: string;
+      repositoryName: Di;
       where: (value: number) => FindOptionsWhere<unknown>;
     }): this;
   }
