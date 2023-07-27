de2e865c9444c120e61df86cb50aaf0e528ae30d
pawel idzikowski <iddziku@gmail.com>
2023-07-27 19:09:02 +0200

    Update validateRequestWith.ts

--- backend/src/middleware/validateRequestWith.ts
+++ backend/src/middleware/validateRequestWith.ts
@@ -1,9 +1,11 @@
-import { z, ZodSchema, ZodType } from "zod";
+import { SuperSchema } from "super-schema-types";
 import type { NextFunction, Response } from "express";
 import { StatusCodes as HTTP } from "http-status-codes";
 import { SuperKeys, SuperSchemaRunner } from "super-schema-types";
 
-import { getVersionSchema } from "@schemas/common/getVersionSchema";
+import { formatError } from "@helpers/yup/formatError";
+
+import { useVersionSchema } from "@schemas/common/getVersionSchema";
 
 export const validateRequestWith = <P, B, Q>(
   useSuperSchemaRunner: SuperSchemaRunner<CustomRequest<P, B, Q>>,
@@ -20,93 +22,50 @@ export const validateRequestWith = <P, B, Q>(
       request,
     });
 
-    const versionSchema = getVersionSchema(data.versions);
-
-    const generalSchemasError = await runGeneralSchemasOnMissingKeys(
-      Object.keys(superSchema),
-      request,
-      {
-        versionSchema,
-      }
-    );
-
-    if (generalSchemasError) {
-      return response.status(HTTP.BAD_REQUEST).send({
-        general: generalSchemasError.format(),
-      });
-    }
+    includeGeneralQuerySchemas(superSchema, data);
 
     for (const key in superSchema) {
       const propertyName = <keyof SuperKeys>key;
 
-      const useSchemaProvider = superSchema[propertyName];
-
-      if (!useSchemaProvider) {
-        return response
-          .status(HTTP.INTERNAL_SERVER_ERROR)
-          .send(`Super Schema incorrectly implements '${key}' schema.`);
-      }
-
-      const schema = await useSchemaProvider();
+      const schema = superSchema[propertyName];
 
       if (!schema) {
         return response
           .status(HTTP.INTERNAL_SERVER_ERROR)
-          .send(`Failed to load '${key}' schema.`);
+          .send(`Failed to load '${key}' schema!`);
       }
 
-      const extendedSchema = extendSchema(propertyName, schema, {
-        versionSchema,
-      });
+      try {
+        const parsedData = await schema.validate(request[propertyName], {
+          abortEarly: false,
+        });
 
-      const result = await extendedSchema.safeParseAsync(request[propertyName]);
-
-      if (!result.success) {
+        // overwrites body with sanitized result
+        request[propertyName] = parsedData;
+      } catch (error) {
         return response.status(HTTP.BAD_REQUEST).send({
-          [propertyName]: result.error.format(),
+          [propertyName]: formatError(error),
         });
       }
-
-      // overwrites body with sanitized result
-      request[propertyName] = result.data;
     }
 
     next();
   };
 };
 
-async function runGeneralSchemasOnMissingKeys<P, B, Q>(
-  keys: string[],
-  request: CustomRequest<P, B, Q>,
-  schemas: {
-    versionSchema: ZodSchema;
-  }
+function includeGeneralQuerySchemas(
+  superSchema: SuperSchema,
+  data: { versions: number[] }
 ) {
-  if (!keys.includes("query")) {
-    const result = await schemas.versionSchema.safeParseAsync(request.query);
+  const querySchema = superSchema["query"];
 
-    if (!result.success) {
-      return result.error;
-    }
+  const versionSchema = useVersionSchema(data.versions);
 
-    request.query = result.data;
+  if (querySchema) {
+    querySchema.concat(versionSchema);
 
-    return null;
+    return;
   }
 
-  return null;
-}
-
-function extendSchema(
-  key: keyof SuperKeys,
-  schema: ZodType,
-  schemas: {
-    versionSchema: ZodSchema;
-  }
-) {
-  if (key === "query") {
-    return z.intersection(schema, schemas.versionSchema);
-  }
-
-  return schema;
+  superSchema.query = versionSchema;
 }
