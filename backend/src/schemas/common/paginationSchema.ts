3c518bdb10f3e5380927986f4b365ca6a322e526
pawel idzikowski <iddziku@gmail.com>
2023-07-29 01:09:55 +0200

    Update paginationSchema.ts

--- backend/src/schemas/common/paginationSchema.ts
+++ backend/src/schemas/common/paginationSchema.ts
@@ -1,9 +1,5 @@
-import { object, Schema, number } from "yup";
+import { number } from "yup";
 
-import { IPaginationQuery } from "@interfaces/IPaginationQuery";
+export const pageSchema = number().required().integer().min(1);
 
-export const paginationSchema: Schema<IPaginationQuery> = object({
-  page: number().required().integer().min(1),
-
-  perPage: number().required().integer().min(5),
-});
+export const perPageSchema = number().required().integer().min(5);
