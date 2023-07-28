a44807e96edbec61ac09dc3df3fc4776c2f7637b
pawel idzikowski <iddziku@gmail.com>
2023-07-29 00:52:49 +0200

    modify UpdateController (Blueprint) types

--- backend/src/types/controllers/Blueprint/UpdateController.ts
+++ backend/src/types/controllers/Blueprint/UpdateController.ts
@@ -2,14 +2,16 @@ import { AddEditBlueprintDto } from "@dtos/AddEditBlueprintDto";
 
 export namespace UpdateControllerTypes {
   export namespace v1 {
-    export type Query = {
-      workspaceId: number;
-    };
-
     export type Params = {
       id: number;
     };
 
-    export type Request = CustomRequest<Params, AddEditBlueprintDto, Query>;
+    export type Body = AddEditBlueprintDto;
+
+    export type Query = {
+      workspaceId: number;
+    };
+
+    export type Request = CustomRequest<Params, Body, Query>;
   }
 }
