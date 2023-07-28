a0bb92760f6ede5995edaf6c397cc785509e887b
pawel idzikowski <iddziku@gmail.com>
2023-07-28 23:36:15 +0200

    Update GetAllController.ts

--- backend/src/types/controllers/Blueprint/GetAllController.ts
+++ backend/src/types/controllers/Blueprint/GetAllController.ts
@@ -1,15 +1,18 @@
 import { BlueprintMapDto } from "@dtos/mappers/BlueprintMapDto";
+import { IPaginationQuery } from "@interfaces/IPaginationQuery";
 import { IPaginationOptions } from "@interfaces/IPaginationOptions";
 
 import { PaginatedResponse } from "miscellaneous-types";
 
 export namespace GetAllControllerTypes {
   export namespace v1 {
-    type Query = {
+    export type QueryInput = { workspaceId: number } & IPaginationQuery;
+
+    export type QueryOutput = {
       workspaceId: number;
     } & IPaginationOptions;
 
-    export type Request = CustomRequest<undefined, undefined, Query>;
+    export type Request = CustomRequest<undefined, undefined, QueryOutput>;
 
     export type Response = CustomResponse<PaginatedResponse<BlueprintMapDto>>;
   }
