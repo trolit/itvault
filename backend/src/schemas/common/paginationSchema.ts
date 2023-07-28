4edbb8a3813b4621a2f7ad9ef57cd4fb0c2ffafc
pawel idzikowski <iddziku@gmail.com>
2023-07-29 01:10:12 +0200

    Update paginationSchema.ts

--- backend/src/schemas/common/paginationSchema.ts
+++ backend/src/schemas/common/paginationSchema.ts
@@ -1,5 +1,7 @@
 import { number } from "yup";
 
+// @TODO rename file
+
 export const pageSchema = number().required().integer().min(1);
 
 export const perPageSchema = number().required().integer().min(5);
