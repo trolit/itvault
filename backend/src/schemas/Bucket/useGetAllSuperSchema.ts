dcffde6bb37d65bd8c6472151a92463bed7f16df
pawel idzikowski <iddziku@gmail.com>
2023-07-29 20:07:45 +0200

    refactor `Bucket` validators

--- backend/src/schemas/Bucket/useGetAllSuperSchema.ts
+++ backend/src/schemas/Bucket/useGetAllSuperSchema.ts
@@ -1,18 +1,22 @@
-import { SuperSchemaRunner, SchemaProvider } from "super-schema-types";
+import { object } from "yup";
+import { SuperSchemaElement, SuperSchemaRunner } from "super-schema-types";
+import { GetAllControllerTypes } from "types/controllers/Bucket/GetAllController";
 
-import { baseVariantSchemas } from "@schemas/Variant/baseSchemas";
+import { Di } from "@enums/Di";
+
+import { useIdStringSchema } from "@schemas/common/useIdStringSchema";
 import { defineSuperSchemaRunner } from "@schemas/common/defineSuperSchemaRunner";
 
-const { variantIdSchema } = baseVariantSchemas;
+const querySchema: SuperSchemaElement<GetAllControllerTypes.v1.Query> = object({
+  variantId: useIdStringSchema(Di.VariantRepository),
+});
 
-export const useGetAllSuperSchema: SuperSchemaRunner = defineSuperSchemaRunner(
-  () => {
-    return {
-      query: useQuerySchema(),
-    };
-  }
-);
-
-function useQuerySchema(): SchemaProvider {
-  return () => variantIdSchema;
-}
+export const useGetAllSuperSchema: SuperSchemaRunner<
+  void,
+  void,
+  GetAllControllerTypes.v1.Query
+> = defineSuperSchemaRunner(() => {
+  return {
+    query: querySchema,
+  };
+});
