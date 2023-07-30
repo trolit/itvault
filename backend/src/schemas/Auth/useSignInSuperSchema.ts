5c7fa3abf050941a1533842fbfb88f20196609cd
pawel idzikowski <iddziku@gmail.com>
2023-07-30 12:26:36 +0200

    Update useSignInSuperSchema.ts

--- backend/src/schemas/Auth/useSignInSuperSchema.ts
+++ backend/src/schemas/Auth/useSignInSuperSchema.ts
@@ -1,19 +1,21 @@
 import { object, string } from "yup";
-import { SuperSchemaRunner, SchemaProvider } from "super-schema-types";
+import { SuperSchemaElement, SuperSchemaRunner } from "super-schema-types";
 import { SignInControllerTypes } from "types/controllers/Auth/SignInController";
 
 import { defineSuperSchemaRunner } from "@schemas/common/defineSuperSchemaRunner";
 
-const bodySchema: SchemaProvider<SignInControllerTypes.v1.Body> = object({
+const bodySchema: SuperSchemaElement<SignInControllerTypes.v1.Body> = object({
   // @NOTE https://www.rfc-editor.org/rfc/rfc5321#section-4.5.3
   email: string().required().email().max(254),
   password: string().required(),
 });
 
-export const useSignInSuperSchema: SuperSchemaRunner = defineSuperSchemaRunner(
-  () => {
-    return {
-      body: bodySchema,
-    };
-  }
-);
+export const useSignInSuperSchema: SuperSchemaRunner<
+  void,
+  SignInControllerTypes.v1.Body,
+  void
+> = defineSuperSchemaRunner(() => {
+  return {
+    body: bodySchema,
+  };
+});
