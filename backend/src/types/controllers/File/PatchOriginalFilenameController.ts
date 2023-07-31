eea4d97a6ec493b4811b68632180295b2610d654
pawel idzikowski <iddziku@gmail.com>
2023-07-31 19:34:44 +0200

    create `PatchOriginalFilenameController`

---
+++ backend/src/types/controllers/File/PatchOriginalFilenameController.ts
@@ -0,0 +1,17 @@
+import { WorkspaceId } from "miscellaneous-types";
+
+export namespace PatchOriginalFilenameControllerTypes {
+  export namespace v1 {
+    export type Params = {
+      fileId: number;
+    };
+
+    export type Body = {
+      filename: string;
+    };
+
+    export type Query = WorkspaceId;
+
+    export type Request = CustomRequest<Params, Body, Query>;
+  }
+}
