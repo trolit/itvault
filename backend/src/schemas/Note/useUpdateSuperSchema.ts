4ed93075afd23edaae7e8dff57f1aee7de0dc2b1
pawel idzikowski <iddziku@gmail.com>
2023-08-01 15:58:55 +0200

    remove paramsSchema from Note UpdateSuperSchema

--- backend/src/schemas/Note/useUpdateSuperSchema.ts
+++ backend/src/schemas/Note/useUpdateSuperSchema.ts
@@ -1,29 +1,19 @@
-import { object } from "yup";
 import { SuperSchemaRunner, SuperSchemaElement } from "super-schema-types";
 import { UpdateControllerTypes } from "types/controllers/Note/UpdateController";
 
 import { addEditBodySchema } from "./addEditBodySchema";
 
-import { Di } from "@enums/Di";
-
-import { useIdNumberSchema } from "@schemas/common/useIdNumberSchema";
 import { defineSuperSchemaRunner } from "@schemas/common/defineSuperSchemaRunner";
-
-const paramsSchema: SuperSchemaElement<UpdateControllerTypes.v1.Params> =
-  object({
-    id: useIdNumberSchema(Di.NoteRepository),
-  });
 
 const bodySchema: SuperSchemaElement<UpdateControllerTypes.v1.Body> =
   addEditBodySchema;
 
 export const useUpdateSuperSchema: SuperSchemaRunner<
-  UpdateControllerTypes.v1.Params,
+  void,
   UpdateControllerTypes.v1.Body,
   void
 > = defineSuperSchemaRunner(() => {
   return {
-    params: paramsSchema,
     body: bodySchema,
   };
 });
