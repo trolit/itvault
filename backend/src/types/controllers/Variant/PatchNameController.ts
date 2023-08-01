bbf27cb3550447ae2f0e9025be1bae084434b1bf
pawel idzikowski <iddziku@gmail.com>
2023-08-01 18:10:39 +0200

    create `PatchNameController`

---
+++ backend/src/types/controllers/Variant/PatchNameController.ts
@@ -0,0 +1,17 @@
+import { WorkspaceId } from "miscellaneous-types";
+
+export namespace PatchNameControllerTypes {
+  export namespace v1 {
+    export type Params = {
+      id: string;
+    };
+
+    export type Body = {
+      name: string;
+    };
+
+    export type Query = WorkspaceId;
+
+    export type Request = CustomRequest<Params, Body, Query>;
+  }
+}
