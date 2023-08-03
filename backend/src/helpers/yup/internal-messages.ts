ae987ebc89b05898d20538adce18d3ce7edd23f0
pawel idzikowski <iddziku@gmail.com>
2023-08-03 19:24:52 +0200

    Update internal-messages.ts

--- backend/src/helpers/yup/internal-messages.ts
+++ backend/src/helpers/yup/internal-messages.ts
@@ -2,7 +2,8 @@ import { setLocale } from "yup";
 
 setLocale({
   number: {
-    min: "Value should (at least) equal ${value}",
+    min: "Value should (at least) equal ${min}",
+    max: "Value should (at most) equal ${max}",
   },
   // @TODO add more messages
 });
