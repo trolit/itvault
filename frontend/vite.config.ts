cd7eb7db16fe8462da8b20b58a85ed3efc941a94
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-08-09 19:02:45 +0200

    load files from `shared` to `Vite`

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig(({ mode }) => {
     resolve: {
       alias: {
         "@": fileURLToPath(new URL("./src", import.meta.url)),
+        "@shared": fileURLToPath(new URL("../shared/src", import.meta.url)),
       },
     },
     server: {
