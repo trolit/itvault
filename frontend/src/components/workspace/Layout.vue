ce564936c8041c3817c8f50f084c609ef8326cec
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-08-10 18:01:17 +0200

    Create Layout.vue

---
+++ frontend/src/components/workspace/Layout.vue
@@ -0,0 +1,31 @@
+<template>
+  <n-layout has-sider>
+    <n-layout-sider
+      show-trigger
+      :width="440"
+      collapse-mode="width"
+      :collapsed-width="32"
+      :native-scrollbar="false"
+      :inverted="inverted"
+      :collapsed="isCollapsed"
+      @expand="isCollapsed = false"
+      @collapse="isCollapsed = true"
+    >
+      <slot name="blueprints"></slot>
+
+      <slot name="files"></slot>
+    </n-layout-sider>
+
+    <n-layout>
+      <slot name="content"></slot>
+    </n-layout>
+  </n-layout>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { NLayout, NLayoutSider } from "naive-ui";
+
+const inverted = ref(false);
+const isCollapsed = ref(false);
+</script>
