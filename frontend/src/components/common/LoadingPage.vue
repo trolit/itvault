3e3df6207b4a4d33e245d4ec8cc893f446b28d39
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-08-10 17:10:05 +0200

    init `LoadingPage` component

---
+++ frontend/src/components/common/LoadingPage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="loading-page">
+    <div class="wrapper">
+      <div v-if="isLoading" class="state-loading">
+        <div>
+          <n-spin size="large" />
+        </div>
+
+        <div>Loading page...</div>
+      </div>
+
+      <div v-if="!isLoading && isFailed" class="state-error">
+        <n-alert title="Failed to load page" type="error">
+          We were unable to fetch primary data. Please try to reload page or
+          make sure that requested resource exists.
+        </n-alert>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { NSpin, NAlert } from "naive-ui";
+
+defineProps({
+  isLoading: {
+    type: Boolean,
+    required: true,
+  },
+
+  isFailed: {
+    type: Boolean,
+    required: true,
+  },
+});
+</script>
