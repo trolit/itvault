de19d3163ed50f0c294e79850edb7578f9d6fbad
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-08-10 22:42:24 +0200

    modify Sider

--- frontend/src/components/workspace/Sider/Index.vue
+++ frontend/src/components/workspace/Sider/Index.vue
@@ -1,5 +1,37 @@
 <template>
-  <div></div>
+  <div class="sider">
+    <n-tabs
+      animated
+      type="line"
+      size="medium"
+      justify-content="space-evenly"
+      :default-value="defaultTab"
+    >
+      <n-tab-pane
+        :name="key"
+        :tab="text"
+        v-for="({ key, text }, index) in tabs"
+        :key="index"
+      >
+        aha
+      </n-tab-pane>
+    </n-tabs>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { NTabs, NTabPane } from "naive-ui";
+
+const tabs = [
+  {
+    key: "blueprints",
+    text: "Blueprints",
+  },
+  {
+    key: "files",
+    text: "Files",
+  },
+];
+
+const defaultTab = "blueprints";
+</script>
