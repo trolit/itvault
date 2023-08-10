9202b8b7ce074de687d27fb44795bc9748201c70
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-08-10 17:28:10 +0200

    fix GetBySlugController

--- backend/src/types/controllers/Workspace/GetBySlugController.ts
+++ backend/src/types/controllers/Workspace/GetBySlugController.ts
@@ -1,13 +1,10 @@
-import { WorkspaceId } from "../WorkspaceId";
 import { WorkspaceMapper } from "@mappers/WorkspaceMapper";
 
 export namespace GetBySlugControllerTypes {
   export namespace v1 {
     export type Params = { slug: string };
 
-    export type Query = WorkspaceId;
-
-    export type Request = CustomRequest<Params, undefined, Query>;
+    export type Request = CustomRequest<Params, undefined, {}>;
 
     export type Response = CustomResponse<WorkspaceMapper>;
   }
