84b32863e0cddca31e00df5fd0532fc90e116098
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-08-10 20:06:48 +0200

    add scrollbar to Blueprints section

--- frontend/src/components/workspace/sider/blueprints/Index.vue
+++ frontend/src/components/workspace/sider/blueprints/Index.vue
@@ -7,30 +7,144 @@
     </div>
 
     <n-card>
-      <n-list clickable hoverable>
-        <n-list-item v-for="(blueprint, index) in items" :key="index">
-          <div class="wrapper">
-            <div class="content">
-              <div
-                class="thumbnail"
-                :style="{ backgroundColor: blueprint.color }"
-              ></div>
+      <n-scrollbar>
+        <n-list clickable hoverable>
+          <n-list-item v-for="(blueprint, index) in items" :key="index">
+            <div class="wrapper">
+              <div class="content">
+                <div
+                  class="thumbnail"
+                  :style="{ backgroundColor: blueprint.color }"
+                ></div>
 
-              {{ blueprint.name }}
+                {{ blueprint.name }}
+              </div>
             </div>
-          </div>
-        </n-list-item>
-      </n-list>
+          </n-list-item>
+        </n-list>
+      </n-scrollbar>
     </n-card>
   </div>
 </template>
 
 <script setup lang="ts">
-import { NList, NListItem, NButton, NTag, NCard } from "naive-ui";
+import { NList, NListItem, NButton, NTag, NCard, NScrollbar } from "naive-ui";
 
 import { IBlueprintDto } from "@shared/types/dtos/IBlueprintDto";
 
 const items: IBlueprintDto = [
+  {
+    id: 1,
+    name: "This is my blueprint",
+    color: "#7D91FF",
+    createdAt: "113213213-32132",
+    updatedAt: "12313",
+  },
+  {
+    id: 2,
+    name: "This is my blueprint 2",
+    color: "red",
+    createdAt: "113213213-32132",
+    updatedAt: "12313",
+  },
+  {
+    id: 1,
+    name: "This is my blueprint",
+    color: "#7D91FF",
+    createdAt: "113213213-32132",
+    updatedAt: "12313",
+  },
+  {
+    id: 2,
+    name: "This is my blueprint 2",
+    color: "red",
+    createdAt: "113213213-32132",
+    updatedAt: "12313",
+  },
+  {
+    id: 1,
+    name: "This is my blueprint",
+    color: "#7D91FF",
+    createdAt: "113213213-32132",
+    updatedAt: "12313",
+  },
+  {
+    id: 2,
+    name: "This is my blueprint 2",
+    color: "red",
+    createdAt: "113213213-32132",
+    updatedAt: "12313",
+  },
+  {
+    id: 1,
+    name: "This is my blueprint",
+    color: "#7D91FF",
+    createdAt: "113213213-32132",
+    updatedAt: "12313",
+  },
+  {
+    id: 2,
+    name: "This is my blueprint 2",
+    color: "red",
+    createdAt: "113213213-32132",
+    updatedAt: "12313",
+  },
+  {
+    id: 1,
+    name: "This is my blueprint",
+    color: "#7D91FF",
+    createdAt: "113213213-32132",
+    updatedAt: "12313",
+  },
+  {
+    id: 2,
+    name: "This is my blueprint 2",
+    color: "red",
+    createdAt: "113213213-32132",
+    updatedAt: "12313",
+  },
+  {
+    id: 1,
+    name: "This is my blueprint",
+    color: "#7D91FF",
+    createdAt: "113213213-32132",
+    updatedAt: "12313",
+  },
+  {
+    id: 2,
+    name: "This is my blueprint 2",
+    color: "red",
+    createdAt: "113213213-32132",
+    updatedAt: "12313",
+  },
+  {
+    id: 1,
+    name: "This is my blueprint",
+    color: "#7D91FF",
+    createdAt: "113213213-32132",
+    updatedAt: "12313",
+  },
+  {
+    id: 2,
+    name: "This is my blueprint 2",
+    color: "red",
+    createdAt: "113213213-32132",
+    updatedAt: "12313",
+  },
+  {
+    id: 1,
+    name: "This is my blueprint",
+    color: "#7D91FF",
+    createdAt: "113213213-32132",
+    updatedAt: "12313",
+  },
+  {
+    id: 2,
+    name: "This is my blueprint 2",
+    color: "red",
+    createdAt: "113213213-32132",
+    updatedAt: "12313",
+  },
   {
     id: 1,
     name: "This is my blueprint",
