b3b20d6257bd20a3b98cb6c139bb679a66f4eb0b
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-08-12 00:14:36 +0200

    Create useGetTreeSuperSchema.ts

---
+++ backend/src/schemas/Workspace/useGetTreeSuperSchema.ts
@@ -0,0 +1,55 @@
+import { number, object, string } from "yup";
+import { SuperSchema } from "types/SuperSchema";
+import { GetTreeControllerTypes } from "types/controllers/Workspace/GetTreeController";
+
+import { Di } from "@enums/Di";
+
+import { setYupError } from "@helpers/yup/setError";
+import { CUSTOM_MESSAGES } from "@helpers/yup/custom-messages";
+
+import { useIdNumberSchema } from "@schemas/common/useIdNumberSchema";
+import { defineSuperSchemaRunner } from "@schemas/common/defineSuperSchemaRunner";
+
+const requireOneOfError = setYupError(
+  CUSTOM_MESSAGES.GENERAL.REQUIRE_ONE_OF,
+  "blueprintId",
+  "relativePath"
+);
+
+const querySchema: SuperSchema.Fragment<GetTreeControllerTypes.v1.Query> =
+  object().shape(
+    {
+      blueprintId: number()
+        .optional()
+        .when("relativePath", {
+          is: (value: string) => !!value,
+          then: schema => schema.typeError(requireOneOfError),
+          otherwise: schema => schema.required(),
+        }),
+
+      relativePath: string()
+        .optional()
+        .when("blueprintId", {
+          is: (value: string) => !!value,
+          then: schema => schema.typeError(requireOneOfError),
+          otherwise: schema => schema.required(),
+        }),
+    },
+    [["blueprintId", "relativePath"]]
+  );
+
+const paramsSchema: SuperSchema.Fragment<GetTreeControllerTypes.v1.Params> =
+  object({
+    id: useIdNumberSchema(Di.WorkspaceRepository),
+  });
+
+export const useGetTreeSuperSchema: SuperSchema.Runner<
+  GetTreeControllerTypes.v1.Params,
+  void,
+  GetTreeControllerTypes.v1.Query
+> = defineSuperSchemaRunner(() => {
+  return {
+    params: paramsSchema,
+    query: querySchema,
+  };
+});
