de3e28aede47907da8ec1bf03aba89e734440ec3
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-08-12 00:18:01 +0200

    Update workspaces.ts

--- backend/src/routes/v1/workspaces.ts
+++ backend/src/routes/v1/workspaces.ts
@@ -4,18 +4,20 @@ import { Permission } from "@shared/types/enums/Permission";
 
 import { processRequestWith } from "@helpers/processRequestWith";
 import { requirePermissions } from "@middleware/requirePermissions";
-import { validateRequestWith } from "@middleware/validateRequestWith";
 import { transformPagination } from "@middleware/transformPagination";
+import { validateRequestWith } from "@middleware/validateRequestWith";
 
 import { useGetBySlugSchema } from "@schemas/Workspace/useGetBySlugSchema";
 import { useStoreSuperSchema } from "@schemas/Workspace/useStoreSuperSchema";
 import { useGetAllSuperSchema } from "@schemas/Workspace/useGetAllSuperSchema";
 import { useUpdateSuperSchema } from "@schemas/Workspace/useUpdateSuperSchema";
+import { useGetTreeSuperSchema } from "@schemas/Workspace/useGetTreeSuperSchema";
 
 import { BaseController } from "@controllers/BaseController";
 import { StoreController } from "@controllers/Workspace/StoreController";
-import { UpdateController } from "@controllers/Workspace/UpdateController";
 import { GetAllController } from "@controllers/Workspace/GetAllController";
+import { UpdateController } from "@controllers/Workspace/UpdateController";
+import { GetTreeController } from "@controllers/Workspace/GetTreeController";
 import { GetBySlugController } from "@controllers/Workspace/GetBySlugController";
 
 const workspacesRouter = Router();
@@ -35,6 +37,12 @@ workspacesRouter.get(
   "/:slug",
   validateRequestWith({ [v1_0]: useGetBySlugSchema }),
   processRequestWith(GetBySlugController)
+);
+
+workspacesRouter.get(
+  "/:id/tree",
+  validateRequestWith({ [v1_0]: useGetTreeSuperSchema }),
+  processRequestWith(GetTreeController)
 );
 
 workspacesRouter.post(
