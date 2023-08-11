de103ae8eb2d589947a660f91334c9473d651a95
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-08-12 00:07:50 +0200

    include getAll queries in DirectoryRepository

--- backend/src/repositories/DirectoryRepository.ts
+++ backend/src/repositories/DirectoryRepository.ts
@@ -1,6 +1,8 @@
-import { Repository } from "typeorm";
 import { injectable } from "tsyringe";
+import { Like, Not, Repository } from "typeorm";
 import { IDirectoryRepository } from "types/repositories/IDirectoryRepository";
+
+import { FILES } from "@config";
 
 import { BaseRepository } from "./BaseRepository";
 
@@ -15,5 +17,38 @@ export class DirectoryRepository
 
   constructor() {
     super(Directory);
+  }
+
+  getAllByRelativePath(workspaceId: number, relativePath: string) {
+    const relativePathQuery =
+      relativePath === FILES.ROOT
+        ? Not(Like(`${FILES.ROOT}/%/%`))
+        : relativePath;
+
+    return this.database.find({
+      where: {
+        relativePath: relativePathQuery,
+        files: {
+          workspace: {
+            id: workspaceId,
+          },
+        },
+      },
+    });
+  }
+
+  getAllByBlueprintId(workspaceId: number, blueprintId: number) {
+    return this.database.find({
+      where: {
+        files: {
+          workspace: {
+            id: workspaceId,
+            blueprints: {
+              id: blueprintId,
+            },
+          },
+        },
+      },
+    });
   }
 }
