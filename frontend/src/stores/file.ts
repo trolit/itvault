11ad41f21cd9a05194b3fbf411ee5aada6d5b733
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-08-11 12:38:52 +0200

    init files store

---
+++ frontend/src/stores/file.ts
@@ -0,0 +1,50 @@
+import axios from "axios";
+import { defineStore } from "pinia";
+
+import { useWorkspacesStore } from "./workspace";
+import type { IFileDto } from "@shared/types/dtos/IFileDto";
+
+interface IState {
+  items: IFileDto[];
+}
+
+export const useFilesStore = defineStore("files", {
+  state: (): IState => ({
+    items: [],
+  }),
+
+  actions: {
+    async getAllBy(query: { blueprintId?: number; relativePath?: string }) {
+      const { blueprintId, relativePath } = query;
+
+      if ((blueprintId && relativePath) || (!blueprintId && !relativePath)) {
+        console.error("Use one of query options!");
+
+        return;
+      }
+
+      const workspacesStore = useWorkspacesStore();
+
+      const params = {
+        version: 1,
+        workspaceId: workspacesStore.activeItem.id,
+      };
+
+      if (blueprintId) {
+        (params as any)["blueprintId"] = blueprintId;
+      }
+
+      if (relativePath) {
+        (params as any)["relativePath"] = blueprintId;
+      }
+
+      const { data } = await axios.get<IFileDto[]>("v1/blueprints", {
+        params,
+      });
+
+      this.items = Array.prototype.concat(this.items, data);
+
+      return data;
+    },
+  },
+});
