b4928166a7ff1c53727424490e046383fa8cfbc4
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-08-11 10:54:19 +0200

    Create AddEditBlueprintDrawer.vue

---
+++ frontend/src/components/workspace/Sider/AddEditBlueprintDrawer.vue
@@ -0,0 +1,27 @@
+<template>
+  <n-drawer
+    :show="props.isActive"
+    :width="502"
+    placement="right"
+    :show-mask="false"
+    :mask-closable="false"
+    @update:show="$emit('update:is-active', false)"
+  >
+    <n-drawer-content title="Stoner" closable>
+      This is add-edit-blueprint drawer
+    </n-drawer-content>
+  </n-drawer>
+</template>
+
+<script setup lang="ts">
+import { NDrawer, NDrawerContent } from "naive-ui";
+
+const props = defineProps({
+  isActive: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+defineEmits(["update:is-active"]);
+</script>
