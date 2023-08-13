4f9134377d4ada9b9bf485b9f333a06f238f5d7e
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-08-13 13:30:32 +0200

    update (common) empty section

--- frontend/src/components/common/Empty.vue
+++ frontend/src/components/common/Empty.vue
@@ -1,17 +1,21 @@
 <template>
-  <n-empty :description="props.title">
-    <template #icon>
-      <n-icon :component="props.icon" />
-    </template>
+  <n-card class="empty-section">
+    <div class="content">
+      <n-icon :size="70" :component="props.icon" />
 
-    <template #extra>
-      <slot name="extra" />
-    </template>
-  </n-empty>
+      <div class="title">
+        {{ props.title }}
+      </div>
+
+      <div class="extra">
+        <slot name="extra" />
+      </div>
+    </div>
+  </n-card>
 </template>
 
 <script setup lang="ts">
-import { NEmpty, NIcon } from "naive-ui";
+import { NIcon, NCard } from "naive-ui";
 import { Information as DefaultIcon } from "@vicons/carbon";
 
 const props = defineProps({
