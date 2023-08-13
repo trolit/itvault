e4de878bedc7a764625de17fd6e66b58b724c9a1
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-08-13 16:02:46 +0200

    init (active) main content item switcher

--- frontend/src/store/files.ts
+++ frontend/src/store/files.ts
@@ -1,11 +1,27 @@
 import { defineStore } from "pinia";
 
+import { useBundlesStore } from "./bundles";
+import type { IFileDto } from "@shared/types/dtos/IFileDto";
+
 interface IState {
   ROOT: string;
+
+  activeItem: IFileDto | null;
 }
 
 export const useFilesStore = defineStore("files", {
   state: (): IState => ({
     ROOT: ".",
+    activeItem: null,
   }),
+
+  actions: {
+    setActiveItem(activeItem: IFileDto) {
+      this.activeItem = activeItem;
+
+      const bundlesStore = useBundlesStore();
+
+      bundlesStore.activeItem = null;
+    },
+  },
 });
