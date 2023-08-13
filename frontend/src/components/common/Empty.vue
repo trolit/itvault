1a09604b8be1cf5822425907afbc7f6cab25cf17
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-08-13 12:38:19 +0200

    Create Empty.vue

---
+++ frontend/src/components/common/Empty.vue
@@ -0,0 +1,29 @@
+<template>
+  <n-empty :description="props.title">
+    <template #icon>
+      <n-icon :component="props.icon" />
+    </template>
+
+    <template #extra>
+      <slot name="extra" />
+    </template>
+  </n-empty>
+</template>
+
+<script setup lang="ts">
+import { NEmpty, NIcon } from "naive-ui";
+import { Information as DefaultIcon } from "@vicons/carbon";
+
+const props = defineProps({
+  icon: {
+    type: Object,
+    required: false,
+    default: DefaultIcon,
+  },
+
+  title: {
+    type: String,
+    required: true,
+  },
+});
+</script>
