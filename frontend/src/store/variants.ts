63278060b04522a950c52ff74f2fa65a1099b6d6
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-08-13 17:04:16 +0200

    add `getAll` to `variants` store

--- frontend/src/store/variants.ts
+++ frontend/src/store/variants.ts
@@ -1,7 +1,37 @@
+import axios from "axios";
 import { defineStore } from "pinia";
 
-interface IState {}
+import { useFilesStore } from "./files";
+import { useWorkspacesStore } from "./workspaces";
+import type { IVariantDto } from "@shared/types/dtos/IVariantDto";
+
+interface IState {
+  items: IVariantDto[];
+}
 
 export const useVariantsStore = defineStore("variants", {
-  state: (): IState => ({}),
+  state: (): IState => ({
+    items: [],
+  }),
+
+  actions: {
+    async getAll() {
+      const filesStore = useFilesStore();
+      const workspacesStore = useWorkspacesStore();
+
+      const params = {
+        version: 1,
+        fileId: filesStore?.activeItem?.id,
+        workspaceId: workspacesStore?.activeItem?.id,
+      };
+
+      const { data } = await axios.get<IVariantDto[]>("v1/variants", {
+        params,
+      });
+
+      this.items = data;
+
+      return data;
+    },
+  },
 });
