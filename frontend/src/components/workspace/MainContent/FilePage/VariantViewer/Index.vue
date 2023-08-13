e5a32b0b6e121164fae88397c1230bff7ae84e1e
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-08-13 18:50:23 +0200

    init line counter

--- frontend/src/components/workspace/MainContent/FilePage/VariantViewer/Index.vue
+++ frontend/src/components/workspace/MainContent/FilePage/VariantViewer/Index.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="variant-viewer">aha</div>
+  <div class="variant-viewer">
+    <div class="line-numbers">
+      <span v-for="index in numberOfLines" :key="index"></span>
+    </div>
+
+    <text-to-render />
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { h, ref, computed } from "vue";
+
+const text = ref("test1 test2\ntest3\nelo");
+
+const numberOfLines = computed((): number => {
+  return text.value.split("\n").length;
+});
+
+const textToRender = h(
+  "div", // type
+  { id: "foo", class: "bar", innerHTML: text }, // props
+  []
+);
+</script>
