4974a51af4761d62c80fa5a7c84bb1a24b52d594
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-08-13 17:10:13 +0200

    Update CardHeader.vue

--- frontend/src/components/workspace/MainContent/FilePage/CardHeader.vue
+++ frontend/src/components/workspace/MainContent/FilePage/CardHeader.vue
@@ -2,7 +2,7 @@
   <!-- @TODO overflow: auto for timeline! -->
   <div class="card-header">
     <div class="timeline-wrapper" style="overflow: auto">
-      <n-timeline horizontal>
+      <n-timeline v-if="!isLoading" horizontal>
         <n-timeline-item>
           <template #default>
             <n-button>v1</n-button>
@@ -21,6 +21,8 @@
           </template>
         </n-timeline-item>
       </n-timeline>
+
+      <n-spin v-else size="medium" />
     </div>
 
     <div class="actions">
@@ -30,6 +32,28 @@
 </template>
 
 <script setup lang="ts">
+import { onBeforeMount, ref } from "vue";
 import { Add as AddIcon } from "@vicons/carbon";
-import { NButton, NTimeline, NTimelineItem, NIcon } from "naive-ui";
+import { NButton, NTimeline, NTimelineItem, NIcon, NSpin } from "naive-ui";
+
+import { useVariantsStore } from "@/store/variants";
+
+const isLoading = ref(false);
+const variantsStore = useVariantsStore();
+
+onBeforeMount(async () => {
+  await fetchVariants();
+});
+
+async function fetchVariants() {
+  isLoading.value = true;
+
+  try {
+    await variantsStore.getAll();
+  } catch (error) {
+    console.log(error);
+  } finally {
+    isLoading.value = false;
+  }
+}
 </script>
