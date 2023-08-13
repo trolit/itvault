8b52ff72dd7406fd8840410ca7e72eefa3a0c29c
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-08-13 16:10:07 +0200

    init `file-page`

---
+++ frontend/src/components/workspace/MainContent/FilePage/Index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="file-page">
+    <!-- @TODO variant header section -->
+
+    <!-- @TODO notes drawer -->
+
+    <!-- @TODO variant content READ + MANAGE -->
+  </div>
+</template>
