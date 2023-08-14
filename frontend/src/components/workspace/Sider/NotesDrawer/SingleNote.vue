8cfaea02671ef7c8b8080c73fb40a322952a20ce
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-08-14 19:20:25 +0200

    Create SingleNote.vue

---
+++ frontend/src/components/workspace/Sider/NotesDrawer/SingleNote.vue
@@ -0,0 +1,17 @@
+<template>
+  <n-thing content-indented>
+    <template #header> Money </template>
+
+    <template #header-extra> date </template>
+
+    <template #description> Description </template>
+
+    Money is any item or verifiable record that is generally accepted as payment
+    for goods and services and repayment of debts, such as taxes, in a
+    particular country or socio-economic context.
+  </n-thing>
+</template>
+
+<script setup lang="ts">
+import { NThing } from "naive-ui";
+</script>
