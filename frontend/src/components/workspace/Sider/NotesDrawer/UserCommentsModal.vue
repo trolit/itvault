5bcc9836cbdd8e80bae77ff3d74ce690e082209f
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-08-14 21:32:59 +0200

    Create UserCommentsModal.vue

---
+++ frontend/src/components/workspace/Sider/NotesDrawer/UserCommentsModal.vue
@@ -0,0 +1,24 @@
+<template>
+  <n-modal :title="title" preset="dialog">
+    <div>NOTES - TBA</div>
+  </n-modal>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import { NModal } from "naive-ui";
+
+const props = defineProps({
+  id: {
+    type: Number,
+    required: true,
+  },
+
+  fullName: {
+    type: String,
+    required: true,
+  },
+});
+
+const title = computed(() => `${props.fullName}'s notes`);
+</script>
