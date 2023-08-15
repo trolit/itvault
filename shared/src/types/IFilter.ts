ad240bda3072d3e81c1cdd2c4633a90a36f1d0f7
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-08-15 13:08:08 +0200

    add `IFilter` and `IFiltersQuery`

---
+++ shared/src/types/IFilter.ts
@@ -0,0 +1,7 @@
+export type IFilter = {
+  field: string;
+
+  // @TODO type (e.g. not)
+
+  value: string;
+};
---
+++ shared/src/types/IFiltersQuery.ts
@@ -0,0 +1,4 @@
+import { IFilter } from "./IFilter";
+
+// {} or [ {}, {} ]
+export type IFiltersQuery = IFilter[] | IFilter;
