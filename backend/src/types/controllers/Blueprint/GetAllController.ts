cfb8a7891fb270b321202529f049ff28c61e66e4
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-08-15 13:15:22 +0200

    Update GetAllController.ts

--- backend/src/types/controllers/Blueprint/GetAllController.ts
+++ backend/src/types/controllers/Blueprint/GetAllController.ts
@@ -1,14 +1,17 @@
+import { Blueprint } from "@entities/Blueprint";
 import { BlueprintMapper } from "@mappers/BlueprintMapper";
 import { WorkspaceId } from "types/controllers/WorkspaceId";
 import { IPaginationQuery } from "@shared/types/IPaginationQuery";
 import { IPaginationOptions } from "types/IPaginationOptions";
 import { PaginatedResponse } from "@shared/types/PaginatedResponse";
+import { TransformedQueryFilters } from "../TransformedQueryFilters";
 
 export namespace GetAllControllerTypes {
   export namespace v1 {
     export type QueryInput = WorkspaceId & IPaginationQuery;
 
-    export type QueryOutput = WorkspaceId & IPaginationOptions;
+    export type QueryOutput = WorkspaceId &
+      IPaginationOptions & { filters: TransformedQueryFilters<Blueprint> };
 
     export type Request = CustomRequest<undefined, undefined, QueryOutput>;
 
