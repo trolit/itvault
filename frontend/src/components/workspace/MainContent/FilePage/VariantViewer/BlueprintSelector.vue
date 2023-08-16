16b0f4f8d6dc5530036948f7be5054b03a1d2428
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-08-16 17:09:39 +0200

    init BlueprintSelector

---
+++ frontend/src/components/workspace/MainContent/FilePage/VariantViewer/BlueprintSelector.vue
@@ -0,0 +1,18 @@
+<template>
+  <n-popselect v-model:value="value" :options="options">
+    <n-button size="small">pick blueprint</n-button>
+  </n-popselect>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { NButton, NPopselect } from "naive-ui";
+
+const value = ref("");
+const options = [
+  {
+    label: "e",
+    value: "a",
+  },
+];
+</script>
