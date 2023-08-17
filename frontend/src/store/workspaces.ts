b6ae7c9c1d0b84b2472a7d92108acc6d8da974b9
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-08-17 17:26:21 +0200

    define tabs open/close file/variant tabs in workspace store

--- frontend/src/store/workspaces.ts
+++ frontend/src/store/workspaces.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { defineStore } from "pinia";
 
+import type { VariantTab } from "@/types/VariantTab";
+import type { INoteDto } from "@shared/types/dtos/INoteDto";
 import type { IFileDto } from "@shared/types/dtos/IFileDto";
 import type { IDirectoryDto } from "@shared/types/dtos/IDirectoryDto";
 import type { IWorkspaceDto } from "@shared/types/dtos/IWorkspaceDto";
@@ -9,12 +11,19 @@ import type { PaginatedResponse } from "@shared/types/PaginatedResponse";
 
 interface IState {
   total: number;
-
   items: IWorkspaceDto[];
-
   activeItem: IWorkspaceDto;
-
   tree: (IDirectoryDto & IFileDto)[];
+
+  // @TODO REFACTOR!!!!
+
+  activeFileTab: number; // @NOTE id of file
+  tabs: {
+    file: IFileDto;
+    activeVariantTab: string; // @NOTE id of variant
+    notes: { data: INoteDto[]; total: number };
+    variantTabs: VariantTab[];
+  }[];
 }
 
 export const useWorkspacesStore = defineStore("workspaces", {
@@ -23,11 +32,97 @@ export const useWorkspacesStore = defineStore("workspaces", {
     total: 0,
     items: [],
     activeItem: { id: 0, name: "", slug: "", tags: [] },
+    activeFileTab: 0,
+    tabs: [],
   }),
+
+  getters: {
+    activeFileTabValue: state =>
+      state.tabs.find(tab => tab.file.id === state.activeFileTab),
+    activeVariantTabValue(): VariantTab | undefined {
+      const fileTab = this.activeFileTabValue;
+
+      if (!fileTab) {
+        return;
+      }
+
+      return fileTab.variantTabs.find(
+        variantTab => variantTab.variant.id === fileTab.activeVariantTab
+      );
+    },
+  },
 
   actions: {
     setActiveItem(item: IWorkspaceDto) {
       this.activeItem = item;
+    },
+
+    setFileTab(file: IFileDto) {
+      const tab = this.tabs.find(tab => tab.file.id === file.id);
+
+      this.activeFileTab = file.id;
+
+      if (tab) {
+        return;
+      }
+
+      this.tabs.push({
+        file,
+        variantTabs: [],
+        activeVariantTab: "",
+        notes: { data: [], total: 0 },
+      });
+    },
+
+    closeFileTab(id: number) {
+      // @NOTE consider adding "isHidden" flag (like with variant tab)
+      const tabIndex = this.tabs.findIndex(tab => tab.file.id === id);
+
+      if (~tabIndex) {
+        this.tabs.splice(tabIndex, 1);
+
+        if (this.tabs.length) {
+          this.activeFileTab = this.tabs[0].file.id;
+        }
+      }
+    },
+
+    setVariantTab(id: string) {
+      if (!this.activeFileTabValue) {
+        return;
+      }
+
+      const variantTab = this.activeFileTabValue.variantTabs.find(
+        variantTab => variantTab.variant.id === id
+      );
+
+      if (!variantTab) {
+        return;
+      }
+
+      this.activeFileTabValue.activeVariantTab = id;
+
+      variantTab.isVisible = false;
+    },
+
+    closeVariantTab(id: string) {
+      if (!this.activeFileTabValue) {
+        return;
+      }
+
+      const variantTab = this.activeFileTabValue.variantTabs.find(
+        variantTab => variantTab.variant.id === id
+      );
+
+      if (!variantTab) {
+        return;
+      }
+
+      if (this.activeFileTabValue.activeVariantTab === id) {
+        this.activeFileTabValue.activeVariantTab = "";
+      }
+
+      variantTab.isVisible = false;
     },
 
     async getBySlug(slug: string) {
