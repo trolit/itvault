342fbe2c4e42143479eba86284a96a2aef09382e
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-08-18 11:12:46 +0200

    add header & scrollbar to `BundlesTab`

--- frontend/src/components/workspace/Sider/BundlesTab.vue
+++ frontend/src/components/workspace/Sider/BundlesTab.vue
@@ -1,3 +1,40 @@
 <template>
-  <div class="bundles-tab">xd</div>
+  <div class="bundles-tab">
+    <div class="header">
+      <n-button type="info" size="small" dashed>
+        <n-icon :component="ResetIcon" :size="20" />
+      </n-button>
+
+      <!-- @TODO create common component -->
+      <n-input clearable show-count placeholder="Type note or status">
+        <template #prefix>
+          <n-icon :component="SearchIcon" />
+        </template>
+      </n-input>
+
+      <n-button type="info" size="small" dashed>
+        <n-icon :component="AddIcon" :size="25" />
+      </n-button>
+    </div>
+
+    <n-scrollbar>
+      <div v-if="!isLoading">@TBA</div>
+
+      <div v-else class="spinner">
+        <n-spin />
+      </div>
+    </n-scrollbar>
+  </div>
 </template>
+
+<script setup lang="ts">
+import {
+  Add as AddIcon,
+  Reset as ResetIcon,
+  Search as SearchIcon,
+} from "@vicons/carbon";
+import { ref } from "vue";
+import { NIcon, NInput, NButton, NSpin, NScrollbar } from "naive-ui";
+
+const isLoading = ref(false);
+</script>
