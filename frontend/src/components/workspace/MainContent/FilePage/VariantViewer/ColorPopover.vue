be92b95a988f3fff8f9d839d5358806919f715d2
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-08-18 10:13:10 +0200

    Create ColorPopover.vue

---
+++ frontend/src/components/workspace/MainContent/FilePage/VariantViewer/ColorPopover.vue
@@ -0,0 +1,32 @@
+<template>
+  <n-popover trigger="click" placement="bottom">
+    <template #trigger>
+      <span :style="{ backgroundColor: props.color, padding: '3px 0' }">
+        {{ props.part.text }}
+      </span>
+    </template>
+
+    <n-button size="small" type="error" ghost>
+      <n-icon :component="DeleteIcon" />
+    </n-button>
+  </n-popover>
+</template>
+
+<script setup lang="ts">
+import { NPopover, NButton, NIcon } from "naive-ui";
+import { Delete as DeleteIcon } from "@vicons/carbon";
+import type { PropType } from "vue";
+import type { LinePart } from "@/types/LinePart";
+
+const props = defineProps({
+  color: {
+    type: String,
+    required: true,
+  },
+
+  part: {
+    type: Object as PropType<LinePart>,
+    required: true,
+  },
+});
+</script>
