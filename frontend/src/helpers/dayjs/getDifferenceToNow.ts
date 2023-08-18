5733c037b96359ef319835e2d657eec1b83e922e
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-08-19 00:36:32 +0200

    Update getDifferenceToNow.ts

--- frontend/src/helpers/dayjs/getDifferenceToNow.ts
+++ frontend/src/helpers/dayjs/getDifferenceToNow.ts
@@ -1,8 +1,10 @@
 import dayjs, { type UnitTypeLong } from "dayjs";
 
 export default (date: string, unit: UnitTypeLong = "millisecond") => {
-  const parsedDate = dayjs(date);
   const now = dayjs();
+  const parsedDate = dayjs(date);
 
-  return Math.abs(now.diff(parsedDate, unit));
+  const difference = parsedDate.diff(now, unit);
+
+  return difference < 0 ? 0 : difference;
 };
