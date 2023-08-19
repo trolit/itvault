c6708229238fbe51b79f40286b51865b7cfa3c8a
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-08-19 22:32:11 +0200

    create `GetByIdController` (File)

---
+++ backend/src/controllers/File/GetByIdController.ts
@@ -0,0 +1,59 @@
+import { inject, injectable } from "tsyringe";
+import { FileMapper } from "@mappers/FileMapper";
+import { StatusCodes as HTTP } from "http-status-codes";
+import { IFileRepository } from "types/repositories/IFileRepository";
+import { GetByIdControllerTypes } from "types/controllers/File/GetByIdController";
+import { ControllerImplementation } from "types/controllers/ControllerImplementation";
+
+import { Di } from "@enums/Di";
+import { File } from "@entities/File";
+
+import { BaseController } from "@controllers/BaseController";
+
+const { v1_0 } = BaseController.ALL_VERSION_DEFINITIONS;
+
+@injectable()
+export class GetByIdController extends BaseController {
+  constructor(
+    @inject(Di.FileRepository)
+    private _fileRepository: IFileRepository
+  ) {
+    super();
+  }
+
+  implementations: ControllerImplementation[] = [
+    {
+      version: v1_0,
+      handle: this.v1.bind(this),
+    },
+  ];
+
+  static ALL_VERSIONS = [v1_0];
+
+  async v1(
+    request: GetByIdControllerTypes.v1.Request,
+    response: GetByIdControllerTypes.v1.Response
+  ) {
+    const {
+      params: { id },
+      query: { workspaceId },
+    } = request;
+
+    const file = await this._fileRepository.getOne({
+      where: {
+        id,
+        workspace: {
+          id: workspaceId,
+        },
+      },
+    });
+
+    if (!file) {
+      return response.status(HTTP.NOT_FOUND).send();
+    }
+
+    const mappedResult = this.mapper.map<File>(file).to(FileMapper);
+
+    return this.finalizeRequest(response, HTTP.OK, mappedResult);
+  }
+}
