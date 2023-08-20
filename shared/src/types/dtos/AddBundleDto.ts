91cce6e2ea7ed96606461067c1b83439672d2560
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-08-20 13:01:14 +0200

    refactor "AddBundleDto" to include all properties

--- shared/src/types/dtos/AddBundleDto.ts
+++ shared/src/types/dtos/AddBundleDto.ts
@@ -1,5 +1,15 @@
-export type AddBundleDto = {
+import { BundleExpire } from "../enums/BundleExpire";
+
+export type Value = {
   blueprintId: number;
 
   variantIds: string[];
+};
+
+export type AddBundleDto = {
+  note?: string;
+
+  values: Value[];
+
+  expiration: BundleExpire;
 };
