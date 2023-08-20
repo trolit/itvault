7540e9f00e8541a1292a5b7f6f928f816959a919
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-08-20 09:58:14 +0200

    init step related components

---
+++ frontend/src/components/workspace/Sider/BundlesTab/CreateBundleModal/BlueprintsSelectionStep.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>Blueprints (selection)</div>
+</template>
+
+<script setup lang="ts"></script>
---
+++ frontend/src/components/workspace/Sider/BundlesTab/CreateBundleModal/VariantsSelectionStep.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>Variants (selection)</div>
+</template>
+
+<script setup lang="ts"></script>
