d98d0e6985e9fe91cf48faf94bdfc87b1c91e3db
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-08-20 10:23:52 +0200

    Update Index.vue

--- frontend/src/components/workspace/Sider/BundlesTab/CreateBundleModal/Index.vue
+++ frontend/src/components/workspace/Sider/BundlesTab/CreateBundleModal/Index.vue
@@ -1,9 +1,69 @@
 <template>
-  <n-modal preset="card">
-    <div>NOTES - TBA</div>
+  <n-modal
+    preset="card"
+    title="Generate Bundle"
+    :mask-closable="false"
+    class="create-bundle-modal"
+  >
+    <n-space vertical class="steps-wrapper">
+      <!-- @TODO implement status (to cover validation) -->
+      <n-steps :current="current">
+        <n-step
+          v-for="(step, index) in steps"
+          :key="index"
+          :title="step.title"
+          :description="step.description"
+        />
+      </n-steps>
+    </n-space>
+
+    <n-scrollbar class="content-scrollbar">
+      <component :is="stepValue" />
+    </n-scrollbar>
+
+    <div class="actions">
+      <n-button @click="current--" :disabled="current === 1">
+        Previous
+      </n-button>
+
+      <n-button @click="current++" :disabled="current === steps.length">
+        Next
+      </n-button>
+    </div>
   </n-modal>
 </template>
 
 <script setup lang="ts">
-import { NModal } from "naive-ui";
+import { ref, computed } from "vue";
+import { NModal, NSpace, NSteps, NStep, NButton, NScrollbar } from "naive-ui";
+
+import FormStep from "./FormStep.vue";
+import VariantsSelectionStep from "./VariantsSelectionStep.vue";
+import BlueprintsSelectionStep from "./BlueprintsSelectionStep.vue";
+
+const current = ref(1);
+
+const steps = [
+  {
+    title: "Blueprints",
+    description: "Select blueprints that you would like to include in bundle.",
+    value: BlueprintsSelectionStep,
+  },
+
+  {
+    title: "Variants",
+    description: "Pick file variants.",
+    value: VariantsSelectionStep,
+  },
+
+  {
+    title: "Form",
+    description: "Complete basic information about bundle.",
+    value: FormStep,
+  },
+];
+
+const stepValue = computed(
+  () => steps.find((step, index) => index + 1 === current.value)?.value
+);
 </script>
