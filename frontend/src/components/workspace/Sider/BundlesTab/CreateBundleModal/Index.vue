9981eeebd8b86f1ae95b7429d87cf5cd6ddac236
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-08-27 13:13:55 +0200

    Update Index.vue

--- frontend/src/components/workspace/Sider/BundlesTab/CreateBundleModal/Index.vue
+++ frontend/src/components/workspace/Sider/BundlesTab/CreateBundleModal/Index.vue
@@ -20,10 +20,8 @@
 
     <component
       :is="currentStep.value"
-      :form-data="formData"
       v-on="currentStep.events"
       v-bind="currentStep.props"
-      @update:form-data="onFormDataUpdate"
     />
 
     <div class="actions">
@@ -43,34 +41,33 @@
 </template>
 
 <script setup lang="ts">
-import cloneDeep from "lodash/cloneDeep";
 import { ref, computed, type Ref } from "vue";
 import { NModal, NSpace, NSteps, NStep, NButton } from "naive-ui";
 
 import FormStep from "./FormStep.vue";
 import { useMessage } from "naive-ui";
 import { useBundlesStore } from "@/store/bundles";
+import type { BundleModalItem } from "@/types/BundleModalItem";
 import { BundleExpire } from "@shared/types/enums/BundleExpire";
 import VariantsSelectionStep from "./VariantsSelectionStep.vue";
 import BlueprintsSelectionStep from "./BlueprintsSelectionStep.vue";
-import type { AddBundleDto } from "@shared/types/dtos/AddBundleDto";
 import type { IBlueprintDto } from "@shared/types/dtos/IBlueprintDto";
 import type { IFileVariantDto } from "@shared/types/dtos/IFileVariantDto";
-
-const defaultFormData: AddBundleDto = {
-  values: [],
-  expiration: BundleExpire.OneDay,
-};
+import type { Value } from "@shared/types/dtos/AddBundleDto";
 
 const emit = defineEmits(["update:show"]);
 
+const note = ref("");
 const current = ref(1);
 const message = useMessage();
 const isSubmittingForm = ref(false);
 const bundlesStore = useBundlesStore();
-const files: Ref<IFileVariantDto[][]> = ref([]);
-const selectedBlueprints: Ref<IBlueprintDto[]> = ref([]);
-const formData: Ref<AddBundleDto> = ref(cloneDeep(defaultFormData));
+const expiration = ref(BundleExpire.OneDay);
+const items: Ref<BundleModalItem[]> = ref([]);
+
+const selectedBlueprints = computed(() =>
+  items.value.map(({ blueprint }) => blueprint)
+);
 
 const steps = [
   {
@@ -78,6 +75,7 @@ const steps = [
     description: "Choose blueprint(s) that bundle should include.",
     value: BlueprintsSelectionStep,
     props: {
+      items: items.value,
       selectedBlueprints: selectedBlueprints.value,
     },
     events: {
@@ -92,24 +90,27 @@ const steps = [
     description: "Preview file variants and adjust (if needed).",
     value: VariantsSelectionStep,
     props: {
-      files: files.value,
+      items: items.value,
       selectedBlueprints: selectedBlueprints.value,
     },
     events: {
       "add-files": onFilesAdd,
     },
-    isNextButtonDisabled: () =>
-      selectedBlueprints.value.some(
-        (blueprint, index) => files.value[index] === undefined
-      ),
+    isNextButtonDisabled: () => items.value.some(item => !item.files.length),
   },
 
   {
     title: "Information",
     description: "Provide basic information about bundle.",
     value: FormStep,
-    props: {},
-    events: {},
+    props: {
+      note: note.value,
+      expiration: expiration.value,
+    },
+    events: {
+      "update:note": (value: string) => (note.value = value),
+      "update:expiration": (value: BundleExpire) => (expiration.value = value),
+    },
     isNextButtonDisabled: () => current.value === 3,
   },
 ];
@@ -123,72 +124,57 @@ const currentStep = computed(
 const isFinalStep = computed(() => current.value === steps.length);
 
 function onBlueprintSelect(blueprintToAdd: IBlueprintDto) {
-  const blueprintIndex = selectedBlueprints.value.findIndex(
-    element => element.id === blueprintToAdd.id
+  const index = items.value.findIndex(
+    element => element.blueprint.id === blueprintToAdd.id
   );
 
-  if (~blueprintIndex) {
-    onBlueprintDeselect(blueprintToAdd.id);
+  if (~index) {
+    onBlueprintDeselect(blueprintToAdd);
 
     return;
   }
 
-  selectedBlueprints.value.push(blueprintToAdd);
-
-  formData.value.values.push({
-    blueprintId: blueprintToAdd.id,
-    variantIds: [],
-  });
+  items.value.push({ blueprint: blueprintToAdd, files: [] });
 }
 
-function onBlueprintDeselect(id: number) {
-  const blueprintIndex = selectedBlueprints.value.findIndex(
-    element => element.id === id
+function onBlueprintDeselect(blueprintToDeselect: IBlueprintDto) {
+  const index = items.value.findIndex(
+    element => element.blueprint.id === blueprintToDeselect.id
   );
 
-  if (~blueprintIndex) {
-    files.value.splice(blueprintIndex, 1);
-
-    const formDataIndex = formData.value.values.findIndex(
-      value => value.blueprintId === id
-    );
-
-    if (~formDataIndex) {
-      formData.value.values.splice(formDataIndex, 1);
-    }
-
-    selectedBlueprints.value.splice(blueprintIndex, 1);
-
-    return;
+  if (~index) {
+    items.value.splice(index, 1);
   }
 }
 
 function onFilesAdd(blueprintId: number, filesToAdd: IFileVariantDto[]) {
-  files.value.push(filesToAdd);
+  const item = items.value.find(item => item.blueprint.id === blueprintId);
 
-  const formDataValue = formData.value.values.find(
-    value => value.blueprintId === blueprintId
-  );
-
-  // @NOTE set default variantIds
-  if (formDataValue) {
-    for (const file of filesToAdd) {
-      const [firstVariant] = file.variants;
-
-      formDataValue.variantIds.push(firstVariant.id);
-    }
+  if (!item) {
+    return;
   }
-}
 
-function onFormDataUpdate(value: AddBundleDto) {
-  formData.value = { ...value };
+  item.files = filesToAdd.map(fileToAdd => {
+    const [firstVariant] = fileToAdd.variants;
+
+    return { ...fileToAdd, selectedVariantId: firstVariant.id };
+  });
 }
 
 async function onSubmit() {
   isSubmittingForm.value = true;
 
+  const values: Value[] = items.value.map(item => ({
+    blueprintId: item.blueprint.id,
+    variantIds: item.files.map(file => file.selectedVariantId),
+  }));
+
   try {
-    await bundlesStore.store(formData.value);
+    await bundlesStore.store({
+      values,
+      note: note.value,
+      expiration: expiration.value,
+    });
 
     message.success("Bundle successfully queued.");
 
