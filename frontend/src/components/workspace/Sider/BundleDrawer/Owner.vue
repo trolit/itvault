a710a2c802edb62ac508c0c061f35658f9a8bdac
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-09-02 00:25:41 +0200

    if "createdBy" is empty, it means that logged user created bundle

--- frontend/src/components/workspace/Sider/BundleDrawer/Owner.vue
+++ frontend/src/components/workspace/Sider/BundleDrawer/Owner.vue
@@ -1,12 +1,12 @@
 <template>
-  <n-thing v-if="bundle.createdBy">
+  <n-thing>
     <template #avatar>
       <n-avatar>
         <n-icon :component="UserAvatarIcon" />
       </n-avatar>
     </template>
 
-    <template #header> {{ bundle.createdBy.fullName }} </template>
+    <template #header> {{ fullName }} </template>
 
     <template #description>
       created this bundle on
@@ -16,17 +16,28 @@
 </template>
 
 <script setup lang="ts">
+import { computed, type PropType } from "vue";
 import { NThing, NIcon, NAvatar } from "naive-ui";
 import { UserAvatar as UserAvatarIcon } from "@vicons/carbon";
 
+import { useAuthStore } from "@/store/auth";
 import formatDate from "@/helpers/dayjs/formatDate";
-import type { PropType } from "vue";
 import type { IBundleDto } from "@shared/types/dtos/IBundleDto";
 
-defineProps({
+const props = defineProps({
   bundle: {
     type: Object as PropType<IBundleDto>,
     required: true,
   },
+});
+
+const authStore = useAuthStore();
+
+const fullName = computed(() => {
+  if (props.bundle.createdBy) {
+    return props.bundle.createdBy.fullName;
+  }
+
+  return authStore.profile.fullName;
 });
 </script>
