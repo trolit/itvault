98a91b16969f006a6955d9f4b4a9429fc0969c6b
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-09-07 13:46:36 +0200

    Update themes.ts

--- frontend/src/assets/constants/themes.ts
+++ frontend/src/assets/constants/themes.ts
@@ -1,3 +1,5 @@
 export const THEME_DARK = "Dark";
 export const THEME_LIGHT = "Light";
 export const THEME_DARK_DIMMED = "Dark dimmed";
+
+export const ALL_THEMES = [THEME_LIGHT, THEME_DARK, THEME_DARK_DIMMED];
