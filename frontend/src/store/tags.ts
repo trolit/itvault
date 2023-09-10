1686a5a432a5c4f4472b0c168aa3588c7265346d
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-09-10 12:45:08 +0200

    Update tags.ts

--- frontend/src/store/tags.ts
+++ frontend/src/store/tags.ts
@@ -1,10 +1,38 @@
 import axios from "axios";
 import { defineStore } from "pinia";
 
-interface IState {}
+import type { ITagDto } from "@shared/types/dtos/ITagDto";
+import type { PaginatedResponse } from "@shared/types/PaginatedResponse";
+
+interface IState {
+  items: ITagDto[];
+}
 
 export const useTagsStore = defineStore("tags", {
-  state: (): IState => ({}),
+  state: (): IState => ({
+    items: [],
+  }),
 
-  actions: {},
+  actions: {
+    async getBySearch(search: string) {
+      if (!search) {
+        return;
+      }
+
+      const params = {
+        version: 1,
+        search,
+        page: 1,
+        perPage: 6,
+      };
+
+      const { data } = await axios.get<PaginatedResponse<ITagDto>>("v1/tags", {
+        params,
+      });
+
+      this.items = data.result;
+
+      return data;
+    },
+  },
 });
