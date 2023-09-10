9f214c3578b183c754705d92ab094f0e5c964694
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-09-10 16:33:38 +0200

    Update useDateService.ts

--- frontend/src/services/useDateService.ts
+++ frontend/src/services/useDateService.ts
@@ -1,13 +1,19 @@
-import dayjs, { type UnitTypeLong } from "dayjs";
+import dayOfYear from "dayjs/plugin/dayOfYear";
 import relativeTime from "dayjs/plugin/relativeTime";
+import dayjs, { type UnitTypeLong, type UnitType, Dayjs } from "dayjs";
 
+dayjs.extend(dayOfYear);
 dayjs.extend(relativeTime);
 
 export const useDateService = () => ({
-  format: (date: string, template = "YYYY-MM-DD") =>
+  format: (date: string | Dayjs, template = "YYYY-MM-DD") =>
     dayjs(date).format(template),
 
+  dayOfYear: (date: string | Dayjs) => dayjs(date).dayOfYear(),
+
   now: () => dayjs(),
+
+  endOf: (unit: UnitType) => dayjs().endOf(unit),
 
   fromNow: (date: string) => dayjs(date).fromNow(),
 
@@ -20,5 +26,13 @@ export const useDateService = () => ({
     const difference = parsedDate.diff(now, unit);
 
     return difference < 0 ? 0 : difference;
+  },
+
+  difference: (
+    date1: Dayjs,
+    date2: Dayjs,
+    unit: UnitTypeLong = "millisecond"
+  ) => {
+    return date1.diff(date2, unit);
   },
 });
