02b4216d7d89370e98c9af280b5b861ccc74db25
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-09-10 16:53:26 +0200

    init "Roles" and "Users" views

--- frontend/src/assets/constants/routes.ts
+++ frontend/src/assets/constants/routes.ts
@@ -1,6 +1,8 @@
 export const ROUTE_GUEST_NAME = "guest";
 export const ROUTE_GUIDE_NAME = "guide";
 export const ROUTE_LOGIN_NAME = "login";
+export const ROUTE_ROLES_NAME = "roles";
+export const ROUTE_USERS_NAME = "users";
 export const ROUTE_UPDATES_NAME = "updates";
 export const ROUTE_DASHBOARD_NAME = "dashboard";
 export const ROUTE_WORKSPACE_NAME = "workspace";
---
+++ frontend/src/views/Roles.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="roles-page page">Roles</div>
+</template>
