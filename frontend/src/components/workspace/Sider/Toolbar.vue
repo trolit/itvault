823aa67cae88e084eff4aff37aa19a1b75531770
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-09-12 17:24:38 +0200

    init "Toolbar" component

---
+++ frontend/src/components/workspace/Sider/Toolbar.vue
@@ -0,0 +1,42 @@
+<template>
+  <n-thing>
+    <div class="header">
+      <n-button type="warning" size="small" @click="$emit('reset')">
+        <n-icon :component="ResetIcon" :size="20" />
+      </n-button>
+
+      <n-input clearable show-count :placeholder="inputPlaceholder">
+        <template #prefix>
+          <n-icon :component="SearchIcon" />
+        </template>
+      </n-input>
+
+      <require-permission :permission="Permission.CreateBlueprint">
+        <n-button size="small" @click="$emit('add-item')">
+          <n-icon :component="AddIcon" :size="25" />
+        </n-button>
+      </require-permission>
+    </div>
+  </n-thing>
+</template>
+
+<script setup lang="ts">
+import {
+  Add as AddIcon,
+  Reset as ResetIcon,
+  Search as SearchIcon,
+} from "@vicons/carbon";
+import { NIcon, NButton, NThing, NInput } from "naive-ui";
+
+import { Permission } from "@shared/types/enums/Permission";
+import RequirePermission from "@/components/common/RequirePermission.vue";
+
+defineProps({
+  inputPlaceholder: {
+    type: String,
+    required: true,
+  },
+});
+
+defineEmits(["reset", "add-item"]);
+</script>
