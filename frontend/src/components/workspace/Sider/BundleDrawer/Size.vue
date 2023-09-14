9a7e7499237e4b0ce91c4612b68f47cbe6795fd2
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-09-14 14:32:08 +0200

    Frontend - include bundle size information (in bundle drawer) (#301)

---
+++ frontend/src/components/workspace/Sider/BundleDrawer/Size.vue
@@ -0,0 +1,30 @@
+<template>
+  <n-thing>
+    <template #avatar>
+      <n-avatar>
+        <n-icon :component="SizeIcon" />
+      </n-avatar>
+    </template>
+
+    <template #header> Bundle size </template>
+
+    <template #description>
+      {{ bundle.size.value }} ({{ bundle.size.unit }})
+    </template>
+  </n-thing>
+</template>
+
+<script setup lang="ts">
+import type { PropType } from "vue";
+import { NThing, NIcon, NAvatar } from "naive-ui";
+import { Growth as SizeIcon } from "@vicons/carbon";
+
+import type { IBundleDto } from "@shared/types/dtos/IBundleDto";
+
+defineProps({
+  bundle: {
+    type: Object as PropType<IBundleDto>,
+    required: true,
+  },
+});
+</script>
