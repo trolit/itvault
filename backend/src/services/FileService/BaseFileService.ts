58e21b7d896efad28a717a3b8aa6abd15725cbb5
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-09-16 13:36:29 +0200

    Update BaseFileService.ts

--- backend/src/services/FileService/BaseFileService.ts
+++ backend/src/services/FileService/BaseFileService.ts
@@ -1,10 +1,16 @@
+import uniq from "lodash/uniq";
+import { QueryRunner } from "typeorm";
+import { IFormDataFile } from "types/IFormDataFile";
 import { TransactionResult } from "types/TransactionResult";
 import { IBaseFileService } from "types/services/IBaseFileService";
 import { IFileRepository } from "types/repositories/IFileRepository";
 import { TransactionError } from "types/custom-errors/TransactionError";
 
+import { FILES } from "@config";
+
 import { File } from "@entities/File";
 import { Variant } from "@entities/Variant";
+import { Directory } from "@entities/Directory";
 
 export abstract class BaseFileService implements IBaseFileService {
   constructor(protected fileRepository: IFileRepository) {}
@@ -34,5 +40,195 @@ export abstract class BaseFileService implements IBaseFileService {
     } finally {
       await transaction.release();
     }
+  }
+
+  async saveFilesAndDirectories(
+    userId: number,
+    workspaceId: number,
+    formDataFiles: IFormDataFile[]
+  ): Promise<TransactionResult<File[]>> {
+    const transaction = await this.fileRepository.useTransaction();
+
+    try {
+      const directories = await this._upsertDirectories(
+        transaction,
+        formDataFiles
+      );
+
+      const filesToSave = [];
+
+      for (const { key, file } of formDataFiles) {
+        const { originalFilename: filename } = file;
+
+        if (!filename) {
+          continue;
+        }
+
+        const originalFilename = filename.includes("/")
+          ? filename.split("/").pop()
+          : filename;
+
+        if (!originalFilename) {
+          continue;
+        }
+
+        const record = await transaction.manager.findOne(File, {
+          where: {
+            originalFilename,
+          },
+          relations: {
+            variants: true,
+            directory: true,
+          },
+        });
+
+        const directory = directories.find(
+          directory => directory.relativePath === key
+        );
+
+        if (!directory) {
+          continue;
+        }
+
+        filesToSave.push(
+          this._buildFileRecord(
+            transaction,
+            record || {
+              originalFilename,
+            },
+            {
+              userId,
+              directory,
+              workspaceId,
+              size: file.size,
+              filename: file.newFilename,
+            }
+          )
+        );
+      }
+
+      const files = await transaction.manager.save(File, filesToSave, {
+        chunk: 1000,
+      });
+
+      await transaction.commitTransaction();
+
+      return TransactionResult.success(files);
+    } catch (error) {
+      console.log(error);
+
+      await transaction.rollbackTransaction();
+
+      return TransactionResult.failure();
+    } finally {
+      await transaction.release();
+    }
+  }
+
+  private async _upsertDirectories(
+    transaction: QueryRunner,
+    formDataFiles: IFormDataFile[]
+  ): Promise<Directory[]> {
+    const manager = transaction.manager;
+
+    const rootDirectory = await manager.findOneBy(Directory, {
+      relativePath: FILES.ROOT,
+    });
+
+    if (!rootDirectory) {
+      throw new TransactionError("Failed to find root directory!");
+    }
+
+    const directories: Directory[] = [];
+
+    const uniquePaths = uniq(Object.keys(formDataFiles));
+
+    for (const path of uniquePaths) {
+      let directory = await manager.findOneBy(Directory, {
+        relativePath: path,
+      });
+
+      if (!directory) {
+        const splitPath = path.split("/");
+        const splitPathLength = splitPath.length;
+        let previousDirectory = rootDirectory;
+
+        for (let index = 1; index < splitPathLength; index++) {
+          const part = splitPath.slice(0, index + 1);
+          const relativePathToSave = part.join("/");
+
+          const element = await manager.findOneBy(Directory, {
+            relativePath: relativePathToSave,
+          });
+
+          if (element) {
+            element.parentDirectory = previousDirectory;
+
+            await manager.save(Directory, element);
+
+            previousDirectory = element;
+
+            continue;
+          }
+
+          const result = await manager.save(Directory, {
+            relativePath: relativePathToSave,
+            parentDirectory: previousDirectory,
+          });
+
+          previousDirectory = result;
+        }
+
+        directory = previousDirectory;
+      }
+
+      directories.push(directory);
+    }
+
+    return directories;
+  }
+
+  private _buildFileRecord(
+    transaction: QueryRunner,
+    fileData: Partial<File>,
+    additionalData: {
+      size: number;
+      userId: number;
+      filename: string;
+      workspaceId: number;
+      directory: Directory;
+    }
+  ) {
+    const { size, userId, filename, workspaceId, directory } = additionalData;
+
+    const variantName = fileData?.variants
+      ? `v${fileData.variants.length + 1}`
+      : "v1";
+
+    const variant = transaction.manager.create(Variant, {
+      size,
+      filename,
+      createdBy: {
+        id: userId,
+      },
+      name: variantName,
+    });
+
+    let variants: Variant[] = [variant];
+
+    if (fileData?.id && fileData?.variants) {
+      variants = fileData.variants.concat([variant]);
+    }
+
+    const file = transaction.manager.create(File, {
+      ...fileData,
+      variants,
+      workspace: {
+        id: workspaceId,
+      },
+      directory,
+    });
+
+    return file;
   }
 }
