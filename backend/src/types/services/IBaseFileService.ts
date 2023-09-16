aec58302044378dacb909c764ddc3852be6f36cd
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-09-16 13:31:46 +0200

    define "saveFilesAndDirectories" in BaseFileService contract

--- backend/src/types/services/IBaseFileService.ts
+++ backend/src/types/services/IBaseFileService.ts
@@ -1,5 +1,13 @@
+import { File } from "@entities/File";
+import { IFormDataFile } from "types/IFormDataFile";
 import { TransactionResult } from "types/TransactionResult";
 
 export interface IBaseFileService {
   softDeleteFileAndVariants(id: number): Promise<TransactionResult<void>>;
+
+  saveFilesAndDirectories(
+    userId: number,
+    workspaceId: number,
+    formDataFiles: IFormDataFile[]
+  ): Promise<TransactionResult<File[]>>;
 }
