42f12f83607530bf5cd48a3820d2155c39f5c577
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-09-19 16:30:10 +0200

    Update VariantService.ts

--- backend/src/services/VariantService.ts
+++ backend/src/services/VariantService.ts
@@ -10,10 +10,16 @@ import { Variant } from "@entities/Variant";
 @injectable()
 export class VariantService implements IVariantService {
   async getContent(variant: Variant, directory: string): Promise<string> {
-    const file = await fs.readFile(
-      path.join(FILES.BASE_UPLOADS_PATH, directory, variant.filename)
-    );
+    try {
+      const file = await fs.readFile(
+        path.join(FILES.BASE_UPLOADS_PATH, directory, variant.filename)
+      );
 
-    return file.toString();
+      return file.toString();
+    } catch (error) {
+      console.log(error);
+
+      return "";
+    }
   }
 }
