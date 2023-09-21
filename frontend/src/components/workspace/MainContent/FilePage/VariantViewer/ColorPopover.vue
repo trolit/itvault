35d5353c76615d301e67eca3ac587c767e531a48
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-09-21 22:15:48 +0200

    handle delete action in `ColorPopover`

--- frontend/src/components/workspace/MainContent/FilePage/VariantViewer/ColorPopover.vue
+++ frontend/src/components/workspace/MainContent/FilePage/VariantViewer/ColorPopover.vue
@@ -1,24 +1,32 @@
 <template>
   <n-popover trigger="click" placement="bottom">
     <template #trigger>
-      <span :style="{ backgroundColor: props.color, padding: '3px 0' }">
-        {{ props.part.text }}
+      <span :style="{ backgroundColor: color, padding: '3px 0' }">
+        {{ part.text }}
       </span>
     </template>
 
-    <n-button size="medium" type="error" secondary circle>
+    <n-button
+      size="medium"
+      type="error"
+      secondary
+      circle
+      @click="bucketsStore.removePartFromActiveBucket(part)"
+    >
       <n-icon :size="20" :component="DeleteIcon" />
     </n-button>
   </n-popover>
 </template>
 
 <script setup lang="ts">
-import { NPopover, NButton, NIcon } from "naive-ui";
-import { Delete as DeleteIcon } from "@vicons/carbon";
 import type { PropType } from "vue";
-import type { LinePart } from "@/types/LinePart";
+import { NPopover, NButton, NIcon } from "naive-ui";
 
-const props = defineProps({
+import type { LinePart } from "@/types/LinePart";
+import { useBucketsStore } from "@/store/buckets";
+import { Delete as DeleteIcon } from "@vicons/carbon";
+
+defineProps({
   color: {
     type: String,
     required: true,
@@ -29,4 +37,6 @@ const props = defineProps({
     required: true,
   },
 });
+
+const bucketsStore = useBucketsStore();
 </script>
