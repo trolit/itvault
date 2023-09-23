3d6781ad0770bbd492aec3f823f4efef60a1f110
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-09-23 22:50:17 +0200

    Create AssignColorSelectionData.ts

---
+++ frontend/src/types/AssignColorSelectionData.ts
@@ -0,0 +1,13 @@
+export type AssignColorSelectionData = {
+  startLineIndex: number;
+
+  endLineIndex: number;
+
+  anchorOffset: number;
+
+  anchorChildrenIndex: number;
+
+  focusOffset: number;
+
+  focusChildrenIndex: number;
+};
