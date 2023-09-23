b79fd613bdc79796789a3399c8fdf96d2eb2fb60
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-09-23 22:27:44 +0200

    Create UnassignColorPopover.vue

---
+++ frontend/src/components/workspace/MainContent/FilePage/VariantViewer/UnassignColorPopover.vue
@@ -0,0 +1,50 @@
+<template>
+  <n-popover trigger="click" placement="bottom">
+    <template #trigger>
+      <span
+        :style="{ backgroundColor: color, padding: '3px 0' }"
+        :location="part.location.original"
+      >
+        {{ part.text }}
+      </span>
+    </template>
+
+    <n-button
+      size="medium"
+      type="error"
+      secondary
+      circle
+      @click="
+        bucketsStore.removeActiveBucketPart(
+          part.lineIndex,
+          part.location.original
+        )
+      "
+    >
+      <n-icon :size="20" :component="DeleteIcon" />
+    </n-button>
+  </n-popover>
+</template>
+
+<script setup lang="ts">
+import type { PropType } from "vue";
+import { NPopover, NButton, NIcon } from "naive-ui";
+
+import type { LinePart } from "@/types/LinePart";
+import { useBucketsStore } from "@/store/buckets";
+import { Delete as DeleteIcon } from "@vicons/carbon";
+
+defineProps({
+  color: {
+    type: String,
+    required: true,
+  },
+
+  part: {
+    type: Object as PropType<LinePart>,
+    required: true,
+  },
+});
+
+const bucketsStore = useBucketsStore();
+</script>
