51f32479ff28dd81d093e83750d267e1cf47e4a6
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-09-30 19:44:33 +0200

    Update ActionsDropdown.vue

--- frontend/src/components/workspace/Sider/NotesDrawer/ActionsDropdown.vue
+++ frontend/src/components/workspace/Sider/NotesDrawer/ActionsDropdown.vue
@@ -1,10 +1,30 @@
 <template>
-  <n-dropdown trigger="hover" :options="[]" :width="250">
-    <n-icon :component="GearIcon" />
+  <n-dropdown trigger="hover" :options="options" :width="250">
+    <n-button text>
+      <n-icon :size="25" :component="GearIcon" />
+    </n-button>
   </n-dropdown>
 </template>
 
 <script setup lang="ts">
-import { NDropdown, NIcon } from "naive-ui";
+import { h, ref } from "vue";
+import { NDropdown, NIcon, NButton, NText } from "naive-ui";
 import { OperationsField as GearIcon } from "@vicons/carbon";
+
+const options = ref([
+  {
+    label: "Update",
+    key: "update",
+  },
+  {
+    key: "update-any",
+    label: () =>
+      h(NText, { depth: 2, type: "info" }, { default: () => "Update (any)" }),
+  },
+  {
+    key: "delete",
+    label: () =>
+      h(NText, { depth: 2, type: "error" }, { default: () => "Delete" }),
+  },
+]);
 </script>
