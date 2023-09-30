63bafffb88ce32a562dfd350a2e7e250ecdc2e31
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-09-30 19:22:24 +0200

    init "ActionsDropdown"

---
+++ frontend/src/components/workspace/Sider/NotesDrawer/ActionsDropdown.vue
@@ -0,0 +1,10 @@
+<template>
+  <n-dropdown trigger="hover" :options="[]" :width="250">
+    <n-icon :component="GearIcon" />
+  </n-dropdown>
+</template>
+
+<script setup lang="ts">
+import { NDropdown, NIcon } from "naive-ui";
+import { OperationsField as GearIcon } from "@vicons/carbon";
+</script>
