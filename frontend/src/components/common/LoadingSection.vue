446ced4bb2b1fa74982047bddfc26ba56e5e35f7
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-09-30 15:09:10 +0200

    Update LoadingSection.vue

--- frontend/src/components/common/LoadingSection.vue
+++ frontend/src/components/common/LoadingSection.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="loading">
-    <n-spin />
+    <n-spin :size="spinSize || 'medium'" />
   </div>
 </template>
 
 <script setup lang="ts">
 import { NSpin } from "naive-ui";
+
+defineProps({
+  spinSize: {
+    type: Number || "small" || "medium" || "large",
+    required: false,
+  },
+});
 </script>
