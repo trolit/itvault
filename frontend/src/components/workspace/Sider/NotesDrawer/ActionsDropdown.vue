ef9dd1ba80622a6a543960f56abaaef1fa1403f3
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-09-30 20:03:49 +0200

    Update ActionsDropdown.vue

--- frontend/src/components/workspace/Sider/NotesDrawer/ActionsDropdown.vue
+++ frontend/src/components/workspace/Sider/NotesDrawer/ActionsDropdown.vue
@@ -1,5 +1,5 @@
 <template>
-  <n-dropdown trigger="hover" :options="options" :width="250">
+  <n-dropdown trigger="hover" :options="options" placement="bottom-end">
     <n-button text>
       <n-icon :size="25" :component="GearIcon" />
     </n-button>
@@ -7,24 +7,38 @@
 </template>
 
 <script setup lang="ts">
-import { h, ref } from "vue";
+import { h, ref, toRefs } from "vue";
 import { NDropdown, NIcon, NButton, NText } from "naive-ui";
 import { OperationsField as GearIcon } from "@vicons/carbon";
+
+interface IProps {
+  isNoteOwner: boolean;
+
+  canDeleteAnyNote: boolean;
+
+  canUpdateAnyNote: boolean;
+}
+
+const props = defineProps<IProps>();
+
+const { isNoteOwner, canUpdateAnyNote, canDeleteAnyNote } = toRefs(props);
 
 const options = ref([
   {
     label: "Update",
     key: "update",
+    show: isNoteOwner.value && !canUpdateAnyNote.value,
   },
   {
     key: "update-any",
     label: () =>
-      h(NText, { depth: 2, type: "info" }, { default: () => "Update (any)" }),
+      h(NText, { type: "warning" }, { default: () => "Update (any)" }),
+    show: canUpdateAnyNote.value,
   },
   {
     key: "delete",
-    label: () =>
-      h(NText, { depth: 2, type: "error" }, { default: () => "Delete" }),
+    label: () => h(NText, { type: "error" }, { default: () => "Delete" }),
+    show: canDeleteAnyNote.value,
   },
 ]);
 </script>
