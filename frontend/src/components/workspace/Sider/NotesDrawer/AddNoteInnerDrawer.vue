70d52faeab8dcca8f3d096d63502037e860bcac9
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-10-05 22:51:18 +0200

    Create AddNoteInnerDrawer.vue

---
+++ frontend/src/components/workspace/Sider/NotesDrawer/AddNoteInnerDrawer.vue
@@ -0,0 +1,42 @@
+<template>
+  <n-drawer
+    :show="isVisible"
+    resizable
+    placement="bottom"
+    :show-mask="false"
+    :trap-focus="false"
+    to="#notes-drawer-content"
+  >
+    <n-drawer-content>
+      <add-edit-note-form
+        value=""
+        :is-loading="isLoading"
+        @save="addNote"
+        @cancel="$emit('update:is-visible', false)"
+      />
+    </n-drawer-content>
+  </n-drawer>
+</template>
+
+<script setup lang="ts">
+import { ref, toRefs } from "vue";
+import { NDrawer, NDrawerContent } from "naive-ui";
+
+import AddEditNoteForm from "./AddEditNoteForm.vue";
+
+interface IProps {
+  isVisible: boolean;
+}
+
+const props = defineProps<IProps>();
+
+defineEmits(["update:is-visible"]);
+
+const { isVisible } = toRefs(props);
+
+const isLoading = ref(false);
+
+function addNote() {
+  // @TODO
+}
+</script>
