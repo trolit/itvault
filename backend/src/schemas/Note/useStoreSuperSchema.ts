720f3e8474f162e581fa984430c47bb570582846
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-10-12 21:34:18 +0200

    Update useStoreSuperSchema.ts

--- backend/src/schemas/Note/useStoreSuperSchema.ts
+++ backend/src/schemas/Note/useStoreSuperSchema.ts
@@ -5,7 +5,7 @@ import { StoreControllerTypes } from "types/controllers/Note/StoreController";
 
 import { useResourceEntityTest } from "./useResourceEntityTest";
 
-import { Resource } from "@enums/Resource";
+import { NoteResource } from "@shared/types/enums/NoteResource";
 
 import { defineSuperSchemaRunner } from "@schemas/common/defineSuperSchemaRunner";
 
@@ -15,7 +15,7 @@ const bodySchema: SuperSchema.Fragment<StoreControllerTypes.v1.Body> = object({
     .transform(value => sanitizeHtml(value)),
   resource: object({
     id: useResourceEntityTest(),
-    name: string().required().oneOf([Resource.File]),
+    name: string().required().oneOf([NoteResource.File]),
   }),
 });
 
