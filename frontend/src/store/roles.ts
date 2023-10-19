10b6286ad9cfbaf9e92f66a51ffb3a9f1a0d7145
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-10-19 22:36:23 +0200

    init roles store

---
+++ frontend/src/store/roles.ts
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { defineStore } from "pinia";
+
+import type { IRoleDto } from "@shared/types/dtos/IRoleDto";
+import type { IPaginationQuery } from "@shared/types/IPaginationQuery";
+import type { PaginatedResponse } from "@shared/types/PaginatedResponse";
+
+interface IState {
+  total: number;
+  items: IRoleDto[];
+}
+
+export const useRolesStore = defineStore("roles", {
+  state: (): IState => ({
+    total: 0,
+    items: [],
+  }),
+
+  actions: {
+    async getAll(options: IPaginationQuery) {
+      const params = {
+        version: 1,
+        ...options,
+      };
+
+      const { data } = await axios.get<PaginatedResponse<IRoleDto>>(
+        `v1/roles`,
+        { params }
+      );
+
+      this.items = data.result;
+
+      this.total = data.total;
+
+      return data;
+    },
+  },
+});
