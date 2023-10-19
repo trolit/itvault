48bce2caedb18dd85fd8d69847fd75b873f6d5f2
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-10-19 22:58:51 +0200

    Create ScrollSelect.vue

---
+++ frontend/src/components/common/ScrollSelect.vue
@@ -0,0 +1,34 @@
+<template>
+  <n-select
+    :value="value"
+    :options="options"
+    :reset-menu-on-options-change="false"
+    @scroll="handleScroll"
+    @update-value="$emit('select', $event)"
+  />
+</template>
+
+<script setup lang="ts">
+import { NSelect } from "naive-ui";
+
+interface IProps {
+  value: string | number;
+
+  options?: { label: string; value: string | number }[];
+}
+
+defineProps<IProps>();
+
+const emit = defineEmits(["scroll", "select"]);
+
+function handleScroll(event: Event) {
+  const currentTarget = event.currentTarget as HTMLElement;
+
+  if (
+    currentTarget.scrollTop + currentTarget.offsetHeight >=
+    currentTarget.scrollHeight
+  ) {
+    emit("scroll");
+  }
+}
+</script>
