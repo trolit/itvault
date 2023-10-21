96bcdd169329606fbd6c2b0970eef48b413ad919
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-10-22 00:08:35 +0200

    Create TextSwitch.vue

---
+++ frontend/src/components/common/TextSwitch.vue
@@ -0,0 +1,19 @@
+<template>
+  <n-switch>
+    <template #checked> {{ checkedText }} </template>
+
+    <template #unchecked> {{ uncheckedText }} </template>
+  </n-switch>
+</template>
+
+<script setup lang="ts">
+import { NSwitch } from "naive-ui";
+
+interface IProps {
+  checkedText: string;
+
+  uncheckedText: string;
+}
+
+defineProps<IProps>();
+</script>
