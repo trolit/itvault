1ce894f1fa220bc1e2454c29ba0113f6f3352c04
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-11-01 20:41:37 +0100

    instantiate message service in `Main.vue` and assign then to general store

--- frontend/src/store/general.ts
+++ frontend/src/store/general.ts
@@ -1,22 +1,30 @@
 import { defineStore } from "pinia";
-import { useMessage } from "naive-ui";
 
 import { THEME_DARK } from "@/assets/constants/themes";
 import { LoadingState } from "@/types/enums/LoadingState";
+import type { MessageApiInjection } from "naive-ui/es/message/src/MessageProvider";
 
 interface IState {
   theme: string;
   loadingState: LoadingState;
+  message: MessageApiInjection | null;
 }
 
 export const useGeneralStore = defineStore("general", {
   state: (): IState => ({
+    message: null,
     theme: THEME_DARK,
     loadingState: LoadingState.Idle,
   }),
 
   getters: {
-    messageProvider: () => useMessage(),
+    messageProvider(): MessageApiInjection {
+      if (!this.message) {
+        throw Error("Message service not defined!");
+      }
+
+      return this.message;
+    },
   },
 
   actions: {
@@ -26,6 +34,10 @@ export const useGeneralStore = defineStore("general", {
 
     setLoadingState(state: LoadingState) {
       this.loadingState = state;
+    },
+
+    setMessageProvider(message: MessageApiInjection) {
+      this.message = message;
     },
   },
 });
