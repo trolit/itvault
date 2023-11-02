1354acb128bce0893724664c3f7c007a1d208b44
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-11-02 22:25:56 +0100

    Frontend - roles table view (#350)

--- frontend/src/store/roles.ts
+++ frontend/src/store/roles.ts
@@ -5,21 +5,10 @@ import type { IRoleDto } from "@shared/types/dtos/IRoleDto";
 import type { IPaginationQuery } from "@shared/types/IPaginationQuery";
 import type { PaginatedResponse } from "@shared/types/PaginatedResponse";
 
-interface IState {
-  total: number;
-  items: IRoleDto[];
-}
+interface IState {}
 
 export const useRolesStore = defineStore("roles", {
-  state: (): IState => ({
-    total: 0,
-    items: [],
-  }),
-
-  getters: {
-    options: state =>
-      state.items.map(({ id, name }) => ({ label: name, value: id })),
-  },
+  state: (): IState => ({}),
 
   actions: {
     async getAll(options: IPaginationQuery) {
@@ -32,13 +21,6 @@ export const useRolesStore = defineStore("roles", {
         `v1/roles`,
         { params }
       );
-
-      this.items =
-        options.page === 1
-          ? data.result
-          : Array.prototype.concat(this.items, data.result);
-
-      this.total = data.total;
 
       return data;
     },
