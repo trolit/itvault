cea050bd10129338ec00ef4413d550bdf7abca01
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-11-03 21:45:13 +0100

    extract Roles `n-data-table` to separate component

---
+++ frontend/src/components/roles/Table.vue
@@ -0,0 +1,118 @@
+<template>
+  <n-data-table
+    :data="data"
+    :columns="columns"
+    :single-line="false"
+    :single-column="false"
+    :pagination="pagination"
+    :loading="isLoadingRoles"
+    :row-key="(row: IRoleDto) => row.id"
+    @update:page="getRoles"
+  >
+    <template #empty>
+      <n-empty description="No roles found." />
+    </template>
+  </n-data-table>
+</template>
+
+<script setup lang="ts">
+import {
+  NEmpty,
+  NButton,
+  NDataTable,
+  type PaginationProps,
+  type DataTableColumns,
+} from "naive-ui";
+import { ref, reactive, onBeforeMount, type Ref, h } from "vue";
+
+import { useRolesStore } from "@/store/roles";
+import { useGeneralStore } from "@/store/general";
+import type { IRoleDto } from "@shared/types/dtos/IRoleDto";
+
+const rolesStore = useRolesStore();
+const generalStore = useGeneralStore();
+
+const isLoadingRoles = ref(false);
+let data: IRoleDto[] = reactive([]);
+
+const defaultPagination = {
+  page: 1,
+  pageSize: 10,
+};
+
+onBeforeMount(async () => {
+  await getRoles();
+});
+
+const pagination: PaginationProps = reactive({
+  ...defaultPagination,
+  showSizePicker: true,
+  pageSizes: [10, 20, 40],
+  onChange: (page: number) => {
+    pagination.page = page;
+
+    getRoles();
+  },
+  onUpdatePageSize: (pageSize: number) => {
+    pagination.pageSize = pageSize;
+    pagination.page = 1;
+
+    getRoles();
+  },
+});
+
+const columns: Ref<DataTableColumns<IRoleDto>> = ref<
+  DataTableColumns<IRoleDto>
+>([
+  {
+    title: "Name",
+    key: "name",
+    ellipsis: {
+      tooltip: true,
+    },
+  },
+
+  {
+    title: "Action(s)",
+    key: "actions",
+    width: 140,
+    render() {
+      return h("div", { class: "actions-wrapper" }, [
+        h(
+          NButton,
+          {
+            size: "small",
+            onClick: event => {
+              event.stopPropagation();
+            },
+          },
+          { default: () => "Edit" }
+        ),
+      ]);
+    },
+  },
+]);
+
+async function getRoles() {
+  isLoadingRoles.value = true;
+
+  try {
+    const { result, total } = await rolesStore.getAll({
+      page: pagination.page || defaultPagination.page,
+      perPage: pagination.pageSize || defaultPagination.pageSize,
+    });
+
+    pagination.itemCount = total;
+
+    data = result;
+  } catch (error) {
+    console.log(error);
+
+    generalStore.messageProvider.error(
+      "There was an error when trying to load roles."
+    );
+  } finally {
+    isLoadingRoles.value = false;
+  }
+}
+</script>
--- frontend/src/views/Roles.vue
+++ frontend/src/views/Roles.vue
@@ -9,124 +9,26 @@
       </n-input>
     </div>
 
-    <n-data-table
-      :data="data"
-      :columns="columns"
-      :single-line="false"
-      :single-column="false"
-      :pagination="pagination"
-      :loading="isLoadingRoles"
-      :row-key="(row: IRoleDto) => row.id"
-      @update:page="getRoles"
+    <n-grid
+      x-gap="20"
+      y-gap="20"
+      responsive="screen"
+      cols="1 s:1 m:2 l:2 xl:2 2xl:2"
     >
-      <template #empty>
-        <n-empty description="No roles found." />
-      </template>
-    </n-data-table>
+      <n-grid-item span="2">
+        <roles-table />
+      </n-grid-item>
+
+      <n-grid-item span="1">
+        <!-- <add-edit-form /> -->
+      </n-grid-item>
+    </n-grid>
   </div>
 </template>
 
 <script setup lang="ts">
-import {
-  NIcon,
-  NEmpty,
-  NInput,
-  NButton,
-  NDataTable,
-  type PaginationProps,
-  type DataTableColumns,
-} from "naive-ui";
+import { NGrid, NIcon, NInput, NGridItem } from "naive-ui";
 import { Search as SearchIcon } from "@vicons/carbon";
-import { ref, reactive, onBeforeMount, type Ref, h } from "vue";
 
-import { useRolesStore } from "@/store/roles";
-import { useGeneralStore } from "@/store/general";
-import type { IRoleDto } from "@shared/types/dtos/IRoleDto";
-
-const rolesStore = useRolesStore();
-const generalStore = useGeneralStore();
-
-const isLoadingRoles = ref(false);
-let data: IRoleDto[] = reactive([]);
-
-const defaultPagination = {
-  page: 1,
-  pageSize: 10,
-};
-
-onBeforeMount(async () => {
-  await getRoles();
-});
-
-const pagination: PaginationProps = reactive({
-  ...defaultPagination,
-  showSizePicker: true,
-  pageSizes: [10, 20, 40],
-  onChange: (page: number) => {
-    pagination.page = page;
-
-    getRoles();
-  },
-  onUpdatePageSize: (pageSize: number) => {
-    pagination.pageSize = pageSize;
-    pagination.page = 1;
-
-    getRoles();
-  },
-});
-
-const columns: Ref<DataTableColumns<IRoleDto>> = ref<
-  DataTableColumns<IRoleDto>
->([
-  {
-    title: "Name",
-    key: "name",
-    ellipsis: {
-      tooltip: true,
-    },
-  },
-
-  {
-    title: "Action(s)",
-    key: "actions",
-    width: 140,
-    render() {
-      return h("div", { class: "actions-wrapper" }, [
-        h(
-          NButton,
-          {
-            size: "small",
-            onClick: event => {
-              event.stopPropagation();
-            },
-          },
-          { default: () => "Edit" }
-        ),
-      ]);
-    },
-  },
-]);
-
-async function getRoles() {
-  isLoadingRoles.value = true;
-
-  try {
-    const { result, total } = await rolesStore.getAll({
-      page: pagination.page || defaultPagination.page,
-      perPage: pagination.pageSize || defaultPagination.pageSize,
-    });
-
-    pagination.itemCount = total;
-
-    data = result;
-  } catch (error) {
-    console.log(error);
-
-    generalStore.messageProvider.error(
-      "There was an error when trying to load roles."
-    );
-  } finally {
-    isLoadingRoles.value = false;
-  }
-}
+import RolesTable from "@/components/roles/Table.vue";
 </script>
