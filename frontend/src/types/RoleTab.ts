6e90922e834ca4edd9e47393070bfe55e9defd76
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-11-04 23:10:38 +0100

    Update RoleTab.ts

--- frontend/src/types/RoleTab.ts
+++ frontend/src/types/RoleTab.ts
@@ -1,10 +1,14 @@
-import type { IRoleDto } from "@shared/types/dtos/IRoleDto";
+import type { AddEditRoleDto } from "@shared/types/dtos/AddEditRoleDto";
 import type { IRolePermissionDto } from "@shared/types/dtos/IRolePermissionDto";
 
-export type RoleTab = {
-  role: IRoleDto;
-
-  initialPermissions: IRolePermissionDto[];
-
+export type Form = Pick<AddEditRoleDto, "name"> & {
   permissions: IRolePermissionDto[];
+};
+
+export type RoleTab = {
+  roleId: number;
+
+  initialForm: Form;
+
+  currentForm: Form;
 };
