8b6bb03f1ae6e7e51b8774e6b5e063611faee4d5
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-11-06 21:59:56 +0100

    Update useGetAllSuperSchema.ts

--- backend/src/schemas/Role/useGetAllSuperSchema.ts
+++ backend/src/schemas/Role/useGetAllSuperSchema.ts
@@ -2,14 +2,22 @@ import { object, string } from "yup";
 import { SuperSchema } from "types/SuperSchema";
 import { GetAllControllerTypes } from "types/controllers/Role/GetAllController";
 
-import { pageSchema, perPageSchema } from "@schemas/common/paginationSchemas";
 import { defineSuperSchemaRunner } from "@schemas/common/defineSuperSchemaRunner";
+import {
+  pageSchemaV2,
+  perPageSchemaV2,
+} from "@schemas/common/paginationSchemas";
 
 const querySchema: SuperSchema.Fragment<GetAllControllerTypes.v1.QueryInput> =
   object({
-    page: pageSchema,
-    perPage: perPageSchema,
-    search: string().optional(),
+    page: pageSchemaV2,
+    perPage: perPageSchemaV2,
+    // @TODO -> create "q" object or smth
+    name: string().when(["page", "perPage"], {
+      is: (page: number, perPage: number) => !page && !perPage,
+      then: schema => schema.required(),
+      otherwise: schema => schema.optional(),
+    }),
   });
 
 export const useGetAllSuperSchema: SuperSchema.Runner<
