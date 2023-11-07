a617d3b58ad5ad17e298142e871ab1de58e4873c
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-11-07 15:42:16 +0100

    abort idea of  endpoint without page/perPage (roles)

--- backend/src/schemas/Role/useGetAllSuperSchema.ts
+++ backend/src/schemas/Role/useGetAllSuperSchema.ts
@@ -2,22 +2,15 @@ import { object, string } from "yup";
 import { SuperSchema } from "types/SuperSchema";
 import { GetAllControllerTypes } from "types/controllers/Role/GetAllController";
 
+import { pageSchema, perPageSchema } from "@schemas/common/paginationSchemas";
 import { defineSuperSchemaRunner } from "@schemas/common/defineSuperSchemaRunner";
-import {
-  pageSchemaV2,
-  perPageSchemaV2,
-} from "@schemas/common/paginationSchemas";
 
 const querySchema: SuperSchema.Fragment<GetAllControllerTypes.v1.QueryInput> =
   object({
-    page: pageSchemaV2,
-    perPage: perPageSchemaV2,
+    page: pageSchema,
+    perPage: perPageSchema,
     // @TODO -> create "q" object or smth
-    name: string().when(["page", "perPage"], {
-      is: (page: number, perPage: number) => !page && !perPage,
-      then: schema => schema.required(),
-      otherwise: schema => schema.optional(),
-    }),
+    name: string().optional(),
   });
 
 export const useGetAllSuperSchema: SuperSchema.Runner<
--- backend/src/schemas/common/paginationSchemas.ts
+++ backend/src/schemas/common/paginationSchemas.ts
@@ -2,16 +2,10 @@ import { number } from "yup";
 
 import { APP } from "@config";
 
-// @DEPRECATED
 export const pageSchema = number().required().integer().min(1);
 
-export const pageSchemaV2 = number().optional().integer().min(1);
-
-// @DEPRECATED
-export const perPageSchema = number().required().integer().min(5);
-
-export const perPageSchemaV2 = number()
-  .optional()
+export const perPageSchema = number()
+  .required()
   .integer()
   .min(5)
   .max(APP.MAX_ITEMS_PER_PAGE);
