d3bf5862d4d80eee8357fbbfe82077444296d734
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-11-10 17:30:17 +0100

    Update IRoleDto.ts

--- shared/src/types/dtos/IRoleDto.ts
+++ shared/src/types/dtos/IRoleDto.ts
@@ -1,5 +1,15 @@
+import { IAuthorDto } from "./IAuthorDto";
+
 export interface IRoleDto {
   id: number;
 
   name: string;
+
+  createdAt: string;
+
+  updatedAt: string;
+
+  createdBy: IAuthorDto;
+
+  updatedBy: IAuthorDto;
 }
