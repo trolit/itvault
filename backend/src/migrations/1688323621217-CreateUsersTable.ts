6ecf30bc9b2c08dc42b01a577da65b9f9627a88a
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-11-13 18:11:07 +0100

    fix migrations formatting

--- backend/src/migrations/1688323621217-CreateUsersTable.ts
+++ backend/src/migrations/1688323621217-CreateUsersTable.ts
@@ -1,20 +1,41 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class CreateUsersTable1688323621217 implements MigrationInterface {
-    name = 'CreateUsersTable1688323621217'
+  name = "CreateUsersTable1688323621217";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE \`users\` (\`id\` int NOT NULL AUTO_INCREMENT, \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updatedAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`deletedAt\` datetime(6) NULL, \`email\` varchar(255) NOT NULL, \`password\` varchar(255) NULL, \`firstName\` varchar(255) NOT NULL, \`lastName\` varchar(255) NOT NULL, \`fullName\` varchar(255) AS (CONCAT(firstName,' ',lastName)) VIRTUAL NOT NULL, \`roleId\` int NOT NULL, \`createdById\` int NULL, \`signUpCode\` varchar(255) NULL, \`isSignedUp\` tinyint(1) NOT NULL DEFAULT 0, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
-        await queryRunner.query(`INSERT INTO \`itvault\`.\`typeorm_metadata\`(\`database\`, \`schema\`, \`table\`, \`type\`, \`name\`, \`value\`) VALUES (DEFAULT, ?, ?, ?, ?, ?)`, ["itvault","users","GENERATED_COLUMN","fullName","CONCAT(firstName,' ',lastName)"]);
-        await queryRunner.query(`ALTER TABLE \`users\` ADD CONSTRAINT \`FK_368e146b785b574f42ae9e53d5e\` FOREIGN KEY (\`roleId\`) REFERENCES \`roles\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE \`users\` ADD CONSTRAINT \`FK_51d635f1d983d505fb5a2f44c52\` FOREIGN KEY (\`createdById\`) REFERENCES \`users\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE \`users\` (\`id\` int NOT NULL AUTO_INCREMENT, \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updatedAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`deletedAt\` datetime(6) NULL, \`email\` varchar(255) NOT NULL, \`password\` varchar(255) NULL, \`firstName\` varchar(255) NOT NULL, \`lastName\` varchar(255) NOT NULL, \`fullName\` varchar(255) AS (CONCAT(firstName,' ',lastName)) VIRTUAL NOT NULL, \`roleId\` int NOT NULL, \`createdById\` int NULL, \`signUpCode\` varchar(255) NULL, \`isSignedUp\` tinyint(1) NOT NULL DEFAULT 0, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`
+    );
+    await queryRunner.query(
+      `INSERT INTO \`itvault\`.\`typeorm_metadata\`(\`database\`, \`schema\`, \`table\`, \`type\`, \`name\`, \`value\`) VALUES (DEFAULT, ?, ?, ?, ?, ?)`,
+      [
+        "itvault",
+        "users",
+        "GENERATED_COLUMN",
+        "fullName",
+        "CONCAT(firstName,' ',lastName)",
+      ]
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`users\` ADD CONSTRAINT \`FK_368e146b785b574f42ae9e53d5e\` FOREIGN KEY (\`roleId\`) REFERENCES \`roles\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`users\` ADD CONSTRAINT \`FK_51d635f1d983d505fb5a2f44c52\` FOREIGN KEY (\`createdById\`) REFERENCES \`users\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`users\` DROP FOREIGN KEY \`FK_51d635f1d983d505fb5a2f44c52\``);
-        await queryRunner.query(`ALTER TABLE \`users\` DROP FOREIGN KEY \`FK_368e146b785b574f42ae9e53d5e\``);
-        await queryRunner.query(`DELETE FROM \`itvault\`.\`typeorm_metadata\` WHERE \`type\` = ? AND \`name\` = ? AND \`schema\` = ? AND \`table\` = ?`, ["GENERATED_COLUMN","fullName","itvault","users"]);
-        await queryRunner.query(`DROP TABLE \`users\``);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE \`users\` DROP FOREIGN KEY \`FK_51d635f1d983d505fb5a2f44c52\``
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`users\` DROP FOREIGN KEY \`FK_368e146b785b574f42ae9e53d5e\``
+    );
+    await queryRunner.query(
+      `DELETE FROM \`itvault\`.\`typeorm_metadata\` WHERE \`type\` = ? AND \`name\` = ? AND \`schema\` = ? AND \`table\` = ?`,
+      ["GENERATED_COLUMN", "fullName", "itvault", "users"]
+    );
+    await queryRunner.query(`DROP TABLE \`users\``);
+  }
 }
--- backend/src/migrations/1688324413752-CreateBundlesTable.ts
+++ backend/src/migrations/1688324413752-CreateBundlesTable.ts
@@ -1,18 +1,27 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class CreateBundlesTable1688324413752 implements MigrationInterface {
-    name = 'CreateBundlesTable1688324413752'
+  name = "CreateBundlesTable1688324413752";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE \`bundles\` (\`id\` int NOT NULL AUTO_INCREMENT, \`filename\` varchar(255) NULL, \`note\` varchar(255) NULL, \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`expire\` enum ('1-day', '2-days', '3-days', '1-week', '2-weeks', '1-month', 'never') NOT NULL, \`expiresAt\` datetime(6) NULL DEFAULT CURRENT_TIMESTAMP(6), \`status\` enum ('queried', 'building', 'ready', 'failed') NOT NULL, \`size\` int NOT NULL, \`workspaceId\` int NULL, \`createdById\` int NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
-        await queryRunner.query(`ALTER TABLE \`bundles\` ADD CONSTRAINT \`FK_1acd04c0ff0966005978d0a7c3e\` FOREIGN KEY (\`workspaceId\`) REFERENCES \`workspaces\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE \`bundles\` ADD CONSTRAINT \`FK_07b4f7c6e07686faf15e75f49f2\` FOREIGN KEY (\`createdById\`) REFERENCES \`users\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE \`bundles\` (\`id\` int NOT NULL AUTO_INCREMENT, \`filename\` varchar(255) NULL, \`note\` varchar(255) NULL, \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`expire\` enum ('1-day', '2-days', '3-days', '1-week', '2-weeks', '1-month', 'never') NOT NULL, \`expiresAt\` datetime(6) NULL DEFAULT CURRENT_TIMESTAMP(6), \`status\` enum ('queried', 'building', 'ready', 'failed') NOT NULL, \`size\` int NOT NULL, \`workspaceId\` int NULL, \`createdById\` int NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`bundles\` ADD CONSTRAINT \`FK_1acd04c0ff0966005978d0a7c3e\` FOREIGN KEY (\`workspaceId\`) REFERENCES \`workspaces\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`bundles\` ADD CONSTRAINT \`FK_07b4f7c6e07686faf15e75f49f2\` FOREIGN KEY (\`createdById\`) REFERENCES \`users\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`bundles\` DROP FOREIGN KEY \`FK_07b4f7c6e07686faf15e75f49f2\``);
-        await queryRunner.query(`ALTER TABLE \`bundles\` DROP FOREIGN KEY \`FK_1acd04c0ff0966005978d0a7c3e\``);
-        await queryRunner.query(`DROP TABLE \`bundles\``);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE \`bundles\` DROP FOREIGN KEY \`FK_07b4f7c6e07686faf15e75f49f2\``
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`bundles\` DROP FOREIGN KEY \`FK_1acd04c0ff0966005978d0a7c3e\``
+    );
+    await queryRunner.query(`DROP TABLE \`bundles\``);
+  }
 }
