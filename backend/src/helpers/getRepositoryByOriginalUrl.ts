b2aec6310d625852c09a55331dc2fde1d7170a47
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-11-17 16:31:56 +0100

    Update getRepositoryByOriginalUrl.ts

--- backend/src/helpers/getRepositoryByOriginalUrl.ts
+++ backend/src/helpers/getRepositoryByOriginalUrl.ts
@@ -4,15 +4,22 @@ import { IBaseRepository } from "types/repositories/IBaseRepository";
 
 import { getInstanceOf } from "./getInstanceOf";
 
-export const getRepositoryByOriginalUrl = <T>(originalUrl: string) => {
-  // @NOTE e.g. /api/v1/notes/1?version=1
-  const resourceInPlural = originalUrl.split("/").at(-2);
+export const getRepositoryByOriginalUrl = <T>(
+  originalUrl: string,
+  resourceNamePosition = 2
+) => {
+  // @NOTE e.g. (1) /api/v1/notes/1?version=1 (resourceNamePosition = -2)
+  // @NOTE e.g. (2) /api/v1/workspaces/1/pin?version=1 (resourceNamePosition = -3)
+  const resourceInPlural = originalUrl.split("/").at(-resourceNamePosition);
 
   if (!resourceInPlural) {
     return null;
   }
 
   const resource = resourceInPlural.slice(0, -1);
+
+  console.log("xd");
+  console.log(originalUrl.split("/"));
 
   const dependency = `I${capitalize(resource)}Repository`;
 
