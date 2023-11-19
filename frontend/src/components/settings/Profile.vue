c54028e4e89219abf896cae39dd049797601f6c5
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-11-19 20:41:37 +0100

    init Password and Profile (settings) components

---
+++ frontend/src/components/settings/Profile.vue
@@ -0,0 +1,15 @@
+<template>
+  <n-form class="profile-form" label-placement="left">
+    <n-form-item required label="First name">
+      <n-input placeholder="type your first name..." />
+    </n-form-item>
+
+    <n-form-item required label="Last name">
+      <n-input placeholder="type your last name..." />
+    </n-form-item>
+  </n-form>
+</template>
+
+<script setup lang="ts">
+import { NForm, NFormItem, NInput } from "naive-ui";
+</script>
