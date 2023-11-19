39309b97022ad151b880a32904084276be5b8e02
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-11-19 19:02:54 +0100

    adjust `BaseMapper` to work with `useDefineForClassFields`

--- backend/src/mappers/BaseMapper.ts
+++ backend/src/mappers/BaseMapper.ts
@@ -1,13 +1,25 @@
 /* eslint-disable @typescript-eslint/no-extraneous-class */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
+const DATA_SYMBOL = Symbol("DATA");
+
 // @NOTE we could remove it and manually map "inherited" keys but this is thing to consider.
 export abstract class BaseMapper<T> {
-  constructor(data: T, keys: (keyof T)[]) {
-    keys.map(key => {
-      if (typeof data[key] !== undefined) {
-        (this as any)[key] = data[key];
-      }
-    });
+  constructor(data: T, initialKeys: (keyof T)[]) {
+    (this as any)[DATA_SYMBOL] = data;
+
+    this.overwriteInitialKeys(initialKeys);
+  }
+
+  overwriteInitialKeys(inheritedKeys?: (keyof T)[]) {
+    if (inheritedKeys) {
+      inheritedKeys.map(key => {
+        const value = (this as any)[DATA_SYMBOL][key];
+
+        if (typeof value !== undefined) {
+          (this as any)[key] = value;
+        }
+      });
+    }
   }
 }
