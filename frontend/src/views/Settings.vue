134e8ebd844275943ba4d7046b682af849429801
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-11-19 20:41:43 +0100

    Update Settings.vue

--- frontend/src/views/Settings.vue
+++ frontend/src/views/Settings.vue
@@ -1,3 +1,62 @@
 <template>
-  <div class="settings-page page">Settings</div>
+  <div class="settings-page page">
+    <n-layout has-sider>
+      <n-layout-sider bordered :width="200">
+        <n-menu
+          v-model:value="settingsStore.activeKey"
+          :options="menuOptions"
+        />
+      </n-layout-sider>
+
+      <n-layout>
+        <div class="content-wrapper">
+          <component v-if="componentToRender" :is="componentToRender" />
+
+          <div v-else>No component provided!</div>
+        </div>
+      </n-layout>
+    </n-layout>
+  </div>
 </template>
+
+<script setup lang="ts">
+import type { MenuOption } from "naive-ui";
+
+import { useSettingsStore } from "@/store/settings";
+import { NLayout, NLayoutSider, NMenu } from "naive-ui";
+
+import { defineComputed } from "@/helpers/defineComputed";
+import ProfileSettings from "@/components/settings/Profile.vue";
+import PasswordSettings from "@/components/settings/Password.vue";
+
+const settingsStore = useSettingsStore();
+
+const menuOptions: MenuOption[] = [
+  {
+    key: "profile",
+    label: "Profile",
+  },
+  {
+    key: "password",
+    label: "Change password",
+  },
+  {
+    key: "sessions",
+    label: "Sessions",
+  },
+];
+
+const { componentToRender } = defineComputed({
+  componentToRender() {
+    if (settingsStore.activeKey === "profile") {
+      return ProfileSettings;
+    }
+
+    if (settingsStore.activeKey === "password") {
+      return PasswordSettings;
+    }
+
+    return null;
+  },
+});
+</script>
