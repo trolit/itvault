09bb816a88d0c4c5a4d4fe3efc2df3dc19f7fcb0
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-11-27 18:59:49 +0100

    Backend - in patch relative patch, check if name is unique (#383)

--- backend/src/schemas/File/usePatchRelativePathSuperSchema.ts
+++ backend/src/schemas/File/usePatchRelativePathSuperSchema.ts
@@ -1,8 +1,13 @@
-import { object } from "yup";
+import { number, object } from "yup";
 import { SuperSchema } from "types/SuperSchema";
+import { IFileRepository } from "types/repositories/IFileRepository";
 import { PatchRelativePathControllerTypes } from "types/controllers/File/PatchRelativePathController";
 
 import { Di } from "@enums/Di";
+
+import { setYupError } from "@helpers/yup/setError";
+import { getInstanceOf } from "@helpers/getInstanceOf";
+import { CUSTOM_MESSAGES } from "@helpers/yup/custom-messages";
 
 import { useIdNumberSchema } from "@schemas/common/useIdNumberSchema";
 import { useRelativePathTest } from "@schemas/common/useRelativePathTest";
@@ -10,7 +15,7 @@ import { defineSuperSchemaRunner } from "@schemas/common/defineSuperSchemaRunner
 
 const paramsSchema: SuperSchema.Fragment<PatchRelativePathControllerTypes.v1.Params> =
   object({
-    id: useIdNumberSchema(Di.FileRepository),
+    id: number().required(),
   });
 
 const querySchema: SuperSchema.Fragment<PatchRelativePathControllerTypes.v1.Query> =
@@ -18,20 +23,57 @@ const querySchema: SuperSchema.Fragment<PatchRelativePathControllerTypes.v1.Quer
     workspaceId: useIdNumberSchema(Di.WorkspaceRepository),
   });
 
-const bodySchema: SuperSchema.Fragment<PatchRelativePathControllerTypes.v1.Body> =
+const useBodySchema: (
+  id: number
+) => SuperSchema.Fragment<PatchRelativePathControllerTypes.v1.Body> = (
+  id: number
+) =>
   object({
-    // @TODO check if under relative path there is no already file with similiar name
-    relativePath: useRelativePathTest(),
+    relativePath: useRelativePathTest().test(async (value: string, ctx) => {
+      const fileRepository = getInstanceOf<IFileRepository>(Di.FileRepository);
+
+      const file = await fileRepository.getOne({
+        where: { id },
+      });
+
+      if (!file) {
+        return ctx.createError({
+          message: setYupError(
+            CUSTOM_MESSAGES.GENERAL.NOT_AVAILABLE,
+            `file (ref: ${id})`
+          ),
+        });
+      }
+
+      const fileAtRelativePath = await fileRepository.getOne({
+        where: {
+          originalFilename: file.originalFilename,
+          directory: { relativePath: value },
+        },
+      });
+
+      if (fileAtRelativePath) {
+        return ctx.createError({
+          message: setYupError(CUSTOM_MESSAGES.FILE.DUPLICATE_FILE, value),
+        });
+      }
+
+      return true;
+    }),
   });
 
 export const usePatchRelativePathSuperSchema: SuperSchema.Runner<
   PatchRelativePathControllerTypes.v1.Params,
   PatchRelativePathControllerTypes.v1.Body,
   PatchRelativePathControllerTypes.v1.Query
-> = defineSuperSchemaRunner(() => {
+> = defineSuperSchemaRunner(({ request }) => {
+  const {
+    params: { id },
+  } = request;
+
   return {
     query: querySchema,
     params: paramsSchema,
-    body: bodySchema,
+    body: useBodySchema(id),
   };
 });
