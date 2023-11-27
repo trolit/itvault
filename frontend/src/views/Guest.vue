f2490dbc02d0fbff8c0421b0d15e5a2c573135a8
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-11-27 20:07:44 +0100

    Update Guest.vue

--- frontend/src/views/Guest.vue
+++ frontend/src/views/Guest.vue
@@ -12,59 +12,30 @@
       :x-gap="50"
       class="grid"
       responsive="screen"
-      cols="1 s:1 m:3 l:3 xl:3 2xl:3"
+      cols="1 s:1 m:4 l:4 xl:4 2xl:4"
     >
-      <n-grid-item :span="1">
-        <n-grid responsive="screen" cols="1">
-          <n-grid-item>
-            <brand>
-              <template #extra-text>
-                <strong>keep</strong> source code <strong>knowledge</strong>
-              </template>
-            </brand>
-          </n-grid-item>
+      <n-grid-item class="header-grid-item" :span="4">
+        <div class="title">COMPANY_NAME</div>
 
-          <n-grid-item class="top-buttons">
-            <router-link :to="ROUTE_LOGIN_NAME">
-              <n-button ghost type="tertiary" :focusable="false">
-                <div class="primary-icon">
-                  <icon :value="OpenIcon" />
-                </div>
+        <div class="subtitle">
+          <n-icon :component="ArchiveIcon" />
 
-                <div class="text">open</div>
-              </n-button>
-            </router-link>
-          </n-grid-item>
-        </n-grid>
+          <span>/vault</span>
+        </div>
       </n-grid-item>
 
-      <n-grid-item :span="2">
-        <n-grid responsive="screen" cols="1" :y-gap="40">
-          <n-grid-item
-            v-for="({ icon, header, text }, index) of features"
-            :key="index"
-            :span="1"
-            class="feature"
-          >
-            <n-descriptions label-placement="top" :column="4">
-              <n-descriptions-item>
-                <div class="icon-wrapper">
-                  <icon :value="icon" />
-                </div>
-              </n-descriptions-item>
+      <n-grid-item class="quote-grid-item" :span="4">
+        <div class="wrapper">
+          <div class="text"><em>Memory is the mother of all wisdom.</em></div>
 
-              <n-descriptions-item>
-                <div class="text-wrapper">
-                  <div class="header">{{ header }}</div>
+          <div class="author">-Aeschylus</div>
+        </div>
+      </n-grid-item>
 
-                  <div class="text">
-                    {{ text }}
-                  </div>
-                </div>
-              </n-descriptions-item>
-            </n-descriptions>
-          </n-grid-item>
-        </n-grid>
+      <n-grid-item class="button-grid-item" :span="4">
+        <router-link :to="ROUTE_LOGIN_NAME">
+          <n-button type="info" secondary :focusable="false"> launch </n-button>
+        </router-link>
       </n-grid-item>
     </n-grid>
   </div>
@@ -72,53 +43,12 @@
 
 <script setup lang="ts">
 import {
-  NGrid,
-  NButton,
-  NGridItem,
-  NDescriptions,
-  NDescriptionsItem,
-} from "naive-ui";
-import {
-  Timer as TimerIcon,
-  Network4 as OpenIcon,
-  TaskStar as YagniIcon,
+  Archive as ArchiveIcon,
   RainDrop as RainDropIcon,
-  DataCenter as PersistCodeIcon,
-  ListChecked as ExtractOnDemandIcon,
 } from "@vicons/carbon";
-import { ref, shallowRef, type Ref } from "vue";
+import { NGrid, NIcon, NButton, NGridItem } from "naive-ui";
 
 import Icon from "@/components/common/Icon.vue";
-import Brand from "@/components/common/Brand.vue";
 import { ROUTE_LOGIN_NAME } from "@/assets/constants/routes";
 import ThemeSelector from "@/components/common/ThemeSelector.vue";
-
-interface Feature {
-  icon: object;
-  header: string;
-  text: string;
-}
-
-const features: Ref<Feature[]> = ref([
-  {
-    icon: shallowRef(ExtractOnDemandIcon),
-    header: "Extract features on demand",
-    text: "Extract project features (related source code) from vault when needed.",
-  },
-  {
-    icon: shallowRef(PersistCodeIcon),
-    header: "Persist code knowledge",
-    text: "Provide more insights on your/company codebase for developer(s). Reduce time needed to understand/remind how specific features are/were implemented.",
-  },
-  {
-    icon: shallowRef(YagniIcon),
-    header: "Boot new project with YAGNI",
-    text: "Initialize new project without excessive code. Expand it when needed.",
-  },
-  {
-    icon: shallowRef(TimerIcon),
-    header: "Save developer(s) time",
-    text: "Use vault to allow developer(s) to instantly obtain feature related source code instead of asking others to help implement it in different project.",
-  },
-]);
 </script>
