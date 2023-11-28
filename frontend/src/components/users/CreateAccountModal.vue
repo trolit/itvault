ca7c2f06bdd480c9f78a5a6eac899c49c172840c
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-11-28 18:05:06 +0100

    init `CreateAccountModal` component

---
+++ frontend/src/components/users/CreateAccountModal.vue
@@ -0,0 +1,34 @@
+<template>
+  <n-modal
+    :show="isVisible"
+    segmented
+    title="Create account"
+    preset="card"
+    :bordered="true"
+    :closable="!isLoading"
+    :close-on-esc="false"
+    :mask-closable="false"
+    :style="{ width: '600px' }"
+    @close="close"
+  >
+    TBA
+  </n-modal>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+interface IProps {
+  isVisible: boolean;
+}
+
+defineProps<IProps>();
+
+const emits = defineEmits(["update:is-visible"]);
+
+const isLoading = ref(false);
+
+function close() {
+  emits("update:is-visible", false);
+}
+</script>
