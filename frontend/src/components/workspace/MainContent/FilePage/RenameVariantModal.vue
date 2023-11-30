21a9f545df95ca3e39e315da9c017c0c3a7f603a
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-11-30 14:35:52 +0100

    Create RenameVariantModal.vue

---
+++ frontend/src/components/workspace/MainContent/FilePage/RenameVariantModal.vue
@@ -0,0 +1,110 @@
+<template>
+  <n-modal
+    :show="isVisible"
+    segmented
+    title="Rename variant"
+    preset="card"
+    :bordered="true"
+    :closable="!isLoading"
+    :close-on-esc="false"
+    :mask-closable="false"
+    :style="{ width: '600px' }"
+    @close="close"
+  >
+    <n-form>
+      <n-form-item
+        label="Name"
+        :required="true"
+        :feedback="getError('name')"
+        :validation-status="hasError('name')"
+      >
+        <n-input v-model:value="name" type="text" placeholder="Name" />
+      </n-form-item>
+    </n-form>
+
+    <template #footer>
+      <n-space justify="space-between">
+        <n-button @click="close" :disabled="isLoading"> Cancel </n-button>
+
+        <n-button type="warning" :loading="isLoading" @click="onSubmit">
+          Confirm
+        </n-button>
+      </n-space>
+    </template>
+  </n-modal>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { object, string } from "yup";
+import { NForm, NFormItem, NInput, NModal, NSpace, NButton } from "naive-ui";
+
+import { useFilesStore } from "@/store/files";
+import { defineForm } from "@/helpers/defineForm";
+import { useGeneralStore } from "@/store/general";
+import { useVariantsStore } from "@/store/variants";
+
+interface IProps {
+  isVisible: boolean;
+
+  variantId: string;
+}
+
+const filesStore = useFilesStore();
+const generalStore = useGeneralStore();
+const variantsStore = useVariantsStore();
+
+const props = defineProps<IProps>();
+
+const emits = defineEmits(["update:is-visible"]);
+
+const isLoading = ref(false);
+
+const tab = filesStore.activeTab
+  ? filesStore.activeTab.variantTabs.find(
+      tab => tab.variant.id === props.variantId
+    )
+  : undefined;
+
+const { fields, getError, hasError, handleSubmit, setValidationErrors } =
+  defineForm(
+    {
+      name: tab ? tab.variant.name : "",
+    },
+    object({
+      name: string().required(),
+    })
+  );
+
+const {
+  name: { value: name },
+} = fields;
+
+function close() {
+  emits("update:is-visible", false);
+}
+
+const onSubmit = handleSubmit.withControlled(async formData => {
+  if (isLoading.value) {
+    return;
+  }
+
+  isLoading.value = true;
+
+  try {
+    await variantsStore.patchName(props.variantId, formData.name);
+
+    generalStore.messageProvider.success(`Variant renamed!`);
+
+    close();
+  } catch (error) {
+    console.error(error);
+
+    setValidationErrors(error);
+
+    generalStore.messageProvider.error(`Variant rename operation failed!`);
+  } finally {
+    isLoading.value = false;
+  }
+});
+</script>
