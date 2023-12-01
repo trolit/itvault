c54a424f73ac6ed2fb84fee7d02a043365fb5f26
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-12-01 19:31:51 +0100

    Create ApiRequestDefinition.ts

---
+++ frontend/src/types/ApiRequestDefinition.ts
@@ -0,0 +1,15 @@
+import type { GenericObject } from "vee-validate";
+
+type PrintStatus = (message: string) => void;
+
+export type FormCallHandler = <T extends GenericObject>(
+  formData: T,
+  printSuccess: PrintStatus
+) => void | Promise<void>;
+
+export type CallHandler = (printSuccess: PrintStatus) => Promise<void>;
+
+export type ErrorHandler = (
+  error: unknown,
+  printError: PrintStatus
+) => void | Promise<void>;
