1275f935b8155fd54d07301c5c104e60018f27e7
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-12-05 21:32:51 +0100

    Update initializeEngineIO.ts

--- backend/src/utils/initializeEngineIO.ts
+++ backend/src/utils/initializeEngineIO.ts
@@ -1,13 +1,15 @@
-import { attach } from "engine.io";
-import { Server, IncomingMessage, ServerResponse } from "http";
+import { Server } from "engine.io";
 
 import { APP } from "@config";
 
-export const initializeEngineIO = (
-  server: Server<typeof IncomingMessage, typeof ServerResponse>
-) =>
-  attach(server, {
+export const initializeEngineIO = () => {
+  return new Server({
     cors: {
       origin: APP.URL,
     },
+
+    allowRequest: (request, callback) => {
+      return callback(null, true);
+    },
   });
+};
