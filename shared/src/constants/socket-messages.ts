dcd0c124e9308387001dfbd4b8e3dcfe507a4f25
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-12-08 00:33:04 +0100

    Update socket-messages.ts

--- shared/src/constants/socket-messages.ts
+++ shared/src/constants/socket-messages.ts
@@ -1,15 +1,20 @@
 // [SECTION]
-//    NAME - message (FE -> BE)
+//    TYPE - message (FE -> BE)
 //    OUT - possible messages (BE -> FE))
 
 export default {
-  VIEW_WORKSPACE: {
-    NAME: "view-workspace",
+  VIEW_DASHBOARD: {
+    TYPE: "view-dashboard",
     ACTIONS: {
       CREATE_WORKSPACE: "create-workspace",
       UPDATE_WORKSPACE: "update-workspace",
       DELETE_WORKSPACE: "delete-workspace",
+    },
+  },
 
+  VIEW_WORKSPACE: {
+    TYPE: "view-workspace",
+    ACTIONS: {
       CREATE_BLUEPRINT: "create-blueprint",
       UPDATE_BLUEPRINT: "update-blueprint",
       DELETE_BLUEPRINT: "delete-blueprint",
