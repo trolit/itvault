0ed1fec731471452e25939be6906957f9261cfba
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-12-07 23:58:59 +0100

    engine.io - handshake on valid stoken

--- backend/src/utils/initializeEngineIO.ts
+++ backend/src/utils/initializeEngineIO.ts
@@ -1,6 +1,12 @@
 import { Server } from "engine.io";
+import { IAuthService } from "types/services/IAuthService";
 
 import { APP } from "@config";
+
+import { Di } from "@enums/Di";
+
+import { getInstanceOf } from "@helpers/getInstanceOf";
+import { getTokenCookieValue } from "@helpers/getTokenCookieValue";
 
 export const initializeEngineIO = () => {
   return new Server({
@@ -9,7 +15,28 @@ export const initializeEngineIO = () => {
       credentials: true,
     },
 
-    allowRequest: (request, callback) => {
+    // @NOTE handshake (only) on valid token
+    allowRequest: async (request, callback) => {
+      const cookie = request.headers.cookie;
+
+      if (!cookie) {
+        return callback("Missing credentials!", false);
+      }
+
+      const token = getTokenCookieValue(cookie);
+
+      if (!token) {
+        return callback(null, false);
+      }
+
+      const authService = getInstanceOf<IAuthService>(Di.AuthService);
+
+      const result = authService.verifyToken(token);
+
+      if (result.error) {
+        return callback(null, false);
+      }
+
       return callback(null, true);
     },
   });
