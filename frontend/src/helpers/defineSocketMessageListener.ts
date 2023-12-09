9c7e34c6f451ee67ebb94d80e9d1f22c976a9548
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-12-09 19:17:03 +0100

    Create defineSocketMessageListener.ts

---
+++ frontend/src/helpers/defineSocketMessageListener.ts
@@ -0,0 +1,32 @@
+import { watch } from "vue";
+import { storeToRefs } from "pinia";
+
+import { useAuthStore } from "@/store/auth";
+import type { SocketReceiveMessage } from "@shared/types/transport/SocketReceiveMessage";
+
+export const defineSocketMessageListener = (options: {
+  onMessage: <T>(data: SocketReceiveMessage<T>) => void;
+}) => {
+  const authStore = useAuthStore();
+  const { socket } = storeToRefs(authStore);
+
+  const { onMessage } = options;
+
+  watch(socket, value => {
+    if (!value) {
+      return;
+    }
+
+    value.on("message", data => {
+      let parsedData;
+
+      try {
+        parsedData = JSON.parse(data);
+
+        onMessage(parsedData);
+      } catch (error) {
+        console.log("Failed to parse incoming message!");
+      }
+    });
+  });
+};
