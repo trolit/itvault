1259362df16bbe32e02ab3350dabedf80ae8c100
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-12-14 00:09:50 +0100

    Backend - get all workspaces - filter by name (#402)

--- backend/src/schemas/Workspace/useGetAllSuperSchema.ts
+++ backend/src/schemas/Workspace/useGetAllSuperSchema.ts
@@ -1,5 +1,5 @@
-import { number, object } from "yup";
 import { SuperSchema } from "types/SuperSchema";
+import { boolean, number, object, string } from "yup";
 import { GetAllControllerTypes } from "types/controllers/Workspace/GetAllController";
 
 import { pageSchema, perPageSchema } from "@schemas/common/paginationSchemas";
@@ -9,12 +9,14 @@ const querySchema: SuperSchema.Fragment<GetAllControllerTypes.v1.QueryInput> =
   object({
     page: pageSchema.optional(),
     perPage: perPageSchema.optional(),
+    ignorePagination: boolean().default(false),
     filters: object()
       .default({})
       .transform(value => JSON.parse(value))
       .required()
       .shape({
         userId: number().optional().min(1),
+        name: string().optional().min(1),
       }),
   });
 
