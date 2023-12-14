956d3de9f77cdd94505fea928297be7111b4f182
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-12-14 13:59:33 +0100

    Update ManageWorkspacesDrawer.vue

--- frontend/src/components/users/ManageWorkspacesDrawer.vue
+++ frontend/src/components/users/ManageWorkspacesDrawer.vue
@@ -15,18 +15,47 @@
         justifyContent: 'space-between',
       }"
     >
-      <!-- @TODO -->
+      <n-h2 v-if="!!user">{{ user.fullName }} workspaces</n-h2>
+
+      <loading-section v-if="isLoading" />
+
+      <empty v-else-if="!data.value.length" title="Workspaces not found." />
+
+      <div v-else>
+        <n-space :size="[10, 15]">
+          <n-tag
+            v-for="(workspace, index) in data.value"
+            :key="index"
+            round
+            closable
+            type="warning"
+            @close="excludeWorkspace(workspace.id)"
+          >
+            {{ workspace.name }}
+          </n-tag>
+        </n-space>
+      </div>
 
       <template #footer>
-        <n-button secondary :loading="isLoading" @click="$emit('close')">
-          Cancel
-        </n-button>
+        <n-space>
+          <n-button secondary :loading="isLoading" @click="$emit('close')">
+            Cancel
+          </n-button>
+
+          <n-button
+            type="warning"
+            :loading="isLoading"
+            @click="onReset"
+            :disabled="isInitialValue"
+          >
+            Reset
+          </n-button>
+        </n-space>
 
         <n-button
           type="info"
-          secondary
           :loading="isLoading"
-          :disabled="isLoading"
+          :disabled="isLoading || isInitialValue"
         >
           Save
         </n-button>
@@ -36,10 +65,18 @@
 </template>
 
 <script setup lang="ts">
-import { ref, toRefs } from "vue";
-import { NButton, NDrawer, NDrawerContent } from "naive-ui";
+import cloneDeep from "lodash/cloneDeep";
+import { reactive, ref, toRefs } from "vue";
+import { NButton, NDrawer, NDrawerContent, NH2, NTag, NSpace } from "naive-ui";
 
+import { useGeneralStore } from "@/store/general";
+import Empty from "@/components/common/Empty.vue";
+import { useWorkspacesStore } from "@/store/workspaces";
+import { defineComputed } from "@/helpers/defineComputed";
+import { defineWatchers } from "@/helpers/defineWatchers";
 import type { IUserDto } from "@shared/types/dtos/IUserDto";
+import LoadingSection from "@/components/common/LoadingSection.vue";
+import type { IWorkspaceDto } from "@shared/types/dtos/IWorkspaceDto";
 
 interface IProps {
   isVisible: boolean;
@@ -47,11 +84,73 @@ interface IProps {
   user: IUserDto | null;
 }
 
+const generalStore = useGeneralStore();
+const workspacesStore = useWorkspacesStore();
+
 const props = defineProps<IProps>();
-
-const emits = defineEmits(["close"]);
-
 const { isVisible } = toRefs(props);
 
+defineEmits(["close"]);
+
 const isLoading = ref(false);
+const data: { initialValue: IWorkspaceDto[]; value: IWorkspaceDto[] } =
+  reactive({ initialValue: [], value: [] });
+
+function onReset() {
+  data.value = cloneDeep(data.initialValue);
+}
+
+function excludeWorkspace(id: number) {
+  const workspaceIndex = data.value.findIndex(workspace => workspace.id === id);
+
+  if (~workspaceIndex) {
+    data.value.splice(workspaceIndex, 1);
+  }
+}
+
+async function fetchWorkspaces(name?: string) {
+  if (!props.user) {
+    return;
+  }
+
+  isLoading.value = true;
+
+  try {
+    const { result } = await workspacesStore.getAll({
+      ignorePagination: true,
+      filters: { userId: props.user.id, name },
+    });
+
+    data.value = result;
+
+    data.initialValue = cloneDeep(result);
+  } catch (error) {
+    console.log(error);
+
+    generalStore.messageProvider.error(
+      "There was an error when trying to load user workspaces."
+    );
+  } finally {
+    isLoading.value = false;
+  }
+}
+
+const { isInitialValue } = defineComputed({
+  isInitialValue() {
+    return JSON.stringify(data.value) === JSON.stringify(data.initialValue);
+  },
+});
+
+defineWatchers({
+  isVisible: {
+    source: isVisible,
+    handler: () => {
+      if (!isVisible.value) {
+        return;
+      }
+
+      fetchWorkspaces();
+    },
+  },
+});
 </script>
