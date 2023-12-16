453ddc3cd9c664e112229b99fdbc4edc1f70bede
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-12-16 01:34:29 +0100

    Update UserService.spec.ts

--- backend/tests/unit/services/UserService.spec.ts
+++ backend/tests/unit/services/UserService.spec.ts
@@ -8,6 +8,19 @@ import { UserService } from "@services/UserService";
 import { UserRepository } from "@repositories/UserRepository";
 import { DataStoreService } from "@services/DataStoreService";
 
+const roleId = 1;
+
+const usersToUpdate: UpdateUserDto[] = [
+  {
+    id: 1,
+    data: { roleId },
+  },
+  {
+    id: 2,
+    data: { roleId },
+  },
+];
+
 describe("UserService tests", () => {
   let sandbox: SinonSandbox;
   let dataStoreService: DataStoreService;
@@ -24,17 +37,6 @@ describe("UserService tests", () => {
 
   it("should throw error that not all roles are available", async () => {
     // Arrange
-    const usersToUpdate: UpdateUserDto[] = [
-      {
-        id: 1,
-        data: { roleId: 1 },
-      },
-      {
-        id: 2,
-        data: { roleId: 3 },
-      },
-    ];
-
     const userRepository = mockRepository(UserRepository, sandbox);
 
     const userService = new UserService(userRepository, dataStoreService);
@@ -49,5 +51,26 @@ describe("UserService tests", () => {
       result.error,
       "Not all roles are available to perform operation."
     );
+  });
+
+  it("should save users", async () => {
+    // Arrange
+    const userRepository = mockRepository(UserRepository, sandbox, {
+      fakeManager: {
+        find: sandbox.stub().callsFake(() => [{ id: roleId }]),
+      },
+    });
+
+    const userService = new UserService(userRepository, dataStoreService);
+
+    // Act
+    const result = await userService.updateMany(usersToUpdate);
+
+    console.log(result.value);
+
+    // Assert
+    assert.equal(result.isSuccess, true);
+
+    assert.equal(result.error, undefined);
   });
 });
