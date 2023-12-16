55772a31334a2c9d4de7be4f808afd23a8a0d1c7
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-12-16 01:14:34 +0100

    Create mockRepository.ts

---
+++ backend/tests/unit/mockRepository.ts
@@ -0,0 +1,38 @@
+import * as typeorm from "typeorm";
+import { SinonSandbox, SinonStubbedInstance } from "sinon";
+
+export const mockRepository = <T>(
+  Class: { new (): T },
+  sandbox: SinonSandbox,
+  overrides: {
+    fakeManager?: Partial<typeorm.EntityManager>;
+    fakeQueryRunner?: Partial<typeorm.QueryRunner>;
+  } = {}
+): SinonStubbedInstance<T> => {
+  const {
+    fakeManager: fakeManagerOverrides,
+    fakeQueryRunner: fakeQueryRunnerOverrides,
+  } = overrides;
+
+  const fakeManager = {
+    getRepository: sandbox.stub().returnsThis(),
+    createQueryBuilder: sandbox.stub().returnsThis(),
+    useTransaction: sandbox.stub().returnsThis(),
+    where: sandbox.stub().returnsThis(),
+    find: sandbox.stub().returnsThis(),
+    ...fakeManagerOverrides,
+  };
+
+  const fakeQueryRunner = {
+    manager: fakeManager,
+    release: sandbox.stub().returnsThis(),
+    rollbackTransaction: sandbox.stub().returnsThis(),
+    ...fakeQueryRunnerOverrides,
+  };
+
+  const repository = sandbox.createStubInstance(Class);
+
+  (repository as any).useTransaction = () => fakeQueryRunner;
+
+  return repository;
+};
