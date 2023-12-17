968cd43657577207a72e2d2e2daabc7f8b097554
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-12-17 20:27:29 +0100

    Update useSignInSuperSchema.ts

--- backend/src/schemas/Auth/useSignInSuperSchema.ts
+++ backend/src/schemas/Auth/useSignInSuperSchema.ts
@@ -2,13 +2,13 @@ import { object, string } from "yup";
 import { SuperSchema } from "types/SuperSchema";
 import { SignInControllerTypes } from "types/controllers/Auth/SignInController";
 
-import { AUTH_RULES } from "@shared/constants/rules";
+import { ACCOUNT_RULES } from "@shared/constants/rules";
 
 import { defineSuperSchemaRunner } from "@schemas/common/defineSuperSchemaRunner";
 
 const bodySchema: SuperSchema.Fragment<SignInControllerTypes.v1.Body> = object({
   // @NOTE https://www.rfc-editor.org/rfc/rfc5321#section-4.5.3
-  email: string().required().email().max(AUTH_RULES.MAX_EMAIL_LENGTH),
+  email: string().required().email().max(ACCOUNT_RULES.EMAIL.MAX_LENGTH),
   password: string().required(),
 });
 
