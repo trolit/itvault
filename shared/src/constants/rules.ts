d0d89807f081dffeca874f7b19d6fa277eb2d493
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-12-17 20:06:25 +0100

    init "AUTH_RULES" constants

---
+++ shared/src/constants/rules.ts
@@ -0,0 +1,8 @@
+export const AUTH_RULES = {
+  MAX_EMAIL_LENGTH: 254,
+  MIN_PASSWORD_LENGTH: 7,
+  ONE_DIGIT_LETTER_REGEX: /\d/,
+  ONE_LOWERCASE_LETTER_REGEX: /[a-z]/,
+  ONE_UPPERCASE_LETTER_REGEX: /[A-Z]/,
+  ONE_SPECIAL_CHARACTER_REGEX: /[*.!@#$%^&(){}[\]:;<>,.?/~_+-=|]/,
+};
