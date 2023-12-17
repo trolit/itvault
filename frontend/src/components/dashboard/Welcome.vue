d031e36d69d39f0324db25dbd763be84a6896130
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-12-17 22:17:39 +0100

    Update Welcome.vue

--- frontend/src/components/dashboard/Welcome.vue
+++ frontend/src/components/dashboard/Welcome.vue
@@ -1,28 +1,34 @@
 <template>
-  <n-h1> _Dashboard </n-h1>
+  <span
+    :style="{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+    }"
+  >
+    <div :style="{ fontSize: '25px' }">
+      Welcome
+      <n-gradient-text> {{ authStore.profile.fullName }} </n-gradient-text>
+    </div>
 
-  <n-h2>
-    Welcome
-    <n-gradient-text> {{ authStore.profile.fullName }} </n-gradient-text>
-  </n-h2>
+    <div class="text-center">
+      <n-text>
+        <n-tag>{{ formattedNow }}</n-tag> ({{ dayOfYear }} day)
+      </n-text>
 
-  <div>
-    <n-text>
-      Today is <n-tag>{{ formattedNow }}</n-tag> ({{ dayOfYear }} day)</n-text
-    >
-  </div>
-
-  <div>
-    <n-text depth="3">
-      <small>
-        ({{ remainingDays }} days left till the end of the year 2023)
-      </small>
-    </n-text>
-  </div>
+      <div>
+        <n-text depth="3">
+          <small>
+            ({{ remainingDays }} days left till the end of the year 2023)
+          </small>
+        </n-text>
+      </div>
+    </div>
+  </span>
 </template>
 
 <script setup lang="ts">
-import { NH2, NH1, NGradientText, NText, NTag } from "naive-ui";
+import { NGradientText, NText, NTag } from "naive-ui";
 
 import { useAuthStore } from "@/store/auth";
 import { useDateService } from "@/services/useDateService";
