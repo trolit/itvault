27194df5c3d2ac8619445812a26534d802b43572
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-12-17 20:23:46 +0100

    restructure rules

--- shared/src/constants/rules.ts
+++ shared/src/constants/rules.ts
@@ -1,8 +1,21 @@
-export const AUTH_RULES = {
-  MAX_EMAIL_LENGTH: 254,
-  MIN_PASSWORD_LENGTH: 7,
-  ONE_DIGIT_LETTER_REGEX: /\d/,
-  ONE_LOWERCASE_LETTER_REGEX: /[a-z]/,
-  ONE_UPPERCASE_LETTER_REGEX: /[A-Z]/,
-  ONE_SPECIAL_CHARACTER_REGEX: /[*.!@#$%^&(){}[\]:;<>,.?/~_+-=|]/,
+export const ACCOUNT_RULES = {
+  FIRST_NAME: {
+    MIN_LENGTH: 2,
+  },
+
+  LAST_NAME: {
+    MIN_LENGTH: 2,
+  },
+
+  EMAIL: {
+    MAX_LENGTH: 254,
+  },
+
+  PASSWORD: {
+    MIN_LENGTH: 7,
+    ONE_DIGIT_LETTER_REGEX: /\d/,
+    ONE_LOWERCASE_LETTER_REGEX: /[a-z]/,
+    ONE_UPPERCASE_LETTER_REGEX: /[A-Z]/,
+    ONE_SPECIAL_CHARACTER_REGEX: /[*.!@#$%^&(){}[\]:;<>,.?/~_+-=|]/,
+  },
 };
