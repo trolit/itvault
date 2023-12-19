3887cb1612ced72964786ab579596318823dccf3
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-12-20 00:38:38 +0100

    init PermissionsModal

---
+++ frontend/src/components/header/PermissionsModal.vue
@@ -0,0 +1,27 @@
+<template>
+  <n-modal
+    :show="isVisible"
+    segmented
+    title="Your permissions:"
+    preset="card"
+    :bordered="true"
+    :closable="true"
+    :close-on-esc="true"
+    :mask-closable="false"
+    :style="{ width: '600px' }"
+    @close="$emit('update:is-visible', false)"
+  >
+  </n-modal>
+</template>
+
+<script setup lang="ts">
+import { NModal } from "naive-ui";
+
+interface IProps {
+  isVisible: boolean;
+}
+
+defineProps<IProps>();
+
+defineEmits(["update:is-visible"]);
+</script>
