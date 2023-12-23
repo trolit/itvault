5cdca282e486a693e8b363a32f44c92e27b82847
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-12-23 11:57:14 +0100

    Update IRoleService.ts

--- backend/src/types/services/IRoleService.ts
+++ backend/src/types/services/IRoleService.ts
@@ -1,16 +1,16 @@
 import { Role } from "@entities/Role";
+import { IRoleAddEditDto } from "@shared/types/dtos/Role";
 import { TransactionResult } from "types/TransactionResult";
-import { AddEditRoleDto } from "@shared/types/dtos/AddEditRoleDto";
 
 export interface IRoleService {
   create(
     userId: number,
-    data: AddEditRoleDto
+    data: IRoleAddEditDto
   ): Promise<TransactionResult<Role>>;
 
   update(
     id: number,
     userId: number,
-    data: AddEditRoleDto
+    data: IRoleAddEditDto
   ): Promise<TransactionResult<Role>>;
 }
