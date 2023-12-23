e6e3a54eeb912564a79c9b9b95d4e0de53754381
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-12-23 12:41:34 +0100

    Update AddVariantForm.ts

--- frontend/src/types/AddVariantForm.ts
+++ frontend/src/types/AddVariantForm.ts
@@ -1,4 +1,4 @@
-import { IAddVariantDto } from "@shared/types/dtos/Variant";
+import type { IAddVariantDto } from "@shared/types/dtos/Variant";
 
 export type AddVariantForm = Pick<IAddVariantDto, "name"> & {
   file: File | null;
