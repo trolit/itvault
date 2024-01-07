353c0b98529dd7fdf5d02bbcaea7a393cde801e5
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-01-07 13:26:03 +0100

    remove deprecated action

--- backend/src/types/repositories/IVariantRepository.ts
+++ backend/src/types/repositories/IVariantRepository.ts
@@ -1,15 +1,4 @@
 import { Variant } from "@entities/Variant";
 import { IBaseRepository } from "./IBaseRepository";
-import { IFormDataFile } from "types/IFormDataFile";
 
-export interface IVariantRepository extends IBaseRepository<Variant> {
-  save(
-    userId: number,
-    formDataBody: {
-      name: string;
-      fileId: number;
-      variantId?: string;
-    },
-    formDataFiles: IFormDataFile[]
-  ): Promise<Variant | null>;
-}
+export interface IVariantRepository extends IBaseRepository<Variant> {}
