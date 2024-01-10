e7080f95035c862491970e487bf3b5f684e00600
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-01-10 20:47:00 +0100

    dynamically show fullName in ProfileDropdown

--- frontend/src/components/header/ProfileDropdown.vue
+++ frontend/src/components/header/ProfileDropdown.vue
@@ -5,7 +5,7 @@
     :width="250"
     @select="handleSelect"
   >
-    <n-button quaternary>{{ authStore.profile.email }}</n-button>
+    <n-button quaternary>{{ authStore.profile.fullName }}</n-button>
   </n-dropdown>
 </template>
 
