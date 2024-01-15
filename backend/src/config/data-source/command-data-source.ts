53e589a7f917980d2ef285ae4a5a5897342ba172
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-01-15 23:10:46 +0100

    Backend - seeders with production coverage (#429)

---
+++ backend/src/config/data-source/command-data-source.ts
@@ -0,0 +1,41 @@
+import { SeederOptions } from "typeorm-extension";
+import { DataSource, DataSourceOptions } from "typeorm";
+
+import { DATABASE, APP } from "@config";
+
+import RoleSeeder from "./seeds/1690115009-RoleSeeder";
+import PermissionSeeder from "./seeds/1690115077-PermissionSeeder";
+import PermissionToRoleSeeder from "./seeds/1690115090-PermissionToRoleSeeder";
+
+import { Environment } from "@enums/Environment";
+
+const { ENV } = APP;
+const { HOST, NAME, PORT, TYPE, ROOT } = DATABASE;
+
+const isProduction = ENV === Environment.Production;
+
+const getGlobPattern = (path: string) => `src/${path}`;
+
+const options: DataSourceOptions & SeederOptions = {
+  type: TYPE,
+  host: HOST,
+  port: PORT,
+  username: ROOT.USERNAME,
+  password: ROOT.PASSWORD,
+  database: NAME,
+  entities: [getGlobPattern(`entities/*`)],
+  migrations: [getGlobPattern(`migrations/*`)],
+  subscribers: [getGlobPattern(`subscribers/*`)],
+  logging: true,
+  synchronize: false,
+
+  seeds: isProduction
+    ? [RoleSeeder, PermissionSeeder, PermissionToRoleSeeder]
+    : [`${getGlobPattern(`config/data-source/seeds/*`)}`],
+  factories: isProduction
+    ? []
+    : [`${getGlobPattern(`config/data-source/factories/*`)}`],
+};
+
+// @NOTE separate DS config to simplify primary DS (nevertheless of NODE_ENV, TypeORM commands need to reach for src)
+export const commandDataSource = new DataSource(options);
