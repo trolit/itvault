b0a68a434355700ea7c98314e0a66fde87416d5e
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-01-18 00:38:06 +0100

    add SoftDeleteController for "ChatMessage"

--- backend/src/routes/v1/chat-messages.ts
+++ backend/src/routes/v1/chat-messages.ts
@@ -4,16 +4,18 @@ import { WorkspaceId } from "types/controllers/WorkspaceId";
 import { processRequestWith } from "@helpers/processRequestWith";
 import { validateRequestWith } from "@middleware/validateRequestWith";
 import { IsWorkspaceAvailable } from "@middleware/isWorkspaceAvailable";
+import { requireEndpointVersion } from "@middleware/requireEndpointVersion";
 import { requireWorkspaceAccess } from "@middleware/requireWorkspaceAccess";
 
 import { useAddSuperSchema } from "@schemas/ChatMessage/useAddSuperSchema";
-import { useUpdateSuperSchema } from "@schemas/ChatMessage/useUpdateSuperSchema";
 import { useGetAllSuperSchema } from "@schemas/ChatMessage/useGetAllSuperSchema";
+import { useUpdateSuperSchema } from "@schemas/ChatMessage/useUpdateSuperSchema";
 
 import { BaseController } from "@controllers/BaseController";
 import { AddController } from "@controllers/ChatMessage/AddController";
 import { UpdateController } from "@controllers/ChatMessage/UpdateController";
 import { GetAllController } from "@controllers/ChatMessage/GetAllController";
+import { SoftDeleteController } from "@controllers/ChatMessage/SoftDeleteController";
 
 const chatMessagesRouter = Router();
 
@@ -42,6 +44,12 @@ chatMessagesRouter.put(
   "/:id",
   validateRequestWith({ [v1]: useUpdateSuperSchema }),
   processRequestWith(UpdateController)
+);
+
+chatMessagesRouter.delete(
+  "/:id",
+  requireEndpointVersion(SoftDeleteController.ALL_VERSIONS),
+  processRequestWith(SoftDeleteController)
 );
 
 export = chatMessagesRouter;
