a9f4f0a2c6d33e854e55336395e3b1142895008c
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-01-18 22:48:13 +0100

    check if max depth not reached when adding message

--- backend/src/schemas/ChatMessage/useAddSuperSchema.ts
+++ backend/src/schemas/ChatMessage/useAddSuperSchema.ts
@@ -2,6 +2,14 @@ import sanitizeHtml from "sanitize-html";
 import { number, object, string } from "yup";
 import { SuperSchema } from "types/SuperSchema";
 import { AddControllerTypes } from "types/controllers/ChatMessage/AddController";
+import { IChatMessageRepository } from "types/repositories/IChatMessageRepository";
+
+import { Di } from "@enums/Di";
+import { WORKSPACE_CHAT_MAX_DEPTH } from "@shared/constants/config";
+
+import { setYupError } from "@helpers/yup/setError";
+import { getInstanceOf } from "@helpers/getInstanceOf";
+import { CUSTOM_MESSAGES } from "@helpers/yup/custom-messages";
 
 import { defineSuperSchemaRunner } from "@schemas/common/defineSuperSchemaRunner";
 
@@ -14,7 +22,36 @@ const bodySchema: SuperSchema.Fragment<AddControllerTypes.v1.Body> = object({
     .required()
     .transform(value => sanitizeHtml(value)),
 
-  replyToId: number().optional(),
+  replyToId: number()
+    .optional()
+    .test(async (value: number | undefined, ctx) => {
+      const chatMessageRepository = getInstanceOf<IChatMessageRepository>(
+        Di.ChatMessageRepository
+      );
+
+      const message = await chatMessageRepository.getOne({
+        where: {
+          id: value || 0,
+        },
+      });
+
+      if (!message) {
+        return ctx.createError({
+          message: setYupError(
+            CUSTOM_MESSAGES.GENERAL.NOT_AVAILABLE,
+            "Message"
+          ),
+        });
+      }
+
+      if (message.depth + 1 > WORKSPACE_CHAT_MAX_DEPTH) {
+        return ctx.createError({
+          message: setYupError(CUSTOM_MESSAGES.CHAT_MESSAGE.MAX_DEPTH_REACHED),
+        });
+      }
+
+      return true;
+    }),
 });
 
 export const useAddSuperSchema: SuperSchema.Runner<
