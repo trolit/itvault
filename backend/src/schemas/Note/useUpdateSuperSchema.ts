214e1ff43cbe1ce64580631bbb767e0162593a6f
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-01-19 00:12:27 +0100

    Update useUpdateSuperSchema.ts

--- backend/src/schemas/Note/useUpdateSuperSchema.ts
+++ backend/src/schemas/Note/useUpdateSuperSchema.ts
@@ -2,11 +2,13 @@ import { object } from "yup";
 import { SuperSchema } from "types/SuperSchema";
 import { UpdateControllerTypes } from "types/controllers/Note/UpdateController";
 
-import { textSchema } from "@schemas/common/useTextSchema";
+import { NOTE_RULES } from "@shared/constants/rules";
+
+import { useTextSchema } from "@schemas/common/useTextSchema";
 import { defineSuperSchemaRunner } from "@schemas/common/defineSuperSchemaRunner";
 
 const bodySchema: SuperSchema.Fragment<UpdateControllerTypes.v1.Body> = object({
-  text: textSchema,
+  text: useTextSchema(NOTE_RULES.VALUE.MAX_LENGTH),
 });
 
 export const useUpdateSuperSchema: SuperSchema.Runner<
