3fefbbd0d4b43c14fc14f5306fecfb7118ca55b0
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-01-19 13:21:08 +0100

    UpdateController - find entity also by workspaceId

--- backend/src/types/controllers/ChatMessage/UpdateController.ts
+++ backend/src/types/controllers/ChatMessage/UpdateController.ts
@@ -1,13 +1,16 @@
+import { WorkspaceId } from "types/controllers/WorkspaceId";
 import { IUpdateChatMessageDTO } from "@shared/types/DTOs/ChatMessage";
 
 export namespace UpdateControllerTypes {
   export namespace v1 {
+    export type Query = WorkspaceId;
+
     export type Params = {
       id: number;
     };
 
     export type Body = IUpdateChatMessageDTO;
 
-    export type Request = CustomRequest<Params, Body>;
+    export type Request = CustomRequest<Params, Body, Query>;
   }
 }
