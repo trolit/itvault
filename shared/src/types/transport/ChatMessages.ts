e055ad29c2471793c0c703e166feabe4443b273b
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-01-19 11:33:52 +0100

    use `Pick` when defining `DeleteChatMessageData`

--- shared/src/types/transport/ChatMessages.ts
+++ shared/src/types/transport/ChatMessages.ts
@@ -2,6 +2,6 @@ import { IChatMessageDTO } from "../DTOs/ChatMessage";
 
 export type AddChatMessageData = IChatMessageDTO;
 
-export type DeleteChatMessageData = { id: number };
+export type DeleteChatMessageData = Pick<IChatMessageDTO, "id">;
 
 export type UpdateChatMessageData = Pick<IChatMessageDTO, "id" | "value">;
