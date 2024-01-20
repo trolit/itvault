7b62b13e826a048901605ef48f3b133c9d5061a7
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-01-21 00:09:00 +0100

    Update 1705442163737-CreateChatMessagesTable.ts

--- backend/src/migrations/1705442163737-CreateChatMessagesTable.ts
+++ backend/src/migrations/1705442163737-CreateChatMessagesTable.ts
@@ -7,11 +7,7 @@ export class CreateChatMessagesTable1705442163737
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE \`chat_messages\` (\`id\` int NOT NULL AUTO_INCREMENT, \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updatedAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`deletedAt\` datetime(6) NULL, \`value\` longtext NOT NULL, \`depth\` int NOT NULL DEFAULT '1', \`repliesCount\` int NOT NULL DEFAULT '0', \`workspaceId\` int NULL, \`createdById\` int NULL, \`replyToId\` int NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`
-    );
-
-    await queryRunner.query(
-      `ALTER TABLE \`chat_messages\` ADD CONSTRAINT \`FK_af9f7aa7aafd85f11cb9df9e5b0\` FOREIGN KEY (\`workspaceId\`) REFERENCES \`workspaces\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`
+      `CREATE TABLE \`chat_messages\` (\`id\` int NOT NULL AUTO_INCREMENT, \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updatedAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`deletedAt\` datetime(6) NULL, \`value\` longtext NOT NULL, \`depth\` int NOT NULL DEFAULT '1', \`repliesCount\` int NOT NULL DEFAULT '0', \`createdById\` int NULL, \`replyToId\` int NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`
     );
 
     await queryRunner.query(
@@ -30,10 +26,6 @@ export class CreateChatMessagesTable1705442163737
 
     await queryRunner.query(
       `ALTER TABLE \`chat_messages\` DROP FOREIGN KEY \`FK_5bcea85525fc4290894ba6d96b5\``
-    );
-
-    await queryRunner.query(
-      `ALTER TABLE \`chat_messages\` DROP FOREIGN KEY \`FK_af9f7aa7aafd85f11cb9df9e5b0\``
     );
 
     await queryRunner.query(`DROP TABLE \`chat_messages\``);
