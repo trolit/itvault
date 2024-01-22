59c11830b43b653178031cee84732a79e270c8e2
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-01-22 20:23:30 +0100

    Create Index.vue

---
+++ frontend/src/components/GlobalChat/Index.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="global-chat">
+    <loading-section v-if="chatMessagesStore.items.length === 0 && isLoading" />
+
+    <div class="wrapper" v-else>
+      <n-scrollbar>
+        <message
+          v-for="(item, index) in chatMessagesStore.items"
+          :key="index"
+          :value="item"
+        />
+      </n-scrollbar>
+
+      <div class="footer">
+        <n-button :disabled="isLoading" size="small" ghost>
+          <n-icon :component="AddIcon" :size="25" />
+        </n-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, toRefs, watch } from "vue";
+import { Add as AddIcon } from "@vicons/carbon";
+import { NScrollbar, NButton, NIcon } from "naive-ui";
+
+import Message from "./Message.vue";
+import { useGeneralStore } from "@/store/general";
+import { useChatMessagesStore } from "@/store/chat-messages";
+import LoadingSection from "@/components/common/LoadingSection.vue";
+
+const generalStore = useGeneralStore();
+const chatMessagesStore = useChatMessagesStore();
+const { isChatVisible } = toRefs(generalStore);
+
+const isLoading = ref(false);
+
+async function getMessages(page: number, messageId?: number) {
+  if (isLoading.value) {
+    return;
+  }
+
+  isLoading.value = true;
+
+  try {
+    await chatMessagesStore.getAll({
+      page,
+      perPage: chatMessagesStore.ITEMS_PER_PAGE,
+      messageId,
+    });
+  } catch (error) {
+    console.log(error);
+
+    // @TODO
+  } finally {
+    isLoading.value = false;
+  }
+}
+
+watch(isChatVisible, () => {
+  if (!isChatVisible.value || chatMessagesStore.items.length) {
+    return;
+  }
+
+  getMessages(1);
+});
+</script>
