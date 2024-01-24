724d461263140a07fdc17795e823bf2cc15f14db
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-01-24 23:46:36 +0100

    Update Index.vue

--- frontend/src/components/GlobalChat/Index.vue
+++ frontend/src/components/GlobalChat/Index.vue
@@ -4,10 +4,12 @@
 
     <div class="wrapper" v-else>
       <n-scrollbar>
-        <message
-          v-for="(item, index) in chatMessagesStore.items"
+        <thread
+          v-for="(item, index) in NESTED_ITEMS_BY_DEPTH"
           :key="index"
           :item="item"
+          :message-ids-under-load="messageIdsToLoadRepliesTo.value"
+          @load-replies="onRepliesLoad"
         />
       </n-scrollbar>
 
@@ -21,22 +23,27 @@
 </template>
 
 <script setup lang="ts">
-import { ref, toRefs, watch } from "vue";
+import { reactive, ref, toRefs, watch } from "vue";
 import { Add as AddIcon } from "@vicons/carbon";
 import { NScrollbar, NButton, NIcon } from "naive-ui";
 
-import Message from "./Message.vue";
+import Thread from "./Thread.vue";
 import { useGeneralStore } from "@/store/general";
 import { useChatMessagesStore } from "@/store/chat-messages";
 import LoadingSection from "@/components/common/LoadingSection.vue";
+import type { ChatMessage } from "@/types/ChatMessage";
 
 const generalStore = useGeneralStore();
 const chatMessagesStore = useChatMessagesStore();
+
 const { isChatVisible } = toRefs(generalStore);
+const { NESTED_ITEMS_BY_DEPTH } = toRefs(chatMessagesStore);
 
 const isLoading = ref(false);
 
-async function getMessages(page: number, messageId?: number) {
+const messageIdsToLoadRepliesTo: { value: number[] } = reactive({ value: [] });
+
+async function onLoad() {
   if (isLoading.value) {
     return;
   }
@@ -45,9 +52,8 @@ async function getMessages(page: number, messageId?: number) {
 
   try {
     await chatMessagesStore.getAll({
-      page,
+      page: 1,
       perPage: chatMessagesStore.ITEMS_PER_PAGE,
-      messageId,
     });
   } catch (error) {
     console.log(error);
@@ -58,11 +64,39 @@ async function getMessages(page: number, messageId?: number) {
   }
 }
 
-watch(isChatVisible, () => {
+async function onRepliesLoad(message: ChatMessage, page: number) {
+  if (messageIdsToLoadRepliesTo.value.includes(message.id)) {
+    return;
+  }
+
+  messageIdsToLoadRepliesTo.value.push(message.id);
+
+  try {
+    await chatMessagesStore.getAll({
+      page,
+      perPage: chatMessagesStore.ITEMS_PER_PAGE,
+      messageId: message.id,
+    });
+  } catch (error) {
+    console.log(error);
+
+    // @TODO
+  } finally {
+    const idIndex = messageIdsToLoadRepliesTo.value.findIndex(
+      id => id === message.id
+    );
+
+    if (~idIndex) {
+      messageIdsToLoadRepliesTo.value.splice(idIndex, 1);
+    }
+  }
+}
+
+watch(isChatVisible, async () => {
   if (!isChatVisible.value || chatMessagesStore.items.length) {
     return;
   }
 
-  getMessages(1);
+  onLoad();
 });
 </script>
