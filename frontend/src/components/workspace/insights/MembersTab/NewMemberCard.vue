474535c09cfd11c01820d7b9908230245e01a507
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-01-30 21:55:16 +0100

    Create NewMemberCard.vue

---
+++ frontend/src/components/workspace/insights/MembersTab/NewMemberCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <n-card class="new-member-card">
+    <n-icon :component="AddIcon" :depth="3" :size="80" />
+
+    <n-text :depth="3">Add member to workspace</n-text>
+
+    <!-- @TODO - use AsynchronousSelect -->
+    <n-input
+      :style="{ marginTop: '20px' }"
+      disabled
+      type="text"
+      placeholder="Start typing to find user.."
+    />
+  </n-card>
+</template>
+
+<script setup lang="ts">
+import { AddAlt as AddIcon } from "@vicons/carbon";
+import { NCard, NInput, NIcon, NText } from "naive-ui";
+
+interface IProps {}
+
+defineProps<IProps>();
+</script>
