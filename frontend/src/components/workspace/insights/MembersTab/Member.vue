eda47c9e0c87d3b54c60a25e9969a952e8776f57
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-01-30 21:37:44 +0100

    Create Member.vue

---
+++ frontend/src/components/workspace/insights/MembersTab/Member.vue
@@ -0,0 +1,37 @@
+<template>
+  <n-card class="member" :title="item.fullName">
+    <n-list>
+      <n-list-item>
+        <template #prefix> Role </template>
+
+        <n-tag>{{ item.roleName }}</n-tag>
+      </n-list-item>
+
+      <n-list-item>
+        <template #prefix> Email </template>
+
+        <n-text :depth="3"> {{ item.email }} </n-text>
+      </n-list-item>
+
+      <!-- @TODO -->
+      <require-permission :permission="Permission.ManageUserWorkspaces">
+        <n-list-item>
+          <n-button disabled type="error"> Remove from workspace </n-button>
+        </n-list-item>
+      </require-permission>
+    </n-list>
+  </n-card>
+</template>
+
+<script setup lang="ts">
+import { NCard, NText, NList, NListItem, NTag, NButton } from "naive-ui";
+
+import type { IUserDTO } from "@shared/types/DTOs/User";
+import { Permission } from "@shared/types/enums/Permission";
+import RequirePermission from "@/components/common/RequirePermission.vue";
+interface IProps {
+  item: IUserDTO;
+}
+
+defineProps<IProps>();
+</script>
