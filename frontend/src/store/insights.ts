d75cd165d2ad82725f0819db37a47e4dc308d3e1
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-01-30 23:07:01 +0100

    Update insights.ts

--- frontend/src/store/insights.ts
+++ frontend/src/store/insights.ts
@@ -1,15 +1,45 @@
 import { defineStore } from "pinia";
 
+import type { IUserDTO } from "@shared/types/DTOs/User";
+import type { IPaginationQuery } from "@shared/types/IPaginationQuery";
+
 interface IState {
   activeKey: string;
+
+  membersTabData: {
+    total: number;
+
+    items: IUserDTO[];
+
+    pagination: IPaginationQuery;
+  };
 }
 
 export const useInsightsStore = defineStore("insights", {
   state: (): IState => ({
     activeKey: "members",
+
+    membersTabData: {
+      total: 0,
+      items: [],
+      pagination: {
+        page: 1,
+        perPage: 10,
+      },
+    },
   }),
 
   getters: {},
 
-  actions: {},
+  actions: {
+    resetState() {
+      this.activeKey = "members";
+
+      this.membersTabData = {
+        total: 0,
+        items: [],
+        pagination: { page: 1, perPage: 10 },
+      };
+    },
+  },
 });
