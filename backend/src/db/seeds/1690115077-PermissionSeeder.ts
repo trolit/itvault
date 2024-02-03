cd24b187d941952d98e37aa82c142d795907e246
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-02-03 18:58:10 +0100

    Update 1690115077-PermissionSeeder.ts

--- backend/src/db/seeds/1690115077-PermissionSeeder.ts
+++ backend/src/db/seeds/1690115077-PermissionSeeder.ts
@@ -2,12 +2,12 @@ import { DataSource } from "typeorm";
 import { Seeder } from "typeorm-extension";
 import { Permission } from "@db/entities/Permission";
 
-import { ALL_PERMISSIONS } from "@config/permissions";
+import { PERMISSIONS } from "@config/permissions";
 
 export default class PermissionSeeder implements Seeder {
   public async run(dataSource: DataSource) {
     const repository = dataSource.getRepository(Permission);
 
-    await repository.save(ALL_PERMISSIONS);
+    await repository.save(Object.values(PERMISSIONS));
   }
 }
