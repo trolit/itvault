db8f2937130ec52d780be781c9337c266e30529e
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-02-04 16:09:36 +0100

    Update GlobalSubscriber.ts

--- backend/src/db/subscribers/GlobalSubscriber.ts
+++ backend/src/db/subscribers/GlobalSubscriber.ts
@@ -1,14 +1,28 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 import { Note } from "@db/entities/Note";
+import { Bundle } from "@db/entities/Bundle";
+import { Bucket } from "@db/entities/Bucket";
+import { Variant } from "@db/entities/Variant";
+import { Workspace } from "@db/entities/Workspace";
+import { Blueprint } from "@db/entities/Blueprint";
 import { WorkspaceEvent } from "@db/entities/WorkspaceEvent";
 import {
   InsertEvent,
   EventSubscriber,
   EntitySubscriberInterface,
+  EntityManager,
 } from "typeorm";
 
 import { Action } from "@shared/types/enums/Action";
+
+const ENTITIES_TO_LISTEN_TO = [
+  Note.name,
+  Bucket.name,
+  Bundle.name,
+  Variant.name,
+  Blueprint.name,
+];
 
 @EventSubscriber()
 export class WorkspacePartsSubscriber implements EntitySubscriberInterface {
@@ -19,21 +33,48 @@ export class WorkspacePartsSubscriber implements EntitySubscriberInterface {
       metadata: { name: entityName },
     } = event;
 
-    // @TODO
-    // if (entityName === "Note") {
-    //   const castedEntity = <Note>entity;
+    if (!ENTITIES_TO_LISTEN_TO.includes(entityName)) {
+      return;
+    }
 
-    //   const record = manager.create(WorkspaceEvent, {
-    //     entity: entityName,
-    //     action: Action.Create,
-    //     user: castedEntity.createdBy,
-    //     workspace: castedEntity.file.workspace,
-    //     targetId: castedEntity.id.toString(),
-    //   });
+    if (entityName === Note.name) {
+      await onNoteEvent({
+        entity: <Note>entity,
+        action: Action.Create,
+        manager,
+      });
 
-    //   await manager.save(record);
-
-    //   return;
-    // }
+      return;
+    }
   }
+}
+
+async function onNoteEvent(arg: {
+  entity: Note;
+  action: Action;
+  manager: EntityManager;
+}) {
+  // @TODO DELETE event
+
+  const { action, manager, entity } = arg;
+
+  const workspace = await manager.findOneOrFail(Workspace, {
+    where: {
+      files: {
+        id: entity.file.id,
+      },
+    },
+  });
+
+  const record = manager.create(WorkspaceEvent, {
+    entity: Note.name,
+    action,
+    workspace,
+    user: {
+      id: action === Action.Create ? entity.createdBy.id : entity.updatedBy.id,
+    },
+    targetId: entity.id.toString(),
+  });
+
+  await manager.save(record);
 }
