d4dde5d63db3e18c7434ee7ebf2851537738b78b
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-02-04 15:10:38 +0100

    Update 1690115149-BlueprintSeeder.ts

--- backend/src/db/seeds/1690115149-BlueprintSeeder.ts
+++ backend/src/db/seeds/1690115149-BlueprintSeeder.ts
@@ -1,12 +1,16 @@
 import random from "lodash/random";
 import { DataSource } from "typeorm";
 import { faker } from "@faker-js/faker";
+import { User } from "@db/entities/User";
 import { Seeder } from "typeorm-extension";
 import { Workspace } from "@db/entities/Workspace";
 import { Blueprint } from "@db/entities/Blueprint";
 
+import { getRandomRecords } from "./helpers/getRandomRecords";
+
 export default class BlueprintSeeder implements Seeder {
   public async run(dataSource: DataSource) {
+    const userRepository = dataSource.getRepository(User);
     const workspaceRepository = dataSource.getRepository(Workspace);
     const blueprintRepository = dataSource.getRepository(Blueprint);
 
@@ -16,11 +20,15 @@ export default class BlueprintSeeder implements Seeder {
       const descriptionCount = random(8, 25);
 
       for (let index = 0; index < 20; index++) {
+        const [user] = await getRandomRecords(userRepository, 1);
+
         await blueprintRepository.save({
           name: `BR${index + 1}`,
           color: faker.color.rgb({ format: "hex", casing: "upper" }),
           description: faker.random.words(descriptionCount),
           workspace,
+          createdBy: user,
+          updatedBy: user,
         });
       }
     }
