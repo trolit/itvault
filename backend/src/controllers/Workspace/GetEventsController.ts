c449c5db547a31c4a1c7b64d53d641355a82bbf0
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-02-04 22:50:36 +0100

    Create GetEventsController.ts

---
+++ backend/src/controllers/Workspace/GetEventsController.ts
@@ -0,0 +1,66 @@
+import { autoInjectable, inject } from "tsyringe";
+import { StatusCodes as HTTP } from "http-status-codes";
+import { WorkspaceEvent } from "@db/entities/WorkspaceEvent";
+import { WorkspaceEventMapper } from "@mappers/WorkspaceEventMapper";
+import { ControllerImplementation } from "types/controllers/ControllerImplementation";
+import { IWorkspaceEventRepository } from "types/repositories/IWorkspaceEventRepository";
+import { GetEventsControllerTypes } from "types/controllers/Workspace/GetEventsController";
+
+import { Di } from "@enums/Di";
+
+import { BaseController } from "@controllers/BaseController";
+
+const { v1 } = BaseController.ALL_VERSION_DEFINITIONS;
+
+@autoInjectable()
+export class GetEventsController extends BaseController {
+  constructor(
+    @inject(Di.WorkspaceEventRepository)
+    private _workspaceEventRepository: IWorkspaceEventRepository
+  ) {
+    super();
+  }
+
+  implementations: ControllerImplementation[] = [
+    {
+      version: v1,
+      handle: this.v1.bind(this),
+    },
+  ];
+
+  static ALL_VERSIONS = [v1];
+
+  async v1(
+    request: GetEventsControllerTypes.v1.Request,
+    response: GetEventsControllerTypes.v1.Response
+  ) {
+    const {
+      query: { skip, take, workspaceId },
+    } = request;
+
+    const [data, total] = await this._workspaceEventRepository.getAllAndCount({
+      skip,
+      take,
+      order: {
+        createdAt: "desc",
+      },
+      where: {
+        workspace: {
+          id: workspaceId,
+        },
+      },
+      relations: {
+        user: true,
+      },
+    });
+
+    const result = this.mapper
+      .map<WorkspaceEvent>(data)
+      .to(WorkspaceEventMapper);
+
+    return this.finalizeRequest(response, HTTP.OK, {
+      result,
+      total,
+    });
+  }
+}
