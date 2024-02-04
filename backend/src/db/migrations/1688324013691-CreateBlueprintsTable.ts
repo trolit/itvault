f0586f86df9e85161bdec85a8277c7ec359e406a
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-02-04 15:37:12 +0100

    Update 1688324013691-CreateBlueprintsTable.ts

--- backend/src/db/migrations/1688324013691-CreateBlueprintsTable.ts
+++ backend/src/db/migrations/1688324013691-CreateBlueprintsTable.ts
@@ -5,7 +5,7 @@ export class CreateBlueprintsTable1688324013691 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE \`blueprints\` (\`id\` int NOT NULL AUTO_INCREMENT, \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updatedAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`deletedAt\` datetime(6) NULL, \`name\` varchar(255) NOT NULL, \`pinnedAt\` datetime NULL, \`description\` varchar(255) NULL, \`color\` varchar(7) NOT NULL, PRIMARY KEY (\`id\`), \`createdById\` int NULL, \`updatedById\` int NULL) ENGINE=InnoDB`
+      `CREATE TABLE \`blueprints\` (\`id\` int NOT NULL AUTO_INCREMENT, \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updatedAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`deletedAt\` datetime(6) NULL, \`name\` varchar(255) NOT NULL, \`pinnedAt\` datetime NULL, \`description\` varchar(255) NULL, \`color\` varchar(7) NOT NULL, \`createdById\` int NULL, \`updatedById\` int NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`
     );
 
     await queryRunner.query(
