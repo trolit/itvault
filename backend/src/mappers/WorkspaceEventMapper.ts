a28772154866e926bd5711b9af004851b2b2b427
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-02-04 22:38:35 +0100

    Create WorkspaceEventMapper.ts

---
+++ backend/src/mappers/WorkspaceEventMapper.ts
@@ -0,0 +1,33 @@
+import { WorkspaceEvent } from "@db/entities/WorkspaceEvent";
+
+import { BaseMapper } from "./BaseMapper";
+
+import { Action } from "@shared/types/enums/Action";
+import { IWorkspaceEventDTO } from "@shared/types/DTOs/WorkspaceEvent";
+
+export class WorkspaceEventMapper
+  extends BaseMapper<WorkspaceEvent>
+  implements IWorkspaceEventDTO
+{
+  id: number;
+  entity: string;
+  action: Action;
+  targetId: string;
+  createdAt: string;
+  createdBy: { id: number; fullName: string };
+
+  constructor(data: WorkspaceEvent) {
+    super(data, ["id", "entity", "action", "targetId", "createdAt"]);
+
+    this.assignInitialKeys();
+
+    const { user } = data;
+
+    this.createdBy = {
+      id: user.id,
+      fullName: user.fullName,
+    };
+
+    return this;
+  }
+}
