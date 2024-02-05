f2642b0a03f24f131768e9c51cf629a1c081cb88
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-02-05 15:30:46 +0100

    Update JobConfig.ts

--- backend/src/types/jobs/JobConfig.ts
+++ backend/src/types/jobs/JobConfig.ts
@@ -2,6 +2,7 @@ import { CronCommand } from "cron";
 
 export type JobConfig = {
   time: string;
+  // @NOTE onComplete — A function that will fire when the job is complete, when it is >> stopped <<
   runners: { onTick: CronCommand; onComplete?: CronCommand };
   options?: {
     timeZone?: string;
