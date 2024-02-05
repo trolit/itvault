8f28deacc61e0be175bff90365b98ccb0bda19c1
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-02-05 15:28:21 +0100

    Create DisableFilesUploadJob.ts

---
+++ backend/src/jobs/DisableFilesUploadJob.ts
@@ -0,0 +1,32 @@
+import { injectable } from "tsyringe";
+import { JobConfig } from "types/jobs/JobConfig";
+
+import { APP } from "@config";
+
+import { BaseJob } from "./BaseJob";
+
+@injectable()
+export class DisableFilesUploadJob extends BaseJob {
+  jobName = DisableFilesUploadJob.name;
+
+  config: JobConfig = {
+    time: "55 23 * * *",
+    runners: {
+      onTick: this.onTick.bind(this),
+      onComplete: this.onComplete.bind(this),
+    },
+    options: {
+      runOnInit: false,
+    },
+  };
+
+  constructor() {
+    super();
+  }
+
+  async onTick() {
+    APP.IS_CLEARING_TEMPORARY_UPLOADS_DIR = true;
+
+    await this.onComplete();
+  }
+}
