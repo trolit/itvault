14f26c552591906651feab98be8dcf3affc5d196
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-02-07 15:54:39 +0100

    Create Service.ts

---
+++ backend/src/types/enums/Service.ts
@@ -0,0 +1,12 @@
+export enum Service {
+  yup = "yup",
+  Redis = "Redis",
+  TypeORM = "TypeORM",
+  tsyringe = "tsyringe",
+  nodemailer = "nodemailer",
+  formidable = "formidable",
+  RabbitMQ = "RabbitMQ",
+  cron = "cron",
+  Express = "Express",
+  EngineIO = "Engine.io",
+}
