20fd7822f9162f18231427c2555b882a3b762276
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-02-12 13:13:33 +0100

    add `getUniqueDatesToPrecision`

--- backend/src/services/DateService.ts
+++ backend/src/services/DateService.ts
@@ -1,6 +1,18 @@
+import utc from "dayjs/plugin/utc";
 import { injectable } from "tsyringe";
 import dayjs, { Dayjs, ManipulateType } from "dayjs";
 import { IDateService } from "types/services/IDateService";
+
+import { DatePrecision } from "@shared/types/enums/DatePrecision";
+
+dayjs.extend(utc);
+
+const formatters = {
+  [DatePrecision.Minutes]: "YYYY-MM-DDTHH:mm",
+  [DatePrecision.Hours]: "YYYY-MM-DDTHH",
+  [DatePrecision.Days]: "YYYY-MM-DD",
+  [DatePrecision.Months]: "YYYY-MM",
+};
 
 @injectable()
 export class DateService implements IDateService {
@@ -20,5 +32,19 @@ export class DateService implements IDateService {
     const expiresAt = now.add(parseInt(value), <ManipulateType>unit);
 
     return expiresAt.format();
+  }
+
+  getUniqueDatesToPrecision(dates: string[], precision: DatePrecision) {
+    const uniqueDates: string[] = [];
+
+    for (const date of dates) {
+      const formattedDate = dayjs(date).utc().format(formatters[precision]);
+
+      if (!uniqueDates.includes(formattedDate)) {
+        uniqueDates.push(formattedDate);
+      }
+    }
+
+    return uniqueDates;
   }
 }
--- backend/src/types/services/IDateService.ts
+++ backend/src/types/services/IDateService.ts
@@ -1,7 +1,13 @@
 import { Dayjs } from "dayjs";
+import { DatePrecision } from "@shared/types/enums/DatePrecision";
 
 export interface IDateService {
   now(): Dayjs;
 
   getExpirationDate(expiration: string, separator?: string): string | null;
+
+  getUniqueDatesToPrecision(
+    dates: string[],
+    precision: DatePrecision
+  ): string[];
 }
