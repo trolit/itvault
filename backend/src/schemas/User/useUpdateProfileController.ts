a8d10700a5ec702458dd746b4066c05785d68cbc
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-02-13 00:28:45 +0100

    extract firstName, lastName schemas to common file

--- backend/src/schemas/User/useUpdateProfileController.ts
+++ backend/src/schemas/User/useUpdateProfileController.ts
@@ -1,18 +1,16 @@
-import { object, string } from "yup";
+import { object } from "yup";
 import { SuperSchema } from "types/SuperSchema";
 import { UpdateProfileControllerTypes } from "types/controllers/User/UpdateProfileController";
 
-import { ACCOUNT_RULES } from "@shared/constants/rules";
+import { firstNameSchema, lastNameSchema } from "./nameSchemas";
 
 import { defineSuperSchemaRunner } from "@schemas/common/defineSuperSchemaRunner";
 
-const { FIRST_NAME, LAST_NAME } = ACCOUNT_RULES;
-
 const bodySchema: SuperSchema.Fragment<UpdateProfileControllerTypes.v1.Body> =
   object({
-    firstName: string().required().min(FIRST_NAME.MIN_LENGTH),
+    firstName: firstNameSchema,
 
-    lastName: string().required().min(LAST_NAME.MIN_LENGTH),
+    lastName: lastNameSchema,
   });
 
 export const useUpdateProfileSuperSchema: SuperSchema.Runner<
