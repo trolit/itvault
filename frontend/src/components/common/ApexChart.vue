0ff0672600b005bfbda3aea37182ccaf9fcb801c
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-02-15 22:56:50 +0100

    Init ApexChart component

---
+++ frontend/src/components/common/ApexChart.vue
@@ -0,0 +1,28 @@
+<template>
+  <vue-apex-charts
+    :type="type"
+    :options="options"
+    :series="series"
+    :width="width"
+    :height="height"
+  />
+</template>
+
+<script setup lang="ts">
+import VueApexCharts from "vue3-apexcharts";
+import type { ApexOptions } from "apexcharts";
+
+interface IProps {
+  type: "line";
+
+  options?: ApexOptions;
+
+  series: { name: string; data: any[] }[];
+
+  width?: string | number;
+
+  height?: string | number;
+}
+
+defineProps<IProps>();
+</script>
