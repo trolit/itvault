b42afb134ac3da20b7ac2ee07240a918dd266363
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-02-15 20:33:19 +0100

    sketch insights store required data

--- frontend/src/store/insights.ts
+++ frontend/src/store/insights.ts
@@ -1,46 +1,67 @@
 import { defineStore } from "pinia";
 
-import type { IUserDTO } from "@shared/types/DTOs/User";
+import type {
+  IWorkspaceTraceDTO,
+  IWorkspaceActivityDataPointDTO,
+} from "@shared/types/DTOs/Workspace";
 import type { IPaginationQuery } from "@shared/types/IPaginationQuery";
-import type { IWorkspaceTraceDTO } from "@shared/types/DTOs/Workspace";
+import type { IContributorDTO, IUserDTO } from "@shared/types/DTOs/User";
+
+const DEFAULT_MEMBERS_TAB_DATA = {
+  total: 0,
+  items: [],
+  pagination: { page: 1, perPage: 10 },
+};
+
+const DEFAULT_LOGS_TAB_DATA = {
+  total: 0,
+  items: [],
+  pagination: {
+    page: 1,
+    perPage: 15,
+  },
+};
+
+const DEFAULT_ACTIVITY_TAB_DATA = {
+  from: "",
+  to: "",
+  commonData: [],
+  usersData: [],
+};
 
 interface IState {
   activeKey: string;
+  contributors: IContributorDTO[];
 
   membersTabData: {
     total: number;
     items: IUserDTO[];
     pagination: IPaginationQuery;
   };
-
   logsTabData: {
     total: number;
     items: IWorkspaceTraceDTO[];
     pagination: IPaginationQuery;
+  };
+  activityTabData: {
+    from: string;
+    to: string;
+    commonData: IWorkspaceActivityDataPointDTO[];
+    usersData: {
+      userId: number;
+      data: IWorkspaceActivityDataPointDTO[];
+    }[];
   };
 }
 
 export const useInsightsStore = defineStore("insights", {
   state: (): IState => ({
     activeKey: "members",
+    contributors: [],
 
-    membersTabData: {
-      total: 0,
-      items: [],
-      pagination: {
-        page: 1,
-        perPage: 10,
-      },
-    },
-
-    logsTabData: {
-      total: 0,
-      items: [],
-      pagination: {
-        page: 1,
-        perPage: 15,
-      },
-    },
+    membersTabData: { ...DEFAULT_MEMBERS_TAB_DATA },
+    logsTabData: { ...DEFAULT_LOGS_TAB_DATA },
+    activityTabData: { ...DEFAULT_ACTIVITY_TAB_DATA },
   }),
 
   getters: {},
@@ -49,20 +70,11 @@ export const useInsightsStore = defineStore("insights", {
     resetState() {
       this.activeKey = "members";
 
-      this.membersTabData = {
-        total: 0,
-        items: [],
-        pagination: { page: 1, perPage: 10 },
-      };
+      this.membersTabData = { ...DEFAULT_MEMBERS_TAB_DATA };
 
-      this.logsTabData = {
-        total: 0,
-        items: [],
-        pagination: {
-          page: 1,
-          perPage: 15,
-        },
-      };
+      this.logsTabData = { ...DEFAULT_LOGS_TAB_DATA };
+
+      this.activityTabData = { ...DEFAULT_ACTIVITY_TAB_DATA };
     },
   },
 });
