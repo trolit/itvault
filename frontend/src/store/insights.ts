5fbbf4e3d43baa54e80b354756ce3c54d1963fb1
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-02-17 09:55:19 +0100

    Update insights.ts

--- frontend/src/store/insights.ts
+++ frontend/src/store/insights.ts
@@ -4,6 +4,8 @@ import type {
   IWorkspaceTraceDTO,
   IWorkspaceActivityDataPointDTO,
 } from "@shared/types/DTOs/Workspace";
+import { useDateService } from "@/services/useDateService";
+import type { PairedSeriesPoint } from "@/types/PairedSeriesPoint";
 import type { IPaginationQuery } from "@shared/types/IPaginationQuery";
 import type { IContributorDTO, IUserDTO } from "@shared/types/DTOs/User";
 
@@ -23,8 +25,9 @@ const DEFAULT_LOGS_TAB_DATA = {
 };
 
 const DEFAULT_ACTIVITY_TAB_DATA = {
-  from: "",
-  to: "",
+  timeRangeOption: "days-30",
+  from: 0,
+  to: 0,
   commonData: [],
   usersData: [],
 };
@@ -44,8 +47,9 @@ interface IState {
     pagination: IPaginationQuery;
   };
   activityTabData: {
-    from: string;
-    to: string;
+    timeRangeOption: string;
+    from: number;
+    to: number;
     commonData: IWorkspaceActivityDataPointDTO[];
     usersData: {
       userId: number;
@@ -64,7 +68,43 @@ export const useInsightsStore = defineStore("insights", {
     activityTabData: { ...DEFAULT_ACTIVITY_TAB_DATA },
   }),
 
-  getters: {},
+  getters: {
+    series(): { name: string; data: PairedSeriesPoint<string, number>[] }[] {
+      const { commonData, usersData } = this.activityTabData;
+
+      const result = [
+        {
+          name: "General",
+          data: commonData,
+        },
+      ];
+
+      for (const userData of usersData) {
+        const user = this.contributors.find(
+          user => user.id === userData.userId
+        );
+
+        result.push({
+          name: user ? `${user.fullName}` : "unknown",
+          data: userData.data,
+        });
+      }
+
+      return result;
+    },
+    range(): [number, number] {
+      const { from, to } = this.activityTabData;
+
+      return [from, to];
+    },
+    rangeInUtc(): [number, number] {
+      const dateService = useDateService();
+
+      const { from, to } = this.activityTabData;
+
+      return [dateService.unixToUtcUnix(from), dateService.unixToUtcUnix(to)];
+    },
+  },
 
   actions: {
     resetState() {
@@ -75,6 +115,45 @@ export const useInsightsStore = defineStore("insights", {
       this.logsTabData = { ...DEFAULT_LOGS_TAB_DATA };
 
       this.activityTabData = { ...DEFAULT_ACTIVITY_TAB_DATA };
+    },
+
+    setTimeRange(range?: [number, number]) {
+      let from = 0;
+      let to = 0;
+
+      if (Array.isArray(range)) {
+        from = range[0];
+        to = range[1];
+      } else {
+        const dateService = useDateService();
+
+        const [unit, amount] = this.activityTabData.timeRangeOption.split("-");
+        const result = dateService.unixRange(parseInt(amount), unit);
+
+        from = result[0];
+        to = result[1];
+      }
+
+      this.activityTabData.from = from;
+      this.activityTabData.to = to;
+    },
+
+    appendSeriesData(arg: {
+      data: IWorkspaceActivityDataPointDTO[];
+      userId?: number;
+    }) {
+      const { data, userId } = arg;
+
+      if (userId) {
+        this.activityTabData.usersData.push({
+          userId,
+          data,
+        });
+
+        return;
+      }
+
+      this.activityTabData.commonData = data;
     },
   },
 });
