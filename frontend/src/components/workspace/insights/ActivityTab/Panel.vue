15ae8ab78947a14b6974d6e2ce19cff12e0b608a
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-02-17 09:55:06 +0100

    Create Panel.vue

---
+++ frontend/src/components/workspace/insights/ActivityTab/Panel.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="panel">
+    <!-- @TODO select contributors -->
+
+    <time-range-selector
+      :option="activityTabData.timeRangeOption"
+      :range="range"
+      @update-option="onOptionUpdate"
+      @update-range="insightsStore.setTimeRange($event)"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { storeToRefs } from "pinia";
+
+import { useInsightsStore } from "@/store/insights";
+import TimeRangeSelector from "@/components/common/TimeRangeSelector.vue";
+
+const insightsStore = useInsightsStore();
+
+const { activityTabData, range } = storeToRefs(insightsStore);
+
+function onOptionUpdate(option: string) {
+  activityTabData.value.timeRangeOption = option;
+}
+</script>
