fb55d794c56aa5ecc97a2deeb83e55b51dc7eb3d
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-02-18 15:54:55 +0100

    Create TimePrecisionSelector.vue

---
+++ frontend/src/components/common/TimePrecisionSelector.vue
@@ -0,0 +1,44 @@
+<template>
+  <n-select
+    :value="option"
+    :options="options"
+    :consistent-menu-width="false"
+    @update:value="$emit('update-option', $event)"
+  />
+</template>
+
+<script setup lang="ts">
+import { NSelect } from "naive-ui";
+
+import type { PrimitiveSelectOption } from "@/types/PrimitiveSelectOption";
+import { DatePrecision } from "@shared/types/enums/DatePrecision";
+
+interface IProps {
+  option: string;
+}
+
+defineProps<IProps>();
+
+defineEmits<{
+  (event: "update-option", option: string): void;
+}>();
+
+const options: PrimitiveSelectOption[] = [
+  {
+    label: "minutes",
+    value: DatePrecision.Minutes,
+  },
+  {
+    label: "hours",
+    value: DatePrecision.Hours,
+  },
+  {
+    label: "days",
+    value: DatePrecision.Days,
+  },
+  {
+    label: "months",
+    value: DatePrecision.Months,
+  },
+];
+</script>
