38544ab2c810e8b760b52f51fc84282089343d7e
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-02-20 14:45:57 +0100

    remove "isRegistered" as we're not registering all deps (Controllers)

--- backend/src/helpers/getInstanceOf.ts
+++ backend/src/helpers/getInstanceOf.ts
@@ -1,16 +1,5 @@
 import { container, InjectionToken } from "tsyringe";
 
-import { Dependency } from "@enums/Dependency";
-
 export const getInstanceOf = <T>(token: InjectionToken<T>): T => {
-  if (!container.isRegistered(token)) {
-    const tokenAsString = token.toString();
-
-    log.error({
-      message: `Attempted to resolve unregistered dependency (${tokenAsString})!`,
-      dependency: Dependency.tsyringe,
-    });
-  }
-
   return container.resolve(token);
 };
