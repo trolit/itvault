73d51c12f8f9103869bcda17cf41f6f5014d613e
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-02-23 23:30:13 +0100

    Update buildTests.ts

--- backend/src/tests/integration/helpers/buildTests.ts
+++ backend/src/tests/integration/helpers/buildTests.ts
@@ -1,19 +1,24 @@
 import { ITest } from "../types/ITest";
+import { Method } from "../types/Method";
 
 export const buildTests = <BQ = { version: number }, BB = void>(
   general: {
+    method: Method;
     baseBody?: BB;
     baseQuery: BQ;
   },
-  builder: (arg: { addTest: <Q, B>(data: ITest<Q, B>) => void }) => void
+  builder: (arg: {
+    addTest: <Q, B>(data: Omit<ITest<Q, B>, "method">) => void;
+  }) => void
 ) => {
   const tests: ITest<any, any>[] = [];
 
-  const { baseQuery, baseBody } = general;
+  const { baseQuery, baseBody, method } = general;
 
-  const addTestBuilder = <Q, B>(data: ITest<Q, B>) => {
+  const addTestBuilder = <Q, B>(data: Omit<ITest<Q, B>, "method">) => {
     tests.push({
       ...data,
+      method,
       query: { ...baseQuery, ...data.query },
       body: baseBody ? { ...baseBody, ...data.body } : data.body,
     });
