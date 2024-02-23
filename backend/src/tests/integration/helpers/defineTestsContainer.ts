a3c8a91d64043831ac2bb2d68ba2b5c097dd1ab7
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-02-23 23:32:52 +0100

    Create defineTestsContainer.ts

---
+++ backend/src/tests/integration/helpers/defineTestsContainer.ts
@@ -0,0 +1,76 @@
+import Mocha from "mocha";
+import { expect } from "chai";
+import { ITest } from "../types/ITest";
+import TestAgent from "supertest/lib/agent";
+
+import { versionToString } from "./versionToString";
+
+export const defineTestsContainer = (
+  // @TODO add option to define "before"
+  arg: {
+    name: string;
+    route: string;
+    collection: {
+      controller: string;
+      testData: { routerVersion: number; tests: ITest<any, any>[] }[];
+    }[];
+  }
+) => {
+  const { name, route: entityRoute, collection } = arg;
+
+  return {
+    loadToSuite: (
+      suite: Mocha.Suite,
+      tools: { supertest: TestAgent | null }
+    ) => {
+      const entitySuite = Mocha.Suite.create(suite, `${name}`);
+
+      for (const element of collection) {
+        const { controller, testData } = element;
+
+        const controllerSuite = Mocha.Suite.create(
+          entitySuite,
+          `${controller}`
+        );
+
+        for (const element of testData) {
+          const { routerVersion, tests } = element;
+          const translatedRouterVersion = versionToString(routerVersion);
+
+          for (const test of tests) {
+            const mochaTest = new Mocha.Test(
+              `${translatedRouterVersion} ${test.description}`,
+              async () => {
+                const { supertest } = tools;
+
+                if (!supertest) {
+                  return;
+                }
+
+                const {
+                  method,
+                  query,
+                  body,
+                  expect: { statusCode, callback },
+                } = test;
+
+                const response = await supertest[method](
+                  `/api/${translatedRouterVersion}/${entityRoute}`
+                )
+                  .query(query)
+                  .send(body);
+
+                expect(response.status).to.eql(statusCode);
+
+                if (callback) {
+                  callback(response);
+                }
+              }
+            );
+            controllerSuite.addTest(mochaTest);
+          }
+        }
+      }
+    },
+  };
+};
