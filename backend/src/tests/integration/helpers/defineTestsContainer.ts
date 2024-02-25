eb13e1e9c4b7e5bab96c9d49b5dfddae280fa34c
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-02-25 11:29:36 +0100

    refactor to handle "custom test"

--- backend/src/tests/integration/helpers/defineTestsContainer.ts
+++ backend/src/tests/integration/helpers/defineTestsContainer.ts
@@ -2,7 +2,9 @@ import Mocha from "mocha";
 import { expect } from "chai";
 import { ITest } from "../types/ITest";
 import TestAgent from "supertest/lib/agent";
+import { ICustomTest } from "../types/ICustomTest";
 
+import { isCustomTest } from "./isCustomTest";
 import { versionToString } from "./versionToString";
 
 export const defineTestsContainer = (arg: {
@@ -11,21 +13,31 @@ export const defineTestsContainer = (arg: {
   before?: Mocha.Func;
   collection: {
     controller: string;
-    testData: { routerVersion: number; tests: ITest<any, any>[] }[];
+    testData: {
+      routerVersion: number;
+      tests: (ITest<any, any> | ICustomTest)[];
+    }[];
   }[];
 }) => {
   const { name, route: entityRoute, before: entityBefore, collection } = arg;
 
   return {
+    beforeAll(suite: Mocha.Suite) {
+      if (entityBefore) {
+        const relatedSuite = suite.suites.find(
+          element => element.title === `${name}`
+        );
+
+        if (relatedSuite) {
+          relatedSuite.beforeAll(entityBefore);
+        }
+      }
+    },
     loadToSuite: (
       suite: Mocha.Suite,
       tools: { supertest: TestAgent | null }
     ) => {
       const entitySuite = Mocha.Suite.create(suite, `${name}`);
-
-      if (entityBefore) {
-        entitySuite.beforeAll(entityBefore);
-      }
 
       for (const element of collection) {
         const { controller, testData } = element;
@@ -49,26 +61,12 @@ export const defineTestsContainer = (arg: {
                   return;
                 }
 
-                const {
-                  method,
-                  query,
-                  body,
-                  expect: { statusCode, callback },
-                } = test;
+                const url = `/api/${translatedRouterVersion}/${entityRoute}`;
 
-                const response = await supertest[method](
-                  `/api/${translatedRouterVersion}/${entityRoute}`
-                )
-                  .query(query)
-                  .send(body);
-
-                expect(response.status).to.eql(statusCode);
-
-                if (callback) {
-                  callback(response);
-                }
+                await executeTest({ url, supertest, test });
               }
             );
+
             controllerSuite.addTest(mochaTest);
           }
         }
@@ -76,3 +74,32 @@ export const defineTestsContainer = (arg: {
     },
   };
 };
+
+async function executeTest(arg: {
+  url: string;
+  supertest: TestAgent;
+  test: ITest<any, any> | ICustomTest;
+}) {
+  const { supertest, test, url } = arg;
+
+  if (isCustomTest(test)) {
+    const response = await test.runner({ url, supertest });
+
+    expect(response.status).to.eql(test.statusCode);
+  } else {
+    const {
+      method,
+      query,
+      body,
+      expect: { statusCode, callback },
+    } = test;
+
+    const response = await supertest[method](url).query(query).send(body);
+
+    expect(response.status).to.eql(statusCode);
+
+    if (callback) {
+      callback(response);
+    }
+  }
+}
