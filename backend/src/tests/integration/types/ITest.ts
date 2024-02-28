5be93fcf0b4e4bf7499ec412583e958e520e9747
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-02-28 10:41:42 +0100

    replace `sendAs` with `session`

--- backend/src/tests/integration/types/ITest.ts
+++ backend/src/tests/integration/types/ITest.ts
@@ -1,9 +1,10 @@
 import { Response } from "supertest";
 
 import { IBaseTest } from "./IBaseTest";
+import { TestAgentTypes } from "./TestAgent";
 
 export interface ITest<Q = void, B = void> extends IBaseTest<Q, B> {
-  sendAs?: string;
+  session?: TestAgentTypes.UserSession;
 
   query?: Q & { version: number };
 
