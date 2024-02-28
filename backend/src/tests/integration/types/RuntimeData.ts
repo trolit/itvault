8bf12701936717a3b48ba63509a2b88628ac0c48
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-02-28 11:57:47 +0100

    Update RuntimeData.ts

--- backend/src/tests/integration/types/RuntimeData.ts
+++ backend/src/tests/integration/types/RuntimeData.ts
@@ -1,10 +1,11 @@
 import { Server } from "http";
 import TestAgent from "supertest/lib/agent";
+import { TestAgentTypes } from "./TestAgent";
 
 export type RuntimeData = {
   app?: Server;
 
   supertest?: TestAgent;
 
-  tokens: Record<string, string>;
+  jsonwebtokens: Pick<TestAgentTypes.TestData, "jsonwebtokens">;
 };
