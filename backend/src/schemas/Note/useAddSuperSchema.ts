7750e215d522cb2b7a9dd21d9e5ba01484f9c83c
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-03-05 15:05:54 +0100

    fix Note/AddController (include workspaceId)

--- backend/src/schemas/Note/useAddSuperSchema.ts
+++ backend/src/schemas/Note/useAddSuperSchema.ts
@@ -1,5 +1,5 @@
-import { object, string } from "yup";
 import sanitizeHtml from "sanitize-html";
+import { number, object, string } from "yup";
 import { SuperSchema } from "types/SuperSchema";
 import { AddControllerTypes } from "types/controllers/Note/AddController";
 
@@ -7,6 +7,10 @@ import { Di } from "@enums/Di";
 
 import { useIdNumberSchema } from "@schemas/common/useIdNumberSchema";
 import { defineSuperSchemaRunner } from "@schemas/common/defineSuperSchemaRunner";
+
+const querySchema: SuperSchema.Fragment<AddControllerTypes.v1.Query> = object({
+  workspaceId: number().required(),
+});
 
 const bodySchema: SuperSchema.Fragment<AddControllerTypes.v1.Body> = object({
   text: string()
@@ -18,9 +22,10 @@ const bodySchema: SuperSchema.Fragment<AddControllerTypes.v1.Body> = object({
 export const useAddSuperSchema: SuperSchema.Runner<
   void,
   AddControllerTypes.v1.Body,
-  void
+  AddControllerTypes.v1.Query
 > = defineSuperSchemaRunner(() => {
   return {
+    query: querySchema,
     body: bodySchema,
   };
 });
--- backend/src/types/controllers/Note/AddController.ts
+++ backend/src/types/controllers/Note/AddController.ts
@@ -1,11 +1,14 @@
+import { WorkspaceId } from "../WorkspaceId";
 import { NoteMapper } from "@mappers/NoteMapper";
 import { IAddNoteDTO } from "@shared/types/DTOs/Note";
 
 export namespace AddControllerTypes {
   export namespace v1 {
+    export type Query = WorkspaceId;
+
     export type Body = IAddNoteDTO;
 
-    export type Request = CustomRequest<undefined, Body>;
+    export type Request = CustomRequest<undefined, Body, Query>;
 
     export type Response = CustomResponse<NoteMapper>;
   }
