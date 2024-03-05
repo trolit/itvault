8fcc9febb13fec5bff7a38df73c4f5275db6dd76
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-03-05 15:10:03 +0100

    include `workspaceId` in `PatchValueController` logic

--- backend/src/types/controllers/Note/PatchValueController.ts
+++ backend/src/types/controllers/Note/PatchValueController.ts
@@ -2,12 +2,16 @@ import { IPatchNoteValueDTO } from "@shared/types/DTOs/Note";
 
 export namespace PatchValueControllerTypes {
   export namespace v1 {
+    export type Query = {
+      workspaceId: number;
+    };
+
     export type Params = {
       id: number;
     };
 
     export type Body = IPatchNoteValueDTO;
 
-    export type Request = CustomRequest<Params, Body>;
+    export type Request = CustomRequest<Params, Body, Query>;
   }
 }
