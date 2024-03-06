b7866dde1738542b55a0c42c9fa89ff9d812efb1
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-03-06 21:53:22 +0100

    add `apiServer` to e2e env

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -4,6 +4,9 @@ export default defineConfig({
   e2e: {
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
     baseUrl: "http://localhost:4173",
+    env: {
+      apiServer: "http://localhost:8080/api",
+    },
   },
   component: {
     specPattern: "src/**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}",
