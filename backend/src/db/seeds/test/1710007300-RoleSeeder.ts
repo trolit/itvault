c348991f5d781d4de1b509d760323536f3621e85
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-03-10 01:35:42 +0100

    Update 1710007300-RoleSeeder.ts

--- backend/src/db/seeds/test/1710007300-RoleSeeder.ts
+++ backend/src/db/seeds/test/1710007300-RoleSeeder.ts
@@ -4,19 +4,23 @@ import { Seeder } from "typeorm-extension";
 
 import { ALL_PERMISSIONS_ROLE, NO_PERMISSIONS_ROLE } from "./roles";
 
-import { HEAD_ADMIN_ROLE } from "@shared/constants/config";
+import {
+  ALL_PERMISSIONS_ROLE_ID,
+  NO_PERMISSIONS_ROLE_ID,
+} from "@shared/constants/tests";
 
 export default class RoleSeeder implements Seeder {
   public async run(dataSource: DataSource) {
     const repository = dataSource.getRepository(Role);
 
     await repository.save({
-      id: HEAD_ADMIN_ROLE.id,
+      id: ALL_PERMISSIONS_ROLE_ID,
       name: ALL_PERMISSIONS_ROLE.name,
       description: ALL_PERMISSIONS_ROLE.description,
     });
 
     await repository.save({
+      id: NO_PERMISSIONS_ROLE_ID,
       name: NO_PERMISSIONS_ROLE.name,
       description: NO_PERMISSIONS_ROLE.description,
     });
