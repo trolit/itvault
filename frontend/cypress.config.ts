ea1c09514123352b7e49b12e878107ffa4a7a212
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-03-10 16:42:51 +0100

    use constants from shared module (PW, super user/user email)

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,14 +1,20 @@
 import { defineConfig } from "cypress";
 
+import {
+  SUPER_USER_EMAIL,
+  USER_EMAIL,
+  PASSWORD,
+} from "@shared/constants/tests";
+
 export default defineConfig({
   e2e: {
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
     baseUrl: "http://localhost:3000",
     env: {
       API_PREFIX: "api",
-      PASSWORD: "1234",
-      SUPER_USER_EMAIL: "head.admin@itvault.dev",
-      USER_EMAIL: "member@itvault.dev",
+      PASSWORD,
+      SUPER_USER_EMAIL,
+      USER_EMAIL,
     },
     experimentalSessionAndOrigin: true,
   },
