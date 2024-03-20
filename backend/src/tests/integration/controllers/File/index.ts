db39cd4368278f946dba257443b868fb461e0cb8
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-03-20 11:46:21 +0100

    initialize `PatchRelativePath` controller tests

--- backend/src/tests/integration/controllers/File/index.ts
+++ backend/src/tests/integration/controllers/File/index.ts
@@ -9,11 +9,16 @@ import {
   PATCH_FILENAME_CONTROLLER_V1_BEFORE_HOOK,
   PATCH_FILENAME_CONTROLLER_V1_TESTS,
 } from "./PatchFilenameController";
+import {
+  PATCH_RELATIVE_PATH_CONTROLLER_V1_BEFORE_HOOK,
+  PATCH_RELATIVE_PATH_CONTROLLER_V1_TESTS,
+} from "./PatchRelativePathController";
 
 import { BaseController } from "@controllers/BaseController";
 import { GetAllController } from "@controllers/File/GetAllController";
 import { GetByIdController } from "@controllers/File/GetByIdController";
 import { PatchFilenameController } from "@controllers/File/PatchFilenameController";
+import { PatchRelativePathController } from "@controllers/File/PatchRelativePathController";
 
 const { v1 } = BaseController.ALL_VERSION_DEFINITIONS;
 
@@ -51,12 +56,23 @@ export const FILE_TESTS = defineTestsGroup({
         },
       ],
     },
+    {
+      action: "",
+      controller: PatchRelativePathController.name,
+      testData: [
+        {
+          routerVersion: v1,
+          tests: PATCH_RELATIVE_PATH_CONTROLLER_V1_TESTS,
+        },
+      ],
+    },
   ],
   hooks: {
     before: function () {
       return Promise.all([
         GET_BY_ID_CONTROLLER_V1_BEFORE_HOOK(),
         PATCH_FILENAME_CONTROLLER_V1_BEFORE_HOOK(),
+        PATCH_RELATIVE_PATH_CONTROLLER_V1_BEFORE_HOOK(),
       ]);
     },
   },
