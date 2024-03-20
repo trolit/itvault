71b3294be3482cbd60af65beffc2b9fdd43deae4
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-03-20 10:39:28 +0100

    add GetById tests (File entity)

--- backend/src/tests/integration/controllers/File/index.ts
+++ backend/src/tests/integration/controllers/File/index.ts
@@ -1,9 +1,14 @@
 import { defineTestsGroup } from "@integration-tests/probata";
 
 import { GET_ALL_CONTROLLER_V1_TESTS } from "./GetAllController";
+import {
+  GET_BY_ID_CONTROLLER_V1_TESTS,
+  GET_BY_ID_CONTROLLER_V1_BEFORE_HOOK,
+} from "./GetByIdController";
 
 import { BaseController } from "@controllers/BaseController";
 import { GetAllController } from "@controllers/File/GetAllController";
+import { GetByIdController } from "@controllers/File/GetByIdController";
 
 const { v1 } = BaseController.ALL_VERSION_DEFINITIONS;
 
@@ -21,5 +26,20 @@ export const FILE_TESTS = defineTestsGroup({
         },
       ],
     },
+    {
+      action: "",
+      controller: GetByIdController.name,
+      testData: [
+        {
+          routerVersion: v1,
+          tests: GET_BY_ID_CONTROLLER_V1_TESTS,
+        },
+      ],
+    },
   ],
+  hooks: {
+    before: function () {
+      return Promise.all([GET_BY_ID_CONTROLLER_V1_BEFORE_HOOK()]);
+    },
+  },
 });
