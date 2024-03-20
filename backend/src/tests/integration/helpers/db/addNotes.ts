02b6eeaefb2a9f136bc7088214365750949ffe43
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-03-20 16:45:32 +0100

    Create addNotes.ts

---
+++ backend/src/tests/integration/helpers/db/addNotes.ts
@@ -0,0 +1,46 @@
+import { faker } from "@faker-js/faker";
+import { Note } from "@db/entities/Note";
+import { INoteRepository } from "types/repositories/INoteRepository";
+
+import { Di } from "@enums/Di";
+
+import { getInstanceOf } from "@helpers/getInstanceOf";
+import { getOptionsOfTraceRelatedEntity } from "@helpers/getOptionsOfTraceRelatedEntity";
+
+export const addNotes = async (
+  notesToAdd: {
+    id: number;
+    value?: string;
+    fileId: number;
+    createdById?: number;
+    workspaceId: number;
+  }[]
+) => {
+  const notes: Note[] = [];
+
+  const noteRepository = getInstanceOf<INoteRepository>(Di.NoteRepository);
+
+  for (const noteToAdd of notesToAdd) {
+    const { id, value, fileId, createdById, workspaceId } = noteToAdd;
+
+    const noteEntity = noteRepository.createEntity({
+      id,
+      value: value || faker.lorem.words(10),
+      file: {
+        id: fileId,
+      },
+    });
+
+    const note = await noteRepository.primitiveSave(
+      noteEntity,
+      getOptionsOfTraceRelatedEntity({
+        userId: createdById || 1,
+        workspaceId,
+      })
+    );
+
+    notes.push(note);
+  }
+
+  return notes;
+};
