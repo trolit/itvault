05cb6b7ca46bc59da5ce127fc5ecad8accb06f35
p4w31 \!d21k0w541 <iddziku@gmail.com>
2024-05-17 20:15:52 +0200

    remove duplicate import statement

--- frontend-vue/src/store/blueprints.ts
+++ frontend-vue/src/store/blueprints.ts
@@ -5,10 +5,9 @@ import { useGeneralStore } from "./general";
 import { useVariantsStore } from "./variants";
 import { useWorkspacesStore } from "./workspaces";
 import { useDateService } from "@/services/useDateService";
-import type { IBlueprintDTO } from "@shared/types/DTOs/Blueprint";
 import type { IPaginationQuery } from "@shared/types/IPaginationQuery";
-import type { IAddEditBlueprintDTO } from "@shared/types/DTOs/Blueprint";
 import type { PaginatedResponse } from "@shared/types/PaginatedResponse";
+import type { IBlueprintDTO, IAddEditBlueprintDTO } from "@shared/types/DTOs/Blueprint";
 
 interface IState {
   total: number;
