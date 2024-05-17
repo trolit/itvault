370254529bcce72ea9b1a635e28114f1788e57a1
p4w31 \!d21k0w541 <iddziku@gmail.com>
2024-05-17 20:19:40 +0200

    remove redundant jump

--- frontend-vue/src/store/roles.ts
+++ frontend-vue/src/store/roles.ts
@@ -92,8 +92,6 @@ export const useRolesStore = defineStore("roles", {
           description: tab.currentForm.description,
           permissions: cloneDeep(tab.currentForm.permissions),
         };
-
-        return;
       }
     },
 
